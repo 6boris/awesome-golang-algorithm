3cb043d2cb9afbb61586c11300112f95940bdb73
Kyle Liu <kylesliu@outlook.com>
2018-10-31 10:28:33 +0800

    :boar: Fix some bug and continue CI test

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@
 set -e
 echo "" > coverage.txt
 
-#for d in $(go list ./... | grep -v vendor); do
-#    echo $d
-#    go test -coverprofile=profile.out -covermode=atomic $d
-#    if [ -f profile.out ]; then
-#        cat profile.out >> coverage.txt
-#        rm profile.out
-#    fi
-#done
+for d in $(go list ./... | grep -v vendor); do
+    echo $d
+    go test -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
 
