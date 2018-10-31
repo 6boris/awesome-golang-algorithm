bbfb5b776f074b59aae0c514a03c6021d5dcbcc7
Kyle Liu <kylesliu@outlook.com>
2018-10-31 10:08:31 +0800

    :boar: CI test

---
+++ test.sh
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env bash
+
+set -e
+echo "" > coverage.txt
+
+for d in $(go list ./... | grep -v vendor); do
+    echo $d
+    go test -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
