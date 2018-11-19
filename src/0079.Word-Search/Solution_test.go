f9c978e9ce23a77515d3204fa4f1294a98367b12
彭建 <jian.peng@fengjr.com>
2018-11-19 23:16:22 +0800

    Add 0079.Word-Search

--- src/0079.Word-Search/Solution_test.go
+++ src/0079.Word-Search/Solution_test.go
@@ -1,40 +1,19 @@
 package Solution
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
-	//	测试用例
-	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
-	}{
-		{"TestCacse 1", true, true},
-		{"TestCacse 1", true, true},
-		{"TestCacse 1", false, false},
+	borad := [][]byte{
+		[]byte{'A', 'B', 'C', 'E'},
+		[]byte{'S', 'F', 'C', 'S'},
+		[]byte{'A', 'D', 'E', 'E'},
 	}
-
-	//	开始测试
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			ret := Solution(c.inputs)
-			if !reflect.DeepEqual(ret, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, ret, c.inputs)
-			}
-		})
-	}
-}
-
-//	压力测试
-func BenchmarkSolution(b *testing.B) {
-
-}
-
-//	使用案列
-func ExampleSolution() {
-
+	// Given word = "ABCCED", return true.
+	// Given word = "SEE", return true.
+	// Given word = "ABCB", return false.
+	t.Log("ABCCED=", exist(borad, "ABCCED"))
+	t.Log("SEE=", exist(borad, "SEE"))
+	t.Log("ABCB=", exist(borad, "ABCB"))
 }
