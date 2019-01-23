2dc8116d7a082b7fdffb21588d845106171f0619
Sandy <openset.wang@gmail.com>
2019-01-23 12:49:47 +0800

    Update lengthOfLastWord

--- src/0058.Length-of-Last-Word/Solution.go
+++ src/0058.Length-of-Last-Word/Solution.go
@@ -1,25 +1,13 @@
 package Solution
 
-import (
-	"strings"
-)
-
 func lengthOfLastWord(s string) int {
-	start := len(s) - 1
-	//	寻找最后一个单词的尾下标
-	for start >= 0 && s[start] == ' ' {
-		start--
+	wc, l := 0, len(s)-1
+	for i := l; i >= 0; i-- {
+		if s[i] != ' ' {
+			wc++
+		} else if wc > 0 {
+			return wc
+		}
 	}
-	//	寻找最后一个单词的初始下标
-	end := start
-	for start >= 0 && s[start] != ' ' {
-		start--
-	}
-
-	return end - start
-}
-
-func lengthOfLastWord1(s string) int {
-	s = strings.TrimRight(s, " ")
-	return len(s) - strings.LastIndex(s, " ") - 1
+	return wc
 }
