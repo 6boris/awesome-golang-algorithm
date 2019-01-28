946da418defe8570e3c7750553cc02e0e3594d07
Kyle Liu <kylesliu@outlook.com>
2019-01-28 19:12:39 +0700

    添加平台判断测试

--- cmd/contribution_test.go
+++ cmd/contribution_test.go
@@ -2,29 +2,50 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 )
 
 func TestGetContributorString(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		return
+	}
+
 	str := GetContributorString()
 	fmt.Println(str)
 }
 
 func TestGetContributorJosnMarshal(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		return
+	}
+
 	str := GetContributorJosnMarshal("", "	")
 	fmt.Println(str)
 }
 
 func TestGetContributorInstance(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		return
+	}
+
 	contributors := GetContributorInstance()
 	fmt.Println(contributors)
 }
 
 func TestGenerateContributorTemplete(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		return
+	}
+
 	GenerateContributorTemplete()
 }
 
 func BenchmarkGetContributorInstance(b *testing.B) {
+	if runtime.GOOS != "windows" {
+		return
+	}
+
 	b.N = 2000000
 	contributors := GetContributorInstance()
 	fmt.Println(contributors)
