0dc55e02ce0ea9ee4cda34ae2c99e9f1a42a377a
Kyle Liu <kylesliu@outlook.com>
2019-01-31 16:04:45 +0800

    add 120 solution

---
+++ src/0120.Triangle/Solution_test.go
@@ -0,0 +1,86 @@
+package Solution
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	//	测试用例
+	cases := []struct {
+		name   string
+		inputs [][]int
+		expect int
+	}{
+		{"TestCacse 1",
+			[][]int{
+				{2},
+				{3, 4},
+				{6, 5, 7},
+				{4, 1, 8, 3},
+			},
+			11},
+		{"TestCacse 2",
+			[][]int{
+				{1},
+				{2, 3},
+			},
+			3},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := minimumTotal(c.inputs)
+			if !reflect.DeepEqual(got, c.expect) {
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+					c.expect, got, c.inputs)
+			}
+		})
+	}
+}
+
+func TestSolution1(t *testing.T) {
+	//	测试用例
+	cases := []struct {
+		name   string
+		inputs [][]int
+		expect int
+	}{
+		{"TestCacse 1",
+			[][]int{
+				{2},
+				{3, 4},
+				{6, 5, 7},
+				{4, 1, 8, 3},
+			},
+			11},
+		{"TestCacse 2",
+			[][]int{
+				{1},
+				{2, 3},
+			},
+			3},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := minimumTotal1(c.inputs)
+			if !reflect.DeepEqual(got, c.expect) {
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+					c.expect, got, c.inputs)
+			}
+		})
+	}
+}
+
+//	压力测试
+func BenchmarkSolution(b *testing.B) {
+
+}
+
+//	使用案列
+func ExampleSolution() {
+
+}
