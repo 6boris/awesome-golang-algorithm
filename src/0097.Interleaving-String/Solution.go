9c7025d134d532af34c1b5d9940eebee8fddd098
Kyle Liu <kylesliu@outlook.com>
2019-03-25 17:55:46 +0800

    add 204 find primes problem

--- src/0097.Interleaving-String/Solution.go
+++ src/0097.Interleaving-String/Solution.go
@@ -21,8 +21,7 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
 			} else if j == 0 {
 				dp[i][j] = dp[i-1][j] && s1[i-1] == s3[i+j-1]
 			} else {
-				dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1]) ||
-					(dp[i][j-1] && s2[j-1] == s3[i+j-1])
+				dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1]) || (dp[i][j-1] && s2[j-1] == s3[i+j-1])
 			}
 		}
 	}
