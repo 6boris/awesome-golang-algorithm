a99b26b074fcb17515a21e9aa184f31553abbefc
Kyle Liu <kylesliu@outlook.com>
2019-05-20 18:41:36 +0800

    add 322 solution

---
+++ src/0322.Coin-Change/Solution_test.go
@@ -0,0 +1,41 @@
+package Solution
+
+import (
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	//	测试用例
+	cases := []struct {
+		name   string
+		inputs []int
+		amount int
+		expect int
+	}{
+		{"TestCase", []int{1, 2, 5}, 11, 3},
+		{"TestCase", []int{2}, 3, -1},
+	}
+
+	//	开始测试
+	for i, c := range cases {
+		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
+			got := coinChange(c.inputs, c.amount)
+			if !reflect.DeepEqual(got, c.expect) {
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+					c.expect, got, c.inputs)
+			}
+		})
+	}
+}
+
+//	压力测试
+func BenchmarkSolution(b *testing.B) {
+
+}
+
+//	使用案列
+func ExampleSolution() {
+
+}
