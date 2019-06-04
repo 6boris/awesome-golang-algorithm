6e118ee09463a93a4fb072bbafe4fb0487d6c83f
Kyle Liu <kylesliu@outlook.com>
2019-06-04 23:24:46 +0800

    update the generate bash

--- generate.sh
+++ generate.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash#!/usr/bin/env bash
 
+# Git
+git checkout master
+git reset --hard
+git fetch --all
+git reset --hard origin/master
+git pull
+
 #   GitBook
-gitbook serve --config book.json . gitbook
-#gitbook build --config book.json . gitbook
+#gitbook serve --config book.json . gitbook
+gitbook build --config book.json . gitbook
 
 #   all-contributor
 
-npm install --save-dev all-contributors-cli
-npm run contributors:generate
-all-contributors add kylesliu code,blog,design,doc
-all-contributors generate
+#npm install --save-dev all-contributors-cli
+#npm run contributors:generate
+#all-contributors add kylesliu code,blog,design,doc
+#all-contributors generate
