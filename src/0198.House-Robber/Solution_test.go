db7433e0d3d118c252aefb8235c763f93f5ac52f
Kyle Liu <kyle.liu.cn@gmail.com>
2020-03-26 20:25:26 +0800

    update 198 Solution

--- src/0198.House-Robber/Solution_test.go
+++ src/0198.House-Robber/Solution_test.go
@@ -52,6 +52,29 @@ func TestSolution2(t *testing.T) {
 	}
 }
 
+func TestSolution3(t *testing.T) {
+	//	测试用例
+	cases := []struct {
+		name   string
+		inputs []int
+		expect int
+	}{
+		{"TestCacse 1", []int{1, 2, 3, 1}, 4},
+		{"TestCacse 1", []int{2, 7, 9, 3, 1}, 12},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := rob3(c.inputs)
+			if !reflect.DeepEqual(got, c.expect) {
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+					c.expect, got, c.inputs)
+			}
+		})
+	}
+}
+
 //	压力测试
 func BenchmarkSolution(b *testing.B) {
 
