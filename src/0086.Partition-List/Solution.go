1880175256e307515cd347af875653f6c85a474d
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-20 17:18:46 +0530

    :sparkles: Added solution to 86

--- src/0086.Partition-List/Solution.go
+++ src/0086.Partition-List/Solution.go
@@ -1,5 +1,20 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(head *ListNode, x int) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	before, after := &ListNode{}, &ListNode{}
+	bh, ah := before, after
+	for ; head != nil; head = head.Next {
+		if head.Val < x {
+			before.Next = head
+			before = before.Next
+		} else {
+			after.Next = head
+			after = after.Next
+		}
+	}
+	after.Next, before.Next = nil, ah.Next
+	return bh.Next
 }
