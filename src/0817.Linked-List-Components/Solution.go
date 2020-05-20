06798c78906f10a1a93f544f7d2a1611e65870ce
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-20 12:58:28 +0530

    :sparkles: Added solution to 817

--- src/0817.Linked-List-Components/Solution.go
+++ src/0817.Linked-List-Components/Solution.go
@@ -1,5 +1,21 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(head *ListNode, G []int) int {
+	GMap := make(map[int]bool)
+	for _, val := range G {
+		GMap[val] = true
+	}
+	ans, connected := 0, false
+	for head != nil {
+		if GMap[head.Val] {
+			if !connected {
+				ans++
+			}
+			connected = true
+		} else {
+			connected = false
+		}
+		head = head.Next
+	}
+	return ans
 }
--- src/0817.Linked-List-Components/Solution_test.go
+++ src/0817.Linked-List-Components/Solution_test.go
@@ -10,21 +10,20 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		head   *ListNode
+		G      []int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase", MakeListNode([]int{0, 1, 2, 3}), []int{0, 1, 3}, 2},
+		{"TestCase", MakeListNode([]int{0, 1, 2, 3, 4}), []int{0, 3, 1, 4}, 2},
 	}
 
-	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.head, c.G)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.head, c.G)
 			}
 		})
 	}
