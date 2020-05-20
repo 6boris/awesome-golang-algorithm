498a34429c9e79c134422b9377ffbd0c02f13f8a
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-20 12:58:36 +0530

    :sparkles: Added solution to 1019

--- src/1019.Next-Greater-Node-In-Linked-List/Solution.go
+++ src/1019.Next-Greater-Node-In-Linked-List/Solution.go
@@ -1,5 +1,22 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(head *ListNode) []int {
+	var nodes []int
+	for head != nil {
+		nodes = append(nodes, head.Val)
+		head = head.Next
+	}
+	ans := make([]int, len(nodes))
+	if len(nodes) == 0 {
+		return ans
+	}
+	var stack []int
+	for i := 0; i < len(nodes); i++ {
+		for len(stack) > 0 && nodes[stack[len(stack)-1]] < nodes[i] {
+			ans[stack[len(stack)-1]] = nodes[i]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+	return ans
 }
