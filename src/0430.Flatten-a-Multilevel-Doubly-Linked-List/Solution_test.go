38515368dbe86773532d89b956670a06c7ad99c2
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-20 15:16:14 +0530

    :sparkles: Added solution to 430

--- src/0430.Flatten-a-Multilevel-Doubly-Linked-List/Solution_test.go
+++ src/0430.Flatten-a-Multilevel-Doubly-Linked-List/Solution_test.go
@@ -1,33 +1,47 @@
 package Solution
 
 import (
-	"reflect"
-	"strconv"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
 	//	测试用例
-	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
-	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
-	}
-
-	//	开始测试
-	for i, c := range cases {
-		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
-			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
-			}
-		})
-	}
+	t.Run("TestCase", func(t *testing.T) {
+		main := MakeNode([]int{1, 2, 3, 4, 5, 6})
+		child1 := MakeNode([]int{7, 8, 9, 10})
+		child2 := MakeNode([]int{11, 12})
+		child1.Next.Child = child2
+		curr := main
+		for i := 0; i < 2; i++ {
+			curr = curr.Next
+		}
+		curr.Child = child1
+		got := Solution(main)
+		expect := MakeNode([]int{1, 2, 3, 7, 8, 11, 12, 9, 10, 4, 5, 6})
+		if !Check(got, expect) {
+			t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+				expect, got, main)
+		}
+	})
+	t.Run("TestCase", func(t *testing.T) {
+		main := MakeNode([]int{1, 2})
+		main.Child = &Node{Val: 3}
+		got := Solution(main)
+		expect := MakeNode([]int{1, 3, 2})
+		if !Check(got, expect) {
+			t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+				expect, got, main)
+		}
+	})
+	t.Run("TestCase", func(t *testing.T) {
+		main := MakeNode([]int{})
+		got := Solution(main)
+		expect := MakeNode([]int{})
+		if !Check(got, expect) {
+			t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+				expect, got, main)
+		}
+	})
 }
 
 //	压力测试
