a3a488d71ee27b072d16f38300b706384de78b8a
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-20 13:03:04 +0530

    :sparkles: Added solution to 328

--- src/0328.Odd-Even-Linked-List/Solution_test.go
+++ src/0328.Odd-Even-Linked-List/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs *ListNode
+		expect *ListNode
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase", MakeListNode([]int{1, 2, 3, 4, 5}), MakeListNode([]int{1, 3, 5, 2, 4})},
+		{"TestCase", MakeListNode([]int{2, 1, 3, 5, 6, 4, 7}), MakeListNode([]int{2, 3, 6, 7, 1, 5, 4})},
+		{"TestCase", MakeListNode([]int{}), MakeListNode([]int{})},
 	}
 
 	//	开始测试
