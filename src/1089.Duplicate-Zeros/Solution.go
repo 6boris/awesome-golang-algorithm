c5ae4791123f17211f9d3ad2242bd3e82923f456
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-23 15:54:44 +0530

    :sparkles: Added solution to 1089

--- src/1089.Duplicate-Zeros/Solution.go
+++ src/1089.Duplicate-Zeros/Solution.go
@@ -1,6 +1,6 @@
 package Solution
 
-func Solution1(arr []int) {
+func Solution(arr []int) {
 	n := len(arr)
 	if n > 1 {
 		for i := 0; i < n; i++ {
@@ -12,24 +12,4 @@ func Solution1(arr []int) {
 			}
 		}
 	}
-}
-
-func Solution2(arr []int) {
-	n := len(arr)
-	newArr := make([]int, 0)
-	c := 0
-	for i := 0; i < n; i++ {
-		if arr[i] == 0 {
-			newArr = append(newArr, 0)
-			newArr = append(newArr, 0)
-			c += 2
-		} else {
-			newArr = append(newArr, arr[i])
-			c++
-		}
-		if c == n {
-			break
-		}
-	}
-	arr = newArr
 }
--- src/1089.Duplicate-Zeros/Solution_test.go
+++ src/1089.Duplicate-Zeros/Solution_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestSolution1(t *testing.T) {
+func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
@@ -21,31 +21,7 @@ func TestSolution1(t *testing.T) {
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			Solution1(c.inputs)
-			if !reflect.DeepEqual(c.inputs, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, c.inputs, c.inputs)
-			}
-		})
-	}
-}
-
-func TestSolution2(t *testing.T) {
-	//	测试用例
-	cases := []struct {
-		name   string
-		inputs []int
-		expect []int
-	}{
-		{"TestCase", []int{1, 0, 2, 3, 0, 4, 5, 0}, []int{1, 0, 0, 2, 3, 0, 0, 4}},
-		{"TestCase", []int{1, 2, 3}, []int{1, 2, 3}},
-		{"TestCase", []int{}, []int{}},
-	}
-
-	//	开始测试
-	for i, c := range cases {
-		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			Solution1(c.inputs)
+			Solution(c.inputs)
 			if !reflect.DeepEqual(c.inputs, c.expect) {
 				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
 					c.expect, c.inputs, c.inputs)
