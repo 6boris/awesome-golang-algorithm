b71227dbc902a38ce2d98bf14ad8992d665fb62e
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-23 15:53:23 +0530

    :sparkles: Added solution to 1089

--- src/1089.Duplicate-Zeros/Solution.go
+++ src/1089.Duplicate-Zeros/Solution.go
@@ -1,5 +1,35 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution1(arr []int) {
+	n := len(arr)
+	if n > 1 {
+		for i := 0; i < n; i++ {
+			if arr[i] == 0 {
+				for j := n - 1; j > i; j-- {
+					arr[j] = arr[j-1]
+				}
+				i++
+			}
+		}
+	}
+}
+
+func Solution2(arr []int) {
+	n := len(arr)
+	newArr := make([]int, 0)
+	c := 0
+	for i := 0; i < n; i++ {
+		if arr[i] == 0 {
+			newArr = append(newArr, 0)
+			newArr = append(newArr, 0)
+			c += 2
+		} else {
+			newArr = append(newArr, arr[i])
+			c++
+		}
+		if c == n {
+			break
+		}
+	}
+	arr = newArr
 }
