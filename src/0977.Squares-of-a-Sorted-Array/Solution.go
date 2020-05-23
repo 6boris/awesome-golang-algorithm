2d8c9210b3de3b502e998aa58888af73dbb88d23
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-23 11:19:03 +0530

    :sparkles: Added solution to 977

--- src/0977.Squares-of-a-Sorted-Array/Solution.go
+++ src/0977.Squares-of-a-Sorted-Array/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "math"
+
+func Solution(A []int) []int {
+	n := len(A)
+	if n == 0 {
+		return A
+	}
+	abs := func(num int) int { return int(math.Abs(float64(num))) }
+	ans := make([]int, n)
+	i, j, k := 0, n-1, n-1
+	for i <= j {
+		if abs(A[i]) > abs(A[j]) {
+			ans[k] = A[i] * A[i]
+			i++
+		} else {
+			ans[k] = A[j] * A[j]
+			j--
+		}
+		k--
+	}
+	return ans
 }
