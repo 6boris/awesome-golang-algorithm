81f717245f203b9bf71860beecf3178162e76c3a
Kyle Liu <kyle.liu.cn@gmail.com>
2020-05-25 00:08:29 +0800

    add 278 solution

--- src/0278.First-Bad-Version/Solution_test.go
+++ src/0278.First-Bad-Version/Solution_test.go
@@ -10,18 +10,16 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase", 5, 4},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := firstBadVersion(c.inputs)
 			if !reflect.DeepEqual(got, c.expect) {
 				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
 					c.expect, got, c.inputs)
