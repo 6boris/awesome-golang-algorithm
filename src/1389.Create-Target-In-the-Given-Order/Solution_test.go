a99405b442876ac79aa92d2b5301906eff3a9976
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-24 19:12:17 +0530

    :sparkles: Added solution to 1389

---
+++ src/1389.Create-Target-In-the-Given-Order/Solution_test.go
@@ -0,0 +1,42 @@
+package Solution
+
+import (
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	//	测试用例
+	cases := []struct {
+		name   string
+		nums   []int
+		index  []int
+		expect []int
+	}{
+		{"TestCase", []int{0,1,2,3,4}, []int{0,1,2,2,1}, []int{0,4,1,3,2}},
+		{"TestCase", []int{1,2,3,4,0}, []int{0,1,2,3,0}, []int{0,1,2,3,4}},
+		{"TestCase", []int{1}, []int{0}, []int{1}},
+	}
+
+	//	开始测试
+	for i, c := range cases {
+		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
+			got := Solution(c.nums, c.index)
+			if !reflect.DeepEqual(got, c.expect) {
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.nums, c.index)
+			}
+		})
+	}
+}
+
+//	压力测试
+func BenchmarkSolution(b *testing.B) {
+
+}
+
+//	使用案列
+func ExampleSolution() {
+
+}
