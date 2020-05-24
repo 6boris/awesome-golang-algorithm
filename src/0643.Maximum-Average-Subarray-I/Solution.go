82b65972810400e123b75b8f824c209e37650467
Kyle Liu <kyle.liu.cn@gmail.com>
2020-05-24 23:54:58 +0800

    add 643 solution

--- src/0643.Maximum-Average-Subarray-I/Solution.go
+++ src/0643.Maximum-Average-Subarray-I/Solution.go
@@ -1,19 +1,19 @@
 package Solution
 
 func findMaxAverage(nums []int, k int) float64 {
-	tmp, ans := 0, -1
-	for i := 0; i < len(nums); i++ {
-		tmp += nums[i]
-		if i >= k-1 {
-			ans = max(ans, tmp)
-			tmp -= nums[i-k+1]
-		}
+	sum := make([]int, len(nums))
+	sum[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		sum[i] = sum[i-1] + nums[i]
 	}
-
-	return float64(ans) / float64(k)
+	ans := float64(sum[k-1]) / float64(k)
+	for i := k; i < len(nums); i++ {
+		ans = max(ans, float64(sum[i]-sum[i-k])/float64(k))
+	}
+	return ans
 }
 
-func max(x, y int) int {
+func max(x, y float64) float64 {
 	if x > y {
 		return x
 	}
