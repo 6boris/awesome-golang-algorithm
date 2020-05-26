7d435e7f62b6b6eaae1801c221ff529597232353
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-26 20:21:24 +0530

    :sparkles: Added solution to 1002

--- src/1002.Find-Common-Characters/Solution.go
+++ src/1002.Find-Common-Characters/Solution.go
@@ -1,5 +1,35 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "math"
+
+func Solution(A []string) []string {
+	commonChars := make([]string, 0)
+	minFrequencies := [26]int{}
+	for i := range minFrequencies {
+		minFrequencies[i] = math.MaxInt32
+	}
+	for _, word := range A {
+		charFrequencies := [26]int{}
+		for _, char := range word {
+			charFrequencies[char-'a']++
+		}
+		for i := 0; i < 26; i++ {
+			minFrequencies[i] = min(minFrequencies[i], charFrequencies[i])
+		}
+	}
+	for i := 0; i < 26; i++ {
+		for minFrequencies[i] > 0 {
+			commonChars = append(commonChars, string(i+'a'))
+			minFrequencies[i]--
+		}
+	}
+	return commonChars
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
 }
--- src/1002.Find-Common-Characters/Solution_test.go
+++ src/1002.Find-Common-Characters/Solution_test.go
@@ -10,12 +10,13 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []string
+		expect []string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase", []string{"bella", "label", "roller"}, []string{"e", "l", "l"}},
+		{"TestCase", []string{"cool", "lock", "cook"}, []string{"c", "o"}},
+		{"TestCase", []string{"sathish", "babu"}, []string{"a"}},
+		{"TestCase", []string{}, []string{}},
 	}
 
 	//	开始测试
