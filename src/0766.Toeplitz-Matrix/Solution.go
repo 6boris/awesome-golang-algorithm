0892235c04c269737b793da945452b5af3df97d2
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-27 13:01:29 +0530

    :sparkles: Added solution to 766

--- src/0766.Toeplitz-Matrix/Solution.go
+++ src/0766.Toeplitz-Matrix/Solution.go
@@ -1,5 +1,12 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(matrix [][]int) bool {
+	for i := 0; i < len(matrix)-1; i++ {
+		for j := 0; j < len(matrix[i])-1; j++ {
+			if matrix[i][j] != matrix[i+1][j+1] {
+				return false
+			}
+		}
+	}
+	return true
 }
