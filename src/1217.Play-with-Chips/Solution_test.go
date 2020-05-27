0626e21d6440a4a0b9f7a4f95536a525b19b23b7
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-27 16:10:32 +0530

    :sparkles: Added solution to 1217

--- src/1217.Play-with-Chips/Solution_test.go
+++ src/1217.Play-with-Chips/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase", []int{1, 2, 3}, 1},
+		{"TestCase", []int{2, 2, 2, 3, 3}, 2},
+		{"TestCase", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5},
 	}
 
 	//	开始测试
