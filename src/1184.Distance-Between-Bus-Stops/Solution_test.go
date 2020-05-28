75064ff3f942c5d1c4bd077854c40628bceb8b3b
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-28 13:44:12 +0530

    :sparkles: Added solution to 1184

--- src/1184.Distance-Between-Bus-Stops/Solution_test.go
+++ src/1184.Distance-Between-Bus-Stops/Solution_test.go
@@ -9,22 +9,26 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name        string
+		distance    []int
+		start       int
+		destination int
+		expect      int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase", []int{1, 2, 3, 4}, 0, 1, 1},
+		{"TestCase", []int{1, 2, 3, 4}, 0, 2, 3},
+		{"TestCase", []int{1, 2, 3, 4}, 0, 3, 4},
+		{"TestCase", []int{1, 2, 3, 4}, 1, 4, 1},
+		{"TestCase", []int{1, 2, 3, 4}, 3, 2, 3},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.distance, c.start, c.destination)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.distance, c.start, c.destination)
 			}
 		})
 	}
