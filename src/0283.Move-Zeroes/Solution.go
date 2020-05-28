916a4da273c1d99d3a7d64e354314db41a178eba
Sathish Babu <sathish.babu@zohocorp.com>
2020-05-28 11:33:26 +0530

    :bento: Updated solution to 283

--- src/0283.Move-Zeroes/Solution.go
+++ src/0283.Move-Zeroes/Solution.go
@@ -14,3 +14,16 @@ func moveZeroes(nums []int) []int {
 	}
 	return nums
 }
+
+func moveZeroes2(nums []int) []int {
+	n := len(nums)
+	if n > 1 {
+		for z, nz := 0, 0; nz < n; nz++ {
+			if nums[nz] != 0 {
+				nums[z], nums[nz] = nums[nz], nums[z]
+				z++
+			}
+		}
+	}
+	return nums
+}
