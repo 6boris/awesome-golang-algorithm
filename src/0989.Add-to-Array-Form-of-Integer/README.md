5819cbb168bbea7019c9b734bf578e71a37087c9
Sathish Babu <sathish.babu@zohocorp.com>
2020-06-04 12:45:06 +0530

    :sparkles: Added solution to 989

--- src/0989.Add-to-Array-Form-of-Integer/README.md
+++ src/0989.Add-to-Array-Form-of-Integer/README.md
@@ -5,11 +5,40 @@
 
 ## Description
 
+For a non-negative integer X, the array-form of X is an array of its digits in left to right order.  For example, if X = 1231, then the array form is [1,2,3,1].
+
+Given the array-form A of a non-negative integer X, return the array-form of the integer X+K.
+
 **Example 1:**
 
 ```
-Input: a = "11", b = "1"
-Output: "100"
+Input: A = [1,2,0,0], K = 34
+Output: [1,2,3,4]
+Explanation: 1200 + 34 = 1234
+```
+
+**Example 2:**
+
+```
+Input: A = [2,7,4], K = 181
+Output: [4,5,5]
+Explanation: 274 + 181 = 455
+```
+
+**Example 3:**
+
+```
+Input: A = [2,1,5], K = 806
+Output: [1,0,2,1]
+Explanation: 215 + 806 = 1021
+```
+
+**Example 4:**
+
+```
+Input: A = [9,9,9,9,9,9,9,9,9,9], K = 1
+Output: [1,0,0,0,0,0,0,0,0,0,0]
+Explanation: 9999999999 + 1 = 10000000000
 ```
 
 ## 题意
--- src/0989.Add-to-Array-Form-of-Integer/Solution_test.go
+++ src/0989.Add-to-Array-Form-of-Integer/Solution_test.go
@@ -10,21 +10,22 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		A      []int
+		K      int
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase", []int{1, 2, 0, 0}, 34, []int{1, 2, 3, 4}},
+		{"TestCase", []int{0}, 0, []int{0}},
+		{"TestCase", []int{9, 9, 9, 9, 9}, 1, []int{1, 0, 0, 0, 0, 0}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.A, c.K)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.A, c.K)
 			}
 		})
 	}
