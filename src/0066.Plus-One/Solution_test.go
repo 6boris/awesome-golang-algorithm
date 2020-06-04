ed50c8befa25051d8968cdb97ff34665a10245bc
Sathish Babu <sathish.babu@zohocorp.com>
2020-06-04 15:50:21 +0530

    :sparkles: Added solution to 66

--- src/0066.Plus-One/Solution_test.go
+++ src/0066.Plus-One/Solution_test.go
@@ -2,6 +2,7 @@ package Solution
 
 import (
 	"reflect"
+	"strconv"
 	"testing"
 )
 
@@ -21,6 +22,28 @@ func TestSolution(t *testing.T) {
 			if !reflect.DeepEqual(ret, c.expect) {
 				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
 					c.expect, ret, c.inputs)
+			}
+		})
+	}
+}
+
+func TestSolution2(t *testing.T) {
+	cases := []struct {
+		name   string
+		input  []int
+		expect []int
+	}{
+		{"TestCase", []int{1, 2, 3}, []int{1, 2, 4}},
+		{"TestCase", []int{9, 9, 9}, []int{1, 0, 0, 0}},
+		{"TestCase", []int{5, 9, 9}, []int{6, 0, 0}},
+		{"TestCase", []int{}, []int{1}},
+	}
+	for i, c := range cases {
+		t.Run(c.name+" "+strconv.Itoa(i+1), func(t *testing.T) {
+			got := Solution(c.input)
+			if !reflect.DeepEqual(got, c.expect) {
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+					c.expect, got, c.input)
 			}
 		})
 	}
