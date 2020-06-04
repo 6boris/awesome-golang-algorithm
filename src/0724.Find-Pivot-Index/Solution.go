72ceaf98a69514020376c9797be1d265eef1298f
Sathish Babu <sathish.babu@zohocorp.com>
2020-06-04 13:15:53 +0530

    :sparkles: Added solution to 724

--- src/0724.Find-Pivot-Index/Solution.go
+++ src/0724.Find-Pivot-Index/Solution.go
@@ -1,5 +1,21 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) int {
+	n := len(nums)
+	if n < 1 {
+		return -1
+	}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	tsum := 0
+	for i, num := range nums {
+		sum -= num
+		if tsum == sum {
+			return i
+		}
+		tsum += num
+	}
+	return -1
 }
--- src/0724.Find-Pivot-Index/Solution_test.go
+++ src/0724.Find-Pivot-Index/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase", []int{1, 7, 3, 6, 5, 6}, 3},
+		{"TestCase", []int{1, 2, 3}, -1},
+		{"TestCase", []int{}, -1},
 	}
 
 	//	开始测试
