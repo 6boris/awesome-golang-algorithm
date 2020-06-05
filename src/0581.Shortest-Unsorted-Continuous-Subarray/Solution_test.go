43322d45c74583c925cc148d006b5cd16e174cc2
Sathish Babu <sathish.babu@zohocorp.com>
2020-06-05 16:43:39 +0530

    :sparkles: Added solution to 581

--- src/0581.Shortest-Unsorted-Continuous-Subarray/Solution_test.go
+++ src/0581.Shortest-Unsorted-Continuous-Subarray/Solution_test.go
@@ -29,6 +29,30 @@ func TestSolution(t *testing.T) {
 	}
 }
 
+func TestSolution2(t *testing.T) {
+	//	测试用例
+	cases := []struct {
+		name   string
+		inputs []int
+		expect int
+	}{
+		{"TestCase", []int{2, 6, 4, 8, 10, 9, 15}, 5},
+		{"TestCase", []int{1, 2, 3, 3, 3}, 0},
+		{"TestCase", []int{1}, 0},
+	}
+
+	//	开始测试
+	for i, c := range cases {
+		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
+			got := Solution2(c.inputs)
+			if !reflect.DeepEqual(got, c.expect) {
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+					c.expect, got, c.inputs)
+			}
+		})
+	}
+}
+
 //	压力测试
 func BenchmarkSolution(b *testing.B) {
 
