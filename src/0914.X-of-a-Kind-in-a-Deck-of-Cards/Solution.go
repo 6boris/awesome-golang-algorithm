aec3a37aa4732185d3e29afe94cbbd7e7060be76
Sathish Babu <sathish.babu@zohocorp.com>
2020-06-05 10:32:39 +0530

    :sparkles: Added solution to 914

--- src/0914.X-of-a-Kind-in-a-Deck-of-Cards/Solution.go
+++ src/0914.X-of-a-Kind-in-a-Deck-of-Cards/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
+func Solution(deck []int) bool {
+	n := len(deck)
+	if n < 2 {
+		return false
+	}
+	hash := make(map[int]int)
+	for _, val := range deck {
+		hash[val]++
+	}
+	ans := hash[deck[0]]
+	for _, val := range hash {
+		ans = gcd(ans, val)
+	}
+	return ans >= 2
+}
+
+func gcd(x, y int) int {
+	for y > 0 {
+		x, y = y, x%y
+	}
 	return x
 }
