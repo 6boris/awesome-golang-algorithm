8b2f096570843e962dae8e786a0e180ee2009aee
Sathish Babu <sathish.babu@zohocorp.com>
2020-06-05 13:08:28 +0530

    :sparkles: Added solution to 189

--- src/0189.Rotate-Array/Solution_test.go
+++ src/0189.Rotate-Array/Solution_test.go
@@ -10,21 +10,25 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		nums   []int
+		k      int
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase", []int{1, 2, 3, 4, 5, 6, 7}, 3, []int{5, 6, 7, 1, 2, 3, 4}},
+		{"TestCase", []int{1, 2, 3, 4, 5, 6, 7}, 7, []int{1, 2, 3, 4, 5, 6, 7}},
+		{"TestCase", []int{1, 2, 3, 4, 5, 6, 7}, 21, []int{1, 2, 3, 4, 5, 6, 7}},
+		{"TestCase", []int{1, 2, 3, 4, 5, 6, 7}, 6, []int{2, 3, 4, 5, 6, 7, 1}},
+		{"TestCase", []int{1, 2, 3, 4, 5, 6, 7}, 20, []int{2, 3, 4, 5, 6, 7, 1}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
-			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+			tmp := c.nums
+			Solution(c.nums, c.k)
+			if !reflect.DeepEqual(c.nums, c.expect) {
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, c.nums, tmp, c.k)
 			}
 		})
 	}
