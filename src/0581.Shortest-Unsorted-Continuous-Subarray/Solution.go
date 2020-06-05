61bde0b3f1895db6a2bbf44f1ca7ab5c658d1a5e
Sathish Babu <sathish.babu@zohocorp.com>
2020-06-05 16:42:12 +0530

    :sparkles: Added solution to 581

--- src/0581.Shortest-Unsorted-Continuous-Subarray/Solution.go
+++ src/0581.Shortest-Unsorted-Continuous-Subarray/Solution.go
@@ -1,5 +1,20 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "math"
+
+func Solution(nums []int) int {
+	start, end, min, max := 0, -1, math.MaxInt32, math.MinInt32
+	for i, j := 0, len(nums)-1; i < len(nums); i, j = i+1, j-1 {
+		if nums[i] >= max {
+			max = nums[i]
+		} else {
+			end = i
+		}
+		if nums[j] <= min {
+			min = nums[j]
+		} else {
+			start = j
+		}
+	}
+	return end - start + 1
 }
