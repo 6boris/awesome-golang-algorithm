06a4fb86a65db399c50f14a7e142f1261206d450
Kyle Liu <kyle.liu.cn@gmail.com>
2020-07-01 19:17:00 +0800

    add 42 solution

--- src/0042.Trapping-Rain-Water/Solution.go
+++ src/0042.Trapping-Rain-Water/Solution.go
@@ -1,5 +1,44 @@
 package Solution
 
-func Solution(x bool) bool {
+import "fmt"
+
+// 三次线性扫描
+func trap(height []int) int {
+	if len(height) == 0 {
+		return 0
+	}
+
+	n, ans := len(height), 0
+	leftMax, rightMax := make([]int, n), make([]int, n)
+
+	//	求第i个矩形左边最高矩形的高度
+	leftMax[0] = height[0]
+	for i := 1; i < n; i++ {
+		leftMax[i] = max(leftMax[i-1], height[i])
+	}
+	//	求第i个矩形右边最高矩形的高度
+	rightMax[n-1] = height[n-1]
+	for i := n - 2; i >= 0; i-- {
+		rightMax[i] = max(rightMax[i+1], height[i])
+	}
+
+	for i := 0; i < n; i++ {
+		fmt.Println(i, leftMax[i], rightMax[i], height[i])
+		ans += min(leftMax[i], rightMax[i]) - height[i]
+	}
+
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
 	return x
 }
