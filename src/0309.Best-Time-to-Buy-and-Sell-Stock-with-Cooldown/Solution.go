888f681c5e210ae6eb80d30a7bd27b7adb226327
Kyle Liu <kyle.liu.cn@gmail.com>
2020-07-29 16:46:03 +0800

    add 309 solution

--- src/0309.Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/Solution.go
+++ src/0309.Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/Solution.go
@@ -1,5 +1,21 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func maxProfit(prices []int) int {
+	sold, held, reset := -1<<31, -1<<31, 0
+
+	for _, price := range prices {
+		preSold := sold
+		sold = held + price
+		held = max(held, reset-price)
+		reset = max(reset, preSold)
+
+	}
+	return max(sold, reset)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
