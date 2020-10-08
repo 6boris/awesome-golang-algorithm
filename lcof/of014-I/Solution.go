5f76a710b9cb1f9664327b2560244d03fcda1d87
Kyle Liu <kyle@gin.sh>
2020-10-08 15:10:30 +0800

    update 14 jz solution

--- lcof/of014-I/Solution.go
+++ lcof/of014-I/Solution.go
@@ -1,22 +1,18 @@
 package Solution
 
 func cuttingRope(n int) int {
-	dp := make(map[int]int)
-	dp[1] = 1 // 首项
-	for i := 2; i < n+1; i++ {
-		j, k := 1, i-1
-		ans := 0
-		for j <= k {
-			ans = max(ans, max(j, dp[j])*max(k, dp[k])) // 递推公式
-			j++
-			k--
+	dp := make([]int, n+1)
+	for i := 2; i <= n; i++ {
+		curMax := 0
+		for j := 1; j < i; j++ {
+			curMax = max(curMax, max(j*(i-j), j*dp[i-j]))
 		}
-		dp[i] = ans
+		dp[i] = curMax
 	}
 	return dp[n]
 }
 
-func max(x int, y int) int {
+func max(x, y int) int {
 	if x > y {
 		return x
 	}
