f2c709d8aaa2630c9ab071edb45598e95547a82a
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-06 13:35:20 +0530

    ✨ Add solution and test cases to problem 68

--- leetcode/1-100/0068.Text-Justification/Solution.go
+++ leetcode/1-100/0068.Text-Justification/Solution.go
@@ -1,5 +1,29 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "strings"
+
+func Solution(words []string, k int) []string {
+	result := make([]string, 0);
+	currWords := make([]string, 0);
+	currWordsLen := 0;
+	for _, word := range words {
+		if currWordsLen + len(currWords) + len(word) > k {
+			if len(currWords) == 1 {
+				result = append(result, currWords[0] + strings.Repeat(" ", k - currWordsLen));
+			} else {
+				spaces := k - currWordsLen
+				spacesBetween, extras := spaces / (len(currWords) - 1), spaces % (len(currWords) - 1);
+				for i := 0; i < extras; i++ {
+					currWords[i] += " ";
+				}
+				result = append(result, strings.Join(currWords, strings.Repeat(" ", spacesBetween)));
+			}
+			currWords = make([]string, 0);
+			currWordsLen = 0;
+		}
+		currWords = append(currWords, word);
+		currWordsLen += len(word);
+	}
+	result = append(result, strings.Join(currWords, " ") + strings.Repeat(" ", k - currWordsLen - len(currWords) + 1));
+	return result;
 }
