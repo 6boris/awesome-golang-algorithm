37dd55f027c4d911fdd36698270d228b90a53153
0xff-dev <stevenshuang521@gmail.com>
2021-07-24 00:23:18 +0800

    Add solution and test-cases for problem 814

--- leetcode/801-900/0814.Binary-Tree-Pruning/Solution.go
+++ leetcode/801-900/0814.Binary-Tree-Pruning/Solution.go
@@ -1,5 +1,36 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(root *TreeNode) *TreeNode {
+	newTree, del := pruneTreeAux(root)
+	if del {
+		return nil
+	}
+	return newTree
+}
+
+func pruneTreeAux(root *TreeNode) (*TreeNode, bool) {
+	if root == nil {
+		return nil, true
+	}
+
+	leftNode, delLeft := pruneTreeAux(root.Left)
+	rightNode, delRight := pruneTreeAux(root.Right)
+	delSelf := true
+	n := &TreeNode{}
+	if !delLeft {
+		delSelf = false
+		n.Left = leftNode
+	}
+	if !delRight {
+		delSelf = false
+		n.Right = rightNode
+	}
+
+	delSelf = root.Val == 0 && delSelf
+	if delSelf {
+		return nil, delSelf
+	}
+	n.Val = root.Val
+	n.Left, n.Right = leftNode, rightNode
+	return n, delSelf
 }
