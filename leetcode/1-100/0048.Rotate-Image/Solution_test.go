4687f0f6fa9e6f20020e85e014407260de586384
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-24 22:08:01 +0530

    :sparkles: Add solution and testcases to problem 48

--- leetcode/1-100/0048.Rotate-Image/Solution_test.go
+++ leetcode/1-100/0048.Rotate-Image/Solution_test.go
@@ -9,21 +9,30 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs [][]int
+		expect [][]int
 	}{
-		{"TestCacse 1", true, true},
-		{"TestCacse 1", true, true},
-		{"TestCacse 1", false, false},
+		{"TestCase 1", [][]int{
+			{1,2,3}, {4,5,6}, {7,8,9},
+		}, [][]int{
+			{7,4,1}, {8,5,2}, {9,6,3},
+		}},
+		{"TestCase 2", [][]int{
+			{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16},
+		}, [][]int{
+			{15,13,2,5},{14,3,4,1},{12,6,8,9},{16,7,10,11},
+		}},
 	}
 
 	//	开始测试
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			ret := Solution(c.inputs)
-			if !reflect.DeepEqual(ret, c.expect) {
+			input := [][]int{}
+			copy(input, c.inputs)
+			Solution(c.inputs)
+			if !reflect.DeepEqual(c.inputs, c.expect) {
 				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, ret, c.inputs)
+					c.expect, c.inputs, input)
 			}
 		})
 	}
