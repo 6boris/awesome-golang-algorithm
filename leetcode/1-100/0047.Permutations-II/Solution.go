0ada554b5fa2db29791f410417eeb9df0e4ee391
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-24 22:00:11 +0530

    :sparkles: Add solution and testcases to problem 47

--- leetcode/1-100/0047.Permutations-II/Solution.go
+++ leetcode/1-100/0047.Permutations-II/Solution.go
@@ -1,5 +1,35 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(nums []int) [][]int {
+    res := [][]int{}
+    sort.Slice(nums, func(i, j int) bool {
+        return nums[i] < nums[j]
+    })
+    visited := make(map[int]bool)
+    helper(nums, []int{}, visited, &res)
+    return res
+}
+
+func helper(nums, res []int, visited map[int]bool, final *[][]int) {
+    if len(nums) == len(res) {
+        temp := make([]int, len(res))
+        copy(temp, res)
+        *final = append(*final, temp)
+        return
+    }
+    for i := 0; i < len(nums); i++ {
+        if visited[i] {
+            continue
+        }
+        if i > 0 && nums[i] == nums[i - 1] && !visited[i - 1] {
+            continue
+        }
+        res = append(res, nums[i])
+        visited[i] = true
+        helper(nums, res, visited, final)
+        visited[i] = false
+        res = res[:len(res) - 1]
+    }
 }
