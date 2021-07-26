058ff8c30914c7fb4eb53f561933d0b8837131aa
0xff-dev <stevenshuang521@gmail.com>
2021-07-26 22:09:18 +0800

    Add solution and test-cases for problem 108

--- leetcode/101-200/0108.Convert-Sorted-Array-to-Binary-Search-Tree/Solution.go
+++ leetcode/101-200/0108.Convert-Sorted-Array-to-Binary-Search-Tree/Solution.go
@@ -1,5 +1,19 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	node := &TreeNode{}
+	mid := len(nums) / 2
+	node.Val = nums[mid]
+	if mid > 0 {
+		node.Left = Solution(nums[:mid])
+	}
+	if mid < len(nums)-1 {
+		node.Right = Solution(nums[mid+1:])
+	}
+
+	return node
 }
