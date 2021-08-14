a0f14f326e83c7e218ea2869ca2a42a381896e26
0xff-dev <stevenshuang521@gmail.com>
2021-08-14 16:31:42 +0800

    Add solution and test-cases for problem 1314

--- leetcode/1301-1400/1314.Matrix-Block-Sum/Solution_test.go
+++ leetcode/1301-1400/1314.Matrix-Block-Sum/Solution_test.go
@@ -10,21 +10,21 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		matrix [][]int
+		k      int
+		expect [][]int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, 1, [][]int{{12, 21, 16}, {27, 45, 33}, {24, 39, 28}}},
+		{"TestCase2", [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, 2, [][]int{{45, 45, 45}, {45, 45, 45}, {45, 45, 45}}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.matrix, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with input-mat: %v, input-k: %v",
+					c.expect, got, c.matrix, c.k)
 			}
 		})
 	}
