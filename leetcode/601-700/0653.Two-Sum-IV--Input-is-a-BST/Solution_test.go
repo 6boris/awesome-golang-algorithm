690bb61fa50108f569f8f702c4999c203177f17c
0xff-dev <stevenshuang521@gmail.com>
2021-08-24 22:00:29 +0800

    Add solution and test-cases for problem 653

--- leetcode/601-700/0653.Two-Sum-IV--Input-is-a-BST/Solution_test.go
+++ leetcode/601-700/0653.Two-Sum-IV--Input-is-a-BST/Solution_test.go
@@ -10,21 +10,46 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
+		inputs *TreeNode
+		target int
 		expect bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", &TreeNode{
+			Val: 5,
+			Left: &TreeNode{
+				Val:   3,
+				Left:  &TreeNode{Val: 2},
+				Right: &TreeNode{Val: 4},
+			},
+			Right: &TreeNode{
+				Val:   6,
+				Right: &TreeNode{Val: 7},
+			},
+		}, 9, true},
+		{"TestCase2", &TreeNode{
+			Val: 5,
+			Left: &TreeNode{
+				Val:   3,
+				Left:  &TreeNode{Val: 2},
+				Right: &TreeNode{Val: 4},
+			},
+			Right: &TreeNode{
+				Val:   6,
+				Right: &TreeNode{Val: 7},
+			},
+		}, 28, false},
+		{"TestCase3", &TreeNode{Val: 2, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 3}}, 4, true},
+		{"TestCase4", &TreeNode{Val: 2, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 3}}, 1, false},
+		{"TestCase4", &TreeNode{Val: 2, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 3}}, 3, true},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.target)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v, target: %v",
+					c.expect, got, c.inputs, c.target)
 			}
 		})
 	}
