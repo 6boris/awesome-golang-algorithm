491bc24b23fda5ee69aa3b2295fc803555ff7229
Kyle Liu <kyle.liu.cn@gmail.com>
2021-08-31 23:39:30 +0800

    update 146 Solution.go

--- leetcode/101-200/0146.LRU-Cache/Solution.go
+++ leetcode/101-200/0146.LRU-Cache/Solution.go
@@ -1,50 +1,82 @@
 package Solution
 
-import (
-	"container/list"
-	"fmt"
-)
-
 type LRUCache struct {
-	Capacity int
-	Data     *list.List
-	//*list.List
+	size       int
+	capacity   int
+	cache      map[int]*DLinkedNode
+	head, tail *DLinkedNode
+}
+
+type DLinkedNode struct {
+	key, value int
+	prev, next *DLinkedNode
+}
+
+func initDLinkedNode(key, value int) *DLinkedNode {
+	return &DLinkedNode{
+		key:   key,
+		value: value,
+	}
 }
 
 func Constructor(capacity int) LRUCache {
-	lru := LRUCache{
-		Capacity: capacity,
-		Data:     list.New(),
+	l := LRUCache{
+		cache:    map[int]*DLinkedNode{},
+		head:     initDLinkedNode(0, 0),
+		tail:     initDLinkedNode(0, 0),
+		capacity: capacity,
 	}
-	return lru
+	l.head.next = l.tail
+	l.tail.prev = l.head
+	return l
 }
 
 func (this *LRUCache) Get(key int) int {
-	for e := this.Data.Front(); e != nil; e = e.Next() {
-
-		//fmt.Println(key, e.Value.(list.Element).Value.(map[int]int)[key])
-
-		if v, ok := e.Value.(list.Element).Value.(map[int]int)[key]; ok {
-			this.Data.MoveBefore(e, this.Data.Front())
-			return v
-		}
+	if _, ok := this.cache[key]; !ok {
+		return -1
 	}
-	return -1
+	node := this.cache[key]
+	this.moveToHead(node)
+	return node.value
 }
 
 func (this *LRUCache) Put(key int, value int) {
-	this.Data.PushFront(
-		list.Element{
-			Value: map[int]int{key: value},
-		},
-	)
-	if this.Data.Len() > this.Capacity {
-		this.Data.Remove(this.Data.Back())
+	if _, ok := this.cache[key]; !ok {
+		node := initDLinkedNode(key, value)
+		this.cache[key] = node
+		this.addToHead(node)
+		this.size++
+		if this.size > this.capacity {
+			removed := this.removeTail()
+			delete(this.cache, removed.key)
+			this.size--
+		}
+	} else {
+		node := this.cache[key]
+		node.value = value
+		this.moveToHead(node)
 	}
 }
 
-func (this *LRUCache) Print() {
-	for e := this.Data.Front(); e != nil; e = e.Next() {
-		fmt.Println(e.Value.(list.Element).Value.(map[int]int))
-	}
+func (this *LRUCache) addToHead(node *DLinkedNode) {
+	node.prev = this.head
+	node.next = this.head.next
+	this.head.next.prev = node
+	this.head.next = node
+}
+
+func (this *LRUCache) removeNode(node *DLinkedNode) {
+	node.prev.next = node.next
+	node.next.prev = node.prev
+}
+
+func (this *LRUCache) moveToHead(node *DLinkedNode) {
+	this.removeNode(node)
+	this.addToHead(node)
+}
+
+func (this *LRUCache) removeTail() *DLinkedNode {
+	node := this.tail.prev
+	this.removeNode(node)
+	return node
 }
