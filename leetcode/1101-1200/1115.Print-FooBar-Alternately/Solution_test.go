a54d383905aed65cc78fbebd473ac43aad7c77c0
Kyle Liu <kyle.liu.cn@gmail.com>
2021-09-17 22:13:22 +0800

    update Makefile

---
+++ leetcode/1101-1200/1115.Print-FooBar-Alternately/Solution_test.go
@@ -0,0 +1,48 @@
+package Solution
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Solution func Info
+type SolutionFuncType func(x bool) bool
+
+var SolutionFuncList = []SolutionFuncType{
+	Solution_1,
+	Solution_2,
+}
+
+// Test case info struct
+type Case struct {
+	name   string
+	input  bool
+	expect bool
+}
+
+// Test case
+var cases = []Case{
+	{name: "TestCase 1", input: true, expect: true},
+	{name: "TestCase 2", input: false, expect: false},
+}
+
+// TestSolution Run test case for all solutions
+func TestSolution(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, f := range SolutionFuncList {
+		funcName := strings.Split(runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name(), ".")[1]
+		for _, c := range cases {
+			t.Run(fmt.Sprintf("%s %s", funcName, c.name), func(t *testing.T) {
+				got := f(c.input)
+				ast.Equal(c.expect, got,
+					"func: %v case: %v ", funcName, c.name)
+			})
+		}
+	}
+}
--- test.sh
+++ test.sh
@@ -1,16 +1,31 @@
+#!/bin/bash
 #!/usr/bin/env bash
 
 set -e
 echo "" > coverage.txt
 
-echo "test dir " $1
+function test_lcof(){
+    test_dir="lcof"
+    echo "Test Dir ${test_dir}"
+    go clean -testcache
+    go test -coverprofile=profile.out -covermode=atomic -parallel 16 -p 16 ./$test_dir/*
+}
+function test_leetcode(){
+  go clean -testcache
+  test_dir="leetcode"
+  echo "Test Dir ${test_dir}"
+  for d in $(ls $test_dir); do
+    {
+     echo $d
+      go test -coverprofile=profile.out -covermode=atomic -parallel 16 -p 16 ./$test_dir/$d/*
+      if [ -f profile.out ]; then
+          cat profile.out >> coverage.txt
+          rm profile.out
+      fi
+    } &
+  done
+  wait
+}
 
-for d in $(go list ./$1... | grep -v vendor); do
-    echo $d
-    go test -json -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
-done
-
+test_lcof
+test_leetcode
