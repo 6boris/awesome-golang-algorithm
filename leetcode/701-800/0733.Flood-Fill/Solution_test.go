552336188130698e44da034a8d55bf464acdc047
0xff-dev <stevenshuang521@gmail.com>
2021-10-04 16:06:27 +0800

    Add solution and test-cases for problem 733

--- leetcode/701-800/0733.Flood-Fill/Solution_test.go
+++ leetcode/701-800/0733.Flood-Fill/Solution_test.go
@@ -9,31 +9,25 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name             string
+		inputs           [][]int
+		sr, sc, newColor int
+		expect           [][]int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}, 1, 1, 2, [][]int{{2, 2, 2}, {2, 2, 0}, {2, 0, 1}}},
+		{"TestCase2", [][]int{{1}}, 0, 0, 2, [][]int{{2}}},
+		{"TestCase3", [][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}, 1, 1, 2, [][]int{{1, 1, 1}, {1, 2, 1}, {1, 1, 1}}},
+		{"TestCase4", [][]int{{0, 0, 0}, {0, 0, 0}}, 0, 0, 2, [][]int{{2, 2, 2}, {2, 2, 2}}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.sr, c.sc, c.newColor)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs-image: %v, input-sr: %v, input-sc: %v",
+					c.expect, got, c.inputs, c.sr, c.sc)
 			}
 		})
 	}
-}
-
-//	压力测试
-func BenchmarkSolution(b *testing.B) {
-}
-
-//	使用案列
-func ExampleSolution() {
 }
