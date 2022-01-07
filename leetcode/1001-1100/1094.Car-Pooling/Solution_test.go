1323289d94a0294b929b23b850e5ea5ad29ace6c
0xff-dev <stevenshuang521@gmail.com>
2022-01-07 09:29:16 +0800

    Add solution and test-cases for problem 1094

--- leetcode/1001-1100/1094.Car-Pooling/Solution_test.go
+++ leetcode/1001-1100/1094.Car-Pooling/Solution_test.go
@@ -10,18 +10,39 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
+		inputs [][]int
+		cap    int
 		expect bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{
+			{2, 1, 5},
+			{3, 3, 7},
+		}, 4, false},
+		{"TestCase2", [][]int{
+			{2, 1, 5},
+			{3, 3, 7},
+		}, 5, true},
+		{"TestCase3", [][]int{
+			{1, 1, 8},
+			{2, 2, 7},
+			{3, 3, 6},
+		}, 5, false},
+		{"TestCase4", [][]int{
+			{1, 1, 8},
+			{2, 2, 7},
+			{3, 3, 6},
+		}, 6, true,
+		},
+		{"TestCase5", [][]int{
+			{2, 1, 5},
+			{3, 5, 7},
+		}, 3, true},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.cap)
 			if !reflect.DeepEqual(got, c.expect) {
 				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
 					c.expect, got, c.inputs)
