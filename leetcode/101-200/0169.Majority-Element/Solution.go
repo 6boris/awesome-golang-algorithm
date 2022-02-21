65552da79655512b657c954a6aab7c8240e5d29d
0xff-dev <stevenshuang521@gmail.com>
2022-02-21 22:14:05 +0800

    Add solution and README for problem 169

--- leetcode/101-200/0169.Majority-Element/Solution.go
+++ leetcode/101-200/0169.Majority-Element/Solution.go
@@ -49,3 +49,20 @@ func majorityElement_3(nums []int) int {
 
 	return majorityElement_3(numsTmp)
 }
+
+func majorityElement_4(nums []int) int {
+	times := 1
+	target := nums[0]
+	for idx := 1; idx < len(nums); idx++ {
+		if nums[idx] == target {
+			times++
+			continue
+		}
+		times--
+		if times == 0 {
+			target = nums[idx]
+			times = 1
+		}
+	}
+	return target
+}
