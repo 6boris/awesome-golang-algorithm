9297c497d3c3a9369d12557962b7575f87642c9a
0xff-dev <stevenshuang521@gmail.com>
2022-02-27 14:51:35 +0800

    Add solution and test-cases for problem  673

--- leetcode/601-700/0673.Number-of-Longest-Increasing-Subsequence/Solution.go
+++ leetcode/601-700/0673.Number-of-Longest-Increasing-Subsequence/Solution.go
@@ -1,5 +1,35 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) int {
+	dp := make([]int, len(nums))
+	maxCount := make([]int, len(nums))
+	dp[0], maxCount[0] = 1, 1
+	longestSubSeq := 1
+	for idx := 1; idx < len(nums); idx++ {
+		dp[idx] = 1
+		maxCount[idx] = 1
+		for pre := idx - 1; pre >= 0; pre-- {
+			if nums[pre] < nums[idx] {
+				times := dp[pre] + 1
+				if times == dp[idx] {
+					maxCount[idx] += maxCount[pre]
+					continue
+				}
+				if times > dp[idx] {
+					dp[idx] = times
+					maxCount[idx] = maxCount[pre]
+				}
+				if longestSubSeq < dp[idx] {
+					longestSubSeq = dp[idx]
+				}
+			}
+		}
+	}
+	result := 0
+	for idx := 0; idx < len(nums); idx++ {
+		if longestSubSeq == dp[idx] {
+			result += maxCount[idx]
+		}
+	}
+	return result
 }
