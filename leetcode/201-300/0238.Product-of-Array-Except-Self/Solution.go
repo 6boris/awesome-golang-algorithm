f4805aca61c449df0309b92a423ccfdf63b01748
0xff-dev <stevenshuang521@gmail.com>
2022-03-13 01:34:43 +0800

    Add solution and test-cases for problem 238

--- leetcode/201-300/0238.Product-of-Array-Except-Self/Solution.go
+++ leetcode/201-300/0238.Product-of-Array-Except-Self/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) []int {
+	length := len(nums)
+	r := make([]int, length)
+	if length == 0 || length == 1 {
+		return r
+	}
+
+	r[0] = nums[0]
+	for i := 1; i < length-1; i++ {
+		r[i] = r[i-1] * nums[i]
+	}
+	r[length-1] = r[length-2]
+
+	rightProduct := nums[length-1]
+	for idx := length - 2; idx > 0; idx-- {
+		r[idx] = r[idx-1] * rightProduct
+		rightProduct *= nums[idx]
+	}
+	r[0] = rightProduct
+
+	return r
 }
