e5972072834a168db45fdc28dbfec4ca102841dc
0xff-dev <stevenshuang521@gmail.com>
2022-04-30 18:00:31 +0800

    Add solution and test-cases for problem 401

--- leetcode/401-500/0401.Binary-Watch/README.md
+++ leetcode/401-500/0401.Binary-Watch/README.md
@@ -1,26 +1,33 @@
 # [401.Binary Watch][title]
 
-> [!WARNING|style:flat]
-> This question is temporarily unanswered if you have good ideas. Welcome to [Create Pull Request PR](https://github.com/kylesliu/awesome-golang-algorithm)
-
 ## Description
+A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the bottom represent the minutes (0-59). Each LED represents a zero or one, with the least significant bit on the right.
+
+- For example, the below binary watch reads `"4:51"`  
+![watch](./binarywatch.jpg)
+
+Given an integer `turnedOn` which represents the number of LEDs that are currently on, return all possible times the watch could represent. You may return the answer in **any order**.
+
+The hour must not contain a leading zero.
+
+- For example, `"01:00"` is not valid. It should be `"1:00"`.
+
+The minute must be consist of two digits and may contain a leading zero.
+
+- For example, `"10:2"` is not valid. It should be `"10:02"`.
 
 **Example 1:**
 
 ```
-Input: a = "11", b = "1"
-Output: "100"
+Input: turnedOn = 1
+Output: ["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
 ```
 
-## 题意
-> ...
+**Example 2:**
 
-## 题解
-
-### 思路1
-> ...
-Binary Watch
-```go
+```
+Input: turnedOn = 9
+Output: []
 ```
 
 
--- leetcode/401-500/0401.Binary-Watch/Solution.go
+++ leetcode/401-500/0401.Binary-Watch/Solution.go
@@ -1,5 +1,64 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "fmt"
+
+/*
+func countOf1(n int) int {
+	cnt := 0
+	for n > 0 {
+		cnt++
+		n = n & (n-1)
+	}
+	return cnt
+}
+
+func gen() {
+	r := make(map[int][]string)
+	for n := 1; n < 60 ; n++ {
+		c := countOf1(n)
+		if _, ok := r[c]; !ok {
+			r[c] = make([]string, 0)
+		}
+		r[c] = append(r[c], fmt.Sprintf("%d", n))
+	}
+	for k, v := range r {
+		fmt.Printf(`%d: '"%s"'`, k, strings.Join(v, `","`))
+	}
+}
+*/
+var (
+	hour = [4][]string{
+		{"0"},
+		{"1", "2", "4", "8"},
+		{"3", "5", "6", "9", "10"},
+		{"7", "11"},
+	}
+
+	minute = [6][]string{
+		{"00"},
+		{"01", "02", "04", "08", "16", "32"},
+		{"03", "05", "06", "09", "10", "12", "17", "18", "20", "24", "33", "34", "36", "40", "48"},
+		{"07", "11", "13", "14", "19", "21", "22", "25", "26", "28", "35", "37", "38", "41", "42", "44", "49", "50", "52", "56"},
+		{"15", "23", "27", "29", "30", "39", "43", "45", "46", "51", "53", "54", "57", "58"},
+		{"31", "47", "55", "59"},
+	}
+)
+
+func Solution(turnedOn int) []string {
+	ans := make([]string, 0)
+	for top := 0; top <= turnedOn && top < 4; top++ {
+		binaryWatch(top, turnedOn-top, &ans)
+	}
+	return ans
+}
+
+func binaryWatch(top, down int, ans *[]string) {
+	if down >= 6 {
+		return
+	}
+	for _, h := range hour[top] {
+		for _, m := range minute[down] {
+			*ans = append(*ans, fmt.Sprintf("%s:%s", h, m))
+		}
+	}
 }
