f09ca2ee2868324daa1001f0fb7cc50fd9039bc9
0xff-dev <stevenshuang521@gmail.com>
2022-05-02 15:06:44 +0800

    Add solution and test-cases for problem 905

--- leetcode/901-1000/0905.Sort-Array-By-Parity/Solution.go
+++ leetcode/901-1000/0905.Sort-Array-By-Parity/Solution.go
@@ -1,5 +1,49 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) []int {
+	inQuickSort(nums, 0, len(nums)-1, less)
+	return nums
+}
+
+func inQuickSort(nums []int, start, end int, less func(i, j int) bool) {
+	if r := end - start + 1; r == 0 || r == 1 {
+		return
+	}
+	cmpObj := nums[start]
+	lessIdx := start
+	for idx := start + 1; idx <= end; idx++ {
+		if less(nums[idx], cmpObj) {
+			lessIdx++
+			nums[lessIdx], nums[idx] = nums[idx], nums[lessIdx]
+		}
+	}
+	nums[start], nums[lessIdx] = nums[lessIdx], nums[start]
+	inQuickSort(nums, start, lessIdx-1, less)
+	inQuickSort(nums, lessIdx+1, end, less)
+}
+
+func less(i, j int) bool {
+	iEven := i&1 == 0
+	jEven := j&1 == 0
+	if iEven && jEven || !iEven && !jEven {
+		return i < j
+	}
+	if !jEven {
+		return true
+	}
+	return false
+}
+
+func Solution1(nums []int) []int {
+	s, e := 0, len(nums)-1
+	for s < e {
+		for ; s < e && nums[s]&1 == 0; s++ {
+		}
+		for ; s < e && nums[e]&1 == 1; e-- {
+		}
+		if s < e {
+			nums[s], nums[e] = nums[e], nums[s]
+		}
+	}
+	return nums
 }
