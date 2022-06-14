ab52178f3e7a85dfee18798ca66dda1b809cfbf7
0xff-dev <stevenshuang521@gmail.com>
2022-06-14 22:40:40 +0800

    Add solution and test-cases for problem 583

--- leetcode/501-600/0583.Delete-Operation-for-Two-Strings/README.md
+++ leetcode/501-600/0583.Delete-Operation-for-Two-Strings/README.md
@@ -1,28 +1,24 @@
 # [583.Delete Operation for Two Strings][title]
 
-> [!WARNING|style:flat]
-> This question is temporarily unanswered if you have good ideas. Welcome to [Create Pull Request PR](https://github.com/kylesliu/awesome-golang-algorithm)
-
 ## Description
+Given two strings `word1` and `word2`, return the minimum number of **steps** required to make `word1` and `word2` the same.
+
+In one **step**, you can delete exactly one character in either string.
 
 **Example 1:**
 
 ```
-Input: a = "11", b = "1"
-Output: "100"
+Input: word1 = "sea", word2 = "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
 ```
 
-## 题意
-> ...
+**Example 2:**
 
-## 题解
-
-### 思路1
-> ...
-Delete Operation for Two Strings
-```go
 ```
-
+Input: word1 = "leetcode", word2 = "etco"
+Output: 4
+```
 
 ## 结语
 
--- leetcode/501-600/0583.Delete-Operation-for-Two-Strings/Solution_test.go
+++ leetcode/501-600/0583.Delete-Operation-for-Two-Strings/Solution_test.go
@@ -9,22 +9,24 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name         string
+		word1, word2 string
+		expect       int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "sea", "eat", 2},
+		{"TestCase2", "leetcode", "etco", 4},
+		{"TestCase3", "a", "a", 0},
+		{"TestCase4", "aaaaaaa", "aaaa", 3},
+		{"TestCase4", "a", "b", 2},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.word1, c.word2)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.word1, c.word2)
 			}
 		})
 	}
