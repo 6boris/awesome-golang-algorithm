183997e0ca68d8f27c6b7976ec94670293e4f4eb
0xff-dev <stevenshuang521@gmail.com>
2022-07-09 16:41:33 +0800

    Add solution and test-cases for problem  2236

--- leetcode/2201-2300/2236.Root-Equals-Sum-of-Children/Solution.go
+++ leetcode/2201-2300/2236.Root-Equals-Sum-of-Children/Solution.go
@@ -1,5 +1,25 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Left, Right *TreeNode
+	Val         int
+}
+
+func Solution(root *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	sum := 0
+	checkTree2236(root, &sum)
+	return sum == root.Val*2
+}
+
+func checkTree2236(root *TreeNode, sum *int) {
+	if root == nil {
+		return
+	}
+
+	*sum += root.Val
+	checkTree2236(root.Left, sum)
+	checkTree2236(root.Right, sum)
 }
