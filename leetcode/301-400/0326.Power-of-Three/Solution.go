8c35c5328971310b8e6e2d1994b10b11e8838168
0xff-dev <stevenshuang521@gmail.com>
2022-08-24 21:33:52 +0800

    Add solution and test-cases for problem 326

--- leetcode/301-400/0326.Power-of-Three/Solution.go
+++ leetcode/301-400/0326.Power-of-Three/Solution.go
@@ -1,5 +1,7 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int) bool {
+	// n = 3^x ==> x= log3 n ==> log(n)/log(3)
+	// 看到另一个解法，找到int32范围最大幂次，然后确实mod
+	return n > 0 && 1162261467%n == 0
 }
