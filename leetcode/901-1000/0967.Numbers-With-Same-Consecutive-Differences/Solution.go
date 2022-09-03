fd2770850eb9122f63417144909480b8b7205274
0xff-dev <stevenshuang521@gmail.com>
2022-09-03 15:51:29 +0800

    Add solution and test-cases for problem 967

--- leetcode/901-1000/0967.Numbers-With-Same-Consecutive-Differences/Solution.go
+++ leetcode/901-1000/0967.Numbers-With-Same-Consecutive-Differences/Solution.go
@@ -1,5 +1,32 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "fmt"
+
+func Solution(n int, k int) []int {
+
+	ans := make([]int, 0)
+	bs := make([]byte, n)
+	for idx := 1; idx <= 9; idx++ {
+		ans = append(ans, helper967(0, idx, k, n, bs)...)
+	}
+	return ans
+}
+
+func helper967(deep, now, k, n int, bs []byte) []int {
+
+	ans := make([]int, 0)
+	bs[deep] = uint8(now) + '0'
+	if deep == n-1 {
+		var num int
+		fmt.Sscanf(string(bs), "%d", &num)
+		return []int{num}
+	}
+	if r := now - k; r >= 0 && r <= 9 {
+		ans = append(ans, helper967(deep+1, r, k, n, bs)...)
+	}
+	if r := now + k; r >= 0 && r <= 9 && k != 0 {
+		ans = append(ans, helper967(deep+1, r, k, n, bs)...)
+	}
+
+	return ans
 }
