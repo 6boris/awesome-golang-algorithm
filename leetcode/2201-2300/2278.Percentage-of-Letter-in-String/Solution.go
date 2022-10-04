86d296b58fd5a99e117ad4057ee7a42f03963510
0xff-dev <stevenshuang521@gmail.com>
2022-10-04 12:13:59 +0800

    Add solution and test-cases for problem 2278

--- leetcode/2201-2300/2278.Percentage-of-Letter-in-String/Solution.go
+++ leetcode/2201-2300/2278.Percentage-of-Letter-in-String/Solution.go
@@ -1,5 +1,12 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string, letter byte) int {
+	count := 0
+	for _, b := range s {
+		if byte(b) == letter {
+			count++
+		}
+	}
+
+	return count * 100 / len(s)
 }
--- leetcode/2201-2300/2278.Percentage-of-Letter-in-String/Solution_test.go
+++ leetcode/2201-2300/2278.Percentage-of-Letter-in-String/Solution_test.go
@@ -10,30 +10,30 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs string
+		letter byte
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "foobar", byte('o'), 33},
+		{"TestCase2", "jjjj", byte('k'), 0},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.letter)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.inputs, c.letter)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
