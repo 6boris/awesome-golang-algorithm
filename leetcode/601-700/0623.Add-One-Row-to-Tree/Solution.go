553a350dbe897757d01157972a904d54381754d3
0xff-dev <stevenshuang521@gmail.com>
2022-10-05 19:31:34 +0800

    Add solution and test-cases for problem 623

--- leetcode/601-700/0623.Add-One-Row-to-Tree/Solution.go
+++ leetcode/601-700/0623.Add-One-Row-to-Tree/Solution.go
@@ -1,5 +1,35 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func Solution(root *TreeNode, val int, depth int) *TreeNode {
+	if depth == 1 {
+		node := &TreeNode{Val: val, Left: root}
+		return node
+	}
+	addOneRow623(root, val, 1, depth)
+	return root
+}
+
+func addOneRow623(root *TreeNode, val, depth, target int) {
+	if root == nil {
+		return
+	}
+
+	if depth == target-1 {
+		// need to add node
+		leftNode := &TreeNode{Val: val, Left: root.Left}
+		rightNode := &TreeNode{Val: val, Right: root.Right}
+
+		root.Left = leftNode
+		root.Right = rightNode
+		return
+	}
+
+	addOneRow623(root.Left, val, depth+1, target)
+	addOneRow623(root.Right, val, depth+1, target)
 }
