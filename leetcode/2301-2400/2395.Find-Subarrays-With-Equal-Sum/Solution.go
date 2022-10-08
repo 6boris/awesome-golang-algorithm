73247ab0bd9cea289ab272d864bc4d39079af6a4
0xff-dev <stevenshuang521@gmail.com>
2022-10-08 22:32:27 +0800

    Add solution and test-cases for problem 2395

--- leetcode/2301-2400/2395.Find-Subarrays-With-Equal-Sum/Solution.go
+++ leetcode/2301-2400/2395.Find-Subarrays-With-Equal-Sum/Solution.go
@@ -1,5 +1,13 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) bool {
+	sumMap := make(map[int]struct{})
+	for loop := 0; loop < len(nums)-1; loop++ {
+		s := nums[loop] + nums[loop+1]
+		if _, ok := sumMap[s]; ok {
+			return true
+		}
+		sumMap[s] = struct{}{}
+	}
+	return false
 }
