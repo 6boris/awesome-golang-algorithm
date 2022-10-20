444d5fff22d3f0d3d41db42087273bba66aa6efe
0xff-dev <stevenshuang521@gmail.com>
2022-10-20 22:54:00 +0800

    Add solution and test-cases for problem 2325

--- leetcode/2301-2400/2325.Decode-the-Message/Solution.go
+++ leetcode/2301-2400/2325.Decode-the-Message/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "bytes"
+
+func Solution(key string, message string) string {
+	bucket := make([]uint8, 26) // space
+
+	char := 'a'
+	for _, b := range key {
+		if b != ' ' && bucket[b-'a'] == 0 {
+			bucket[b-'a'] = uint8(char)
+			char++
+		}
+	}
+	buf := bytes.NewBuffer([]byte{})
+	for _, msg := range message {
+		if msg == ' ' {
+			buf.WriteByte(byte(msg))
+			continue
+		}
+		buf.WriteByte(bucket[msg-'a'])
+	}
+	return buf.String()
 }
