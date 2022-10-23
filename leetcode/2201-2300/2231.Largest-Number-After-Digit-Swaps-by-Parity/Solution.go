a311e3c60d45b88b9d96289d953523e8f2275624
0xff-dev <stevenshuang521@gmail.com>
2022-10-23 23:09:53 +0800

    Add solution and test-cases for problem 2231

--- leetcode/2201-2300/2231.Largest-Number-After-Digit-Swaps-by-Parity/Solution.go
+++ leetcode/2201-2300/2231.Largest-Number-After-Digit-Swaps-by-Parity/Solution.go
@@ -1,5 +1,43 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "fmt"
+
+func Solution(num int) int {
+	odd := make([]byte, 10)
+	oddPos := make([]int, 0)
+	even := make([]byte, 10)
+	evenPos := make([]int, 0)
+	numString := fmt.Sprintf("%d", num)
+	for idx, b := range numString {
+		if b&1 == 1 {
+			odd[b-'0']++
+			oddPos = append(oddPos, idx)
+			continue
+		}
+		even[b-'0']++
+		evenPos = append(evenPos, idx)
+	}
+
+	ans := make([]byte, len(numString))
+	if len(oddPos) > 0 {
+		start := 0
+		for idx := 9; idx >= 0; idx-- {
+			for times := odd[idx]; times > 0; times-- {
+				ans[oddPos[start]] = byte(idx + '0')
+				start++
+			}
+		}
+	}
+	if len(evenPos) > 0 {
+		start := 0
+		for idx := 9; idx >= 0; idx-- {
+			for times := even[idx]; times > 0; times-- {
+				ans[evenPos[start]] = byte(idx + '0')
+				start++
+			}
+		}
+	}
+	var n int
+	fmt.Sscanf(string(ans), "%d", &n)
+	return n
 }
