143ec5aa284c28d7fff076f994b24c540daeabfc
0xff-dev <stevenshuang521@gmail.com>
2022-11-02 00:31:32 +0800

    Add solution and test-cases for problem 1706

--- leetcode/1701-1800/1706.Where-Will-the-Ball-Fall/Solution.go
+++ leetcode/1701-1800/1706.Where-Will-the-Ball-Fall/Solution.go
@@ -1,5 +1,22 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(grid [][]int) []int {
+	m, n := len(grid), len(grid[0])
+	ans := make([]int, n)
+
+	for col := 0; col < n; col++ {
+		cur := col
+		for row := 0; row < m; row++ {
+			nextCol := cur + grid[row][cur]
+
+			/* /\  \/ */
+			if !(nextCol >= 0 && nextCol < n && grid[row][cur] == grid[row][nextCol]) {
+				ans[col] = -1
+				break
+			}
+			ans[col] = nextCol
+			cur = nextCol
+		}
+	}
+	return ans
 }
