c388038475ebeb10d93936ea324d47eeb29c4c1b
0xff-dev <stevenshuang521@gmail.com>
2022-11-04 23:56:11 +0800

    Add solution and test-cases for problem 345

--- leetcode/301-400/0345.Reverse-Vowels-of-a-String/Solution.go
+++ leetcode/301-400/0345.Reverse-Vowels-of-a-String/Solution.go
@@ -1,5 +1,26 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(ss string) string {
+	bs := []byte(ss)
+	s, e := 0, len(bs)-1
+	for s < e {
+		shouldExchange := true
+		if !isVowels(bs[s]) {
+			s++
+			shouldExchange = false
+		}
+		if !isVowels(bs[e]) {
+			e--
+			shouldExchange = false
+		}
+		if shouldExchange {
+			bs[s], bs[e] = bs[e], bs[s]
+			s, e = s+1, e-1
+		}
+	}
+	return string(bs)
+}
+
+func isVowels(b byte) bool {
+	return b == 'A' || b == 'a' || b == 'e' || b == 'E' || b == 'i' || b == 'I' || b == 'o' || b == 'O' || b == 'u' || b == 'U'
 }
