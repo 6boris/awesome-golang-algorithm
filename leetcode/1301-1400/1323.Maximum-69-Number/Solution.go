b1f264c49007f35b65dcd634aa710b37747ae3a8
0xff-dev <stevenshuang521@gmail.com>
2022-11-07 22:21:05 +0800

    Add solution and test-cases for problem 1323

--- leetcode/1301-1400/1323.Maximum-69-Number/Solution.go
+++ leetcode/1301-1400/1323.Maximum-69-Number/Solution.go
@@ -1,5 +1,25 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(num int) int {
+	bits := [4]int{}
+	idx, six := 0, -1
+	source := num
+
+	for num > 0 {
+		n := num % 10
+		bits[idx] = n
+		if n == 6 {
+			six = idx
+		}
+		idx, num = idx+1, num/10
+	}
+	if six == -1 {
+		return source
+	}
+	base := 0
+	bits[six] = 9
+	for i := idx - 1; i >= 0; i-- {
+		base = base*10 + bits[i]
+	}
+	return base
 }
