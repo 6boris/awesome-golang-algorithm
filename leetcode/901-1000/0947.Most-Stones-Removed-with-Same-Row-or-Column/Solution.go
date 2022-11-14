75f8ddaa59fcbf701015fc642e9de73ec3b3b904
0xff-dev <stevenshuang521@gmail.com>
2022-11-14 23:01:06 +0800

    Add solution and test-cases for problem 947

--- leetcode/901-1000/0947.Most-Stones-Removed-with-Same-Row-or-Column/Solution.go
+++ leetcode/901-1000/0947.Most-Stones-Removed-with-Same-Row-or-Column/Solution.go
@@ -1,5 +1,33 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(stones [][]int) int {
+	visited := make([]bool, len(stones))
+	combindStones := 0
+	var dfs func(int)
+	dfs = func(i int) {
+		visited[i] = true
+		for idx := 0; idx < len(stones); idx++ {
+			if visited[idx] {
+				continue
+			}
+
+			// 横着或者竖着
+			if canRemove(stones[i], stones[idx]) {
+				combindStones++
+				dfs(idx)
+			}
+		}
+	}
+	for i := 0; i < len(stones); i++ {
+		if visited[i] {
+			continue
+		}
+		dfs(i)
+	}
+
+	return combindStones
+}
+
+func canRemove(p1, p2 []int) bool {
+	return p1[0] == p2[0] || p1[1] == p2[1]
 }
--- leetcode/901-1000/0947.Most-Stones-Removed-with-Same-Row-or-Column/Solution_test.go
+++ leetcode/901-1000/0947.Most-Stones-Removed-with-Same-Row-or-Column/Solution_test.go
@@ -10,12 +10,35 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs [][]int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{
+			{0, 0},
+			{0, 1},
+			{1, 0},
+			{1, 2},
+			{2, 1},
+			{2, 2},
+		}, 5},
+		{"TestCase2", [][]int{
+			{0, 0},
+			{0, 2},
+			{1, 1},
+			{2, 0},
+			{2, 2},
+		}, 3},
+		{"TestCase3", [][]int{
+			{0, 0},
+		}, 0},
+		{"TestCase4", [][]int{
+			{3, 2},
+			{3, 1},
+			{4, 4},
+			{1, 1},
+			{0, 2},
+			{4, 0},
+		}, 4},
 	}
 
 	//	开始测试
@@ -30,10 +53,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
