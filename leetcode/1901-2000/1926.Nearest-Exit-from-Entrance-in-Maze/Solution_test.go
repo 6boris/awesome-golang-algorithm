3596e46be9a8b1ebed597bfaf8d2ae6604b41b23
0xff-dev <stevenshuang521@gmail.com>
2022-11-21 22:52:11 +0800

    Add solution and test-cases for problem 1926

--- leetcode/1901-2000/1926.Nearest-Exit-from-Entrance-in-Maze/Solution_test.go
+++ leetcode/1901-2000/1926.Nearest-Exit-from-Entrance-in-Maze/Solution_test.go
@@ -9,31 +9,42 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name    string
+		inputs  [][]byte
+		entrace []int
+		expect  int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]byte{
+			{'+', '+', '.', '+'},
+			{'.', '.', '.', '+'},
+			{'+', '+', '+', '.'},
+		}, []int{1, 2}, 1},
+		{"TestCase2", [][]byte{
+			{'+', '+', '+'},
+			{'.', '.', '.'},
+			{'+', '+', '+'},
+		}, []int{1, 0}, 2},
+		{"TestCase3", [][]byte{
+			{'.', '+'},
+		}, []int{0, 0}, -1},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.entrace)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.inputs, c.entrace)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
