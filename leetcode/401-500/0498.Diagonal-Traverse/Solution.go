9ea6cf9cb89dbc8fd14d493f6a9d987f51f95e5b
0xff-dev <stevenshuang521@gmail.com>
2022-11-24 21:54:00 +0800

    Add solution and test-cases for problem 498

--- leetcode/401-500/0498.Diagonal-Traverse/Solution.go
+++ leetcode/401-500/0498.Diagonal-Traverse/Solution.go
@@ -1,5 +1,37 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(mat [][]int) []int {
+	x, y := 0, 0
+	m, n := len(mat), len(mat[0])
+	idx := 0
+	xDir, yDir := -1, 1
+	ans := make([]int, m*n)
+	for ; idx < m*n; idx++ {
+		ans[idx] = mat[x][y]
+		switch {
+		case x == 0 && yDir == 1:
+			if y < n-1 {
+				y++
+			} else {
+				x++
+			}
+			xDir, yDir = 1, -1
+		case y == 0 && xDir == 1:
+			if x < m-1 {
+				x++
+			} else {
+				y++
+			}
+			xDir, yDir = -1, 1
+		case x == m-1 && yDir == -1:
+			y++
+			xDir, yDir = -1, 1
+		case y == n-1 && xDir == -1:
+			x++
+			xDir, yDir = 1, -1
+		default:
+			x, y = x+xDir, y+yDir
+		}
+	}
+	return ans
 }
--- leetcode/401-500/0498.Diagonal-Traverse/Solution_test.go
+++ leetcode/401-500/0498.Diagonal-Traverse/Solution_test.go
@@ -10,12 +10,38 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs [][]int
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8, 9},
+		}, []int{1, 2, 4, 7, 5, 3, 6, 8, 9}},
+		{"TestCase2", [][]int{
+			{1, 2},
+			{3, 4},
+		}, []int{1, 2, 3, 4}},
+		{"TestCase3", [][]int{{1}}, []int{1}},
+		{"TestCase4", [][]int{
+			{1, 2, 3, 4},
+			{5, 6, 7, 8},
+		}, []int{1, 2, 5, 6, 3, 4, 7, 8}},
+		{"TestCase5", [][]int{
+			{1, 2},
+			{3, 4},
+			{5, 6},
+			{7, 8},
+		}, []int{1, 2, 3, 5, 4, 6, 7, 8}},
+		{"TestCase6", [][]int{
+			{1},
+			{2},
+			{3},
+			{4},
+		}, []int{1, 2, 3, 4}},
+		{"TestCase7", [][]int{
+			{1, 2, 3, 4},
+		}, []int{1, 2, 3, 4}},
 	}
 
 	//	开始测试
@@ -30,10 +56,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
