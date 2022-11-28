efa901dc74e6696b22bf64563d732ad7d34e0534
0xff-dev <stevenshuang521@gmail.com>
2022-11-28 21:37:05 +0800

    Add solution and test-cases for problem 2225

--- leetcode/2201-2300/2225.Find-Players-With-Zero-or-One-Losses/Solution.go
+++ leetcode/2201-2300/2225.Find-Players-With-Zero-or-One-Losses/Solution.go
@@ -1,5 +1,22 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(matches [][]int) [][]int {
+	ans := make([][]int, 2)
+	ans[0] = make([]int, 0)
+	ans[1] = make([]int, 0)
+	winners, losers := make([]int, 100001), make([]int, 100001)
+	for _, match := range matches {
+		winners[match[0]]++
+		losers[match[1]]++
+	}
+
+	for win := 0; win < 100001; win++ {
+		if losers[win] == 1 {
+			ans[1] = append(ans[1], win)
+		}
+		if winners[win] > 0 && losers[win] == 0 {
+			ans[0] = append(ans[0], win)
+		}
+	}
+	return ans
 }
