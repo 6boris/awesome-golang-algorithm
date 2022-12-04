0acdf8a77a577c20126df99622dee5011dad7802
0xff-dev <stevenshuang521@gmail.com>
2022-12-04 12:01:16 +0800

    Add solution and test-cases for problem 2256

--- leetcode/2201-2300/2256.Minimum-Average-Difference/Solution.go
+++ leetcode/2201-2300/2256.Minimum-Average-Difference/Solution.go
@@ -1,5 +1,30 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) int {
+	minIndex := -1
+	minValue := 0
+	length := len(nums)
+	if length == 1 {
+		return 0
+	}
+	for idx := 1; idx < length; idx++ {
+		nums[idx] += nums[idx-1]
+	}
+
+	for idx := 0; idx < length; idx++ {
+		left := nums[idx] / (idx + 1)
+		right := 0
+		if idx != length-1 {
+			right = (nums[length-1] - nums[idx]) / (length - idx - 1)
+		}
+		diff := left - right
+		if diff < 0 {
+			diff = ^diff + 1
+		}
+		if minIndex == -1 || minValue > diff {
+			minIndex = idx
+			minValue = diff
+		}
+	}
+	return minIndex
 }
