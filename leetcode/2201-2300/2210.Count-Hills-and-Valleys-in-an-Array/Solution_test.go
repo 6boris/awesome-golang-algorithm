00d80772ee1dde52426ad46e7d16e7e073eef245
0xff-dev <stevenshuang521@gmail.com>
2022-12-06 23:47:09 +0800

    Add solution and test-cases for problem 2210

--- leetcode/2201-2300/2210.Count-Hills-and-Valleys-in-an-Array/Solution_test.go
+++ leetcode/2201-2300/2210.Count-Hills-and-Valleys-in-an-Array/Solution_test.go
@@ -10,12 +10,11 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{2, 4, 1, 1, 6, 5}, 3},
+		{"TestCase2", []int{6, 6, 5, 5, 4, 1}, 0},
 	}
 
 	//	开始测试
@@ -30,10 +29,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
