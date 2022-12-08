affd191e92c3aee4539451c5d690a8bffb902915
0xff-dev <stevenshuang521@gmail.com>
2022-12-08 22:37:52 +0800

    Add solution and test-cases for problem 872

--- leetcode/801-900/0872.Leaf-Similar-Trees/README.md
+++ leetcode/801-900/0872.Leaf-Similar-Trees/README.md
@@ -1,26 +1,30 @@
 # [872.Leaf-Similar Trees][title]
 
-> [!WARNING|style:flat]
-> This question is temporarily unanswered if you have good ideas. Welcome to [Create Pull Request PR](https://github.com/kylesliu/awesome-golang-algorithm)
-
 ## Description
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a **leaf value sequence**.  
+![tree1](./tree.png)
 
-**Example 1:**
+For example, in the given tree above, the leaf value sequence is `(6, 7, 4, 9, 8)`.
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return `true` if and only if the two given trees with head nodes `root1` and `root2` are leaf-similar.
+
+
+**Example 1:**  
+![tree2](./leaf-similar-1.jpg)
 
 ```
-Input: a = "11", b = "1"
-Output: "100"
+Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
 ```
 
-## 题意
-> ...
+**Example 2:**  
+![tree3](./leaf-similar-2.jpg)
 
-## 题解
-
-### 思路1
-> ...
-Leaf-Similar Trees
-```go
+```
+Input: root1 = [1,2,3], root2 = [1,3,2]
+Output: false
 ```
 
 
--- leetcode/801-900/0872.Leaf-Similar-Trees/Solution_test.go
+++ leetcode/801-900/0872.Leaf-Similar-Trees/Solution_test.go
@@ -10,30 +10,69 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
+		t1, t2 *TreeNode
 		expect bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val:  5,
+				Left: &TreeNode{Val: 6},
+				Right: &TreeNode{
+					Val:   2,
+					Left:  &TreeNode{Val: 7},
+					Right: &TreeNode{Val: 4},
+				},
+			},
+			Right: &TreeNode{
+				Val:   1,
+				Left:  &TreeNode{Val: 9},
+				Right: &TreeNode{Val: 8},
+			},
+		}, &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val:   5,
+				Left:  &TreeNode{Val: 6},
+				Right: &TreeNode{Val: 7},
+			},
+			Right: &TreeNode{
+				Val:  1,
+				Left: &TreeNode{Val: 4},
+				Right: &TreeNode{
+					Val:   2,
+					Left:  &TreeNode{Val: 9},
+					Right: &TreeNode{Val: 8},
+				},
+			},
+		}, true},
+		{"TestCase2", &TreeNode{
+			Val:   1,
+			Left:  &TreeNode{Val: 2},
+			Right: &TreeNode{Val: 3},
+		}, &TreeNode{
+			Val:   1,
+			Left:  &TreeNode{Val: 3},
+			Right: &TreeNode{Val: 2},
+		}, false},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.t1, c.t2)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.t1, c.t2)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
