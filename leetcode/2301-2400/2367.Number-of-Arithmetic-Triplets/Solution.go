a0c4798a5a858133bcf8dfdcbe1da64d5f8bb016
0xff-dev <stevenshuang521@gmail.com>
2022-12-11 21:13:04 +0800

    Add solution and test-cases for problem 2367

--- leetcode/2301-2400/2367.Number-of-Arithmetic-Triplets/Solution.go
+++ leetcode/2301-2400/2367.Number-of-Arithmetic-Triplets/Solution.go
@@ -1,5 +1,23 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, diff int) int {
+	count := 0
+
+	bucket := make([]int, 201)
+	for i, n := range nums {
+		bucket[n] = i + 1
+	}
+
+	for idx := 0; idx <= len(nums)-3; idx++ {
+		next := nums[idx] + diff
+		if !(next <= 200 && bucket[next] > 0) {
+			continue
+		}
+		nextNext := next + diff
+		if !(nextNext <= 200 && bucket[nextNext] > 0) {
+			continue
+		}
+		count++
+	}
+	return count
 }
