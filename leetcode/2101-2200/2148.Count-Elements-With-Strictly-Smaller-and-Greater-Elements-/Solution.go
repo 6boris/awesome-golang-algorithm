661af7b9a999f41809133fb68ab24692783bd557
0xff-dev <stevenshuang521@gmail.com>
2022-12-13 00:05:48 +0800

    Add solution and test-cases for problem 2148

--- leetcode/2101-2200/2148.Count-Elements-With-Strictly-Smaller-and-Greater-Elements-/Solution.go
+++ leetcode/2101-2200/2148.Count-Elements-With-Strictly-Smaller-and-Greater-Elements-/Solution.go
@@ -1,5 +1,19 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) int {
+	max, min := nums[0], nums[0]
+	check := make(map[int]int)
+	for _, n := range nums {
+		if n > max {
+			max = n
+		}
+		if n < min {
+			min = n
+		}
+		check[n]++
+	}
+	if len(check) > 2 {
+		return len(nums) - check[max] - check[min]
+	}
+	return 0
 }
