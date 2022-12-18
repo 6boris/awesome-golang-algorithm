59732861c36afb1de90d28b19c638382d09ad9d8
0xff-dev <stevenshuang521@gmail.com>
2022-12-18 11:29:18 +0800

    Add solution and test-cases for problem 739

--- leetcode/701-800/0739.Daily-Temperatures/Solution_test.go
+++ leetcode/701-800/0739.Daily-Temperatures/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{73, 74, 75, 71, 69, 72, 76, 73}, []int{1, 1, 4, 2, 1, 1, 0, 0}},
+		{"TestCase2", []int{30, 40, 50, 60}, []int{1, 1, 1, 0}},
+		{"TestCase3", []int{30, 60, 90}, []int{1, 1, 0}},
 	}
 
 	//	开始测试
@@ -30,10 +30,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
