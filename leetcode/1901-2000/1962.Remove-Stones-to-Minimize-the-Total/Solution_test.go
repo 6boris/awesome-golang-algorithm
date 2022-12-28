e90cd0fc4999687907b12875dcc7dc7fefc6de6d
0xff-dev <stevenshuang521@gmail.com>
2022-12-28 20:52:51 +0800

    Add solution and test-cases for problem 1962

--- leetcode/1901-2000/1962.Remove-Stones-to-Minimize-the-Total/Solution_test.go
+++ leetcode/1901-2000/1962.Remove-Stones-to-Minimize-the-Total/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		k      int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{5, 4, 9}, 2, 12},
+		{"TestCase2", []int{4, 3, 6, 7}, 3, 12},
+		{"TestCase3", []int{10, 88, 74, 63, 28, 99, 131, 74, 97, 201, 383, 274}, 6, 865},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.inputs, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
