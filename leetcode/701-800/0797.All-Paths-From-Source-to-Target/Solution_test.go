e63f0f1358a557c17b9c845c809bddd5cd49f815
0xff-dev <stevenshuang521@gmail.com>
2022-12-30 20:43:16 +0800

    Add solution and test-cases for problem 797

--- leetcode/701-800/0797.All-Paths-From-Source-to-Target/Solution_test.go
+++ leetcode/701-800/0797.All-Paths-From-Source-to-Target/Solution_test.go
@@ -10,12 +10,19 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs [][]int
+		expect [][]int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{
+			{1, 2}, {3}, {3}, {},
+		}, [][]int{
+			{0, 1, 3}, {0, 2, 3},
+		}},
+		{"TestCase2", [][]int{
+			{4, 3, 1}, {3, 2, 4}, {3}, {4}, {},
+		}, [][]int{
+			{0, 4}, {0, 3, 4}, {0, 1, 3, 4}, {0, 1, 2, 3, 4}, {0, 1, 4},
+		}},
 	}
 
 	//	开始测试
@@ -30,10 +37,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
