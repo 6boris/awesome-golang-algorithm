b77a27b19de74340ef15beea112ab8753cb66f3b
0xff-dev <stevenshuang521@gmail.com>
2023-01-01 16:47:06 +0800

    Add solution and test-cases for problem 290

--- leetcode/201-300/0290.Word-Pattern/Solution.go
+++ leetcode/201-300/0290.Word-Pattern/Solution.go
@@ -1,5 +1,33 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "strings"
+
+func Solution(pattern string, s string) bool {
+	words := strings.Split(s, " ")
+	if len(words) != len(pattern) {
+		return false
+	}
+	b2w := make(map[byte]string)
+	w2b := make(map[string]byte)
+	b, w := pattern[0], words[0]
+	b2w[b] = w
+	w2b[w] = b
+
+	for i := 1; i < len(pattern); i++ {
+		if pattern[i] == b {
+			if words[i] != w {
+				return false
+			}
+			continue
+		}
+		b, w = pattern[i], words[i]
+		v, ok := b2w[pattern[i]]
+		v1, ok1 := w2b[words[i]]
+		if (ok && words[i] != v) || (ok1 && pattern[i] != v1) {
+			return false
+		}
+		b2w[b] = w
+		w2b[w] = b
+	}
+	return true
 }
