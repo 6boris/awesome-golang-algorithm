aeefa5e61b02abbf09cf3ff42d000c508c0feece
0xff-dev <stevenshuang521@gmail.com>
2023-01-03 22:03:20 +0800

    Add solution and test-cases for problem 944

--- leetcode/901-1000/0944.Delete-Columns-to-Make-Sorted/Solution.go
+++ leetcode/901-1000/0944.Delete-Columns-to-Make-Sorted/Solution.go
@@ -1,5 +1,18 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(strs []string) int {
+	ans := 0
+	rows := len(strs)
+	cols := len(strs[0])
+	for col := 0; col < cols; col++ {
+		c := strs[0][col]
+		for row := 1; row < rows; row++ {
+			if strs[row][col] < c {
+				ans++
+				break
+			}
+			c = strs[row][col]
+		}
+	}
+	return ans
 }
