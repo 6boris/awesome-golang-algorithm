c4f82b036453305ca96c752ceb6c77fd2fc6ed2a
0xff-dev <stevenshuang521@gmail.com>
2023-01-06 20:32:45 +0800

    Add solution and test-cases for problem  1833

--- leetcode/1801-1900/1833.Maximum-Ice-Cream-Bars/Solution.go
+++ leetcode/1801-1900/1833.Maximum-Ice-Cream-Bars/Solution.go
@@ -1,5 +1,18 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+// heap, sort
+func Solution(costs []int, coins int) int {
+	ans := 0
+	sum := 0
+	sort.Ints(costs)
+	for i := 0; i < len(costs); i++ {
+		sum += costs[i]
+		if sum > coins {
+			break
+		}
+		ans++
+	}
+	return ans
 }
