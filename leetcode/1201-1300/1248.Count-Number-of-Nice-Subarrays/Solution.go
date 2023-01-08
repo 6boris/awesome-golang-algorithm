cecb86041192d160a6472645cabefd892a222b3b
0xff-dev <stevenshuang521@gmail.com>
2023-01-08 11:33:55 +0800

    Add solution and test-cases for problem 1248

--- leetcode/1201-1300/1248.Count-Number-of-Nice-Subarrays/Solution.go
+++ leetcode/1201-1300/1248.Count-Number-of-Nice-Subarrays/Solution.go
@@ -1,5 +1,37 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type tmp1248 struct {
+	left, right int
+}
+
+func Solution(nums []int, k int) int {
+	ans := 0
+	l := len(nums)
+	indies := make([]tmp1248, 0)
+	even := 0
+
+	for i := 0; i < l; i++ {
+		if nums[i]&1 == 0 {
+			even++
+			continue
+		}
+		// left can start with itself so, left = even+1
+		indies = append(indies, tmp1248{left: even + 1})
+		li := len(indies)
+		if li > 1 {
+			indies[li-2].right = even
+		}
+		even = 0
+	}
+	if len(indies) == 0 || k > len(indies) {
+		return 0
+	}
+	li := len(indies)
+	indies[li-1].right = even
+
+	start := 0
+	for ; start <= len(indies)-k; start++ {
+		ans += indies[start].left * (indies[start+k-1].right + 1)
+	}
+	return ans
 }
