393fc0c85500539ce36eff8733f6684631f62cb1
0xff-dev <stevenshuang521@gmail.com>
2023-01-12 22:28:28 +0800

    Add solution and test-cases for problem 1519

--- leetcode/1501-1600/1519.Number-of-Nodes-in-the-Sub-Tree-With-the-Same-Label/Solution.go
+++ leetcode/1501-1600/1519.Number-of-Nodes-in-the-Sub-Tree-With-the-Same-Label/Solution.go
@@ -1,5 +1,37 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int, edges [][]int, labels string) []int {
+	ans := make([]int, n)
+	graph := make(map[int][]int)
+	for _, e := range edges {
+		if _, ok := graph[e[0]]; !ok {
+			graph[e[0]] = make([]int, 0)
+		}
+		if _, ok := graph[e[1]]; !ok {
+			graph[e[1]] = make([]int, 0)
+		}
+		graph[e[0]] = append(graph[e[0]], e[1])
+		graph[e[1]] = append(graph[e[1]], e[0])
+	}
+	var dfs func(int, int) []int
+	dfs = func(node, parent int) []int {
+		rels := graph[node]
+		total := make([]int, 26)
+		total[labels[node]-'a']++
+		for _, next := range rels {
+			if next == parent {
+				continue
+			}
+			next := dfs(next, node)
+			for i, n := range next {
+				if n != 0 {
+					total[i] += n
+				}
+			}
+		}
+		ans[node] = total[labels[node]-'a']
+		return total
+	}
+	dfs(0, -1)
+	return ans
 }
--- leetcode/1501-1600/1519.Number-of-Nodes-in-the-Sub-Tree-With-the-Same-Label/Solution_test.go
+++ leetcode/1501-1600/1519.Number-of-Nodes-in-the-Sub-Tree-With-the-Same-Label/Solution_test.go
@@ -10,30 +10,38 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		n      int
+		edges  [][]int
+		labels string
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 7, [][]int{
+			{0, 1}, {0, 2}, {1, 4}, {1, 5}, {2, 3}, {2, 6},
+		}, "abaedcd", []int{2, 1, 1, 1, 1, 1, 1}},
+		{"TestCase2", 4, [][]int{
+			{0, 1}, {1, 2}, {0, 3},
+		}, "bbbb", []int{4, 2, 1, 1}},
+		{"TestCase3", 5, [][]int{
+			{0, 1}, {0, 2}, {1, 3}, {0, 4},
+		}, "aabab", []int{3, 2, 1, 1, 1}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.edges, c.labels)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.n, c.edges, c.labels)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
