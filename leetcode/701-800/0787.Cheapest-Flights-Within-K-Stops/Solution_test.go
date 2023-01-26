c1eb648b29ebc7cdd9615e02ffa7b604cf570b48
0xff-dev <stevenshuang521@gmail.com>
2023-01-26 21:36:59 +0800

    Add solution and test-cases for problem 787

--- leetcode/701-800/0787.Cheapest-Flights-Within-K-Stops/Solution_test.go
+++ leetcode/701-800/0787.Cheapest-Flights-Within-K-Stops/Solution_test.go
@@ -9,31 +9,33 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name        string
+		n           int
+		flights     [][]int
+		src, dst, k int
+		expect      int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 4, [][]int{{0, 1, 100}, {1, 2, 100}, {2, 0, 100}, {1, 3, 600}, {2, 3, 200}}, 0, 3, 1, 700},
+		{"TestCase2", 3, [][]int{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 1, 200},
+		{"TestCase3", 3, [][]int{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 0, 500},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.flights, c.src, c.dst, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v %v %v",
+					c.expect, got, c.n, c.flights, c.src, c.dst, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
