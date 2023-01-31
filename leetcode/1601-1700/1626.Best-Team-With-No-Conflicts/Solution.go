40f0c4076fa6ad572407499168b7edf8eb0f3dcc
0xff-dev <stevenshuang521@gmail.com>
2023-01-31 19:12:10 +0800

    Add solution and test-cases for problem 1626

--- leetcode/1601-1700/1626.Best-Team-With-No-Conflicts/Solution.go
+++ leetcode/1601-1700/1626.Best-Team-With-No-Conflicts/Solution.go
@@ -1,5 +1,38 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func sort1626(scores, ages []int) {
+	l := len(scores)
+	for i := 0; i < l-1; i++ {
+		for j := 0; j < l-1-i; j++ {
+			if (ages[j] > ages[j+1]) || (ages[j] == ages[j+1] && scores[j] > scores[j+1]) {
+				ages[j], ages[j+1] = ages[j+1], ages[j]
+				scores[j], scores[j+1] = scores[j+1], scores[j]
+			}
+		}
+	}
+}
+func Solution(scores []int, ages []int) int {
+	sort1626(scores, ages)
+	l := len(scores)
+	dp := make([]int, l)
+	ans := 0
+	for i := 0; i < l; i++ {
+		dp[i] = scores[i]
+		if dp[i] > ans {
+			ans = dp[i]
+		}
+	}
+	for i := 1; i < l; i++ {
+		for pre := i - 1; pre >= 0; pre-- {
+			if scores[i] >= scores[pre] {
+				if r := dp[pre] + scores[i]; r > dp[i] {
+					dp[i] = r
+				}
+			}
+		}
+		if dp[i] > ans {
+			ans = dp[i]
+		}
+	}
+	return ans
 }
--- leetcode/1601-1700/1626.Best-Team-With-No-Conflicts/Solution_test.go
+++ leetcode/1601-1700/1626.Best-Team-With-No-Conflicts/Solution_test.go
@@ -9,31 +9,31 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name         string
+		scores, ages []int
+		expect       int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 3, 5, 10, 15}, []int{1, 2, 3, 4, 5}, 34},
+		{"TestCase2", []int{4, 5, 6, 5}, []int{2, 1, 2, 1}, 16},
+		{"TestCase3", []int{1, 2, 3, 5}, []int{8, 9, 10, 1}, 6},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.scores, c.ages)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.scores, c.ages)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
