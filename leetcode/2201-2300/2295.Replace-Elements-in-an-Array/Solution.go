22c9b1c7c87089e5c6332cc0e432a32b36638616
0xff-dev <stevenshuang521@gmail.com>
2023-02-04 23:45:43 +0800

    Add solution and test-cases for problem  2295

--- leetcode/2201-2300/2295.Replace-Elements-in-an-Array/Solution.go
+++ leetcode/2201-2300/2295.Replace-Elements-in-an-Array/Solution.go
@@ -1,5 +1,14 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, operations [][]int) []int {
+	indies := make(map[int]int)
+	for i, n := range nums {
+		indies[n] = i
+	}
+	for _, op := range operations {
+		i := indies[op[0]]
+		nums[i] = op[1]
+		indies[nums[i]] = i
+	}
+	return nums
 }
