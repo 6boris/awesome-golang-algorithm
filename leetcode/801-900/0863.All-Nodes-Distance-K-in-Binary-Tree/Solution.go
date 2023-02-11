baf2275e78dc2ab67cda8b608dbfda92a82153a2
0xff-dev <stevenshuang521@gmail.com>
2023-02-11 21:59:54 +0800

    Add solution and test-cases for problem 863

--- leetcode/801-900/0863.All-Nodes-Distance-K-in-Binary-Tree/Solution.go
+++ leetcode/801-900/0863.All-Nodes-Distance-K-in-Binary-Tree/Solution.go
@@ -1,5 +1,60 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func findFather(root *TreeNode, father map[int]*TreeNode) {
+	if root == nil {
+		return
+	}
+	if root.Left != nil {
+		father[root.Left.Val] = root
+		findFather(root.Left, father)
+	}
+	if root.Right != nil {
+		father[root.Right.Val] = root
+		findFather(root.Right, father)
+	}
+}
+
+func Solution(root *TreeNode, target *TreeNode, k int) []int {
+	ans := make([]int, 0)
+	father := make(map[int]*TreeNode)
+	findFather(root, father)
+	visited := make(map[int]struct{})
+	visited[target.Val] = struct{}{}
+	queue := []*TreeNode{target}
+	for len(queue) > 0 && k > 0 {
+		nextQ := make([]*TreeNode, 0)
+		for _, item := range queue {
+			if item.Left != nil {
+				if _, ok := visited[item.Left.Val]; !ok {
+					visited[item.Left.Val] = struct{}{}
+					nextQ = append(nextQ, item.Left)
+				}
+			}
+			if item.Right != nil {
+				if _, ok := visited[item.Right.Val]; !ok {
+					visited[item.Right.Val] = struct{}{}
+					nextQ = append(nextQ, item.Right)
+				}
+			}
+
+			// father
+			if fa, ok := father[item.Val]; ok {
+				if _, ok1 := visited[fa.Val]; !ok1 {
+					visited[fa.Val] = struct{}{}
+					nextQ = append(nextQ, fa)
+				}
+			}
+		}
+		queue = nextQ
+		k--
+	}
+	for _, item := range queue {
+		ans = append(ans, item.Val)
+	}
+	return ans
 }
