f9c488ca88364fc5360ea320b65ba4e2056357c6
0xff-dev <stevenshuang521@gmail.com>
2023-02-16 21:34:17 +0800

    Add solution and test-cases for problem 513

--- leetcode/501-600/0513.Find-Bottom-Left-Tree-Value/Solution.go
+++ leetcode/501-600/0513.Find-Bottom-Left-Tree-Value/Solution.go
@@ -1,5 +1,29 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func Solution(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	queue := []*TreeNode{root}
+	var ans *TreeNode
+	for len(queue) > 0 {
+		nextQ := make([]*TreeNode, 0)
+		ans = queue[0]
+		for _, n := range queue {
+			if n.Left != nil {
+				nextQ = append(nextQ, n.Left)
+			}
+			if n.Right != nil {
+				nextQ = append(nextQ, n.Right)
+			}
+		}
+		queue = nextQ
+	}
+	return ans.Val
 }
--- leetcode/501-600/0513.Find-Bottom-Left-Tree-Value/Solution_test.go
+++ leetcode/501-600/0513.Find-Bottom-Left-Tree-Value/Solution_test.go
@@ -10,12 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs *TreeNode
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", &TreeNode{
+			Val:   2,
+			Left:  &TreeNode{Val: 1},
+			Right: &TreeNode{Val: 3},
+		}, 1},
+		{"TestCase2", &TreeNode{
+			Val: 1,
+			Left: &TreeNode{
+				Val:  2,
+				Left: &TreeNode{Val: 4},
+			},
+			Right: &TreeNode{
+				Val: 3,
+				Left: &TreeNode{
+					Val: 5,
+					Left: &TreeNode{
+						Val: 7,
+					},
+				},
+				Right: &TreeNode{Val: 6},
+			},
+		}, 7},
 	}
 
 	//	开始测试
@@ -30,10 +49,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
