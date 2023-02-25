5420eb3062ed4fcf1b3ef0dd6f8b6bc8b22cd130
0xff-dev <stevenshuang521@gmail.com>
2023-02-25 22:23:44 +0800

    Add solution and test-cases for problem 1247

--- leetcode/1201-1300/1247.Minimum-Swaps-to-Make-Strings-Equal/Solution.go
+++ leetcode/1201-1300/1247.Minimum-Swaps-to-Make-Strings-Equal/Solution.go
@@ -1,5 +1,29 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s1 string, s2 string) int {
+	// 寻找diff的情况
+	if len(s1) != len(s2) {
+		return -1
+	}
+	zero, one := 0, 0
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[i] {
+			if s1[i] == 'x' {
+				// xy
+				zero++
+			} else {
+				// yx
+				one++
+			}
+		}
+	}
+	if (one+zero)&1 == 1 {
+		return -1
+	}
+	ans := one / 2
+	ans += zero / 2
+	if one&1 == 1 {
+		ans += 2
+	}
+	return ans
 }
