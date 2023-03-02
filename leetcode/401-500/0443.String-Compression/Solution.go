fe065d8934a3d19a563138c831a0c275b3795d95
0xff-dev <stevenshuang521@gmail.com>
2023-03-02 23:58:26 +0800

    Add solution and test-cases for problem 443

--- leetcode/401-500/0443.String-Compression/Solution.go
+++ leetcode/401-500/0443.String-Compression/Solution.go
@@ -1,5 +1,32 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "fmt"
+
+func Solution(chars []byte) int {
+	length := len(chars)
+	if length == 0 {
+		return length
+	}
+	nowByte, sameCount := chars[0], 1
+	targetIndex := 0
+	for idx := 1; idx <= length; idx++ {
+		if idx < length && chars[idx] == nowByte {
+			sameCount++
+			continue
+		}
+		chars[targetIndex] = nowByte
+		targetIndex++
+		if idx < length {
+			nowByte = chars[idx]
+		}
+		if sameCount == 1 {
+			continue
+		}
+		for _, b := range fmt.Sprintf("%d", sameCount) {
+			chars[targetIndex] = byte(b)
+			targetIndex++
+		}
+		sameCount = 1
+	}
+	return targetIndex
 }
