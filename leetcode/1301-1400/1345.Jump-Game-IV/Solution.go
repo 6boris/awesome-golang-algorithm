2665bc004f9e92c66e3555a8803f57faad6e4d91
0xff-dev <stevenshuang521@gmail.com>
2023-03-05 12:07:38 +0800

    Add solution and test-cases for problem 1345

--- leetcode/1301-1400/1345.Jump-Game-IV/Solution.go
+++ leetcode/1301-1400/1345.Jump-Game-IV/Solution.go
@@ -1,5 +1,49 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(arr []int) int {
+	length := len(arr)
+	if length <= 1 {
+		return 0
+	}
+	// bfs
+	step := 0
+	indices := make(map[int][]int)
+	visited := make([]bool, length)
+	for i := 0; i < length; i++ {
+		if _, ok := indices[arr[i]]; !ok {
+			indices[arr[i]] = make([]int, 0)
+		}
+		indices[arr[i]] = append(indices[arr[i]], i)
+	}
+	visited[0] = true
+	queue := []int{0}
+
+	for len(queue) > 0 {
+		nextQ := make([]int, 0)
+		for _, index := range queue {
+			if index-1 >= 0 && !visited[index-1] {
+				visited[index-1] = true
+				nextQ = append(nextQ, index-1)
+			}
+			if index+1 < length && !visited[index+1] {
+				visited[index+1] = true
+				nextQ = append(nextQ, index+1)
+			}
+			for _, rel := range indices[arr[index]] {
+				if !visited[rel] {
+					visited[rel] = true
+					nextQ = append(nextQ, rel)
+				}
+			}
+			// 注意这里的超时点
+			delete(indices, arr[index])
+		}
+		step++
+		if visited[length-1] {
+			return step
+		}
+		queue = nextQ
+
+	}
+	return 0
 }
--- leetcode/1301-1400/1345.Jump-Game-IV/Solution_test.go
+++ leetcode/1301-1400/1345.Jump-Game-IV/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{100, -23, -23, 404, 100, 23, 23, 23, 3, 404}, 3},
+		{"TestCase2", []int{7}, 0},
+		{"TestCase3", []int{7, 6, 9, 6, 9, 6, 9, 7}, 1},
 	}
 
 	//	开始测试
@@ -30,10 +30,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
