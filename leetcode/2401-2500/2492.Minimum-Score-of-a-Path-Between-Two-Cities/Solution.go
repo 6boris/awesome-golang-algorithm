8e7e7df82f942be31e75fca48514315e59b18d13
0xff-dev <stevenshuang521@gmail.com>
2023-03-22 22:22:47 +0800

    Add solution and test-cases for problem 2492

--- leetcode/2401-2500/2492.Minimum-Score-of-a-Path-Between-Two-Cities/Solution.go
+++ leetcode/2401-2500/2492.Minimum-Score-of-a-Path-Between-Two-Cities/Solution.go
@@ -1,5 +1,41 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "fmt"
+
+func Solution(n int, roads [][]int) int {
+	score := -1
+	edges := make(map[int]map[int]int)
+	for _, r := range roads {
+		from, to, distance := r[0], r[1], r[2]
+		if _, ok := edges[from]; !ok {
+			edges[from] = make(map[int]int)
+		}
+		if _, ok := edges[to]; !ok {
+			edges[to] = make(map[int]int)
+		}
+		edges[from][to] = distance
+		edges[to][from] = distance
+	}
+	var dfs func(int)
+	visited := make(map[string]struct{})
+	dfs = func(city int) {
+
+		for to, distnace := range edges[city] {
+			key := fmt.Sprintf("%d-%d", city, to)
+			if _, ok := visited[key]; ok {
+				continue
+			}
+			revKey := fmt.Sprintf("%d-%d", to, city)
+			visited[key] = struct{}{}
+			visited[revKey] = struct{}{}
+
+			if score == -1 || score > distnace {
+				score = distnace
+			}
+			dfs(to)
+		}
+	}
+
+	dfs(1)
+	return score
 }
