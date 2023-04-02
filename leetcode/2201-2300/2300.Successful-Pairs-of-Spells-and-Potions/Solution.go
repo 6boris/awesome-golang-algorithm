2768b1f71a5eac4e9e7998ab8031f94caf4983bc
0xff-dev <stevenshuang521@gmail.com>
2023-04-02 13:12:29 +0800

    Add solution and test-cases for problem 2300

--- leetcode/2201-2300/2300.Successful-Pairs-of-Spells-and-Potions/Solution.go
+++ leetcode/2201-2300/2300.Successful-Pairs-of-Spells-and-Potions/Solution.go
@@ -1,5 +1,18 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(spells []int, potions []int, success int64) []int {
+	ans := make([]int, len(spells))
+	sort.Ints(potions)
+	for i, n := range spells {
+		idx := 0
+		for ; idx < len(potions); idx++ {
+			if int64(potions[idx])*int64(n) >= success {
+				break
+			}
+		}
+		ans[i] = len(potions) - idx
+	}
+	return ans
 }
--- leetcode/2201-2300/2300.Successful-Pairs-of-Spells-and-Potions/Solution_test.go
+++ leetcode/2201-2300/2300.Successful-Pairs-of-Spells-and-Potions/Solution_test.go
@@ -9,31 +9,31 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name    string
+		s, p    []int
+		success int64
+		expect  []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{5, 1, 3}, []int{1, 2, 3, 4, 5}, 7, []int{4, 0, 3}},
+		{"TestCase2", []int{3, 1, 2}, []int{8, 5, 8}, 16, []int{2, 0, 2}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.s, c.p, c.success)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.s, c.p, c.success)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
