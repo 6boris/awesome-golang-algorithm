3db72cd23e668e4e88a5fa2431763141a97bf34d
0xff-dev <stevenshuang521@gmail.com>
2023-04-06 21:43:30 +0800

    Add solution and test-cases for problem 1254

--- leetcode/1201-1300/1254.Number-of-Closed-Islands/Solution.go
+++ leetcode/1201-1300/1254.Number-of-Closed-Islands/Solution.go
@@ -1,5 +1,51 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(grid [][]int) int {
+	ans := 0
+	rows, cols := len(grid), len(grid[0])
+	var (
+		find func(int, int)
+		dir  = [][]int{
+			{1, 0}, {0, 1}, {-1, 0}, {0, -1},
+		}
+	)
+	find = func(x, y int) {
+		if x < 0 || x >= rows || y < 0 || y >= cols {
+			return
+		}
+		if grid[x][y] == 1 {
+			return
+		}
+		grid[x][y] = 1
+		for _, d := range dir {
+			nx, ny := d[0]+x, d[1]+y
+			find(nx, ny)
+		}
+	}
+
+	for y := 0; y < cols; y++ {
+		if grid[0][y] == 0 {
+			find(0, y)
+		}
+		if grid[rows-1][y] == 0 {
+			find(rows-1, y)
+		}
+	}
+	for x := 1; x < rows-1; x++ {
+		if grid[x][0] == 0 {
+			find(x, 0)
+		}
+		if grid[x][cols-1] == 0 {
+			find(x, cols-1)
+		}
+	}
+	for x := 1; x < rows-1; x++ {
+		for y := 1; y < cols-1; y++ {
+			if grid[x][y] == 0 {
+				ans++
+				find(x, y)
+			}
+		}
+	}
+	return ans
 }
