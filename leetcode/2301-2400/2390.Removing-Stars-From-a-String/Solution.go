7b05d7169c90a0c7ccb8290de9fa15c15e20d3b5
0xff-dev <stevenshuang521@gmail.com>
2023-04-11 20:16:48 +0800

    Add solution and test-cases for problem 2390

--- leetcode/2301-2400/2390.Removing-Stars-From-a-String/Solution.go
+++ leetcode/2301-2400/2390.Removing-Stars-From-a-String/Solution.go
@@ -1,5 +1,17 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string) string {
+	bs := []byte(s)
+	index := 0
+	for i := 0; i < len(s); i++ {
+		if bs[i] == '*' {
+			if index != 0 {
+				index--
+			}
+			continue
+		}
+		bs[index] = s[i]
+		index++
+	}
+	return string(bs[:index])
 }
