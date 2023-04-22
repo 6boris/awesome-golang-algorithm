1f48035d29507989df4c3594c6aceffbe509b6f3
0xff-dev <stevenshuang521@gmail.com>
2023-04-23 00:27:14 +0800

    Add solution and test-cases for problem 2432

--- leetcode/2401-2500/2432.The-Employee-That-Worked-on-the-Longest-Task/Solution_test.go
+++ leetcode/2401-2500/2432.The-Employee-That-Worked-on-the-Longest-Task/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs int
+		logs   [][]int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 10, [][]int{{0, 3}, {2, 5}, {0, 9}, {1, 15}}, 1},
+		{"TestCase2", 26, [][]int{{1, 1}, {3, 7}, {2, 12}, {7, 17}}, 3},
+		{"TestCase3", 2, [][]int{{0, 10}, {1, 20}}, 0},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.logs)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.inputs, c.logs)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
