dd445da38c6721d3f9b361c18c8694897faa271a
0xff-dev <stevenshuang521@gmail.com>
2023-04-25 21:50:34 +0800

    Add solution and test-cases for problem 2336

--- leetcode/2301-2400/2336.Smallest-Number-in-Infinite-Set/Solution.go
+++ leetcode/2301-2400/2336.Smallest-Number-in-Infinite-Set/Solution.go
@@ -1,5 +1,75 @@
 package Solution
 
-func Solution(x bool) bool {
+import "container/heap"
+
+type item2336 []int
+
+func (r *item2336) Len() int {
+	return len(*r)
+}
+func (r *item2336) Less(i, j int) bool {
+	return (*r)[i] < (*r)[j]
+}
+func (r *item2336) Swap(i, j int) {
+	(*r)[i], (*r)[j] = (*r)[j], (*r)[i]
+}
+
+func (r *item2336) Push(x interface{}) {
+	*r = append(*r, x.(int))
+}
+func (r *item2336) Pop() interface{} {
+	old := *r
+	l := len(old)
+	x := old[l-1]
+	*r = old[:l-1]
 	return x
+}
+
+type SmallestInfiniteSet struct {
+	index int
+	store item2336
+}
+
+func Constructor2336() SmallestInfiniteSet {
+	return SmallestInfiniteSet{index: 1, store: item2336{}}
+}
+
+func (this *SmallestInfiniteSet) PopSmallest() int {
+	if len(this.store) != 0 {
+		x := heap.Pop(&this.store).(int)
+		return x
+	}
+	x := this.index
+	this.index++
+	return x
+}
+
+func (this *SmallestInfiniteSet) AddBack(num int) {
+	if num >= this.index {
+		return
+	}
+	for _, n := range this.store {
+		if n == num {
+			return
+		}
+	}
+	heap.Push(&this.store, num)
+}
+
+type Op struct {
+	name string
+	val  int
+}
+
+func Solution(ops []Op) []int {
+	ans := make([]int, 0)
+	c := Constructor2336()
+	for _, o := range ops {
+		if o.name == "addBack" {
+			c.AddBack(o.val)
+			continue
+		}
+		ans = append(ans, c.PopSmallest())
+	}
+	return ans
 }
--- leetcode/2301-2400/2336.Smallest-Number-in-Infinite-Set/Solution_test.go
+++ leetcode/2301-2400/2336.Smallest-Number-in-Infinite-Set/Solution_test.go
@@ -10,12 +10,10 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []Op
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []Op{{"addBack", 2}, {}, {}, {}, {"addBack", 1}, {}, {}, {}}, []int{1, 2, 3, 1, 4, 5}},
 	}
 
 	//	开始测试
@@ -30,10 +28,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
