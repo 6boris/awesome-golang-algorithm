53e364f76c2b0fac2814ef4a6ff6a2b6a19880ab
0xff-dev <stevenshuang521@gmail.com>
2023-05-03 11:46:53 +0800

    Add solution and test-cases for problem 2215

--- leetcode/2201-2300/2215.Find-the-Difference-of-Two-Arrays/Solution_test.go
+++ leetcode/2201-2300/2215.Find-the-Difference-of-Two-Arrays/Solution_test.go
@@ -10,30 +10,29 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		n1, n2 []int
+		expect [][]int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 2, 3}, []int{2, 4, 6}, [][]int{{3, 1}, {6, 4}}},
+		{"TestCase2", []int{1, 2, 3, 3}, []int{1, 1, 2, 2}, [][]int{{3}, {}}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n1, c.n2)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.n1, c.n2)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
