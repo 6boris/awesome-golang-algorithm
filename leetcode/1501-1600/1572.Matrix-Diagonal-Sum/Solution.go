9ff668f4449434df8ed3a964b880ae736736fe1b
0xff-dev <stevenshuang521@gmail.com>
2023-05-08 20:58:29 +0800

    Add solution and test-cases for problem 1572

--- leetcode/1501-1600/1572.Matrix-Diagonal-Sum/Solution.go
+++ leetcode/1501-1600/1572.Matrix-Diagonal-Sum/Solution.go
@@ -1,5 +1,17 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(mat [][]int) int {
+	length := len(mat)
+	x, y := 0, 0
+	x1, y1 := 0, length-1
+	ans := 0
+	for x < length {
+		ans += mat[x][y]
+		if x1 != x || y1 != y {
+			ans += mat[x1][y1]
+		}
+		x, y = x+1, y+1
+		x1, y1 = x1+1, y1-1
+	}
+	return ans
 }
