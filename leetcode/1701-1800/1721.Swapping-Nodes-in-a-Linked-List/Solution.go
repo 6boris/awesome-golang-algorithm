78cadee57ee91398b845166615bb52b60f1f5b4b
0xff-dev <stevenshuang521@gmail.com>
2023-05-15 22:11:33 +0800

    Add solution and test-cases for problem 1721

--- leetcode/1701-1800/1721.Swapping-Nodes-in-a-Linked-List/Solution.go
+++ leetcode/1701-1800/1721.Swapping-Nodes-in-a-Linked-List/Solution.go
@@ -1,5 +1,42 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func Solution(head *ListNode, k int) *ListNode {
+	l := 0
+	for walker := head; walker != nil; walker = walker.Next {
+		l++
+	}
+	if k > l/2 {
+		k = l - k + 1
+	}
+	steps := l - k*2 + 1
+	fromHead := head
+	for i := 1; i < k; i++ {
+		fromHead = fromHead.Next
+	}
+	fromEnd := fromHead
+	for ; steps > 0; steps-- {
+		fromEnd = fromEnd.Next
+	}
+	fromHead.Val, fromEnd.Val = fromEnd.Val, fromHead.Val
+
+	return head
+}
+
+func Solution1(head *ListNode, k int) *ListNode {
+	walker := head
+	for i := 1; i < k; i++ {
+		walker = walker.Next
+	}
+	one := walker
+	two := head
+	for ; walker.Next != nil; walker = walker.Next {
+		two = two.Next
+	}
+	one.Val, two.Val = two.Val, one.Val
+	return head
 }
