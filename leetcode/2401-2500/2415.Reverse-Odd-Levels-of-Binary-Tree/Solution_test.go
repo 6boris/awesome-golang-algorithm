06960d753b711bbac8a3ef8245b07e681bd24fde
0xff-dev <stevenshuang521@gmail.com>
2023-05-16 22:34:01 +0800

    Add solution and test-cases for problem 2415

--- leetcode/2401-2500/2415.Reverse-Odd-Levels-of-Binary-Tree/Solution_test.go
+++ leetcode/2401-2500/2415.Reverse-Odd-Levels-of-Binary-Tree/Solution_test.go
@@ -10,12 +10,43 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs *TreeNode
+		expect *TreeNode
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val:   3,
+				Left:  &TreeNode{Val: 8},
+				Right: &TreeNode{Val: 13},
+			},
+			Right: &TreeNode{
+				Val:   5,
+				Left:  &TreeNode{Val: 21},
+				Right: &TreeNode{Val: 34},
+			},
+		}, &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val:   5,
+				Left:  &TreeNode{Val: 8},
+				Right: &TreeNode{Val: 13},
+			},
+			Right: &TreeNode{
+				Val:   3,
+				Left:  &TreeNode{Val: 21},
+				Right: &TreeNode{Val: 34},
+			},
+		}},
+		{"TestCase2", &TreeNode{
+			Val:   7,
+			Left:  &TreeNode{Val: 13},
+			Right: &TreeNode{Val: 11},
+		}, &TreeNode{
+			Val:   7,
+			Left:  &TreeNode{Val: 11},
+			Right: &TreeNode{Val: 13},
+		}},
 	}
 
 	//	开始测试
@@ -29,11 +60,65 @@ func TestSolution(t *testing.T) {
 		})
 	}
 }
+func TestSolution1(t *testing.T) {
+	//	测试用例
+	cases := []struct {
+		name   string
+		inputs *TreeNode
+		expect *TreeNode
+	}{
+		{"TestCase1", &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val:   3,
+				Left:  &TreeNode{Val: 8},
+				Right: &TreeNode{Val: 13},
+			},
+			Right: &TreeNode{
+				Val:   5,
+				Left:  &TreeNode{Val: 21},
+				Right: &TreeNode{Val: 34},
+			},
+		}, &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val:   5,
+				Left:  &TreeNode{Val: 8},
+				Right: &TreeNode{Val: 13},
+			},
+			Right: &TreeNode{
+				Val:   3,
+				Left:  &TreeNode{Val: 21},
+				Right: &TreeNode{Val: 34},
+			},
+		}},
+		{"TestCase2", &TreeNode{
+			Val:   7,
+			Left:  &TreeNode{Val: 13},
+			Right: &TreeNode{Val: 11},
+		}, &TreeNode{
+			Val:   7,
+			Left:  &TreeNode{Val: 11},
+			Right: &TreeNode{Val: 13},
+		}},
+	}
 
-//	压力测试
+	//	开始测试
+	for i, c := range cases {
+		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
+			got := Solution1(c.inputs)
+			if !reflect.DeepEqual(got, c.expect) {
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+					c.expect, got, c.inputs)
+			}
+		})
+	}
+}
+
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
