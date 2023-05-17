29cd951a3c34e46f524ca2b32ce7fc393ccdd060
0xff-dev <stevenshuang521@gmail.com>
2023-05-17 22:14:53 +0800

    Add solution and test-cases for problem 2130

--- leetcode/2101-2200/2130.Maximum-Twin-Sum-of-a-Linked-List/Solution.go
+++ leetcode/2101-2200/2130.Maximum-Twin-Sum-of-a-Linked-List/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func Solution(head *ListNode) int {
+	ans := 0
+	pairSum1(head, &head, &ans)
+	return ans
+}
+
+func pairSum1(h1 *ListNode, h2 **ListNode, ans *int) {
+	if h1.Next != nil {
+		pairSum1(h1.Next, h2, ans)
+	}
+
+	h := *h2
+	if r := h1.Val + h.Val; r > *ans {
+		*ans = r
+	}
+	*h2 = h.Next
 }
