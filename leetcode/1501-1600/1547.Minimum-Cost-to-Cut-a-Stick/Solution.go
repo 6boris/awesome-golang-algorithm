613e4b6c13d519d8dd3a8e5df99a9e02aa3d4935
0xff-dev <stevenshuang521@gmail.com>
2023-05-28 15:32:17 +0800

    Add solution and test-cases for problem 1547

--- leetcode/1501-1600/1547.Minimum-Cost-to-Cut-a-Stick/Solution.go
+++ leetcode/1501-1600/1547.Minimum-Cost-to-Cut-a-Stick/Solution.go
@@ -1,5 +1,47 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int, cuts []int) int {
+	dp := make(map[int]map[int]int)
+	/*
+		       n  很大，这么开内存受不了
+				dp := make([][]int, n+1)
+				for i := 0; i <= n; i++ {
+					dp[i] = make([]int, n+1)
+				}
+	*/
+	dp[0] = make(map[int]int)
+	dp[n] = make(map[int]int)
+	for _, c := range cuts {
+		dp[c] = make(map[int]int)
+	}
+
+	var dfs func(int, int) int
+	dfs = func(start, end int) int {
+		if dp[start][end] != 0 {
+			return dp[start][end]
+		}
+		need := end - start
+		if need <= 1 {
+			return 0
+		}
+
+		m := 0
+		found := false
+		for _, pos := range cuts {
+			if pos > start && pos < end {
+				found = true
+				left := dfs(start, pos)
+				right := dfs(pos, end)
+				if m == 0 || left+right < m {
+					m = left + right
+				}
+			}
+		}
+		if !found {
+			return 0
+		}
+		dp[start][end] = need + m
+		return dp[start][end]
+	}
+	return dfs(0, n)
 }
--- leetcode/1501-1600/1547.Minimum-Cost-to-Cut-a-Stick/Solution_test.go
+++ leetcode/1501-1600/1547.Minimum-Cost-to-Cut-a-Stick/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs int
+		cuts   []int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 7, []int{1, 3, 4, 5}, 16},
+		{"TestCase2", 9, []int{5, 6, 1, 4, 2}, 22},
+		{"TestCase3", 2, []int{1}, 2},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.cuts)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.inputs, c.cuts)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
