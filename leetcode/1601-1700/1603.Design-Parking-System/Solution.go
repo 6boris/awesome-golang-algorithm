0b87c0ca61653fc1abdba0af54949ed7adf59a3f
0xff-dev <stevenshuang521@gmail.com>
2023-05-29 21:36:04 +0800

    Add solution and test-cases for problem 1603

--- leetcode/1601-1700/1603.Design-Parking-System/Solution.go
+++ leetcode/1601-1700/1603.Design-Parking-System/Solution.go
@@ -1,5 +1,26 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type ParkingSystem struct {
+	solt [3]int
+}
+
+func Constructor(big int, medium int, small int) ParkingSystem {
+	return ParkingSystem{solt: [3]int{big, medium, small}}
+}
+
+func (this *ParkingSystem) AddCar(carType int) bool {
+	if this.solt[carType-1] > 0 {
+		this.solt[carType-1]--
+		return true
+	}
+	return false
+}
+
+func Solution(big, medium, small int, ops []int) []bool {
+	c := Constructor(big, medium, small)
+	ans := make([]bool, len(ops))
+	for i, op := range ops {
+		ans[i] = c.AddCar(op)
+	}
+	return ans
 }
--- leetcode/1601-1700/1603.Design-Parking-System/Solution_test.go
+++ leetcode/1601-1700/1603.Design-Parking-System/Solution_test.go
@@ -9,31 +9,30 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name               string
+		big, medium, small int
+		ops                []int
+		expect             []bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 1, 1, 0, []int{1, 2, 3, 1}, []bool{true, true, false, false}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.big, c.medium, c.small, c.ops)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v %v",
+					c.expect, got, c.big, c.medium, c.small, c.ops)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
