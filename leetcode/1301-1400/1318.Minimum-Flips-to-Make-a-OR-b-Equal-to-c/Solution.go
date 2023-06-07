de8344aa93cf1e8188ad24f3bc9eba081c835c7f
0xff-dev <stevenshuang521@gmail.com>
2023-06-07 22:27:28 +0800

    Add solution and test-cases for problem 1318

--- leetcode/1301-1400/1318.Minimum-Flips-to-Make-a-OR-b-Equal-to-c/Solution.go
+++ leetcode/1301-1400/1318.Minimum-Flips-to-Make-a-OR-b-Equal-to-c/Solution.go
@@ -1,5 +1,42 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"fmt"
+)
+
+func Solution(a int, b int, c int) int {
+	aStr := fmt.Sprintf("%b", a)
+	bStr := fmt.Sprintf("%b", b)
+	cStr := fmt.Sprintf("%b", c)
+
+	la := len(aStr)
+	lb := len(bStr)
+	need := 0
+	ia, ib := la-1, lb-1
+	for i := len(cStr) - 1; i >= 0 || ia >= 0 || ib >= 0; i, ia, ib = i-1, ia-1, ib-1 {
+		aa, bb, cc := byte('0'), byte('0'), byte('0')
+		if ia >= 0 {
+			aa = aStr[ia]
+		}
+		if ib >= 0 {
+			bb = bStr[ib]
+		}
+		if i >= 0 {
+			cc = cStr[i]
+		}
+		if cc == '0' {
+			if aa != '0' {
+				need++
+			}
+			if bb != '0' {
+				need++
+			}
+		}
+		if cc == '1' {
+			if aa == '0' && bb == '0' {
+				need += 1
+			}
+		}
+	}
+	return need
 }
--- leetcode/1301-1400/1318.Minimum-Flips-to-Make-a-OR-b-Equal-to-c/Solution_test.go
+++ leetcode/1301-1400/1318.Minimum-Flips-to-Make-a-OR-b-Equal-to-c/Solution_test.go
@@ -9,31 +9,32 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name    string
+		a, b, c int
+		expect  int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 2, 6, 5, 3},
+		{"TestCase2", 4, 2, 7, 1},
+		{"TestCase3", 1, 2, 3, 0},
+		{"TestCase4", 8, 3, 5, 3},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.a, c.b, c.c)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.a, c.b, c.c)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
