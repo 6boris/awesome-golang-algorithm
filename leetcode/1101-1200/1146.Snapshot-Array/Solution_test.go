8a287d8295300d96cce98990e59eee490cdb5866
0xff-dev <stevenshuang521@gmail.com>
2023-06-11 15:10:24 +0800

    Add solution and test-cases for problem 1146

--- leetcode/1101-1200/1146.Snapshot-Array/Solution_test.go
+++ leetcode/1101-1200/1146.Snapshot-Array/Solution_test.go
@@ -10,30 +10,29 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		length int
+		ops    []op
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 3, []op{{name: "set", index: 0, val: 5}, {name: "snap"}, {name: "set", index: 0, val: 6}, {name: "get", index: 0, id: 0}}, []int{0, 5}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.length, c.ops)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.length, c.ops)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
