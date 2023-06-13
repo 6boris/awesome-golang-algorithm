ab25093446370656434eb0a992814c7892d682e3
0xff-dev <stevenshuang521@gmail.com>
2023-06-13 22:14:15 +0800

    Add solution and test-cases for problem 2352

--- leetcode/2301-2400/2352.Equal-Row-and-Column-Pairs/Solution.go
+++ leetcode/2301-2400/2352.Equal-Row-and-Column-Pairs/Solution.go
@@ -1,5 +1,30 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"fmt"
+	"strings"
+)
+
+func Solution(grid [][]int) int {
+	length := len(grid)
+	ans := 0
+	cols := make([]strings.Builder, length)
+	rowsMap := make(map[string]int)
+
+	var ssb strings.Builder
+	for row := 0; row < length; row++ {
+		for col := 0; col < length; col++ {
+			ssb.WriteString(fmt.Sprintf("%d-", grid[row][col]))
+			cols[col].WriteString(fmt.Sprintf("%d-", grid[row][col]))
+		}
+		rowsMap[ssb.String()]++
+		ssb.Reset()
+	}
+
+	for _, sb := range cols {
+		if count, ok := rowsMap[sb.String()]; ok {
+			ans += count
+		}
+	}
+	return ans
 }
