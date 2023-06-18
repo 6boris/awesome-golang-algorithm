e7b52c0849e18a4bc9336763435f8cfabe867a51
0xff-dev <stevenshuang521@gmail.com>
2023-06-18 16:15:57 +0800

    Add solution and test-cases for problem 329

--- leetcode/301-400/0329.Longest-Increasing-Path-in-a-Matrix/Solution_test.go
+++ leetcode/301-400/0329.Longest-Increasing-Path-in-a-Matrix/Solution_test.go
@@ -10,12 +10,20 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs [][]int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{
+			{9, 9, 4},
+			{6, 6, 8},
+			{2, 1, 1},
+		}, 4},
+		{"TestCase2", [][]int{
+			{3, 4, 5},
+			{3, 2, 6},
+			{2, 2, 1},
+		}, 4},
+		{"TestCase3", [][]int{{1}}, 1},
 	}
 
 	//	开始测试
@@ -30,10 +38,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
