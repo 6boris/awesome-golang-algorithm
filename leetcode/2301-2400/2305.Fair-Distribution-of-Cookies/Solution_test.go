0c2beab87697af28e6c0554c1e8b88456980f55b
0xff-dev <stevenshuang521@gmail.com>
2023-07-01 15:29:11 +0800

    Add solution and test-cases for problem 2305

--- leetcode/2301-2400/2305.Fair-Distribution-of-Cookies/Solution_test.go
+++ leetcode/2301-2400/2305.Fair-Distribution-of-Cookies/Solution_test.go
@@ -9,31 +9,31 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name    string
+		cookies []int
+		k       int
+		expect  int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{8, 15, 10, 20, 8}, 2, 31},
+		{"TestCase2", []int{6, 1, 3, 2, 2, 4, 1, 2}, 3, 7},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.cookies, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.cookies, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
