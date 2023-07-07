613c7934a6a59386df10f9d894e8326e54207bb5
0xff-dev <stevenshuang521@gmail.com>
2023-07-07 20:55:15 +0800

    Add solution and test-cases for problem 712

--- leetcode/701-800/0712.Minimum-ASCII-Delete-Sum-for-Two-Strings/Solution.go
+++ leetcode/701-800/0712.Minimum-ASCII-Delete-Sum-for-Two-Strings/Solution.go
@@ -1,5 +1,36 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s1 string, s2 string) int {
+	l1, l2 := len(s1), len(s2)
+	dp := make([][]int, l1+1)
+	for i := 0; i <= l1; i++ {
+
+		dp[i] = make([]int, l2+1)
+		if i > 0 {
+			dp[i][0] = dp[i-1][0] + int(s1[i-1])
+		}
+		for j := 1; j <= l2; j++ {
+			dp[i][j] = dp[i][j-1] + int(s2[j-1])
+		}
+	}
+	for r := 1; r <= l1; r++ {
+		for c := 1; c <= l2; c++ {
+			x := dp[r-1][c-1]
+			if s1[r-1] == s2[c-1] {
+				dp[r][c] = x
+				continue
+			}
+			y := dp[r-1][c]
+			z := dp[r][c-1]
+			a := x + int(s1[r-1]) + int(s2[c-1])
+			if b := y + int(s1[r-1]); r < a {
+				a = b
+			}
+			if b := z + int(s2[c-1]); b < a {
+				a = b
+			}
+			dp[r][c] = a
+		}
+	}
+	return dp[l1][l2]
 }
--- leetcode/701-800/0712.Minimum-ASCII-Delete-Sum-for-Two-Strings/Solution_test.go
+++ leetcode/701-800/0712.Minimum-ASCII-Delete-Sum-for-Two-Strings/Solution_test.go
@@ -10,30 +10,29 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		s1, s2 string
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "sea", "eat", 231},
+		{"TestCase2", "delete", "leet", 403},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.s1, c.s2)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.s1, c.s2)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
