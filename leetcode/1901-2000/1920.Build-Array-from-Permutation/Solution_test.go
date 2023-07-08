963b5c7ba6d73462ce2bee93e5f761a7db94e2c0
0xff-dev <stevenshuang521@gmail.com>
2023-07-08 22:38:07 +0800

    Add solution and test-cases for problem 1920

--- leetcode/1901-2000/1920.Build-Array-from-Permutation/Solution_test.go
+++ leetcode/1901-2000/1920.Build-Array-from-Permutation/Solution_test.go
@@ -10,12 +10,11 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{0, 2, 1, 5, 3, 4}, []int{0, 1, 2, 4, 5, 3}},
+		{"TestCase2", []int{5, 0, 1, 2, 3, 4}, []int{4, 5, 0, 1, 2, 3}},
 	}
 
 	//	开始测试
@@ -30,10 +29,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
