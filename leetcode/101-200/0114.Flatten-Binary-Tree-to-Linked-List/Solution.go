f098656ada877ad240391b43403e7abe4858d16d
0xff-dev <stevenshuang521@gmail.com>
2023-07-11 22:48:51 +0800

    Add solution and test-cases for problem 114

--- leetcode/101-200/0114.Flatten-Binary-Tree-to-Linked-List/Solution.go
+++ leetcode/101-200/0114.Flatten-Binary-Tree-to-Linked-List/Solution.go
@@ -22,3 +22,27 @@ func Solution(root *TreeNode) {
 	}
 	Solution(root.Right)
 }
+
+
+func flatten114(root *TreeNode) (*TreeNode, *TreeNode) {
+	if root == nil {
+		return nil, nil
+	}
+	end := root
+	rightStart, rightEnd := flatten114(root.Right)
+	leftStart, leftEnd := flatten114(root.Left)
+	if leftStart != nil {
+		end.Right = leftStart
+		end = leftEnd
+	}
+	if rightStart != nil {
+		end.Right = rightStart
+		end = rightEnd
+	}
+	root.Left = nil
+	return root, end
+}
+func Solution2(root *TreeNode) {
+	flatten114(root)
+	return
+}
