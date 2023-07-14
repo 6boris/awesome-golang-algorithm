9fe0caf25e3e6f53c9de8c5575e416e68785f5f6
0xff-dev <stevenshuang521@gmail.com>
2023-07-14 21:39:29 +0800

    Add solution and test-cases for problem 1218

--- leetcode/1201-1300/1218.Longest-Arithmetic-Subsequence-of-Given-Difference/Solution_test.go
+++ leetcode/1201-1300/1218.Longest-Arithmetic-Subsequence-of-Given-Difference/Solution_test.go
@@ -10,30 +10,33 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		arr    []int
+		diff   int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 2, 3, 4}, 1, 4},
+		{"TestCase2", []int{1, 3, 5, 7}, 1, 1},
+		{"TestCase3", []int{1, 5, 7, 8, 5, 3, 4, 2, 1}, -2, 4},
+		{"TestCase4", []int{7, 7, 7, 7, 7, 7, 7}, 0, 7},
+		{"TestCase5", []int{4, 12, 10, 0, -2, 7, -8, 9, -9, -12, -12, 8, 8}, 0, 2},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.arr, c.diff)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.arr, c.diff)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
