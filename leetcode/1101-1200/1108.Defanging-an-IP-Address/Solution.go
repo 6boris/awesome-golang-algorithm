8d37d722cb00cd8ff2a8af3cc43aac12c73b48a9
0xff-dev <stevenshuang521@gmail.com>
2023-07-15 22:13:34 +0800

    Add solution and test-cases for problem 1108

--- leetcode/1101-1200/1108.Defanging-an-IP-Address/Solution.go
+++ leetcode/1101-1200/1108.Defanging-an-IP-Address/Solution.go
@@ -1,5 +1,18 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(address string) string {
+	ans := make([]byte, len(address)+6)
+	index := 0
+	for _, b := range address {
+		if b == '.' {
+			ans[index] = '['
+			ans[index+1] = '.'
+			ans[index+2] = ']'
+			index += 3
+			continue
+		}
+		ans[index] = byte(b)
+		index++
+	}
+	return string(ans)
 }
