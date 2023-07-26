5a8415b4ca506b040fe3103025b986cc5a8a9c8a
0xff-dev <stevenshuang521@gmail.com>
2023-07-26 22:17:56 +0800

    Add solution and test-cases for problem 1870

--- leetcode/1801-1900/1870.Minimum-Speed-to-Arrive-on-Time/Solution_test.go
+++ leetcode/1801-1900/1870.Minimum-Speed-to-Arrive-on-Time/Solution_test.go
@@ -10,30 +10,32 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		dist   []int
+		hour   float64
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 3, 2}, float64(6), 1},
+		{"TestCase2", []int{1, 3, 2}, 2.7, 3},
+		{"TestCase3", []int{1, 3, 2}, 1.9, -1},
+		{"TestCase4", []int{1, 1, 100000}, 2.01, 10000000},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.dist, c.hour)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.dist, c.hour)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
