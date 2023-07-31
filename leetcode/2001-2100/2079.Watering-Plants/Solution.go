b82a2f0ceafc4e50d62759a830c54566aca36e37
0xff-dev <stevenshuang521@gmail.com>
2023-07-31 21:22:20 +0800

    Add solution and test-cases for problem 2079

--- leetcode/2001-2100/2079.Watering-Plants/Solution.go
+++ leetcode/2001-2100/2079.Watering-Plants/Solution.go
@@ -1,5 +1,16 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(plants []int, capacity int) int {
+	cost := 0
+	sum := 0
+	for idx, water := range plants {
+		sum += water
+		if sum <= capacity {
+			cost++
+			continue
+		}
+		cost += 2*idx + 1
+		sum = water
+	}
+	return cost
 }
