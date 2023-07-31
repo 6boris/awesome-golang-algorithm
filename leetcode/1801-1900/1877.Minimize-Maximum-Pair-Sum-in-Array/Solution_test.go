f1a51edb5c931b8b644432b390e7562f5249f606
0xff-dev <stevenshuang521@gmail.com>
2023-07-31 21:44:27 +0800

    Add solution and test-cases for problem 1877

--- leetcode/1801-1900/1877.Minimize-Maximum-Pair-Sum-in-Array/Solution_test.go
+++ leetcode/1801-1900/1877.Minimize-Maximum-Pair-Sum-in-Array/Solution_test.go
@@ -10,12 +10,11 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{3, 5, 2, 3}, 7},
+		{"TestCase2", []int{3, 5, 4, 2, 4, 6}, 8},
 	}
 
 	//	开始测试
@@ -30,10 +29,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
