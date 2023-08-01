c5ff94d06fa08be3a5c9fd9a1ea4378a9e4d8fc8
0xff-dev <stevenshuang521@gmail.com>
2023-08-01 22:05:56 +0800

    Add solution and test-cases for problem 1261

--- leetcode/1201-1300/1261.Find-Elements-in-a-Contaminated-Binary-Tree/Solution.go
+++ leetcode/1201-1300/1261.Find-Elements-in-a-Contaminated-Binary-Tree/Solution.go
@@ -1,5 +1,49 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+type FindElements struct {
+	cache map[int]struct{}
+}
+
+func (f *FindElements) dfs(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	f.cache[root.Val] = struct{}{}
+	if root.Left != nil {
+		root.Left.Val = 2*root.Val + 1
+		f.dfs(root.Left)
+	}
+	if root.Right != nil {
+		root.Right.Val = 2*root.Val + 2
+		f.dfs(root.Right)
+	}
+}
+
+func Constructor1261(root *TreeNode) FindElements {
+	o := FindElements{cache: make(map[int]struct{})}
+	// add root
+	root.Val = 0
+	o.dfs(root)
+
+	return o
+}
+
+func (this *FindElements) Find(target int) bool {
+	_, ok := this.cache[target]
+	return ok
+}
+
+func Solution(tree *TreeNode, targets []int) []bool {
+	ans := make([]bool, len(targets))
+	o := Constructor1261(tree)
+	for idx, target := range targets {
+		ans[idx] = o.Find(target)
+	}
+
+	return ans
 }
