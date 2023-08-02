7a0f639c362c9eed1a61c5576db9bd7d2eb09d1e
0xff-dev <stevenshuang521@gmail.com>
2023-08-02 22:49:08 +0800

    Add solution and test-cases for problem 701

--- leetcode/701-800/0701.Insert-into-a-Binary-Search-Tree/Solution.go
+++ leetcode/701-800/0701.Insert-into-a-Binary-Search-Tree/Solution.go
@@ -1,5 +1,18 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func Solution(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+	if root.Val < val {
+		root.Right = Solution(root.Right, val)
+	} else if root.Val > val {
+		root.Left = Solution(root.Left, val)
+	}
+	return root
 }
--- leetcode/701-800/0701.Insert-into-a-Binary-Search-Tree/Solution_test.go
+++ leetcode/701-800/0701.Insert-into-a-Binary-Search-Tree/Solution_test.go
@@ -10,30 +10,70 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs *TreeNode
+		val    int
+		expect *TreeNode
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val:   2,
+				Left:  &TreeNode{Val: 1},
+				Right: &TreeNode{Val: 3},
+			},
+			Right: &TreeNode{Val: 7},
+		}, 5, &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val:   2,
+				Left:  &TreeNode{Val: 1},
+				Right: &TreeNode{Val: 3},
+			},
+			Right: &TreeNode{Val: 7, Left: &TreeNode{Val: 5}},
+		}},
+		{"TestCase2", &TreeNode{
+			Val: 40,
+			Left: &TreeNode{
+				Val:   20,
+				Left:  &TreeNode{Val: 10},
+				Right: &TreeNode{Val: 30},
+			},
+			Right: &TreeNode{
+				Val:   60,
+				Left:  &TreeNode{Val: 50},
+				Right: &TreeNode{Val: 70},
+			},
+		}, 25, &TreeNode{
+			Val: 40,
+			Left: &TreeNode{
+				Val:   20,
+				Left:  &TreeNode{Val: 10},
+				Right: &TreeNode{Val: 30, Left: &TreeNode{Val: 25}},
+			},
+			Right: &TreeNode{
+				Val:   60,
+				Left:  &TreeNode{Val: 50},
+				Right: &TreeNode{Val: 70},
+			},
+		}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.val)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.inputs, c.val)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
