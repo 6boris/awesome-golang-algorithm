02c6ee961c145e686e19c71254a42f90d5b0cd28
0xff-dev <stevenshuang521@gmail.com>
2023-08-12 16:01:35 +0800

    Add solution and test-cases for problem 1237

--- leetcode/1201-1300/1237.Find-Positive-Integer-Solution-for-a-Given-Equation/Solution.go
+++ leetcode/1201-1300/1237.Find-Positive-Integer-Solution-for-a-Given-Equation/Solution.go
@@ -1,5 +1,26 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(customFunction func(int, int) int, z int) [][]int {
+	ans := make([][]int, 0)
+	for y := 1; y <= 1000; y++ {
+		l, r := 1, 1000
+		if r := customFunction(1, y); r > z {
+			break
+		}
+		for l < r {
+			m := l + (r-l)/2
+			rr := customFunction(m, y)
+
+			if rr == z {
+				ans = append(ans, []int{m, y})
+				break
+			}
+			if rr < z {
+				l = m + 1
+				continue
+			}
+			r = m
+		}
+	}
+	return ans
 }
