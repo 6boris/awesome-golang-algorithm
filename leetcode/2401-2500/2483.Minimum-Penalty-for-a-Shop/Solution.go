e1b239fc1f56c7348b6496934e9f5ac534263264
0xff-dev <stevenshuang521@gmail.com>
2023-08-29 21:36:45 +0800

    Add solution and test-cases for problem 2483

--- leetcode/2401-2500/2483.Minimum-Penalty-for-a-Shop/Solution.go
+++ leetcode/2401-2500/2483.Minimum-Penalty-for-a-Shop/Solution.go
@@ -1,5 +1,31 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(customers string) int {
+	l := len(customers)
+	left := make([]int, l+1)
+	right := make([]int, l+1)
+	n := 0
+	for closeTime := 0; closeTime < l; closeTime++ {
+		if customers[closeTime] == 'Y' {
+			left[closeTime] = n + 1
+			continue
+		}
+		left[closeTime] = n
+		n++
+	}
+	left[l] = n
+	y := 0
+	for closeTime := l - 1; closeTime >= 0; closeTime-- {
+		right[closeTime] = y
+		if customers[closeTime] == 'Y' {
+			y++
+		}
+	}
+	ans := -1
+	for t := 0; t <= l; t++ {
+		if ans == -1 || left[t]+right[t] < left[ans]+right[ans] {
+			ans = t
+		}
+	}
+	return ans
 }
