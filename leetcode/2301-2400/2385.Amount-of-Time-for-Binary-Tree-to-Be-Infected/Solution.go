9344a754715c50396ff7e9efcb78a274f0355a07
0xff-dev <stevenshuang521@gmail.com>
2023-09-03 16:48:23 +0800

    Add solution and test-cases for problem 2385

--- leetcode/2301-2400/2385.Amount-of-Time-for-Binary-Tree-to-Be-Infected/Solution.go
+++ leetcode/2301-2400/2385.Amount-of-Time-for-Binary-Tree-to-Be-Infected/Solution.go
@@ -1,5 +1,55 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func Solution(root *TreeNode, start int) int {
+	if root.Left == nil && root.Right == nil {
+		return 0
+	}
+	father := make(map[*TreeNode]*TreeNode)
+	var startNode *TreeNode
+	var dfs func(*TreeNode)
+	dfs = func(t *TreeNode) {
+		if t == nil {
+			return
+		}
+		if t.Val == start {
+			startNode = t
+		}
+		if t.Left != nil {
+			father[t.Left] = t
+			dfs(t.Left)
+		}
+		if t.Right != nil {
+			father[t.Right] = t
+			dfs(t.Right)
+		}
+	}
+
+	dfs(root)
+	q := []*TreeNode{startNode}
+	visited := make(map[*TreeNode]struct{})
+	visited[startNode] = struct{}{}
+	ans := 0
+	for len(q) > 0 {
+		next := make([]*TreeNode, 0)
+		for _, n := range q {
+			for _, nn := range []*TreeNode{n.Left, n.Right, father[n]} {
+				if nn == nil {
+					continue
+				}
+				if _, ok := visited[nn]; ok {
+					continue
+				}
+				visited[nn] = struct{}{}
+				next = append(next, nn)
+			}
+		}
+		q = next
+		ans++
+	}
+	return ans - 1
 }
