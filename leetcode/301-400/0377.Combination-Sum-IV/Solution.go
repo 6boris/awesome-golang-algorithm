5fa1027dc642f0c7894f000b5916128022e5302b
0xff-dev <stevenshuang521@gmail.com>
2023-09-09 13:39:00 +0800

    Add solution for problem 377

--- leetcode/301-400/0377.Combination-Sum-IV/Solution.go
+++ leetcode/301-400/0377.Combination-Sum-IV/Solution.go
@@ -12,3 +12,30 @@ func combinationSum4(nums []int, target int) int {
 	}
 	return dp[target]
 }
+
+func Solution2(nums []int, target int) int {
+	var lookup func(int) int
+	// dfs + cache
+	cache := make(map[int]int)
+	lookup = func(sum int) int {
+		if sum < 0 {
+			return 0
+		}
+		if sum == 0 {
+			return 1
+		}
+		if v, ok := cache[sum]; ok {
+			return v
+		}
+
+		ans := 0
+		for _, i := range nums {
+			next := sum - i
+			ans += lookup(next)
+		}
+		cache[sum] = ans
+		return ans
+	}
+	lookup(target)
+	return cache[target]
+}
