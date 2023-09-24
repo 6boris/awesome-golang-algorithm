0f9e7967435209a53ef19ba0ff91e3a4e9cd0945
0xff-dev <stevenshuang521@gmail.com>
2023-09-24 19:12:35 +0800

    Add solution and test-cases for problem 799

--- leetcode/701-800/0799.Champagne-Tower/Solution.go
+++ leetcode/701-800/0799.Champagne-Tower/Solution.go
@@ -1,5 +1,22 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(poured int, query_row int, query_glass int) float64 {
+	tower := make([][]float64, 101)
+	for i := 0; i < 101; i++ {
+		tower[i] = make([]float64, 101)
+	}
+	tower[0][0] = float64(poured)
+	for row := 1; row <= query_row; row++ {
+		for col := 0; col < row; col++ {
+			if tower[row-1][col] > 1.0 {
+				left := (tower[row-1][col] - 1.0) / 2.0
+				tower[row][col] += left
+				tower[row][col+1] += left
+			}
+		}
+	}
+	if tower[query_row][query_glass] > 1.0 {
+		return 1.0
+	}
+	return tower[query_row][query_glass]
 }
