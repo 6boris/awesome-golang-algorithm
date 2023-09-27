bfcca2e95ab3ebb837f7f4fac880e5a84099c734
0xff-dev <stevenshuang521@gmail.com>
2023-09-27 23:12:41 +0800

    Add solution and test-cases for problem 260

--- leetcode/201-300/0260.Single-Number-III/Solution_test.go
+++ leetcode/201-300/0260.Single-Number-III/Solution_test.go
@@ -2,6 +2,7 @@ package Solution
 
 import (
 	"reflect"
+	"sort"
 	"strconv"
 	"testing"
 )
@@ -10,18 +11,19 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 2, 1, 3, 2, 5}, []int{3, 5}},
+		{"TestCase2", []int{-1, 0}, []int{-1, 0}},
+		{"TestCase3", []int{1, 0}, []int{0, 1}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
 			got := Solution(c.inputs)
+			sort.Ints(got)
 			if !reflect.DeepEqual(got, c.expect) {
 				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
 					c.expect, got, c.inputs)
@@ -30,10 +32,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
