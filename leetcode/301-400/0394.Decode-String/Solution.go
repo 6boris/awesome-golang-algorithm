ff38c106417a8f4e6f2c84dc7796c0a8ce2fecb4
0xff-dev <stevenshuang521@gmail.com>
2023-09-27 22:27:54 +0800

    Add solution and test-cases for problem 394

--- leetcode/301-400/0394.Decode-String/Solution.go
+++ leetcode/301-400/0394.Decode-String/Solution.go
@@ -1,5 +1,64 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"bytes"
+)
+
+type stackItem struct {
+	num int
+	str string
+}
+
+func Solution(s string) string {
+	l := len(s)
+	ans := bytes.NewBuffer([]byte{})
+
+	stack := make([]stackItem, 0)
+	buf := bytes.NewBuffer([]byte{})
+	base := 0
+	// 3[z] 2[2[y]pq4[2[jk]e1[f]]]ef"
+	//        2
+	// zzz    yypg jkjkef jkjkef jkjkef jkjkef yypg jkjkef jkjkef jkjkef jkjkef  ef
+	for i := 0; i < l; i++ {
+		if s[i] >= 'a' && s[i] <= 'z' {
+			base = 0
+			if len(stack) == 0 {
+				ans.WriteByte(s[i])
+			} else {
+				buf.WriteByte(s[i])
+			}
+			continue
+		}
+		if s[i] >= '0' && s[i] <= '9' {
+			base = base*10 + int(s[i]-'0')
+			r := buf.String()
+			if ls := len(stack); ls > 0 {
+				stack[ls-1].str += r
+			}
+			buf.Reset()
+			continue
+		}
+		if s[i] == '[' {
+			stack = append(stack, stackItem{num: base})
+			base = 0
+			continue
+		}
+		ls := len(stack)
+		top := stack[ls-1]
+		stack = stack[:ls-1]
+		r := buf.String()
+		top.str += r
+		buf.Reset()
+		for i := 0; i < top.num; i++ {
+			buf.WriteString(top.str)
+		}
+		r = buf.String()
+		if ls := len(stack); ls > 0 {
+			stack[ls-1].str += r
+		} else {
+			ans.WriteString(r)
+		}
+		buf.Reset()
+	}
+	return ans.String()
 }
--- leetcode/301-400/0394.Decode-String/Solution_test.go
+++ leetcode/301-400/0394.Decode-String/Solution_test.go
@@ -10,12 +10,14 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs string
+		expect string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "3[a]2[bc]", "aaabcbc"},
+		{"TestCase2", "3[a2[c]]", "accaccacc"},
+		{"TestCase3", "2[abc]3[cd]ef", "abcabccdcdcdef"},
+		{"TestCase4", "abc3[cd]xyz", "abccdcdcdxyz"},
+		{"TestCase5", "3[z]2[2[y]pq4[2[jk]e1[f]]]ef", "zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef"},
 	}
 
 	//	开始测试
@@ -30,10 +32,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
