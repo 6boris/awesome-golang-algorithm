3ebd495cdc1b17e045eb04d6fd2b3fda781e9151
0xff-dev <stevenshuang521@gmail.com>
2023-10-01 13:22:49 +0800

    Add solution and test-cases for problem 454

--- leetcode/401-500/0454.4Sum-II/Solution.go
+++ leetcode/401-500/0454.4Sum-II/Solution.go
@@ -1,5 +1,21 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
+	l := len(nums1)
+	cache := make(map[int]int)
+	for i := 0; i < l; i++ {
+		for j := 0; j < l; j++ {
+			cache[nums1[i]+nums2[j]]++
+		}
+	}
+	ans := 0
+	for i := 0; i < l; i++ {
+		for j := 0; j < l; j++ {
+			target := 0 - nums3[i] - nums4[j]
+			if v, ok := cache[target]; ok {
+				ans += v
+			}
+		}
+	}
+	return ans
 }
