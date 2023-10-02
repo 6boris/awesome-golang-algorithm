8bce205a84b4097ba1caf4b2d501116733f821b7
0xff-dev <stevenshuang521@gmail.com>
2023-10-02 09:41:22 +0800

    Add solution and test-cases for problem 2038

--- leetcode/2001-2100/2038.Remove-Colored-Pieces-if-Both-Neighbors-are-the-Same-Color/Solution.go
+++ leetcode/2001-2100/2038.Remove-Colored-Pieces-if-Both-Neighbors-are-the-Same-Color/Solution.go
@@ -1,5 +1,34 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(colors string) bool {
+	a, b := 0, 0
+	x := 0
+	for _, b := range []byte(colors) {
+		if b == 'B' {
+			if x >= 3 {
+				a += x - 2
+			}
+			x = 0
+			continue
+		}
+		x++
+	}
+	if x >= 3 {
+		a += x - 2
+	}
+	x = 0
+	for _, bb := range []byte(colors) {
+		if bb == 'A' {
+			if x >= 3 {
+				b += x - 2
+			}
+			x = 0
+			continue
+		}
+		x++
+	}
+	if x >= 3 {
+		b += x - 2
+	}
+	return a > b
 }
