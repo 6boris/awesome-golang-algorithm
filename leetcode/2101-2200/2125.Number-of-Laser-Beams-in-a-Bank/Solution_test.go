34954b98e0643e0ae1e686bf7162d2c1a5d9f949
0xff-dev <stevenshuang521@gmail.com>
2023-10-03 20:17:19 +0800

    Add solution and test-cases for problem 2125

--- leetcode/2101-2200/2125.Number-of-Laser-Beams-in-a-Bank/Solution_test.go
+++ leetcode/2101-2200/2125.Number-of-Laser-Beams-in-a-Bank/Solution_test.go
@@ -10,12 +10,15 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []string
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []string{
+			"011001", "000000", "010100", "001000",
+		}, 8},
+		{"TestCase2", []string{
+			"000", "111", "000",
+		}, 0},
 	}
 
 	//	开始测试
@@ -30,10 +33,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
