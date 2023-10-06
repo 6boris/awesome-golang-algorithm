0bf295471a645a64dd96dde6cfcde967d37137c9
0xff-dev <stevenshuang521@gmail.com>
2023-10-06 12:11:54 +0800

    Add solution and test-cases for problem 2120

--- leetcode/2101-2200/2120.Execution-of-All-Suffix-Instructions-Staying-in-a-Grid/Solution.go
+++ leetcode/2101-2200/2120.Execution-of-All-Suffix-Instructions-Staying-in-a-Grid/Solution.go
@@ -1,5 +1,33 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int, startPos []int, s string) []int {
+	ans := make([]int, len(s))
+	var steps func(int, int, int) int
+
+	steps = func(x, y, index int) int {
+		ans := 0
+		for i := index; i < len(s); i++ {
+			a, b := 0, 0
+			if s[i] == 'L' {
+				a, b = 0, -1
+			} else if s[i] == 'R' {
+				a, b = 0, 1
+			} else if s[i] == 'U' {
+				a, b = -1, 0
+			} else {
+				a, b = 1, 0
+			}
+			nx, ny := x+a, y+b
+			if nx < 0 || nx >= n || ny < 0 || ny >= n {
+				return ans
+			}
+			x, y = nx, ny
+			ans++
+		}
+		return ans
+	}
+	for i := 0; i < len(s); i++ {
+		ans[i] = steps(startPos[0], startPos[1], i)
+	}
+	return ans
 }
--- leetcode/2101-2200/2120.Execution-of-All-Suffix-Instructions-Staying-in-a-Grid/Solution_test.go
+++ leetcode/2101-2200/2120.Execution-of-All-Suffix-Instructions-Staying-in-a-Grid/Solution_test.go
@@ -10,30 +10,32 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		n      int
+		pos    []int
+		s      string
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 3, []int{0, 1}, "RRDDLU", []int{1, 5, 4, 3, 1, 0}},
+		{"TestCase2", 2, []int{1, 1}, "LURD", []int{4, 1, 0, 0}},
+		{"TestCase3", 1, []int{0, 0}, "LRUD", []int{0, 0, 0, 0}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.pos, c.s)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.n, c.pos, c.s)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
