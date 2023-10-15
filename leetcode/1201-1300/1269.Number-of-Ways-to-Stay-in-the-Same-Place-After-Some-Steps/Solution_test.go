c40186b0116e09d7995fca0d3de98e582ad44682
0xff-dev <stevenshuang521@gmail.com>
2023-10-15 13:52:24 +0800

    Add solution and test-cases for problem 1269

--- leetcode/1201-1300/1269.Number-of-Ways-to-Stay-in-the-Same-Place-After-Some-Steps/Solution_test.go
+++ leetcode/1201-1300/1269.Number-of-Ways-to-Stay-in-the-Same-Place-After-Some-Steps/Solution_test.go
@@ -9,31 +9,32 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name          string
+		steps, arrLen int
+		expect        int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 3, 2, 4},
+		{"TestCase2", 2, 4, 2},
+		{"TestCase3", 4, 2, 8},
+		{"TestCase4", 430, 148488, 525833932},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.steps, c.arrLen)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.steps, c.arrLen)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
