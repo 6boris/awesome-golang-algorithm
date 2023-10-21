420782bd9bf4e97f424738a669ab274e3c3d3df4
0xff-dev <stevenshuang521@gmail.com>
2023-10-21 15:55:02 +0800

    Add solution and test-cases for problem 1343

--- leetcode/1301-1400/1343.Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold/Solution.go
+++ leetcode/1301-1400/1343.Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold/Solution.go
@@ -1,5 +1,23 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(arr []int, k int, threshold int) int {
+	ans := 0
+	sum := 0
+	for i := 0; i < k; i++ {
+		sum += arr[i]
+	}
+	if sum/k >= threshold {
+		ans++
+	}
+	start := 0
+	// 1, 2, 3
+	for i := k; i < len(arr); i++ {
+		sum -= arr[start]
+		sum += arr[i]
+		if sum/k >= threshold {
+			ans++
+		}
+		start++
+	}
+	return ans
 }
