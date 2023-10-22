ab1cefa871fee0f0f5dce6a7d7e7b9c66dcae87c
0xff-dev <stevenshuang521@gmail.com>
2023-10-22 15:02:36 +0800

    Add solution and test-cases for problem 1793

--- leetcode/1701-1800/1793.Maximum-Score-of-a-Good-Subarray/Solution.go
+++ leetcode/1701-1800/1793.Maximum-Score-of-a-Good-Subarray/Solution.go
@@ -1,5 +1,42 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, k int) int {
+	l := len(nums)
+	minArr := make([]int, l)
+	minArr[k] = nums[k]
+	for i := k - 1; i >= 0; i-- {
+		if nums[i] > minArr[i+1] {
+			minArr[i] = minArr[i+1]
+		} else {
+			minArr[i] = nums[i]
+		}
+	}
+	for j := k + 1; j < l; j++ {
+		if nums[j] > minArr[j-1] {
+			minArr[j] = minArr[j-1]
+		} else {
+			minArr[j] = nums[j]
+		}
+	}
+
+	ans := -1
+	for i := 0; i <= k; i++ {
+		if i > 0 && minArr[i] == minArr[i-1] {
+			continue
+		}
+		for j := l - 1; j >= k; j-- {
+			if j < k-1 && minArr[j] == minArr[j+1] {
+				continue
+			}
+			a := minArr[i]
+			b := minArr[j]
+			if b < a {
+				a = b
+			}
+			if r := a * (j - i + 1); ans == -1 || r > ans {
+				ans = r
+			}
+		}
+	}
+	return ans
 }
