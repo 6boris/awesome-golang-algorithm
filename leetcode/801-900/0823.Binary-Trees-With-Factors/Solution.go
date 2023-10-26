89ee25617b3410210e79a5078367a41e8c483a12
0xff-dev <stevenshuang521@gmail.com>
2023-10-26 22:19:36 +0800

    Add solution and test-cases for problem 823

--- leetcode/801-900/0823.Binary-Trees-With-Factors/Solution.go
+++ leetcode/801-900/0823.Binary-Trees-With-Factors/Solution.go
@@ -1,5 +1,31 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"sort"
+)
+
+const mod823 = 1000000007
+
+func Solution(arr []int) int {
+	ans := 0
+	set := make(map[uint64]int)
+	for _, n := range arr {
+		set[uint64(n)] = 1
+	}
+	sort.Ints(arr)
+	for i := 1; i < len(arr); i++ {
+		for j := i - 1; j >= 0; j-- {
+			if arr[i]%arr[j] == 0 {
+				tmp := uint64(arr[i]) / uint64(arr[j])
+				if count, ok := set[tmp]; ok {
+					a := set[uint64(arr[j])]
+					set[uint64(arr[i])] += (a * count) % mod823
+				}
+			}
+		}
+	}
+	for _, c := range set {
+		ans = (ans + c) % mod823
+	}
+	return ans
 }
