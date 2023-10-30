091cc24625607be176a2f7a9bc07c5aea82e3086
0xff-dev <stevenshuang521@gmail.com>
2023-10-30 21:10:46 +0800

    Add solution and test-cases for problem 1356

--- leetcode/1301-1400/1356.Sort-Integers-by-The-Number-of-1-Bits/Solution.go
+++ leetcode/1301-1400/1356.Sort-Integers-by-The-Number-of-1-Bits/Solution.go
@@ -1,5 +1,23 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func bitOfOne(n int) int {
+	ans := 0
+	for n > 0 {
+		ans++
+		n = n & (n - 1)
+	}
+	return ans
+}
+func Solution(arr []int) []int {
+	sort.Slice(arr, func(i, j int) bool {
+		a := bitOfOne(arr[i])
+		b := bitOfOne(arr[j])
+		if a == b {
+			return arr[i] < arr[j]
+		}
+		return a < b
+	})
+	return arr
 }
