8a0d64d6585da9053f32179569ce64889b5a3bd7
0xff-dev <stevenshuang521@gmail.com>
2023-11-01 20:54:53 +0800

    Add solution and test-cases for problem 501

--- leetcode/501-600/0501.Find-Mode-in-Binary-Search-Tree/Solution_test.go
+++ leetcode/501-600/0501.Find-Mode-in-Binary-Search-Tree/Solution_test.go
@@ -10,12 +10,16 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs *TreeNode
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", &TreeNode{
+			Val:   1,
+			Right: &TreeNode{Val: 2},
+		}, []int{1, 2}},
+		{"TestCase2", &TreeNode{
+			Val: 1,
+		}, []int{1}},
 	}
 
 	//	开始测试
@@ -30,10 +34,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
