ddbcdb3f873fee06514676568ffd520d0da8223a
0xff-dev <stevenshuang521@gmail.com>
2023-11-05 23:09:05 +0800

    Add solution and test-cases for problem 503

--- leetcode/501-600/0503.Next-Greater-Element-II/Solution.go
+++ leetcode/501-600/0503.Next-Greater-Element-II/Solution.go
@@ -1,5 +1,47 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) []int {
+	l := len(nums)
+	if l == 1 {
+		return []int{-1}
+	}
+
+	ans := make([]int, l)
+
+	preStack := make([][]int, l-1)
+	preStack[0] = []int{nums[0]}
+	for i := 1; i < l-1; i++ {
+		if nums[i] > nums[i-1] {
+			preStack[i] = append(preStack[i-1], nums[i])
+		} else {
+			preStack[i] = preStack[i-1]
+		}
+	}
+	stackIndex := l - 1
+
+	stack := make([]int, len(nums))
+	copy(stack, nums)
+	for idx := l - 1; idx >= 0; idx-- {
+		for stackIndex < l && stack[stackIndex] <= nums[idx] {
+			stackIndex++
+		}
+		if stackIndex == l {
+			// empty
+			set := -1
+			if idx != 0 {
+				for _, n := range preStack[idx-1] {
+					if n > nums[idx] {
+						set = n
+						break
+					}
+				}
+			}
+			ans[idx] = set
+		} else {
+			ans[idx] = stack[stackIndex]
+		}
+		stackIndex--
+		stack[stackIndex] = nums[idx]
+	}
+	return ans
 }
