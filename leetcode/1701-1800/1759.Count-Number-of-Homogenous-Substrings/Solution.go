6921e112fe5ba5e0f349cc2c4d7c945cc5e5d2aa
0xff-dev <stevenshuang521@gmail.com>
2023-11-09 20:53:04 +0800

    Add solution and test-cases for problem 1759

--- leetcode/1701-1800/1759.Count-Number-of-Homogenous-Substrings/Solution.go
+++ leetcode/1701-1800/1759.Count-Number-of-Homogenous-Substrings/Solution.go
@@ -1,5 +1,18 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+const mod1795 = 1000000007
+
+func Solution(s string) int {
+	ans := 0
+	count := 1
+	for idx := 1; idx < len(s); idx++ {
+		ans = (ans + count) % mod1795
+		if s[idx] == s[idx-1] {
+			count++
+		} else {
+			count = 1
+		}
+	}
+	ans = (ans + count) % mod1795
+	return ans
 }
