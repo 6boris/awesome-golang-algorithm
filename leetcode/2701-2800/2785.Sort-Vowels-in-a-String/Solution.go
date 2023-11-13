089e5965791fd5552a294c3fb419fb791bd7fa9d
0xff-dev <stevenshuang521@gmail.com>
2023-11-13 20:46:36 +0800

    Add solution and test-cases for problem 2785

--- leetcode/2701-2800/2785.Sort-Vowels-in-a-String/Solution.go
+++ leetcode/2701-2800/2785.Sort-Vowels-in-a-String/Solution.go
@@ -1,5 +1,30 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func isVowel2785(b byte) bool {
+	return b == 'a' || b == 'e' || b == 'i' || b == 'o' || b == 'u' ||
+		b == 'A' || b == 'E' || b == 'I' || b == 'O' || b == 'U'
+}
+
+func Solution(s string) string {
+	vowels := make([]byte, 0)
+	indies := make([]int, 0)
+	t := make([]byte, len(s))
+	for idx, b := range []byte(s) {
+		if isVowel2785(b) {
+			vowels = append(vowels, b)
+			indies = append(indies, idx)
+			continue
+		}
+		t[idx] = b
+	}
+	sort.Slice(vowels, func(i, j int) bool {
+		return vowels[i] < vowels[j]
+	})
+	for idx, targetIdx := range indies {
+		t[targetIdx] = vowels[idx]
+	}
+
+	return string(t)
 }
--- leetcode/2701-2800/2785.Sort-Vowels-in-a-String/Solution_test.go
+++ leetcode/2701-2800/2785.Sort-Vowels-in-a-String/Solution_test.go
@@ -10,12 +10,11 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs string
+		expect string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "lEetcOde", "lEOtcede"},
+		{"TestCase2", "lYmpH", "lYmpH"},
 	}
 
 	//	开始测试
@@ -30,10 +29,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
