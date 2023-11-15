20760b4b0b3920612b907c1000ea687ebf6e2b89
0xff-dev <stevenshuang521@gmail.com>
2023-11-15 20:47:38 +0800

    Add solution and test-cases for problem 1846

--- leetcode/1801-1900/1846.Maximum-Element-After-Decreasing-and-Rearranging/Solution.go
+++ leetcode/1801-1900/1846.Maximum-Element-After-Decreasing-and-Rearranging/Solution.go
@@ -1,5 +1,16 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(arr []int) int {
+	sort.Ints(arr)
+	arr[0] = 1
+	for idx := 1; idx < len(arr); idx++ {
+		diff := arr[idx] - arr[idx-1]
+		if diff > 1 {
+			arr[idx] = arr[idx-1] + 1
+			continue
+		}
+	}
+	return arr[len(arr)-1]
 }
