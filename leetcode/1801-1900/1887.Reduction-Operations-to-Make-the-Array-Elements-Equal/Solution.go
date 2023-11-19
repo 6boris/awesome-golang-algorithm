8be1f5c3be9a5de6d2a9cd88591997b8eac6a190
0xff-dev <stevenshuang521@gmail.com>
2023-11-19 12:30:50 +0800

    Add solution and test-cases for problem 1887

--- leetcode/1801-1900/1887.Reduction-Operations-to-Make-the-Array-Elements-Equal/Solution.go
+++ leetcode/1801-1900/1887.Reduction-Operations-to-Make-the-Array-Elements-Equal/Solution.go
@@ -1,5 +1,23 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(nums []int) int {
+	count := make(map[int]int)
+	distinct := make([]int, 0)
+	for _, n := range nums {
+		count[n]++
+		if count[n] == 1 {
+			distinct = append(distinct, n)
+		}
+	}
+
+	sort.Ints(distinct)
+	ans, sum := 0, 0
+	for i := len(distinct) - 1; i > 0; i-- {
+		sum += count[distinct[i]]
+		ans += sum
+	}
+
+	return ans
 }
