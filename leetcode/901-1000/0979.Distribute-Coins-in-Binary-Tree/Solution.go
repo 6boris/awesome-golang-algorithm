fd217635c7c41f9260ca3dfe4b18be86c499058d
0xff-dev <stevenshuang521@gmail.com>
2023-12-09 19:24:08 +0800

    Add solution and test-cases for problem 979

--- leetcode/901-1000/0979.Distribute-Coins-in-Binary-Tree/Solution.go
+++ leetcode/901-1000/0979.Distribute-Coins-in-Binary-Tree/Solution.go
@@ -1,5 +1,31 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func Solution(root *TreeNode) int {
+	steps := 0
+	var dfs func(*TreeNode) int
+	dfs = func(tree *TreeNode) int {
+		if tree == nil {
+			return 0
+		}
+		left := dfs(tree.Left)
+		right := dfs(tree.Right)
+		next := tree.Val + left + right - 1
+
+		if left < 0 {
+			left = -left
+		}
+		steps += left
+		if right < 0 {
+			right = -right
+		}
+		steps += right
+		return next
+	}
+	dfs(root)
+	return steps
 }
