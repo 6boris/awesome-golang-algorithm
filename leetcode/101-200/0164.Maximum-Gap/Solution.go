81696aeef2e48a2d3da19ad4506b58ac6910c7ec
0xff-dev <stevenshuang521@gmail.com>
2023-12-12 22:58:39 +0800

    Add solution and test-cases for problem 164

--- leetcode/101-200/0164.Maximum-Gap/Solution.go
+++ leetcode/101-200/0164.Maximum-Gap/Solution.go
@@ -1,5 +1,49 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"math"
+)
+
+func radixSort(nums []int) {
+	maximum := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > maximum {
+			maximum = nums[i]
+		}
+	}
+	digits := 0
+	r := maximum
+	for r > 0 {
+		digits++
+		r /= 10
+	}
+	for i := 1; i <= digits; i++ {
+		bucket := [10][]int{}
+		p := math.Pow(10, float64(i-1))
+		for _, n := range nums {
+			bucketIndex := (n / int(p)) % 10
+			bucket[bucketIndex] = append(bucket[bucketIndex], n)
+		}
+		idx := 0
+		for j := 0; j < 10; j++ {
+			for k := 0; k < len(bucket[j]); k++ {
+				nums[idx] = bucket[j][k]
+				idx++
+			}
+		}
+	}
+}
+func Solution(nums []int) int {
+	l := len(nums)
+	if l <= 1 {
+		return 0
+	}
+	radixSort(nums)
+	ans := 0
+	for i := 1; i < l; i++ {
+		if r := nums[i] - nums[i-1]; r > ans {
+			ans = r
+		}
+	}
+	return ans
 }
