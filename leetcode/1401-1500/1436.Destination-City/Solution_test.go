51fd724f7cda17057534608813f142238caa854b
0xff-dev <stevenshuang521@gmail.com>
2023-12-15 09:34:20 +0800

    Add solution and test-cases for problem 1436

--- leetcode/1401-1500/1436.Destination-City/Solution_test.go
+++ leetcode/1401-1500/1436.Destination-City/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs [][]string
+		expect string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]string{{"London", "New York"}, {"New York", "Lima"}, {"Lima", "Sao Paulo"}}, "Sao Paulo"},
+		{"TestCase2", [][]string{{"B", "C"}, {"D", "B"}, {"C", "A"}}, "A"},
+		{"TestCase3", [][]string{{"A", "Z"}}, "Z"},
 	}
 
 	//	开始测试
@@ -30,10 +30,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
