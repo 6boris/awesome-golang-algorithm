ad91c6d3a5236b9f863506b24b7a31cdc924316c
0xff-dev <stevenshuang521@gmail.com>
2023-12-20 21:17:58 +0800

    Add solution and test-cases for problem 2706

--- leetcode/2701-2800/2706.Buy-Two-Chocolates/Solution.go
+++ leetcode/2701-2800/2706.Buy-Two-Chocolates/Solution.go
@@ -1,5 +1,12 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(prices []int, money int) int {
+	sort.Ints(prices)
+	diff := money - prices[0] - prices[1]
+	if diff < 0 {
+		return money
+	}
+	return diff
 }
