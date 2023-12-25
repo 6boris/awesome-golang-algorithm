d60103bebbf66f211ceb8a1e5655103f2209543c
0xff-dev <stevenshuang521@gmail.com>
2023-12-25 21:22:50 +0800

    Add solution and test-cases for problem 2074

--- leetcode/2001-2100/2074.Reverse-Nodes-in-Even-Length-Groups/Solution_test.go
+++ leetcode/2001-2100/2074.Reverse-Nodes-in-Even-Length-Groups/Solution_test.go
@@ -10,12 +10,66 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs *ListNode
+		expect *ListNode
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", &ListNode{
+			Val: 5,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 6,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val: 1,
+								Next: &ListNode{
+									Val: 7,
+									Next: &ListNode{
+										Val: 3,
+										Next: &ListNode{
+											Val:  8,
+											Next: &ListNode{Val: 4},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}, &ListNode{
+			Val: 5,
+			Next: &ListNode{
+				Val: 6,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val: 1,
+								Next: &ListNode{
+									Val: 4,
+									Next: &ListNode{
+										Val: 8,
+										Next: &ListNode{
+											Val:  3,
+											Next: &ListNode{Val: 7},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}},
+		{"TestCase2", &ListNode{Val: 1, Next: &ListNode{Val: 1, Next: &ListNode{Val: 0, Next: &ListNode{Val: 6}}}}, &ListNode{Val: 1, Next: &ListNode{Val: 0, Next: &ListNode{Val: 1, Next: &ListNode{Val: 6}}}}},
+		{"TestCase3", &ListNode{Val: 1, Next: &ListNode{Val: 1, Next: &ListNode{Val: 0, Next: &ListNode{Val: 6, Next: &ListNode{Val: 5}}}}}, &ListNode{Val: 1, Next: &ListNode{Val: 0, Next: &ListNode{Val: 1, Next: &ListNode{Val: 5, Next: &ListNode{Val: 6}}}}}},
 	}
 
 	//	开始测试
@@ -30,10 +84,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
