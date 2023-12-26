7947cc81bc8f3dc5fdc6f9c951b69285907c7110
0xff-dev <stevenshuang521@gmail.com>
2023-12-26 21:08:58 +0800

    Add solution and test-cases for problem 1155

--- leetcode/1101-1200/1155.Number-of-Dice-Rolls-With-Target-Sum/Solution_test.go
+++ leetcode/1101-1200/1155.Number-of-Dice-Rolls-With-Target-Sum/Solution_test.go
@@ -9,31 +9,31 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name         string
+		n, k, target int
+		expect       int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 1, 6, 3, 1},
+		{"TestCase2", 2, 6, 7, 6},
+		{"TestCase3", 30, 30, 500, 222616187},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.k, c.target)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.n, c.k, c.target)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
