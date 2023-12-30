c11f215633bacf1b7cb638b3cdef0529aaa7755e
0xff-dev <stevenshuang521@gmail.com>
2023-12-30 18:26:34 +0800

    Add solution and test-cases for problem 2350

--- leetcode/2301-2400/2350.Shortest-Impossible-Sequence-of-Rolls/Solution.go
+++ leetcode/2301-2400/2350.Shortest-Impossible-Sequence-of-Rolls/Solution.go
@@ -1,5 +1,15 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(rolls []int, k int) int {
+	// 4, 2, 1, 2, 3, 3, 2, 4, 1
+	ans := 1
+	cache := make(map[int]struct{})
+	for i := 0; i < len(rolls); i++ {
+		cache[rolls[i]] = struct{}{}
+		if len(cache) == k {
+			ans++
+			cache = map[int]struct{}{}
+		}
+	}
+	return ans
 }
