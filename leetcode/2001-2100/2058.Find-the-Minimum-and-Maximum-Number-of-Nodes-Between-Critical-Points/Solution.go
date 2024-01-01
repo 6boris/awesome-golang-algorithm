949a59bf69db8f8d2b36363d0ee00e38e43d3144
0xff-dev <stevenshuang521@gmail.com>
2024-01-01 16:17:00 +0800

    Add solution and test-cases for problem 2058

--- leetcode/2001-2100/2058.Find-the-Minimum-and-Maximum-Number-of-Nodes-Between-Critical-Points/Solution.go
+++ leetcode/2001-2100/2058.Find-the-Minimum-and-Maximum-Number-of-Nodes-Between-Critical-Points/Solution.go
@@ -1,5 +1,36 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func Solution(head *ListNode) []int {
+	result := []int{-1, -1}
+	if head == nil || head.Next == nil || head.Next.Next == nil {
+		return result
+	}
+	start, preStart := -1, -1
+	end, index := -1, 1
+	pre, cur := head, head.Next
+
+	for ; cur.Next != nil; pre, cur, index = cur, cur.Next, index+1 {
+		a := pre.Val
+		b := cur.Next.Val
+		if !(cur.Val > a && cur.Val > b || cur.Val < a && cur.Val < b) {
+			continue
+		}
+		end = index
+		if start == -1 {
+			start = index
+		} else if result[0] == -1 || index-preStart < result[0] {
+			result[0] = index - preStart
+		}
+
+		preStart = index
+	}
+	if end != start {
+		result[1] = end - start
+	}
+	return result
 }
