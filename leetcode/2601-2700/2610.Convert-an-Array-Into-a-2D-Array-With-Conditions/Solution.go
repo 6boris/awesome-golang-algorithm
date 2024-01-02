fc94548437ed6323409b6b5ed7de810db329529a
0xff-dev <stevenshuang521@gmail.com>
2024-01-02 09:30:57 +0800

    Add solution and test-cases for problem 2610

--- leetcode/2601-2700/2610.Convert-an-Array-Into-a-2D-Array-With-Conditions/Solution.go
+++ leetcode/2601-2700/2610.Convert-an-Array-Into-a-2D-Array-With-Conditions/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) [][]int {
+	ans := make([][]int, 0)
+	l := len(nums)
+	a := make([]int, l+1)
+	loop := 0
+	for _, n := range nums {
+		a[n]++
+		if a[n] > loop {
+			loop = a[n]
+		}
+	}
+	for index := 0; loop > 0; index, loop = index+1, loop-1 {
+		ans = append(ans, []int{})
+		for i := 1; i <= l; i++ {
+			if a[i] > 0 {
+				ans[index] = append(ans[index], i)
+				a[i]--
+			}
+		}
+	}
+	return ans
 }
