5b7a8990b85615b1335d417f0ee0b4e1d0d6f032
0xff-dev <stevenshuang521@gmail.com>
2024-01-07 11:27:04 +0800

    Add solution and test-cases for problem 677

--- leetcode/601-700/0677.Map-Sum-Pairs/Solution_test.go
+++ leetcode/601-700/0677.Map-Sum-Pairs/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []opt
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []opt{{"i", "apple", 3}, {"", "ap", 0}, {"i", "app", 2}, {"", "ap", 0}}, []int{3, 5}},
+		{"TestCase2", []opt{{"i", "apple", 3}, {"", "ap", 0}, {"i", "app", 2}, {"i", "apple", 2}, {"", "ap", 0}}, []int{3, 4}},
+		{"TestCase3", []opt{{"i", "apple", 3}, {"", "app", 0}, {"i", "app", 2}, {"", "ap", 0}, {"i", "apple", 5}, {"i", "apple", 1}, {"", "apple", 0}}, []int{3, 5, 1}},
 	}
 
 	//	开始测试
@@ -30,10 +30,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
