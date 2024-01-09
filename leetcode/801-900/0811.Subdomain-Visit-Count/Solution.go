2772e9c2f794d8d4982e29066500d1f592a0a4a2
0xff-dev <stevenshuang521@gmail.com>
2024-01-09 22:46:06 +0800

    Add solution and test-cases for problem 811

--- leetcode/801-900/0811.Subdomain-Visit-Count/Solution.go
+++ leetcode/801-900/0811.Subdomain-Visit-Count/Solution.go
@@ -1,5 +1,26 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "fmt"
+
+func Solution(cpdomains []string) []string {
+	domains := make(map[string]int)
+	var (
+		rep    int
+		domain string
+	)
+	for _, cnt := range cpdomains {
+		fmt.Sscanf(cnt, "%d %s", &rep, &domain)
+		domains[domain] += rep
+		for idx := len(domain) - 1; idx >= 0; idx-- {
+			if domain[idx] == '.' {
+				x := domain[idx+1:]
+				domains[x] += rep
+			}
+		}
+	}
+	ans := make([]string, 0)
+	for domain, cnt := range domains {
+		ans = append(ans, fmt.Sprintf("%d %s", cnt, domain))
+	}
+	return ans
 }
