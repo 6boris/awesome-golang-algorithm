15d6c86c829f507740c2bb6a303de9591cfbe7d5
0xff-dev <stevenshuang521@gmail.com>
2024-01-13 09:43:12 +0800

    Add solution and test-cases for problem 1347

--- leetcode/1301-1400/1347.Minimum-Number-of-Steps-to-Make-Two-Strings-Anagram/Solution.go
+++ leetcode/1301-1400/1347.Minimum-Number-of-Steps-to-Make-Two-Strings-Anagram/Solution.go
@@ -1,5 +1,28 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string, t string) int {
+	as := [26]int{}
+	ts := [26]int{}
+	if s == t {
+		return 0
+	}
+
+	for _, b := range s {
+		as[b-'a']++
+	}
+	for _, b := range t {
+		ts[b-'a']++
+	}
+
+	more := 0
+	for i := 0; i < 26; i++ {
+		diff := ts[i] - as[i]
+		if diff == 0 {
+			continue
+		}
+		if diff > 0 {
+			more += diff
+		}
+	}
+	return more
 }
