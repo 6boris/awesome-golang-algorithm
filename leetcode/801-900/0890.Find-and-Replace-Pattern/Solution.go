842f400124fb3e5cd78c56e14d4577173a4dbde5
0xff-dev <stevenshuang521@gmail.com>
2024-01-23 20:51:14 +0800

    Add solution and test-cases for problem 890

--- leetcode/801-900/0890.Find-and-Replace-Pattern/Solution.go
+++ leetcode/801-900/0890.Find-and-Replace-Pattern/Solution.go
@@ -1,5 +1,39 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func uniquePattern(str string) []int {
+	cache := [26]int{}
+	i := 1
+	cache[str[0]-'a'] = 1
+	res := []int{1}
+	for idx := 1; idx < len(str); idx++ {
+		if r := cache[str[idx]-'a']; r != 0 {
+			res = append(res, r)
+			continue
+		}
+		i++
+		res = append(res, i)
+		cache[str[idx]-'a'] = i
+	}
+	return res
+}
+
+func Solution(words []string, pattern string) []string {
+	p := uniquePattern(pattern)
+	res := make([]string, 0)
+	for _, word := range words {
+		p1 := uniquePattern(word)
+		if len(p) != len(p1) {
+			continue
+		}
+		i := 0
+		for ; i < len(p); i++ {
+			if p[i] != p1[i] {
+				break
+			}
+		}
+		if i == len(p) {
+			res = append(res, word)
+		}
+	}
+	return res
 }
