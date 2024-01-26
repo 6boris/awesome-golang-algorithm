904f4a75b92eb642c654dc6028360a81c48f3173
0xff-dev <stevenshuang521@gmail.com>
2024-01-26 22:13:25 +0800

    Add solution and test-cases for problem 2023

--- leetcode/2001-2100/2023.Number-of-Pairs-of-Strings-With-Concatenation-Equal-to-Target/Solution.go
+++ leetcode/2001-2100/2023.Number-of-Pairs-of-Strings-With-Concatenation-Equal-to-Target/Solution.go
@@ -1,5 +1,18 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []string, target string) int {
+	count := make(map[string]int)
+	for _, s := range nums {
+		count[s]++
+	}
+	ans := 0
+	for i := 1; i < len(target); i++ {
+		a, b := target[:i], target[i:]
+		if a != b {
+			ans += count[a] * count[b]
+			continue
+		}
+		ans += count[a] * (count[a] - 1)
+	}
+	return ans
 }
