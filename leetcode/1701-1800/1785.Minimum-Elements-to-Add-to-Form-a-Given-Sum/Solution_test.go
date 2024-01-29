2d14fa75a3c7091833249b9f6d392af7598d7c87
0xff-dev <stevenshuang521@gmail.com>
2024-01-29 20:57:28 +0800

    Add solution and test-cases for problem 1785

--- leetcode/1701-1800/1785.Minimum-Elements-to-Add-to-Form-a-Given-Sum/Solution_test.go
+++ leetcode/1701-1800/1785.Minimum-Elements-to-Add-to-Form-a-Given-Sum/Solution_test.go
@@ -9,31 +9,31 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name        string
+		nums        []int
+		limit, goal int
+		expect      int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, -1, 1}, 3, -4, 2},
+		{"TestCase2", []int{1, -10, 9, 1}, 100, 0, 1},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.nums, c.limit, c.goal)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.nums, c.limit, c.goal)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
