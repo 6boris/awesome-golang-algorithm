5d76d9eb0349f08ffb3b47806b1d1f97528fe2ac
0xff-dev <stevenshuang521@gmail.com>
2024-01-30 20:33:04 +0800

    Add solution and test-cases for problem 930

--- leetcode/901-1000/0930.Binary-Subarrays-With-Sum/Solution_test.go
+++ leetcode/901-1000/0930.Binary-Subarrays-With-Sum/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		nums   []int
+		goal   int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 0, 1, 0, 1}, 2, 4},
+		{"TestCase2", []int{0, 0, 0, 0, 0}, 0, 15},
+		{"TestCase3", []int{1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1}, 8, 20},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.nums, c.goal)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.nums, c.goal)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
