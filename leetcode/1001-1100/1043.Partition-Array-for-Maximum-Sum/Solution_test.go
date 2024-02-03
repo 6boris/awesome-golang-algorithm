96200cc8dfeff8eed0938c71bf7699f1513dbe0a
0xff-dev <stevenshuang521@gmail.com>
2024-02-03 09:19:02 +0800

    Add solution and test-cases for problem 1043

--- leetcode/1001-1100/1043.Partition-Array-for-Maximum-Sum/Solution_test.go
+++ leetcode/1001-1100/1043.Partition-Array-for-Maximum-Sum/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		arr    []int
+		k      int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 15, 7, 9, 2, 5, 10}, 3, 84},
+		{"TestCase2", []int{1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3}, 4, 83},
+		{"TestCase3", []int{1}, 1, 1},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.arr, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.arr, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
