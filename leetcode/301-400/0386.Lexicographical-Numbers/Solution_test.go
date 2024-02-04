849528c4aa5ce310e4ee11b57ba236a0b489f275
0xff-dev <stevenshuang521@gmail.com>
2024-02-04 17:19:35 +0800

    Add solution and test-cases for problem 386

--- leetcode/301-400/0386.Lexicographical-Numbers/Solution_test.go
+++ leetcode/301-400/0386.Lexicographical-Numbers/Solution_test.go
@@ -10,12 +10,11 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs int
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 13, []int{1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9}},
+		{"TestCase2", 2, []int{1, 2}},
 	}
 
 	//	开始测试
@@ -30,10 +29,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
