5bdb1a5ce3d26ec81bed5f04a93da2b00180c724
0xff-dev <stevenshuang521@gmail.com>
2024-02-05 14:51:10 +0800

    Add solution and test-cases for problem 1023

--- leetcode/1001-1100/1023.Camelcase-Matching/Solution_test.go
+++ leetcode/1001-1100/1023.Camelcase-Matching/Solution_test.go
@@ -9,31 +9,32 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name    string
+		queries []string
+		pattern string
+		expect  []bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []string{"FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"}, "FB", []bool{true, false, true, true, false}},
+		{"TestCase2", []string{"FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"}, "FoBa", []bool{true, false, true, false, false}},
+		{"TestCase3", []string{"FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"}, "FoBaT", []bool{false, true, false, false, false}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.queries, c.pattern)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.queries, c.pattern)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
