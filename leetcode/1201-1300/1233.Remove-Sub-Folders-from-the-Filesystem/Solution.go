69a277ac44876bfb68d0be660823ea7355c27692
0xff-dev <stevenshuang521@gmail.com>
2024-02-06 10:09:33 +0800

    Add solution and test-cases for problem 1233

--- leetcode/1201-1300/1233.Remove-Sub-Folders-from-the-Filesystem/Solution.go
+++ leetcode/1201-1300/1233.Remove-Sub-Folders-from-the-Filesystem/Solution.go
@@ -1,5 +1,47 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"sort"
+	"strings"
+)
+
+type filePathNode struct {
+	end  bool
+	data map[string]*filePathNode
+}
+
+func insertFolder(root *filePathNode, path string) bool {
+	walker := root
+	paths := strings.Split(path, "/")
+	for i := 1; i < len(paths); i++ {
+		p := paths[i]
+		_, ok := walker.data[p]
+		if !ok {
+			walker.data[p] = &filePathNode{end: false, data: make(map[string]*filePathNode)}
+		}
+		if i == len(paths)-1 {
+			walker.data[p].end = true
+			continue
+		}
+		if walker.data[p].end {
+			return false
+		}
+		walker = walker.data[p]
+	}
+	return true
+}
+
+func Solution(folder []string) []string {
+	sort.Slice(folder, func(i, j int) bool {
+		return len(folder[i]) < len(folder[j])
+	})
+
+	root := &filePathNode{end: false, data: make(map[string]*filePathNode)}
+	ans := make([]string, 0)
+	for _, str := range folder {
+		if insertFolder(root, str) {
+			ans = append(ans, str)
+		}
+	}
+	return ans
 }
--- leetcode/1201-1300/1233.Remove-Sub-Folders-from-the-Filesystem/Solution_test.go
+++ leetcode/1201-1300/1233.Remove-Sub-Folders-from-the-Filesystem/Solution_test.go
@@ -2,6 +2,7 @@ package Solution
 
 import (
 	"reflect"
+	"sort"
 	"strconv"
 	"testing"
 )
@@ -10,18 +11,19 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []string
+		expect []string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []string{"/a", "/a/b", "/c/d", "/c/d/e", "/c/f"}, []string{"/a", "/c/d", "/c/f"}},
+		{"TestCase2", []string{"/a", "/a/b/c", "/a/b/d"}, []string{"/a"}},
+		{"TestCase3", []string{"/a/b/c", "/a/b/ca", "/a/b/d"}, []string{"/a/b/c", "/a/b/ca", "/a/b/d"}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
 			got := Solution(c.inputs)
+			sort.Strings(got)
 			if !reflect.DeepEqual(got, c.expect) {
 				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
 					c.expect, got, c.inputs)
@@ -30,10 +32,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
