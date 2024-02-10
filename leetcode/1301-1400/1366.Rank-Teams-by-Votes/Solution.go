05c2fcb41af38dfa947658ac92fb99f39755a6df
0xff-dev <stevenshuang521@gmail.com>
2024-02-10 21:46:28 +0800

    Add solution and test-cases for problem  1366

--- leetcode/1301-1400/1366.Rank-Teams-by-Votes/Solution.go
+++ leetcode/1301-1400/1366.Rank-Teams-by-Votes/Solution.go
@@ -1,5 +1,34 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"sort"
+)
+
+func Solution(votes []string) string {
+	count := [26][]int{}
+	l := len(votes[0])
+	sorter := []byte(votes[0])
+	for i := 0; i < 26; i++ {
+		count[i] = make([]int, l)
+	}
+	for _, vote := range votes {
+		for i, c := range vote {
+			count[c-'A'][i]++
+		}
+	}
+
+	sort.Slice(sorter, func(i, j int) bool {
+		ci := sorter[i]
+		cj := sorter[j]
+		for k := 0; k < l; k++ {
+			if count[ci-'A'][k] < count[cj-'A'][k] {
+				return false
+			}
+			if count[ci-'A'][k] > count[cj-'A'][k] {
+				return true
+			}
+		}
+		return ci < cj
+	})
+	return string(sorter)
 }
