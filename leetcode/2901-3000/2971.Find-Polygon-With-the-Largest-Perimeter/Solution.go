e52473fd492cf5a180a4ace33d53996747337e7e
0xff-dev <stevenshuang521@gmail.com>
2024-02-15 08:53:17 +0800

    Add solution and test-cases for problem 2971

--- leetcode/2901-3000/2971.Find-Polygon-With-the-Largest-Perimeter/Solution.go
+++ leetcode/2901-3000/2971.Find-Polygon-With-the-Largest-Perimeter/Solution.go
@@ -1,5 +1,27 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(nums []int) int64 {
+
+	sort.Ints(nums)
+	sum := make([]int, len(nums))
+	sum[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		sum[i] = sum[i-1] + nums[i]
+	}
+
+	ans := int64(-1)
+	for end := len(nums) - 1; end >= 2; end-- {
+		idx := sort.Search(end, func(i int) bool {
+			return sum[i] > nums[end]
+		})
+		if idx == end {
+			continue
+		}
+		if r := int64(nums[end] + sum[end-1]); r > ans {
+			ans = r
+		}
+	}
+	return ans
 }
