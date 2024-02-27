db63157d5cf5fc1e50a9a591d81c7d03389a4131
0xff-dev <stevenshuang521@gmail.com>
2024-02-27 23:09:09 +0800

    Add solution and test-cases for problem 637

--- leetcode/601-700/0637.Average-of-Levels-in-Binary-Tree/Solution.go
+++ leetcode/601-700/0637.Average-of-Levels-in-Binary-Tree/Solution.go
@@ -1,5 +1,27 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func Solution(root *TreeNode) []float64 {
+	queue := []*TreeNode{root}
+	ans := []float64{}
+	for len(queue) > 0 {
+		nq := []*TreeNode{}
+		sum := 0
+		for _, n := range queue {
+			sum += n.Val
+			if n.Left != nil {
+				nq = append(nq, n.Left)
+			}
+			if n.Right != nil {
+				nq = append(nq, n.Right)
+			}
+		}
+		ans = append(ans, float64(sum)/float64(len(queue)))
+		queue = nq
+	}
+	return ans
 }
