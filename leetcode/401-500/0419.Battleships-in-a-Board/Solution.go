80bf8060d74e53c23cac664ac681d7a46dd48e0e
0xff-dev <stevenshuang521@gmail.com>
2024-03-03 14:53:56 +0800

    Add solution and test-cases for problem 419

--- leetcode/401-500/0419.Battleships-in-a-Board/Solution.go
+++ leetcode/401-500/0419.Battleships-in-a-Board/Solution.go
@@ -1,5 +1,20 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(board [][]byte) int {
+	rows, cols := len(board), len(board[0])
+	ans := 0
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if board[i][j] == 'X' {
+				if i > 0 && board[i-1][j] == 'X' {
+					continue
+				}
+				if j > 0 && board[i][j-1] == 'X' {
+					continue
+				}
+				ans++
+			}
+		}
+	}
+	return ans
 }
