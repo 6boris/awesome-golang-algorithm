0477350a292331f5274ccd9b3615d54cb54c3a67
0xff-dev <stevenshuang521@gmail.com>
2024-03-03 17:09:39 +0800

    Add solution and test-cases for problem 693

--- leetcode/601-700/0693.Binary-Number-with-Alternating-Bits/Solution.go
+++ leetcode/601-700/0693.Binary-Number-with-Alternating-Bits/Solution.go
@@ -1,5 +1,15 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int) bool {
+	cur := -1
+	for n > 0 {
+		oneOrZero := n & 1
+		if cur == -1 || cur != oneOrZero {
+			cur = oneOrZero
+			n >>= 1
+			continue
+		}
+		return false
+	}
+	return true
 }
