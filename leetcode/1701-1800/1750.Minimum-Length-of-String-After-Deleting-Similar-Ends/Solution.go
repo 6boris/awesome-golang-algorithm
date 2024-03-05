4c47fce51326cb8c8d8fb90401fa6d36436b1743
0xff-dev <stevenshuang521@gmail.com>
2024-03-05 20:38:20 +0800

    Add solution and test-cases for problem 1750

--- leetcode/1701-1800/1750.Minimum-Length-of-String-After-Deleting-Similar-Ends/Solution.go
+++ leetcode/1701-1800/1750.Minimum-Length-of-String-After-Deleting-Similar-Ends/Solution.go
@@ -1,5 +1,19 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string) int {
+	ans := len(s)
+	l, r := 0, len(s)-1
+	lc, rc := s[0], s[r]
+	if lc != rc {
+		return ans
+	}
+	for l < r && lc == rc {
+		for ; l < r && s[l] == lc; l++ {
+		}
+		for ; r >= l && s[r] == rc; r-- {
+		}
+		ans = min(ans, r-l+1)
+		lc, rc = s[l], s[r]
+	}
+	return ans
 }
