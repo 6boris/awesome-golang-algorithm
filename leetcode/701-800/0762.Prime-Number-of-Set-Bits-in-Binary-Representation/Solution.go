893c2a11ac7db07aea59917f40b86629fe565bbe
0xff-dev <stevenshuang521@gmail.com>
2024-03-06 20:07:57 +0800

    Add solution and test-cases for problem 762

--- leetcode/701-800/0762.Prime-Number-of-Set-Bits-in-Binary-Representation/Solution.go
+++ leetcode/701-800/0762.Prime-Number-of-Set-Bits-in-Binary-Representation/Solution.go
@@ -1,5 +1,31 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func bitOf1(n int) int {
+	c := 0
+	for n > 0 {
+		c++
+		n = n & (n - 1)
+	}
+	return c
+}
+func Solution(left int, right int) int {
+	// 1, 2, 3,  4,  5,   6, 7,   8,   9,    10
+	// 1, 1  11 100  101 110 111 1000  1001  1010
+	notPrimes := make([]bool, 33)
+	notPrimes[1] = true
+	for i := 2; i < 33; i++ {
+		if notPrimes[i] {
+			continue
+		}
+		for next := i + i; next < 33; next += i {
+			notPrimes[next] = true
+		}
+	}
+	ans := 0
+	for i := left; i <= right; i++ {
+		if r := bitOf1(i); !notPrimes[r] {
+			ans++
+		}
+	}
+	return ans
 }
