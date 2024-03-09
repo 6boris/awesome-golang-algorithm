faf2f88beb7a527f5d578905f41217d9eb43f0b0
0xff-dev <stevenshuang521@gmail.com>
2024-03-09 10:00:58 +0800

    Add solution and test-cases for problem 2540

--- leetcode/2501-2600/2540.Minimum-Common-Value/Solution.go
+++ leetcode/2501-2600/2540.Minimum-Common-Value/Solution.go
@@ -1,5 +1,15 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(nums1 []int, nums2 []int) int {
+	l2 := len(nums2)
+	for _, n := range nums1 {
+		if idx := sort.Search(l2, func(i int) bool {
+			return nums2[i] >= n
+		}); idx != l2 && nums2[idx] == n {
+			return n
+		}
+	}
+	return -1
 }
