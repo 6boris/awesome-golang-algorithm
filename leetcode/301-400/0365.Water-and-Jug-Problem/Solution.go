b77c93d6374722efca44388d99f087e7172f6c80
0xff-dev <stevenshuang521@gmail.com>
2024-03-12 22:28:10 +0800

    Add solution and test-cases for problem 365

--- leetcode/301-400/0365.Water-and-Jug-Problem/Solution.go
+++ leetcode/301-400/0365.Water-and-Jug-Problem/Solution.go
@@ -1,5 +1,15 @@
 package Solution
 
-func Solution(x bool) bool {
+func gcd365(x, y int) int {
+	for y != 0 {
+		x, y = y, x%y
+	}
 	return x
+}
+func Solution(x int, y int, target int) bool {
+	if x+y < target {
+		return false
+	}
+	g := gcd365(x, y)
+	return target%g == 0
 }
