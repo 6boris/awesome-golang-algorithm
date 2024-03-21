57b3d36ecf355d909a1b585db84af25067ba4b31
0xff-dev <stevenshuang521@gmail.com>
2024-03-21 23:03:47 +0800

    Add solution and test-cases for problem 432

--- leetcode/401-500/0432.All-O-one-Data-Structure/README.md
+++ leetcode/401-500/0432.All-O-one-Data-Structure/README.md
@@ -1,28 +1,37 @@
 # [432.All O`one Data Structure][title]
 
-> [!WARNING|style:flat]
-> This question is temporarily unanswered if you have good ideas. Welcome to [Create Pull Request PR](https://github.com/kylesliu/awesome-golang-algorithm)
-
 ## Description
+Design a data structure to store the strings' count with the ability to return the strings with minimum and maximum counts.
+
+Implement the `AllOne` class:
+
+- `AllOne()` Initializes the object of the data structure.
+- `inc(String key)` Increments the count of the string `key` by `1`. If `key` does not exist in the data structure, insert it with count `1`.
+- `dec(String key)` Decrements the count of the string `key` by `1`. If the count of `key` is `0` after the decrement, remove it from the data structure. It is guaranteed that `key` exists in the data structure before the decrement.
+- `getMaxKey()` Returns one of the keys with the maximal count. If no element exists, return an empty string `""`.
+- `getMinKey()` Returns one of the keys with the minimum count. If no element exists, return an empty string `""`.
+
+**Note** that each function must run in `O(1)` average time complexity.
 
 **Example 1:**
 
 ```
-Input: a = "11", b = "1"
-Output: "100"
+Input
+["AllOne", "inc", "inc", "getMaxKey", "getMinKey", "inc", "getMaxKey", "getMinKey"]
+[[], ["hello"], ["hello"], [], [], ["leet"], [], []]
+Output
+[null, null, null, "hello", "hello", null, "hello", "leet"]
+
+Explanation
+AllOne allOne = new AllOne();
+allOne.inc("hello");
+allOne.inc("hello");
+allOne.getMaxKey(); // return "hello"
+allOne.getMinKey(); // return "hello"
+allOne.inc("leet");
+allOne.getMaxKey(); // return "hello"
+allOne.getMinKey(); // return "leet"
 ```
-
-## 题意
-> ...
-
-## 题解
-
-### 思路1
-> ...
-All O`one Data Structure
-```go
-```
-
 
 ## 结语
 
--- leetcode/401-500/0432.All-O-one-Data-Structure/Solution.go
+++ leetcode/401-500/0432.All-O-one-Data-Structure/Solution.go
@@ -1,5 +1,128 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"container/list"
+)
+
+type AllOne struct {
+	list  *list.List
+	data  map[string]int
+	index map[int]*list.Element
+}
+
+func Constructor432() AllOne {
+	a := AllOne{
+		data:  make(map[string]int),
+		index: make(map[int]*list.Element),
+	}
+
+	l := list.New()
+	a.index[0] = l.PushBack(map[string]struct{}{})
+	a.list = l
+	return a
+}
+
+func (this *AllOne) Inc(key string) {
+	source := this.data[key]
+
+	node := this.index[source]
+	next, ok := this.index[source+1]
+	if !ok {
+		next = this.list.InsertAfter(map[string]struct{}{}, node)
+		this.index[source+1] = next
+	}
+	v := next.Value.(map[string]struct{})
+	v[key] = struct{}{}
+	next.Value = v
+	if source > 0 {
+		v1 := node.Value.(map[string]struct{})
+		delete(v1, key)
+		if len(v1) == 0 {
+			this.list.Remove(node)
+			delete(this.index, source)
+		} else {
+			node.Value = v1
+		}
+	}
+	this.data[key] = source + 1
+}
+
+func (this *AllOne) Dec(key string) {
+	source := this.data[key]
+	if source == 0 {
+		return
+	}
+	this.data[key]--
+	if this.data[key] == 0 {
+		delete(this.data, key)
+	}
+	node := this.index[source]
+	if source > 1 {
+		pre, ok := this.index[source-1]
+		if !ok {
+			pre = this.list.InsertBefore(map[string]struct{}{}, node)
+			this.index[source-1] = pre
+		}
+		v1 := pre.Value.(map[string]struct{})
+		v1[key] = struct{}{}
+		pre.Value = v1
+	}
+	v := node.Value.(map[string]struct{})
+	delete(v, key)
+	if len(v) == 0 {
+		this.list.Remove(node)
+		delete(this.index, source)
+	} else {
+		node.Value = v
+	}
+
+}
+
+func (this *AllOne) GetMinKey() string {
+	if this.list.Len() == 1 {
+		return ""
+	}
+
+	v := this.list.Front().Next().Value.(map[string]struct{})
+	for k := range v {
+		return k
+	}
+	return ""
+}
+
+func (this *AllOne) GetMaxKey() string {
+	if this.list.Len() == 1 {
+		return ""
+	}
+	v := this.list.Back().Value.(map[string]struct{})
+	for k := range v {
+		return k
+	}
+	return ""
+}
+
+type opt struct {
+	name string
+	v    string
+}
+
+func Solution(opts []opt) []string {
+	c := Constructor432()
+	ans := make([]string, 0)
+	for _, o := range opts {
+		if o.name == "i" {
+			c.Inc(o.v)
+			continue
+		}
+		if o.name == "d" {
+			c.Dec(o.v)
+			continue
+		}
+		if o.name == "max" {
+			ans = append(ans, c.GetMaxKey())
+			continue
+		}
+		ans = append(ans, c.GetMinKey())
+	}
+	return ans
 }
