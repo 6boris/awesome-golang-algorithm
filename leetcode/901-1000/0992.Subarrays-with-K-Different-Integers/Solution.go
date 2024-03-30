9eb815bdd7ed72b6922e29bd58f83085fedea778
0xff-dev <stevenshuang521@gmail.com>
2024-03-30 11:53:56 +0800

    Add solution and test-cases for problem 992

--- leetcode/901-1000/0992.Subarrays-with-K-Different-Integers/Solution.go
+++ leetcode/901-1000/0992.Subarrays-with-K-Different-Integers/Solution.go
@@ -1,5 +1,30 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, k int) int {
+	ll := len(nums)
+	count := make([]int, ll+1)
+	ans := 0
+	l, r := 0, 0
+	cc := 0
+	for ; r < ll; r++ {
+		count[nums[r]]++
+		if count[nums[r]] == 1 {
+			k--
+		}
+		if k < 0 {
+			count[nums[l]]--
+			l++
+			k++
+			cc = 0
+		}
+		if k == 0 {
+			for count[nums[l]] > 1 {
+				count[nums[l]]--
+				l++
+				cc++
+			}
+			ans += (cc + 1)
+		}
+	}
+	return ans
 }
