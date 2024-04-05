0253ea205a24bd85b993acfb8de8259a9d4af86d
0xff-dev <stevenshuang521@gmail.com>
2024-04-05 11:12:17 +0800

    Add solution and test-cases for problem 1296

--- leetcode/1201-1300/1296.Divide-Array-in-Sets-of-K-Consecutive-Numbers/Solution_test.go
+++ leetcode/1201-1300/1296.Divide-Array-in-Sets-of-K-Consecutive-Numbers/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
+		nums   []int
+		k      int
 		expect bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 2, 3, 3, 4, 4, 5, 6}, 4, true},
+		{"TestCase2", []int{3, 2, 1, 2, 3, 4, 3, 4, 5, 9, 10, 11}, 3, true},
+		{"TestCase3", []int{1, 2, 3, 4}, 3, false},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.nums, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.nums, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
