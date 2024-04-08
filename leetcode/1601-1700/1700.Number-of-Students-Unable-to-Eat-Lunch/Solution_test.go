5d983f3c3916f46d07b66acb2270ec11a0a49ce8
0xff-dev <stevenshuang521@gmail.com>
2024-04-08 09:21:26 +0800

    Add solution and test-cases for problem 1700

--- leetcode/1601-1700/1700.Number-of-Students-Unable-to-Eat-Lunch/Solution_test.go
+++ leetcode/1601-1700/1700.Number-of-Students-Unable-to-Eat-Lunch/Solution_test.go
@@ -9,31 +9,30 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name                 string
+		students, sandwiches []int
+		expect               int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 1, 0, 0}, []int{0, 1, 0, 1}, 0},
+		{"TestCase2", []int{1, 1, 1, 0, 0, 1}, []int{1, 0, 0, 0, 1, 1}, 3},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.students, c.sandwiches)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.students, c.sandwiches)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
