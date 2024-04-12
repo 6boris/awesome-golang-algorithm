9301aa7a9290f9a5e683e2036970c877d761d67e
0xff-dev <stevenshuang521@gmail.com>
2024-04-12 11:13:13 +0800

    Add solution and test-cases for problem 2487

--- leetcode/2401-2500/2487.Remove-Nodes-From-Linked-List/Solution.go
+++ leetcode/2401-2500/2487.Remove-Nodes-From-Linked-List/Solution.go
@@ -1,5 +1,21 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func Solution(head *ListNode) *ListNode {
+	stack := []*ListNode{head}
+	w := head.Next
+	for ; w != nil; w = w.Next {
+		for len(stack) > 0 && stack[len(stack)-1].Val < w.Val {
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, w)
+	}
+	for i := 1; i < len(stack); i++ {
+		stack[i-1].Next = stack[i]
+	}
+	return stack[0]
 }
