c93521d38f210676163304e789c0415b4ffda2f3
0xff-dev <stevenshuang521@gmail.com>
2024-04-23 22:32:32 +0800

    Add solution and test-cases for problem 310

--- leetcode/301-400/0310.Minimum-Height-Trees/Solution.go
+++ leetcode/301-400/0310.Minimum-Height-Trees/Solution.go
@@ -1,5 +1,39 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int, edges [][]int) []int {
+	if n == 1 {
+		return []int{0}
+	}
+	adj := make([]map[int]struct{}, n)
+	for i := 0; i < n; i++ {
+		adj[i] = make(map[int]struct{})
+	}
+	for _, edge := range edges {
+		a, b := edge[0], edge[1]
+		adj[a][b] = struct{}{}
+		adj[b][a] = struct{}{}
+	}
+	l := []int{}
+	for i := 0; i < n; i++ {
+		if len(adj[i]) == 1 {
+			l = append(l, i)
+		}
+	}
+	r := n
+	for r > 2 {
+		r -= len(l)
+		nl := []int{}
+		for _, lf := range l {
+			var nei int
+			for n := range adj[lf] {
+				nei = n
+			}
+			delete(adj[nei], lf)
+			if len(adj[nei]) == 1 {
+				nl = append(nl, nei)
+			}
+		}
+		l = nl
+	}
+	return l
 }
--- leetcode/301-400/0310.Minimum-Height-Trees/Solution_test.go
+++ leetcode/301-400/0310.Minimum-Height-Trees/Solution_test.go
@@ -10,30 +10,34 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		n      int
+		edges  [][]int
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 4, [][]int{
+			{1, 0}, {1, 2}, {1, 3},
+		}, []int{1}},
+		{"TestCase2", 6, [][]int{
+			{3, 0}, {3, 1}, {3, 2}, {3, 4}, {5, 4},
+		}, []int{3, 4}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.edges)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.n, c.edges)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
