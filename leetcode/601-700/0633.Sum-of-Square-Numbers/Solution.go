9fe4fb338fa9d16bc79aeddb06df9b07bb411527
0xff-dev <stevenshuang521@gmail.com>
2024-04-24 21:47:02 +0800

    Add solution and test-cases for problem 633

--- leetcode/601-700/0633.Sum-of-Square-Numbers/Solution.go
+++ leetcode/601-700/0633.Sum-of-Square-Numbers/Solution.go
@@ -1,5 +1,20 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "math"
+
+func Solution(c int) bool {
+	end := int(math.Sqrt(float64(c)))
+	x := 0
+	for x <= end {
+		r := x*x + end*end
+		if r == c {
+			return true
+		}
+		if r < c {
+			x++
+			continue
+		}
+		end--
+	}
+	return false
 }
