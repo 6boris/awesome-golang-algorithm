eea84ff8f3c7a6bc01a1dd65e1625411d97611a6
0xff-dev <stevenshuang521@gmail.com>
2024-04-26 22:52:24 +0800

    Add solution and test-cases for problem 671

--- leetcode/601-700/0671.Second-Minimum-Node-In-a-Binary-Tree/Solution_test.go
+++ leetcode/601-700/0671.Second-Minimum-Node-In-a-Binary-Tree/Solution_test.go
@@ -10,12 +10,23 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs *TreeNode
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", &TreeNode{
+			Val:  2,
+			Left: &TreeNode{Val: 2},
+			Right: &TreeNode{
+				Val:   5,
+				Left:  &TreeNode{Val: 5},
+				Right: &TreeNode{Val: 7},
+			},
+		}, 5},
+		{"TestCase", &TreeNode{
+			Val:   2,
+			Left:  &TreeNode{Val: 2},
+			Right: &TreeNode{Val: 2},
+		}, -1},
 	}
 
 	//	开始测试
@@ -30,10 +41,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
