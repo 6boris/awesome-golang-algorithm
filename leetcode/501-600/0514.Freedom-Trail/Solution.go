d2166fbab285a6dcc972e7bc0a542fe9c7f4e64d
0xff-dev <stevenshuang521@gmail.com>
2024-04-27 15:18:01 +0800

    Add solution and test-cases for problem 514

--- leetcode/501-600/0514.Freedom-Trail/Solution.go
+++ leetcode/501-600/0514.Freedom-Trail/Solution.go
@@ -1,5 +1,46 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(ring string, key string) int {
+	// 感觉是dfs+cache实现
+	pos := [26][]int{}
+	for i := 0; i < 26; i++ {
+		pos[i] = make([]int, 0)
+	}
+	for i, r := range ring {
+		pos[r-'a'] = append(pos[r-'a'], i)
+	}
+	lr := len(ring)
+	lk := len(key)
+	cache := make([][]int, lr)
+	for i := 0; i < lr; i++ {
+		cache[i] = make([]int, lk)
+	}
+
+	var dfs func(int, int) int
+	dfs = func(keyIndex, ringIndex int) int {
+		if keyIndex == len(key) {
+			return 0
+		}
+		if cache[ringIndex][keyIndex] != 0 {
+			return cache[ringIndex][keyIndex]
+		}
+
+		curByte := key[keyIndex]
+		ans := -1
+		for _, idx := range pos[curByte-'a'] {
+			diff := ringIndex - idx
+			if diff < 0 {
+				diff = -diff
+			}
+			diff = min(lr-diff, diff)
+			diff++
+			steps := dfs(keyIndex+1, idx)
+			if ans == -1 || diff+steps < ans {
+				ans = diff + steps
+			}
+		}
+		cache[ringIndex][keyIndex] = ans
+		return ans
+	}
+	return dfs(0, 0)
 }
--- leetcode/501-600/0514.Freedom-Trail/Solution_test.go
+++ leetcode/501-600/0514.Freedom-Trail/Solution_test.go
@@ -9,31 +9,32 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name      string
+		ring, key string
+		expect    int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "godding", "gd", 4},
+		{"TestCase2", "godding", "godding", 13},
+		{"TestCase3", "pqwcx", "cpqwx", 13},
+		{"TestCase4", "caotmcaataijjxi", "oatjiioicitatajtijciocjcaaxaaatmctxamacaamjjx", 137},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.ring, c.key)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.ring, c.key)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
