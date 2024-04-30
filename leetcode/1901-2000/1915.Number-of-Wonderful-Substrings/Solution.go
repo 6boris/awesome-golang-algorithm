f830be9897f85ad12ac476a4734a3910cadb5e2d
0xff-dev <stevenshuang521@gmail.com>
2024-04-30 21:23:00 +0800

    Add solution and test-cases for problem 1915

--- leetcode/1901-2000/1915.Number-of-Wonderful-Substrings/Solution.go
+++ leetcode/1901-2000/1915.Number-of-Wonderful-Substrings/Solution.go
@@ -1,5 +1,20 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(word string) int64 {
+	// 10bit
+	ans := int64(0)
+	l := len(word)
+	state := make(map[int]int64)
+	state[0] = 1
+	mask := 0
+	for i := 0; i < l; i++ {
+		mask ^= (1 << int(word[i]-'a'))
+		c := state[mask]
+		ans += c
+		state[mask] = c + 1
+		for i := 0; i < 10; i++ {
+			ans += state[mask^(1<<i)]
+		}
+	}
+	return ans
 }
--- leetcode/1901-2000/1915.Number-of-Wonderful-Substrings/Solution_test.go
+++ leetcode/1901-2000/1915.Number-of-Wonderful-Substrings/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs string
+		expect int64
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "aba", 4},
+		{"TestCase2", "aabb", 9},
+		{"TestCase3", "he", 2},
 	}
 
 	//	开始测试
@@ -30,10 +30,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
