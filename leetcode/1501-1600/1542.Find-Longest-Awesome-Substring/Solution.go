67b464a80e2c6a372ad019cc7b68b1978740eec3
0xff-dev <stevenshuang521@gmail.com>
2024-04-30 21:26:42 +0800

    Add solution and test-cases for problem 1542

--- leetcode/1501-1600/1542.Find-Longest-Awesome-Substring/Solution.go
+++ leetcode/1501-1600/1542.Find-Longest-Awesome-Substring/Solution.go
@@ -1,5 +1,25 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string) int {
+	state := map[int]int{
+		0: -1,
+	}
+
+	mask := 0
+	ans := 0
+	for i := 0; i < len(s); i++ {
+		shift := s[i] - '0'
+		mask ^= (1 << shift)
+		if v, ok := state[mask]; ok {
+			ans = max(ans, i-v)
+		} else {
+			state[mask] = i
+		}
+		for j := 0; j < 10; j++ {
+			if v, ok := state[mask^(1<<j)]; ok {
+				ans = max(ans, i-v)
+			}
+		}
+	}
+	return ans
 }
