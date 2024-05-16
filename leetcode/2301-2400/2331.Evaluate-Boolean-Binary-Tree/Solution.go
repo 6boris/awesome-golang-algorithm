77dd67683677f7ea2671cb01459149e284fdf27b
0xff-dev <stevenshuang521@gmail.com>
2024-05-16 09:02:18 +0800

    Add solution and test-cases for problem 2331

--- leetcode/2301-2400/2331.Evaluate-Boolean-Binary-Tree/Solution.go
+++ leetcode/2301-2400/2331.Evaluate-Boolean-Binary-Tree/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func Solution(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if root.Val == 0 {
+		return false
+	}
+	if root.Val == 1 {
+		return true
+	}
+	l := Solution(root.Left)
+	r := Solution(root.Right)
+	if root.Val == 2 {
+		return l || r
+	}
+	return l && r
 }
