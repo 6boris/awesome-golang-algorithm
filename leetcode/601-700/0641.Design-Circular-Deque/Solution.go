399e43a7a8bc666411ca6ebcdbc3163e29c618af
0xff-dev <stevenshuang521@gmail.com>
2024-05-27 09:03:00 +0800

    Add solution and test-cases for problem 641

--- leetcode/601-700/0641.Design-Circular-Deque/Solution.go
+++ leetcode/601-700/0641.Design-Circular-Deque/Solution.go
@@ -1,5 +1,115 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "container/list"
+
+type MyCircularDeque struct {
+	k    int
+	list *list.List
+}
+
+func Constructor641(k int) MyCircularDeque {
+	return MyCircularDeque{
+		k:    k,
+		list: list.New(),
+	}
+}
+
+func (this *MyCircularDeque) InsertFront(value int) bool {
+	if this.list.Len() == this.k {
+		return false
+	}
+	this.list.PushFront(value)
+	return true
+}
+
+func (this *MyCircularDeque) InsertLast(value int) bool {
+	if this.list.Len() == this.k {
+		return false
+	}
+	this.list.PushBack(value)
+	return true
+}
+
+func (this *MyCircularDeque) DeleteFront() bool {
+	if this.list.Len() == 0 {
+		return false
+	}
+	head := this.list.Front()
+	this.list.Remove(head)
+	return true
+}
+
+func (this *MyCircularDeque) DeleteLast() bool {
+	if this.list.Len() == 0 {
+		return false
+	}
+	head := this.list.Back()
+	this.list.Remove(head)
+	return true
+
+}
+
+func (this *MyCircularDeque) GetFront() int {
+	if this.list.Len() == 0 {
+		return -1
+	}
+	return this.list.Front().Value.(int)
+}
+
+func (this *MyCircularDeque) GetRear() int {
+	if this.list.Len() == 0 {
+		return -1
+	}
+	return this.list.Back().Value.(int)
+
+}
+
+func (this *MyCircularDeque) IsEmpty() bool {
+	return this.list.Len() == 0
+}
+
+func (this *MyCircularDeque) IsFull() bool {
+	return this.k == this.list.Len()
+}
+
+type op struct {
+	n string
+	v int
+}
+
+func Solution(n int, opts []op) []any {
+	c := Constructor641(n)
+	ans := make([]any, 0)
+	for _, o := range opts {
+		if o.n == "if" {
+			ans = append(ans, c.InsertFront(o.v))
+			continue
+		}
+		if o.n == "il" {
+			ans = append(ans, c.InsertLast(o.v))
+			continue
+		}
+		if o.n == "df" {
+			ans = append(ans, c.DeleteFront())
+			continue
+		}
+		if o.n == "dl" {
+			ans = append(ans, c.DeleteLast())
+			continue
+		}
+		if o.n == "gf" {
+			ans = append(ans, c.GetFront())
+			continue
+		}
+		if o.n == "gr" {
+			ans = append(ans, c.GetRear())
+			continue
+		}
+		if o.n == "ie" {
+			ans = append(ans, c.IsEmpty())
+			continue
+		}
+		ans = append(ans, c.IsFull())
+	}
+	return ans
 }
--- leetcode/601-700/0641.Design-Circular-Deque/Solution_test.go
+++ leetcode/601-700/0641.Design-Circular-Deque/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		n      int
+		opts   []op
+		expect []any
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 3, []op{
+			{"il", 1}, {"il", 2}, {"if", 3}, {"if", 4}, {"gr", 0}, {"", 0}, {"dl", 0}, {"if", 4}, {"gf", 0},
+		}, []any{true, true, true, false, 2, true, true, true, 4}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.opts)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.n, c.opts)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
