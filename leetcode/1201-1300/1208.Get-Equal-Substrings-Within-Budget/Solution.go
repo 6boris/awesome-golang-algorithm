ce6ebeece24548698daaa747457414afad985152
0xff-dev <stevenshuang521@gmail.com>
2024-05-28 09:17:42 +0800

    Add solution and test-cases for problem 1208

--- leetcode/1201-1300/1208.Get-Equal-Substrings-Within-Budget/Solution.go
+++ leetcode/1201-1300/1208.Get-Equal-Substrings-Within-Budget/Solution.go
@@ -1,5 +1,25 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string, t string, maxCost int) int {
+	start, end := 0, 0
+	ans, cur, diff := 0, 0, 0
+	for ; end < len(s); end++ {
+		if s[end] > t[end] {
+			diff = int(s[end] - t[end])
+		} else {
+			diff = int(t[end] - s[end])
+		}
+		cur += diff
+		for cur > maxCost && start <= end {
+			if s[start] > t[start] {
+				diff = int(s[start] - t[start])
+			} else {
+				diff = int(t[start] - s[start])
+			}
+			cur -= diff
+			start++
+		}
+		ans = max(ans, end-start+1)
+	}
+	return ans
 }
