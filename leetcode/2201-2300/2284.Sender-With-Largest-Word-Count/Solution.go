786550c9ec75c0b31096fce0d7efb497da82437b
0xff-dev <stevenshuang521@gmail.com>
2024-05-30 08:54:18 +0800

    Add solution and test-cases for problem 2284

--- leetcode/2201-2300/2284.Sender-With-Largest-Word-Count/Solution.go
+++ leetcode/2201-2300/2284.Sender-With-Largest-Word-Count/Solution.go
@@ -1,5 +1,20 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "strings"
+
+func Solution(messages []string, senders []string) string {
+	count := make(map[string]int)
+	mc, ma := 0, ""
+	for idx, msg := range messages {
+		count[senders[idx]] += len(strings.Split(msg, " "))
+		if count[senders[idx]] > mc {
+			mc = count[senders[idx]]
+			ma = senders[idx]
+			continue
+		}
+		if count[senders[idx]] == mc && senders[idx] > ma {
+			ma = senders[idx]
+		}
+	}
+	return ma
 }
--- leetcode/2201-2300/2284.Sender-With-Largest-Word-Count/Solution_test.go
+++ leetcode/2201-2300/2284.Sender-With-Largest-Word-Count/Solution_test.go
@@ -9,31 +9,31 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name     string
+		messages []string
+		senders  []string
+		expect   string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []string{"Hello userTwooo", "Hi userThree", "Wonderful day Alice", "Nice day userThree"}, []string{"Alice", "userTwo", "userThree", "Alice"}, "Alice"},
+		{"TestCase2", []string{"How is leetcode for everyone", "Leetcode is useful for practice"}, []string{"Bob", "Charlie"}, "Charlie"},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.messages, c.senders)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.messages, c.senders)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
