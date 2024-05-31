89547454fb5beae2e50f9a0720f5b5e0428cb5cd
0xff-dev <stevenshuang521@gmail.com>
2024-05-31 09:02:20 +0800

    Add solution and test-cases for problem 1823

--- leetcode/1801-1900/1823.Find-the-Winner-of-the-Circular-Game/Solution.go
+++ leetcode/1801-1900/1823.Find-the-Winner-of-the-Circular-Game/Solution.go
@@ -1,5 +1,9 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int, k int) int {
+	winner := 0
+	for i := 2; i <= n; i++ {
+		winner = (winner + k) % i
+	}
+	return winner + 1
 }
--- leetcode/1801-1900/1823.Find-the-Winner-of-the-Circular-Game/Solution_test.go
+++ leetcode/1801-1900/1823.Find-the-Winner-of-the-Circular-Game/Solution_test.go
@@ -10,30 +10,29 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		n, k   int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 5, 2, 3},
+		{"TestCase2", 6, 5, 1},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.n, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
