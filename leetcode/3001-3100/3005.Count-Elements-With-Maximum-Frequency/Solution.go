ea0e25b7027cad4ac11d4316d2d8126071d15a2e
0xff-dev <stevenshuang521@gmail.com>
2024-06-09 11:52:06 +0800

    Add solution and test-cases for problem 3005

--- leetcode/3001-3100/3005.Count-Elements-With-Maximum-Frequency/Solution.go
+++ leetcode/3001-3100/3005.Count-Elements-With-Maximum-Frequency/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) int {
+	count := make([]int, 101)
+	for _, n := range nums {
+		count[n]++
+	}
+
+	ans := 0
+	freq := -1
+	for _, c := range count {
+		if c == 0 {
+			continue
+		}
+		if c > freq {
+			ans = 0
+			freq = c
+		}
+		if c == freq {
+			ans += freq
+		}
+	}
+	return ans
 }
