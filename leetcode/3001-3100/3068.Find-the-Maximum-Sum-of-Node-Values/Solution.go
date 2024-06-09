60e3ca1c9a2e719379e492b5ec50677592776439
0xff-dev <stevenshuang521@gmail.com>
2024-06-09 12:02:54 +0800

    Add solution and test-cases for problem 3068

--- leetcode/3001-3100/3068.Find-the-Maximum-Sum-of-Node-Values/Solution.go
+++ leetcode/3001-3100/3068.Find-the-Maximum-Sum-of-Node-Values/Solution.go
@@ -1,5 +1,29 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(nums []int, k int, edges [][]int) int64 {
+	// 经过测试，[0,1],[1,2] [1,2,1]这组数据也是6，就是说他选的任何节点俩节点，都可以
+	// 那么我们关注的就是，最大值从小大排列， 而且题目保证了是一棵树，
+	// 所以，只需要每次选俩和是正的就ok
+
+	diff := make([]int, len(nums))
+	ans := int64(0)
+	for i, v := range nums {
+		ans += int64(v)
+		diff[i] = v ^ k - v
+	}
+
+	sort.Slice(diff, func(i, j int) bool {
+		return diff[i] > diff[j]
+	})
+	for i := 0; i < len(diff); i += 2 {
+		if i+1 == len(diff) {
+			break
+		}
+		if s := int64(diff[i]) + int64(diff[i+1]); s > 0 {
+			ans += s
+		}
+	}
+	return ans
 }
--- leetcode/3001-3100/3068.Find-the-Maximum-Sum-of-Node-Values/Solution_test.go
+++ leetcode/3001-3100/3068.Find-the-Maximum-Sum-of-Node-Values/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		nums   []int
+		edges  [][]int
+		k      int
+		expect int64
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 2, 1}, [][]int{{0, 1}, {0, 2}}, 3, 6},
+		{"TestCase2", []int{2, 3}, [][]int{{0, 1}}, 7, 9},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.nums, c.k, c.edges)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.nums, c.edges, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
