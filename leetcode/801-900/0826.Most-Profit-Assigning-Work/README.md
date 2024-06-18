5b831108400c0f4e71e2aea6023f21f1b3ed48a2
0xff-dev <stevenshuang521@gmail.com>
2024-06-18 23:22:43 +0800

    Add solution and test-cases for problem 826

--- leetcode/801-900/0826.Most-Profit-Assigning-Work/README.md
+++ leetcode/801-900/0826.Most-Profit-Assigning-Work/README.md
@@ -1,28 +1,31 @@
 # [826.Most Profit Assigning Work][title]
 
-> [!WARNING|style:flat]
-> This question is temporarily unanswered if you have good ideas. Welcome to [Create Pull Request PR](https://github.com/kylesliu/awesome-golang-algorithm)
-
 ## Description
+You have `n` jobs and m workers. You are given three arrays: `difficulty`, `profit`, and `worker` where:
+
+- `difficulty[i]` and `profit[i]` are the difficulty and the profit of the i<sup>th</sup> job, and
+- `worker[j]` is the ability of j<sup>th</sup> worker (i.e., the j<sup>th</sup> worker can only complete a job with difficulty at most `worker[j]`).
+
+Every worker can be assigned **at most one job**, but one job can be **completed multiple times**.
+
+- For example, if three workers attempt the same job that pays `$1`, then the total profit will be `$3`. If a worker cannot complete any job, their profit is `$0`.
+
+Return the maximum profit we can achieve after assigning the workers to the jobs.
 
 **Example 1:**
 
 ```
-Input: a = "11", b = "1"
-Output: "100"
+Input: difficulty = [2,4,6,8,10], profit = [10,20,30,40,50], worker = [4,5,6,7]
+Output: 100
+Explanation: Workers are assigned jobs of difficulty [4,4,6,6] and they get a profit of [20,20,30,30] separately.
 ```
 
-## 题意
-> ...
+**Example 2:**
 
-## 题解
-
-### 思路1
-> ...
-Most Profit Assigning Work
-```go
 ```
-
+Input: difficulty = [85,47,57], profit = [24,66,99], worker = [40,25,25]
+Output: 0
+```
 
 ## 结语
 
--- leetcode/801-900/0826.Most-Profit-Assigning-Work/Solution.go
+++ leetcode/801-900/0826.Most-Profit-Assigning-Work/Solution.go
@@ -1,5 +1,63 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"math"
+	"sort"
+)
+
+type SegmentTreeNode struct {
+	Left, Right, Max      int
+	LeftChild, RightChild *SegmentTreeNode
+}
+
+func buildSegmentTree(nums []workProfit, left, right int) *SegmentTreeNode {
+	if left == right {
+		return &SegmentTreeNode{Left: left, Right: right, Max: nums[left].p}
+	}
+
+	mid := (left + right) / 2
+	leftNode := buildSegmentTree(nums, left, mid)
+	rightNode := buildSegmentTree(nums, mid+1, right)
+	max := max(leftNode.Max, rightNode.Max)
+
+	return &SegmentTreeNode{Left: left, Right: right, Max: max, LeftChild: leftNode, RightChild: rightNode}
+}
+
+func queryMax(root *SegmentTreeNode, left, right int) int {
+	if root.Left >= left && root.Right <= right {
+		return root.Max
+	}
+
+	if root.Right < left || root.Left > right {
+		return math.MinInt32
+	}
+
+	return max(queryMax(root.LeftChild, left, right), queryMax(root.RightChild, left, right))
+}
+
+type workProfit struct {
+	d, p int
+}
+
+func Solution(difficulty []int, profit []int, worker []int) int {
+	l := len(difficulty)
+	dp := make([]workProfit, l)
+	for i := 0; i < len(difficulty); i++ {
+		dp[i] = workProfit{d: difficulty[i], p: profit[i]}
+	}
+	sort.Slice(dp, func(i, j int) bool {
+		return dp[i].d < dp[j].d
+	})
+
+	tree := buildSegmentTree(dp, 0, l-1)
+	ans := 0
+	for _, n := range worker {
+		if idx := sort.Search(l, func(i int) bool {
+			return dp[i].d > n
+		}); idx != 0 {
+			ans += queryMax(tree, 0, idx-1)
+		}
+	}
+
+	return ans
 }
