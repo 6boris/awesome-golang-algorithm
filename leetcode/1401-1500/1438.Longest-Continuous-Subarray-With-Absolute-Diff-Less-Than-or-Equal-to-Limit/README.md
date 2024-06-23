8c83bba49c9891eafb72031709c9d341593a56a4
0xff-dev <stevenshuang521@gmail.com>
2024-06-23 11:50:23 +0800

    Add solution and test-cases for problem 1438

---
+++ leetcode/1401-1500/1438.Longest-Continuous-Subarray-With-Absolute-Diff-Less-Than-or-Equal-to-Limit/README.md
@@ -0,0 +1,45 @@
+# [1438.Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit][title]
+
+## Description
+Given an array of integers `nums` and an integer `limit`, return the size of the longest **non-empty** subarray such that the absolute difference between any two elements of this subarray is less than or equal to `limit`.
+
+**Example 1:**
+
+```
+Input: nums = [8,2,4,7], limit = 4
+Output: 2 
+Explanation: All subarrays are: 
+[8] with maximum absolute diff |8-8| = 0 <= 4.
+[8,2] with maximum absolute diff |8-2| = 6 > 4. 
+[8,2,4] with maximum absolute diff |8-2| = 6 > 4.
+[8,2,4,7] with maximum absolute diff |8-2| = 6 > 4.
+[2] with maximum absolute diff |2-2| = 0 <= 4.
+[2,4] with maximum absolute diff |2-4| = 2 <= 4.
+[2,4,7] with maximum absolute diff |2-7| = 5 > 4.
+[4] with maximum absolute diff |4-4| = 0 <= 4.
+[4,7] with maximum absolute diff |4-7| = 3 <= 4.
+[7] with maximum absolute diff |7-7| = 0 <= 4. 
+Therefore, the size of the longest subarray is 2.
+```
+
+**Example 2:**
+
+```
+Input: nums = [10,1,2,4,7,2], limit = 5
+Output: 4 
+Explanation: The subarray [2,4,7,2] is the longest since the maximum absolute diff is |2-7| = 5 <= 5.
+```
+
+**Example 3:**
+
+```
+Input: nums = [4,2,2,2,4,4,2,2], limit = 0
+Output: 3
+```
+
+## 结语
+
+如果你同我一样热爱数据结构、算法、LeetCode，可以关注我 GitHub 上的 LeetCode 题解：[awesome-golang-algorithm][me]
+
+[title]: https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit
+[me]: https://github.com/kylesliu/awesome-golang-algorithm
--- leetcode/1401-1500/1438.Longest-Continuous-Subarray-With-Absolute-Diff-Less-Than-or-Equal-to-Limit/Solution.go
+++ leetcode/1401-1500/1438.Longest-Continuous-Subarray-With-Absolute-Diff-Less-Than-or-Equal-to-Limit/Solution.go
@@ -1,5 +1,97 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type SegmentTreeNode1438 struct {
+	Left, Right int
+	Min, Max    int
+	LeftChild   *SegmentTreeNode1438
+	RightChild  *SegmentTreeNode1438
+}
+
+func buildSegmentTree1438(arr []int, left, right int) *SegmentTreeNode1438 {
+	if left == right {
+		return &SegmentTreeNode1438{
+			Left:  left,
+			Right: right,
+			Min:   arr[left],
+			Max:   arr[left],
+		}
+	}
+
+	mid := (left + right) / 2
+	leftChild := buildSegmentTree1438(arr, left, mid)
+	rightChild := buildSegmentTree1438(arr, mid+1, right)
+
+	node := &SegmentTreeNode1438{
+		Left:       left,
+		Right:      right,
+		Min:        min(leftChild.Min, rightChild.Min),
+		Max:        max(leftChild.Max, rightChild.Max),
+		LeftChild:  leftChild,
+		RightChild: rightChild,
+	}
+
+	return node
+}
+
+func queryMin1438(node *SegmentTreeNode1438, left, right int) int {
+	if node.Left == left && node.Right == right {
+		return node.Min
+	}
+
+	mid := (node.Left + node.Right) / 2
+
+	if right <= mid {
+		return queryMin1438(node.LeftChild, left, right)
+	} else if left > mid {
+		return queryMin1438(node.RightChild, left, right)
+	} else {
+		leftMin := queryMin1438(node.LeftChild, left, mid)
+		rightMin := queryMin1438(node.RightChild, mid+1, right)
+		return min(leftMin, rightMin)
+	}
+}
+
+func queryMax1438(node *SegmentTreeNode1438, left, right int) int {
+	if node.Left == left && node.Right == right {
+		return node.Max
+	}
+
+	mid := (node.Left + node.Right) / 2
+
+	if right <= mid {
+		return queryMax1438(node.LeftChild, left, right)
+	} else if left > mid {
+		return queryMax1438(node.RightChild, left, right)
+	} else {
+		leftMax := queryMax1438(node.LeftChild, left, mid)
+		rightMax := queryMax1438(node.RightChild, mid+1, right)
+		return max(leftMax, rightMax)
+	}
+}
+
+// 滑动窗口, 线段树？
+func Solution(nums []int, limit int) int {
+	l := len(nums)
+	tree := buildSegmentTree1438(nums, 0, l-1)
+	start, end := 0, 0
+	ans := 0
+
+	var a, b int
+	for ; end < l; end++ {
+		a = queryMax1438(tree, start, end)
+		b = queryMin1438(tree, start, end)
+		if a-b <= limit {
+			ans = max(ans, end-start+1)
+			continue
+		}
+		for start < end {
+			start++
+			a = queryMax1438(tree, start, end)
+			b = queryMin1438(tree, start, end)
+			if a-b <= limit {
+				break
+			}
+		}
+	}
+	return ans
 }
