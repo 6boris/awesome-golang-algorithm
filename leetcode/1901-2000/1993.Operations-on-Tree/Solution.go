68355cfdf3bcf6a54fcc296d43844b1704c358bf
0xff-dev <stevenshuang521@gmail.com>
2024-06-26 22:19:32 +0800

    Add solution and test-cases for problem 1993

--- leetcode/1901-2000/1993.Operations-on-Tree/Solution.go
+++ leetcode/1901-2000/1993.Operations-on-Tree/Solution.go
@@ -1,5 +1,102 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type LockingTree struct {
+	tree     []int
+	locker   []int
+	children map[int][]int
+}
+
+func Constructor(parent []int) LockingTree {
+	children := make(map[int][]int)
+	for c, p := range parent {
+		if _, ok := children[p]; !ok {
+			children[p] = make([]int, 0)
+		}
+		children[p] = append(children[p], c)
+	}
+	return LockingTree{tree: parent, locker: make([]int, len(parent)), children: children}
+}
+
+// u, u, ul, l, u
+func (this *LockingTree) Lock(num int, user int) bool {
+	if this.locker[num] != 0 {
+		return false
+	}
+	this.locker[num] = user
+	return true
+}
+
+func (this *LockingTree) Unlock(num int, user int) bool {
+	if this.locker[num] == 0 || this.locker[num] != user {
+		return false
+	}
+	this.locker[num] = 0
+	return true
+}
+
+func (this *LockingTree) ancesors(num int) bool {
+	for {
+		if num == -1 {
+			return false
+		}
+		if this.locker[num] != 0 {
+			return true
+		}
+		num = this.tree[num]
+	}
+}
+
+func (this *LockingTree) unlockDescendant(num int) bool {
+	q := []int{num}
+	found := false
+	for len(q) > 0 {
+		nq := make([]int, 0)
+		for _, i := range q {
+			if this.locker[i] != 0 {
+				this.locker[i] = 0
+				found = true
+			}
+			for _, c := range this.children[i] {
+				nq = append(nq, c)
+			}
+		}
+		q = nq
+	}
+	return found
+}
+
+func (this *LockingTree) Upgrade(num int, user int) bool {
+	if this.locker[num] != 0 {
+		return false
+	}
+	if this.ancesors(num) {
+		return false
+	}
+	if !this.unlockDescendant(num) {
+		return false
+	}
+	this.locker[num] = user
+	return true
+}
+
+type op struct {
+	name string
+	a, b int
+}
+
+func Solution(p []int, opts []op) []bool {
+	c := Constructor(p)
+	ans := make([]bool, 0)
+	for _, o := range opts {
+		if o.name == "l" {
+			ans = append(ans, c.Lock(o.a, o.b))
+			continue
+		}
+		if o.name == "u" {
+			ans = append(ans, c.Unlock(o.a, o.b))
+			continue
+		}
+		ans = append(ans, c.Upgrade(o.a, o.b))
+	}
+	return ans
 }
--- leetcode/1901-2000/1993.Operations-on-Tree/Solution_test.go
+++ leetcode/1901-2000/1993.Operations-on-Tree/Solution_test.go
@@ -10,30 +10,36 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		p      []int
+		opts   []op
+		expect []bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{-1, 0, 0, 1, 1, 2, 2}, []op{
+			{"l", 2, 2},
+			{"u", 2, 3},
+			{"u", 2, 2},
+			{"l", 4, 5},
+			{"", 0, 1},
+			{"l", 0, 1},
+		}, []bool{true, false, true, true, true, false}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.p, c.opts)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.p, c.opts)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
