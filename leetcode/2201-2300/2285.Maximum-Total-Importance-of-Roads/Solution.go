a52438b5b73a4906df57f583f585889e1ac5cad5
0xff-dev <stevenshuang521@gmail.com>
2024-06-28 09:02:34 +0800

    Add solution and test-cases for problem 2285

--- leetcode/2201-2300/2285.Maximum-Total-Importance-of-Roads/Solution.go
+++ leetcode/2201-2300/2285.Maximum-Total-Importance-of-Roads/Solution.go
@@ -1,5 +1,21 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(n int, roads [][]int) int64 {
+	ans := int64(0)
+
+	// 检查入读和出度
+	in := make([]int, n)
+	for _, r := range roads {
+		in[r[0]]++
+		in[r[1]]++
+	}
+	sort.Ints(in)
+	v := int64(1)
+	for _, i := range in {
+		ans += v * int64(i)
+		v++
+	}
+	return ans
 }
