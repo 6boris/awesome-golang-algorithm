31cc63498d69e77335f1a3b6e7b1fdb1ec577b0a
0xff-dev <stevenshuang521@gmail.com>
2024-07-20 12:37:51 +0800

    Add solution and test-cases for problem 1605

--- leetcode/1601-1700/1605.Find-Valid-Matrix-Given-Row-and-Column-Sums/Solution_test.go
+++ leetcode/1601-1700/1605.Find-Valid-Matrix-Given-Row-and-Column-Sums/Solution_test.go
@@ -10,30 +10,29 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		a, b   []int
+		expect [][]int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{3, 8}, []int{4, 7}, [][]int{{3, 0}, {1, 7}}},
+		{"TestCase2", []int{5, 7, 10}, []int{8, 6, 8}, [][]int{{5, 0, 0}, {3, 4, 0}, {0, 2, 8}}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.a, c.b)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.a, c.b)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
