75ba6a277fbdf5ad80be6e3647104fcf46846b39
0xff-dev <stevenshuang521@gmail.com>
2024-07-21 11:58:29 +0800

    Add solution and test-cases for problem 2392

--- leetcode/2301-2400/2392.Build-a-Matrix-With-Conditions/Solution_test.go
+++ leetcode/2301-2400/2392.Build-a-Matrix-With-Conditions/Solution_test.go
@@ -9,31 +9,35 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name                         string
+		k                            int
+		rowConditions, colConditions [][]int
+		expect                       [][]int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 3, [][]int{{1, 2}, {3, 2}}, [][]int{{2, 1}, {3, 2}}, [][]int{
+			{0, 0, 1},
+			{3, 0, 0},
+			{0, 2, 0},
+		}},
+		{"TestCase2", 3, [][]int{{1, 2}, {2, 3}, {3, 1}, {2, 3}}, [][]int{{2, 1}}, nil},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.k, c.rowConditions, c.colConditions)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.k, c.rowConditions, c.colConditions)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
