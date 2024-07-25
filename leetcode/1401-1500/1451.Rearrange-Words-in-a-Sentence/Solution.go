ccd61f5213d348f78f9ff67175c1ec978b232b20
0xff-dev <stevenshuang521@gmail.com>
2024-07-25 09:12:45 +0800

    Add solution and test-cases for problem 1451

--- leetcode/1401-1500/1451.Rearrange-Words-in-a-Sentence/Solution.go
+++ leetcode/1401-1500/1451.Rearrange-Words-in-a-Sentence/Solution.go
@@ -1,5 +1,34 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"sort"
+	"strings"
+)
+
+func Solution(text string) string {
+	if len(text) == 0 {
+		return ""
+	}
+	bs := strings.Split(text, " ")
+	bs[0] = strings.ToLower(bs[0])
+
+	indies := make([]int, len(bs))
+	for i := 0; i < len(bs); i++ {
+		indies[i] = i
+	}
+	sort.Slice(indies, func(i, j int) bool {
+		a, b := len(bs[indies[i]]), len(bs[indies[j]])
+		if a == b {
+			return indies[i] < indies[j]
+		}
+		return a < b
+	})
+	ans := make([]string, len(bs))
+	for i := 0; i < len(bs); i++ {
+		ans[i] = bs[indies[i]]
+	}
+	first := []byte(ans[0])
+	first[0] -= 32
+	ans[0] = string(first)
+	return strings.Join(ans, " ")
 }
