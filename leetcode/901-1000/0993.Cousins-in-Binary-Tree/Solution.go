4f4914a1371c91ec4b7a5b613dd5e8003e0aab71
0xff-dev <stevenshuang521@gmail.com>
2024-07-31 09:16:21 +0800

    Add solution and test-cases for problem 993

--- leetcode/901-1000/0993.Cousins-in-Binary-Tree/Solution.go
+++ leetcode/901-1000/0993.Cousins-in-Binary-Tree/Solution.go
@@ -1,5 +1,35 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+func Solution(root *TreeNode, x int, y int) bool {
+	var (
+		xnode  *TreeNode
+		ynode  *TreeNode
+		xd, yd int
+	)
+	var dfs func(*TreeNode, *TreeNode, int)
+	dfs = func(fa, tree *TreeNode, dep int) {
+		if tree == nil {
+			return
+		}
+		if tree.Val == x {
+			xnode = fa
+			xd = dep
+		}
+		if tree.Val == y {
+			ynode = fa
+			yd = dep
+		}
+		dfs(tree, tree.Left, dep+1)
+		dfs(tree, tree.Right, dep+1)
+	}
+	dfs(nil, root, 0)
+	if xd != yd {
+		return false
+	}
+	return xnode != ynode
 }
--- leetcode/901-1000/0993.Cousins-in-Binary-Tree/Solution_test.go
+++ leetcode/901-1000/0993.Cousins-in-Binary-Tree/Solution_test.go
@@ -10,30 +10,57 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
+		tree   *TreeNode
+		x, y   int
 		expect bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", &TreeNode{
+			Val: 1,
+			Left: &TreeNode{
+				Val:  2,
+				Left: &TreeNode{Val: 4},
+			},
+			Right: &TreeNode{Val: 3},
+		}, 4, 3, false},
+		{"TestCase2", &TreeNode{
+			Val: 1,
+			Left: &TreeNode{
+				Val:   2,
+				Right: &TreeNode{Val: 4},
+			},
+			Right: &TreeNode{
+				Val:   3,
+				Right: &TreeNode{Val: 5},
+			},
+		}, 5, 4, true},
+		{"TestCase3", &TreeNode{
+			Val: 1,
+			Left: &TreeNode{
+				Val: 2,
+				Right: &TreeNode{
+					Val: 4,
+				},
+			},
+			Right: &TreeNode{Val: 3},
+		}, 2, 3, false},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.tree, c.x, c.y)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.tree, c.x, c.y)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
