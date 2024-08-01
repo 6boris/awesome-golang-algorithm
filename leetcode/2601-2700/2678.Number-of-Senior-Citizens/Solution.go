ebcb18843046c026eb84ea6a332280ef6da78739
0xff-dev <stevenshuang521@gmail.com>
2024-08-01 08:54:42 +0800

    Add solution and test-cases for problem 2678

--- leetcode/2601-2700/2678.Number-of-Senior-Citizens/Solution.go
+++ leetcode/2601-2700/2678.Number-of-Senior-Citizens/Solution.go
@@ -1,5 +1,14 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "strconv"
+
+func Solution(details []string) int {
+	ans := 0
+	for _, d := range details {
+		age, _ := strconv.Atoi(d[11:13])
+		if age > 60 {
+			ans++
+		}
+	}
+	return ans
 }
