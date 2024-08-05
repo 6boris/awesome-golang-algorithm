8e0c800dd7450ff89d2ba7ab190c1bfbaf122f11
0xff-dev <stevenshuang521@gmail.com>
2024-08-05 08:57:55 +0800

    Add solution and test-cases for problem 2053

--- leetcode/2001-2100/2053.Kth-Distinct-String-in-an-Array/Solution_test.go
+++ leetcode/2001-2100/2053.Kth-Distinct-String-in-an-Array/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		arr    []string
+		k      int
+		expect string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []string{"d", "b", "c", "b", "c", "a"}, 2, "a"},
+		{"TestCase2", []string{"aaa", "aa", "a"}, 1, "aaa"},
+		{"TestCase3", []string{"a", "b", "a"}, 3, ""},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.arr, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.arr, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
