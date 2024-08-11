2c487d259d0380ecfc8fac94ad2be75b0f9544e7
0xff-dev <stevenshuang521@gmail.com>
2024-08-11 12:14:15 +0800

    Add solution and test-cases for problem 1568

--- leetcode/1501-1600/1568.Minimum-Number-of-Days-to-Disconnect-Island/README.md
+++ leetcode/1501-1600/1568.Minimum-Number-of-Days-to-Disconnect-Island/README.md
@@ -1,28 +1,35 @@
 # [1568.Minimum Number of Days to Disconnect Island][title]
 
-> [!WARNING|style:flat]
-> This question is temporarily unanswered if you have good ideas. Welcome to [Create Pull Request PR](https://github.com/kylesliu/awesome-golang-algorithm)
-
 ## Description
+You are given an `m x n` binary `grid` grid where `1` represents land and `0` represents water. An **island** is a maximal **4-directionally** (horizontal or vertical) connected group of `1`'s.
 
-**Example 1:**
+The grid is said to be **connected** if we have **exactly one island**, otherwise is said **disconnected**.
+
+In one day, we are allowed to change any single land cell `(1)` into a water cell `(0)`.
+
+Return the minimum number of days to disconnect the grid.
+
+**Example 1:**  
+
+![1](./1.jpg)
 
 ```
-Input: a = "11", b = "1"
-Output: "100"
+Input: grid = [[0,1,1,0],[0,1,1,0],[0,0,0,0]]
+
+Output: 2
+Explanation: We need at least 2 days to get a disconnected grid.
+Change land grid[1][1] and grid[0][2] to water and get 2 disconnected island.
 ```
 
-## 题意
-> ...
+**Example 2:**  
 
-## 题解
+![2](./2.jpg)
 
-### 思路1
-> ...
-Minimum Number of Days to Disconnect Island
-```go
 ```
-
+Input: grid = [[1,1]]
+Output: 2
+Explanation: Grid of full water is also disconnected ([[1,1]] -> [[0,0]]), 0 islands.
+```
 
 ## 结语
 
--- leetcode/1501-1600/1568.Minimum-Number-of-Days-to-Disconnect-Island/Solution.go
+++ leetcode/1501-1600/1568.Minimum-Number-of-Days-to-Disconnect-Island/Solution.go
@@ -1,5 +1,72 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+var dirs1568 = [][]int{
+	{1, 0}, {-1, 0}, {0, 1}, {0, -1},
+}
+
+func bfs1568(x, y int, grid [][]int) {
+	q := [][2]int{{x, y}}
+	grid[x][y] = 0
+	for len(q) > 0 {
+		nq := make([][2]int, 0)
+		for _, cur := range q {
+			for _, dir := range dirs1568 {
+				nx, ny := cur[0]+dir[0], cur[1]+dir[1]
+				if nx < 0 || nx >= len(grid) || ny < 0 || ny >= len(grid[0]) || grid[nx][ny] == 0 {
+					continue
+				}
+				grid[nx][ny] = 0
+				nq = append(nq, [2]int{nx, ny})
+			}
+		}
+		q = nq
+	}
+}
+
+// 判断联通, 如果开始就有多个区域，直击返回0
+func connected1568(grid [][]int) bool {
+	rows, cols := len(grid), len(grid[0])
+	tmp := make([][]int, len(grid))
+	for i := 0; i < len(grid); i++ {
+		tmp[i] = make([]int, cols)
+		copy(tmp[i], grid[i])
+	}
+	count := 0
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if tmp[i][j] == 1 {
+				if count == 1 {
+					return false
+				}
+				bfs1568(i, j, tmp)
+				count++
+			}
+		}
+	}
+	// 有且只有一个陆地的时候才是联通的
+	return count == 1
+}
+
+// 我的思路是首先通过bfs搜索有几个联通
+func Solution(grid [][]int) int {
+	// 如果去掉一个1的时候无法将联通区域变成多个，那就至少需要两个操作
+	// 两个操作，就类似于两步将一个角的1陆地裁剪出去。
+	if !connected1568(grid) {
+		// 一步都不用做
+		return 0
+	}
+	rows, cols := len(grid), len(grid[0])
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if grid[i][j] == 1 {
+				//尝试更新
+				grid[i][j] = 0
+				if !connected1568(grid) {
+					return 1
+				}
+				grid[i][j] = 1
+			}
+		}
+	}
+	return 2
 }
