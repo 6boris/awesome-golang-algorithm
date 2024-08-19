9f32ef7d411cefb787236f74ba165edaec109d0b
0xff-dev <stevenshuang521@gmail.com>
2024-08-19 09:13:37 +0800

    Add solution and test-cases for problem 819

--- leetcode/801-900/0819.Most-Common-Word/Solution_test.go
+++ leetcode/801-900/0819.Most-Common-Word/Solution_test.go
@@ -10,30 +10,30 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		p      string
+		banned []string
+		expect string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "Bob hit a ball, the hit BALL flew far after it was hit.", []string{"hit"}, "ball"},
+		{"TestCase2", "a.", []string{}, "a"},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.p, c.banned)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.p, c.banned)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
