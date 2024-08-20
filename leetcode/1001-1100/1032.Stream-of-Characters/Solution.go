e0bb9f3f8214abd45822cfc335519ad5928121e6
0xff-dev <stevenshuang521@gmail.com>
2024-08-20 09:37:50 +0800

    Add solution and test-cases for problem 1032

--- leetcode/1001-1100/1032.Stream-of-Characters/Solution.go
+++ leetcode/1001-1100/1032.Stream-of-Characters/Solution.go
@@ -1,5 +1,75 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type trie1032 struct {
+	end   bool
+	child [26]*trie1032
+}
+
+type StreamChecker struct {
+	// trie
+	i  int
+	ml int
+	q  []byte // max length
+	t  *trie1032
+}
+
+func Constructor1032(words []string) StreamChecker {
+	s := StreamChecker{
+		t: &trie1032{},
+	}
+	ml := 0
+	for _, w := range words {
+		ml = max(ml, len(w))
+		s.fill(w)
+	}
+
+	s.q = make([]byte, ml)
+	s.ml = ml
+	return s
+}
+
+func (this *StreamChecker) fill(word string) {
+	root := this.t
+	for i := len(word) - 1; i >= 0; i-- {
+		b := word[i]
+		if root.child[b-'a'] == nil {
+			root.child[b-'a'] = &trie1032{}
+		}
+		if i == 0 {
+			root.child[b-'a'].end = true
+		}
+		root = root.child[b-'a']
+	}
+}
+
+func (this *StreamChecker) Query(letter byte) bool {
+	if this.i == this.ml {
+		next := make([]byte, this.ml)
+		copy(next, this.q[1:])
+		this.i = this.ml - 1
+		this.q = next
+	}
+	this.q[this.i] = letter
+	this.i++
+	root := this.t
+	for i := this.i - 1; i >= 0; i-- {
+		cur := this.q[i] - 'a'
+		if root.child[cur] == nil {
+			return false
+		}
+		if root.child[cur].end {
+			return true
+		}
+		root = root.child[cur]
+	}
+	return false
+}
+
+func Solution(words []string, query []byte) []bool {
+	ans := make([]bool, len(query))
+	c := Constructor1032(words)
+	for i := range query {
+		ans[i] = c.Query(query[i])
+	}
+	return ans
 }
--- leetcode/1001-1100/1032.Stream-of-Characters/Solution_test.go
+++ leetcode/1001-1100/1032.Stream-of-Characters/Solution_test.go
@@ -10,30 +10,29 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		words  []string
+		query  []byte
+		expect []bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []string{"cd", "f", "kl"}, []byte("abcdefghijkl"), []bool{false, false, false, true, false, true, false, false, false, false, false, true}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.words, c.query)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.words, c.query)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
