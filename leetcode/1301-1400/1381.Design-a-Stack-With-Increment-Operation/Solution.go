f9c431aed84ca12214fab8918fbc389583ae629e
0xff-dev <stevenshuang521@gmail.com>
2024-08-26 09:14:44 +0800

    Add solution and test-cases for problem 1381

--- leetcode/1301-1400/1381.Design-a-Stack-With-Increment-Operation/Solution.go
+++ leetcode/1301-1400/1381.Design-a-Stack-With-Increment-Operation/Solution.go
@@ -1,5 +1,67 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type CustomStack struct {
+	index int
+	stack []int
+}
+
+func Constructor(maxSize int) CustomStack {
+	return CustomStack{
+		stack: make([]int, maxSize),
+		index: -1,
+	}
+}
+
+func (this *CustomStack) Push(x int) {
+	if this.index == len(this.stack)-1 {
+		return
+	}
+
+	this.index++
+	this.stack[this.index] = x
+}
+
+func (this *CustomStack) Pop() int {
+	if this.index == -1 {
+		return -1
+	}
+
+	top := this.stack[this.index]
+	this.index--
+	return top
+}
+
+func (this *CustomStack) Increment(k int, val int) {
+	if this.index == -1 {
+		return
+	}
+
+	// 1, 2, 3, 4,
+	// 3
+	for i := 0; i <= this.index && i < k; i++ {
+		this.stack[i] += val
+	}
+}
+
+type op struct {
+	name string
+	a, b int
+}
+
+func Solution(maxSize int, opts []op) []int {
+	c := Constructor(maxSize)
+	ans := make([]int, 0)
+	for _, o := range opts {
+		if o.name == "push" {
+			c.Push(o.a)
+			continue
+		}
+		if o.name == "i" {
+			c.Increment(o.a, o.b)
+			continue
+		}
+
+		ans = append(ans, c.Pop())
+	}
+	return ans
 }
--- leetcode/1301-1400/1381.Design-a-Stack-With-Increment-Operation/Solution_test.go
+++ leetcode/1301-1400/1381.Design-a-Stack-With-Increment-Operation/Solution_test.go
@@ -9,31 +9,40 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name    string
+		maxSize int
+		opts    []op
+		expect  []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 3, []op{
+			{"push", 1, 0}, {"push", 2, 0},
+			{"pop", 0, 0},
+
+			{"push", 2, 0}, {"push", 3, 0}, {"push", 4, 0},
+			{"i", 5, 100}, {"i", 2, 100},
+			{"pop", 0, 0},
+			{"pop", 0, 0},
+			{"pop", 0, 0},
+			{"pop", 0, 0},
+		}, []int{2, 103, 202, 201, -1}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.maxSize, c.opts)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.maxSize, c.opts)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
