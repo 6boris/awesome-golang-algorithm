3ceac883891de6c0110426b6eaa8c16503ec26f6
0xff-dev <stevenshuang521@gmail.com>
2024-09-04 23:22:47 +0800

    Add solution and test-cases for problem 874

--- leetcode/801-900/0874.Walking-Robot-Simulation/README.md
+++ leetcode/801-900/0874.Walking-Robot-Simulation/README.md
@@ -1,28 +1,62 @@
 # [874.Walking Robot Simulation][title]
 
-> [!WARNING|style:flat]
-> This question is temporarily unanswered if you have good ideas. Welcome to [Create Pull Request PR](https://github.com/kylesliu/awesome-golang-algorithm)
-
 ## Description
+A robot on an infinite XY-plane starts at point `(0, 0)` facing north. The robot can receive a sequence of these three possible types of `commands`:
+
+- `-2`: Turn left `90` degrees.
+- `-1`: Turn right `90` degrees.
+- `1 <= k <= 9`: Move forward `k` units, one unit at a time.
+
+Some of the grid squares are `obstacles`. The `ith` obstacle is at grid point `obstacles[i] = (xi, yi)`. If the robot runs into an obstacle, then it will instead stay in its current location and move on to the next command.
+
+Return the **maximum Euclidean distance** that the robot ever gets from the origin **squared** (i.e. if the distance is `5`, return `25`).
+
+**Note**:
+
+- North means +Y direction.
+- East means +X direction.
+- South means -Y direction.
+- West means -X direction.
+- There can be obstacle in [0,0].
 
 **Example 1:**
 
 ```
-Input: a = "11", b = "1"
-Output: "100"
+Input: commands = [4,-1,3], obstacles = []
+Output: 25
+Explanation: The robot starts at (0, 0):
+1. Move north 4 units to (0, 4).
+2. Turn right.
+3. Move east 3 units to (3, 4).
+The furthest point the robot ever gets from the origin is (3, 4), which squared is 32 + 42 = 25 units away.
 ```
 
-## 题意
-> ...
+**Example 2:**
 
-## 题解
-
-### 思路1
-> ...
-Walking Robot Simulation
-```go
+```
+Input: commands = [4,-1,4,-2,4], obstacles = [[2,4]]
+Output: 65
+Explanation: The robot starts at (0, 0):
+1. Move north 4 units to (0, 4).
+2. Turn right.
+3. Move east 1 unit and get blocked by the obstacle at (2, 4), robot is at (1, 4).
+4. Turn left.
+5. Move north 4 units to (1, 8).
+The furthest point the robot ever gets from the origin is (1, 8), which squared is 12 + 82 = 65 units away.
 ```
 
+**Example 3:**
+
+```
+Input: commands = [6,-1,-1,6], obstacles = []
+Output: 36
+Explanation: The robot starts at (0, 0):
+1. Move north 6 units to (0, 6).
+2. Turn right.
+3. Turn right.
+4. Move south 6 units to (0, 0).
+The furthest point the robot ever gets from the origin is (0, 6), which squared is 62 = 36 units away.
+```
 
 ## 结语
 
--- leetcode/801-900/0874.Walking-Robot-Simulation/Solution.go
+++ leetcode/801-900/0874.Walking-Robot-Simulation/Solution.go
@@ -1,5 +1,130 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func nextDir(cur byte, op int) byte {
+	if cur == 'n' {
+		if op == -1 {
+			return 'e'
+		}
+		return 'w'
+	}
+	if cur == 'w' {
+		if op == -1 {
+			return 'n'
+		}
+		return 's'
+	}
+	if cur == 's' {
+		if op == -1 {
+			return 'w'
+		}
+		return 'e'
+	}
+	if op == -1 {
+		return 's'
+	}
+	return 'n'
+}
+
+func nextSteps(cur byte) [2]int {
+	if cur == 'n' {
+		return [2]int{0, 1}
+	}
+	if cur == 'w' {
+		return [2]int{-1, 0}
+	}
+	if cur == 's' {
+		return [2]int{0, -1}
+	}
+	return [2]int{1, 0}
+}
+
+func Solution(commands []int, obstacles [][]int) int {
+	x, y := 0, 0
+	ans := 0
+	snKeys := make([]int, 0)
+	ewKeys := make([]int, 0)
+	snIn := make(map[int][]int)
+	ewIn := make(map[int][]int)
+
+	for _, o := range obstacles {
+		if _, ok := snIn[o[0]]; !ok {
+			snIn[o[0]] = []int{}
+			snKeys = append(snKeys, o[0])
+		}
+		snIn[o[0]] = append(snIn[o[0]], o[1])
+
+		if _, ok := ewIn[o[1]]; !ok {
+			ewIn[o[1]] = []int{}
+			ewKeys = append(ewKeys, o[1])
+		}
+		ewIn[o[1]] = append(ewIn[o[1]], o[0])
+	}
+	sort.Ints(snKeys)
+	sort.Ints(ewKeys)
+
+	for k := range snIn {
+		sort.Ints(snIn[k])
+	}
+	for k := range ewIn {
+		sort.Ints(ewIn[k])
+	}
+
+	var check func(x, y, tx, ty int, cur byte) (int, int)
+	check = func(x, y, tx, ty int, cur byte) (int, int) {
+		if cur == 'n' || cur == 's' {
+			v, ok := snIn[x]
+			if !ok {
+				return tx, ty
+			}
+			idx := sort.Search(len(v), func(i int) bool {
+				return v[i] > y
+			})
+			if cur == 'n' {
+				if idx == len(v) || v[idx] > ty || v[idx] == y {
+					return tx, ty
+				}
+				return x, v[idx] - 1
+			}
+			if idx == 0 || v[idx-1] < ty {
+				return tx, ty
+			}
+			return x, v[idx-1] + 1
+		}
+		if cur == 'w' || cur == 'e' {
+			v, ok := ewIn[y]
+			if !ok {
+				return tx, ty
+			}
+			idx := sort.Search(len(v), func(i int) bool {
+				return v[i] > x
+			})
+			if cur == 'w' {
+				if idx == 0 || v[idx-1] < tx || v[idx-1] == x {
+					return tx, ty
+				}
+				return v[idx-1] + 1, y
+			}
+			if idx == len(v) || v[idx] > tx {
+				return tx, ty
+			}
+			return v[idx] - 1, y
+		}
+		return tx, ty
+	}
+	dir := byte('n')
+	cur := nextSteps(dir)
+	for _, cmd := range commands {
+		if cmd > 0 {
+			nx, ny := x+cur[0]*cmd, y+cur[1]*cmd
+			nx, ny = check(x, y, nx, ny, dir)
+			ans = max(ans, nx*nx+ny*ny)
+			x, y = nx, ny
+			continue
+		}
+		dir = nextDir(dir, cmd)
+		cur = nextSteps(dir)
+	}
+	return ans
 }
