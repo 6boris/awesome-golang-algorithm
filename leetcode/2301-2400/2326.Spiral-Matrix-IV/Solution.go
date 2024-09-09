529ede8c15f885b2fe9ecc18c2e80da35bbbe81d
0xff-dev <stevenshuang521@gmail.com>
2024-09-09 09:12:06 +0800

    Add solution and test-cases for problem 2326

--- leetcode/2301-2400/2326.Spiral-Matrix-IV/Solution.go
+++ leetcode/2301-2400/2326.Spiral-Matrix-IV/Solution.go
@@ -1,5 +1,41 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func Solution(m int, n int, head *ListNode) [][]int {
+	ans := make([][]int, m)
+	for i := range m {
+		ans[i] = make([]int, n)
+		for j := range n {
+			ans[i][j] = -1
+		}
+	}
+
+	x, y := 0, 0
+	width := 0
+	for head != nil {
+		for ; y < n-width && head != nil; y, head = y+1, head.Next {
+			ans[x][y] = head.Val
+		}
+		x, y = x+1, y-1
+
+		for ; x < m-width && head != nil; x, head = x+1, head.Next {
+			ans[x][y] = head.Val
+		}
+		x, y = x-1, y-1
+
+		for ; y >= width && head != nil; y, head = y-1, head.Next {
+			ans[x][y] = head.Val
+		}
+		x, y = x-1, y+1
+		for ; x > width && head != nil; x, head = x-1, head.Next {
+			ans[x][y] = head.Val
+		}
+		width++
+		x, y = width, width
+	}
+	return ans
 }
