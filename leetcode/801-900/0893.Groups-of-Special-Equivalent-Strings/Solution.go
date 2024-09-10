ba11eef19eaf80cf601424233b7ca223e80a1b0e
0xff-dev <stevenshuang521@gmail.com>
2024-09-10 09:08:01 +0800

    Add solution and test-cases for problem 893

--- leetcode/801-900/0893.Groups-of-Special-Equivalent-Strings/Solution.go
+++ leetcode/801-900/0893.Groups-of-Special-Equivalent-Strings/Solution.go
@@ -1,5 +1,17 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(words []string) int {
+	in := make(map[[2][26]int]int)
+	for _, word := range words {
+		k1, k2 := [26]int{}, [26]int{}
+		for i := range word {
+			if i&1 == 0 {
+				k1[word[i]-'a']++
+				continue
+			}
+			k2[word[i]-'a']++
+		}
+		in[[2][26]int{k1, k2}]++
+	}
+	return len(in)
 }
