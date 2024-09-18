340b61c1f7635db9ecee6709d99056a0261961b0
0xff-dev <stevenshuang521@gmail.com>
2024-09-18 09:12:39 +0800

    Add solution and test-cases for problem 1947

--- leetcode/1901-2000/1947.Maximum-Compatibility-Score-Sum/Solution.go
+++ leetcode/1901-2000/1947.Maximum-Compatibility-Score-Sum/Solution.go
@@ -1,5 +1,35 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(students [][]int, mentors [][]int) int {
+	var (
+		dfs   func(int, []bool) int
+		match func(int, int) int
+	)
+	match = func(i, j int) int {
+		ans := 0
+		for k := range students[i] {
+			if mentors[j][k] == students[i][k] {
+				ans++
+			}
+		}
+		return ans
+	}
+
+	dfs = func(index int, used []bool) int {
+		cur := 0
+		if index == len(students) {
+			return cur
+		}
+
+		for i := range mentors {
+			if used[i] {
+				continue
+			}
+			used[i] = true
+			cur = max(cur, match(index, i)+dfs(index+1, used))
+			used[i] = false
+		}
+		return cur
+	}
+	return dfs(0, make([]bool, len(students)))
 }
