150cb113a1427f7d9254eb5ec90b25745803f0ec
0xff-dev <stevenshuang521@gmail.com>
2024-09-23 09:07:27 +0800

    Add solution and test-cases for problem 1078

--- leetcode/1001-1100/1078.Occurrences-After-Bigram/Solution_test.go
+++ leetcode/1001-1100/1078.Occurrences-After-Bigram/Solution_test.go
@@ -9,31 +9,30 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name    string
+		a, b, c string
+		expect  []string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "alice is a good girl she is a good student", "a", "good", []string{"girl", "student"}},
+		{"TestCase2", "we will we will rock you", "we", "will", []string{"we", "rock"}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.a, c.b, c.c)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.a, c.b, c.c)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
