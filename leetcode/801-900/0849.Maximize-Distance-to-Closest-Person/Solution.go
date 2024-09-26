f25d09060e1589b4cc2f24bc3d75055cc867c550
0xff-dev <stevenshuang521@gmail.com>
2024-09-26 09:22:14 +0800

    Add solution and test-cases for problem 849

--- leetcode/801-900/0849.Maximize-Distance-to-Closest-Person/Solution.go
+++ leetcode/801-900/0849.Maximize-Distance-to-Closest-Person/Solution.go
@@ -1,5 +1,30 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(seats []int) int {
+	ans := 0
+	pre := -1
+	i := 0
+	for ; i < len(seats); i++ {
+		s := seats[i]
+		if s == 0 {
+			continue
+		}
+		if pre == -1 {
+			ans = i
+		} else {
+			diff := i - pre - 1
+			add := 0
+			if diff&1 == 1 {
+				add = 1
+			}
+			diff = diff/2 + add
+			ans = max(ans, diff)
+		}
+		pre = i
+	}
+	if seats[i-1] != 1 {
+		diff := i - pre - 1
+		ans = max(ans, diff)
+	}
+	return ans
 }
