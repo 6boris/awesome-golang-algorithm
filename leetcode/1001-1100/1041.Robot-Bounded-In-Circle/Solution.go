982cabb2439c297b77e82d4f68cbd0d131a55359
0xff-dev <stevenshuang521@gmail.com>
2024-09-30 09:45:10 +0800

    Add solution and test-cases for problem 1041

--- leetcode/1001-1100/1041.Robot-Bounded-In-Circle/Solution.go
+++ leetcode/1001-1100/1041.Robot-Bounded-In-Circle/Solution.go
@@ -1,5 +1,44 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+var (
+	turn90Degrees = []byte{'n', 'e', 's', 'w'}
+
+	dirMap = map[byte][2]int{
+		'n': {0, 1},
+		's': {0, -1},
+		'w': {-1, 0},
+		'e': {1, 0},
+	}
+)
+
+func Solution(instructions string) bool {
+	x, y := 0, 0
+	dirIndex := 0
+	var (
+		cur [2]int
+		add int
+	)
+	for i := 0; i < 4; i++ {
+		for _, i := range instructions {
+			cur = dirMap[turn90Degrees[dirIndex]]
+			if i == 'G' {
+				x, y = x+cur[0], y+cur[1]
+				continue
+			}
+
+			if i == 'L' {
+				add = -1
+			}
+			if i == 'R' {
+				add = 1
+			}
+
+			dirIndex = (dirIndex + add + 4) % 4
+		}
+		if x == 0 && y == 0 {
+			return true
+		}
+	}
+
+	return false
 }
