430294cfa7d0612865efd1c6ed5f081a6b09964f
0xff-dev <stevenshuang521@gmail.com>
2024-10-03 16:14:44 +0800

    Add solution and test-cases for problem 1590

--- leetcode/1501-1600/1590.Make-Sum-Divisible-by-P/Solution_test.go
+++ leetcode/1501-1600/1590.Make-Sum-Divisible-by-P/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		nums   []int
+		p      int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{3, 1, 4, 2}, 6, 1},
+		{"TestCase2", []int{6, 3, 5, 2}, 9, 2},
+		{"TestCase3", []int{1, 2, 3}, 3, 0},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.nums, c.p)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.nums, c.p)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
