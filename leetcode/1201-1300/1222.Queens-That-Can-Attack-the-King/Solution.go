23b2130f0af9a98b2291e8ac75c385a505a28a01
0xff-dev <stevenshuang521@gmail.com>
2024-10-23 09:27:33 +0800

    Add solution and test-cases for problem 1222

--- leetcode/1201-1300/1222.Queens-That-Can-Attack-the-King/Solution.go
+++ leetcode/1201-1300/1222.Queens-That-Can-Attack-the-King/Solution.go
@@ -1,5 +1,67 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(queens [][]int, king []int) [][]int {
+	ans := make([][]int, 0)
+	board := [8][8]int{}
+	for _, q := range queens {
+		board[q[0]][q[1]] = 1
+	}
+	x, y := king[0], king[1]
+	for r := y - 1; r >= 0; r-- {
+		if board[x][r] == 1 {
+			ans = append(ans, []int{x, r})
+			break
+		}
+	}
+	for r := y + 1; r < 8; r++ {
+		if board[x][r] == 1 {
+			ans = append(ans, []int{x, r})
+			break
+		}
+	}
+
+	for r := x - 1; r >= 0; r-- {
+		if board[r][y] == 1 {
+			ans = append(ans, []int{r, y})
+			break
+		}
+	}
+
+	for r := x + 1; r < 8; r++ {
+		if board[r][y] == 1 {
+			ans = append(ans, []int{r, y})
+			break
+		}
+	}
+
+	// 对
+	for l, r := x-1, y-1; l >= 0 && r >= 0; l, r = l-1, r-1 {
+		if board[l][r] == 1 {
+			ans = append(ans, []int{l, r})
+			break
+		}
+	}
+
+	for l, r := x-1, y+1; l >= 0 && r < 8; l, r = l-1, r+1 {
+		if board[l][r] == 1 {
+			ans = append(ans, []int{l, r})
+			break
+		}
+	}
+
+	for l, r := x+1, y+1; l < 8 && r < 8; l, r = l+1, r+1 {
+		if board[l][r] == 1 {
+			ans = append(ans, []int{l, r})
+			break
+		}
+	}
+
+	for l, r := x+1, y-1; l < 8 && r >= 0; l, r = l+1, r-1 {
+		if board[l][r] == 1 {
+			ans = append(ans, []int{l, r})
+			break
+		}
+	}
+
+	return ans
 }
