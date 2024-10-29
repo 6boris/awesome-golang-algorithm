0b21d3bf1789905817ce3ce46f24b31d537cc41a
0xff-dev <stevenshuang521@gmail.com>
2024-10-29 09:12:20 +0800

    Add solution and test-cases for problem 2684

--- leetcode/2601-2700/2684.Maximum-Number-of-Moves-in-a-Grid/Solution.go
+++ leetcode/2601-2700/2684.Maximum-Number-of-Moves-in-a-Grid/Solution.go
@@ -1,5 +1,27 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(grid [][]int) int {
+	rows, cols := len(grid), len(grid[0])
+	dp := make([][]int, rows)
+	for i := range rows {
+		dp[i] = make([]int, cols)
+	}
+	ans := 0
+	for j := cols - 2; j >= 0; j-- {
+		for i := 0; i < rows; i++ {
+			if i-1 >= 0 && grid[i-1][j+1] > grid[i][j] {
+				dp[i][j] = max(dp[i][j], dp[i-1][j+1]+1)
+			}
+			if grid[i][j+1] > grid[i][j] {
+				dp[i][j] = max(dp[i][j], dp[i][j+1]+1)
+			}
+			if i+1 < rows && grid[i+1][j+1] > grid[i][j] {
+				dp[i][j] = max(dp[i][j], dp[i+1][j+1]+1)
+			}
+		}
+	}
+	for i := 0; i < rows; i++ {
+		ans = max(ans, dp[i][0])
+	}
+	return ans
 }
--- leetcode/2601-2700/2684.Maximum-Number-of-Moves-in-a-Grid/Solution_test.go
+++ leetcode/2601-2700/2684.Maximum-Number-of-Moves-in-a-Grid/Solution_test.go
@@ -10,12 +10,11 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs [][]int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{{2, 3, 4, 5}, {5, 4, 9, 3}, {3, 4, 2, 11}, {10, 9, 13, 15}}, 3},
+		{"TestCase2", [][]int{{3, 2, 4}, {2, 1, 9}, {1, 1, 7}}, 0},
 	}
 
 	//	开始测试
@@ -30,10 +29,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
