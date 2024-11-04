b1b387240cc0fc98f1b285b6385c217f7915f886
0xff-dev <stevenshuang521@gmail.com>
2024-11-04 09:06:17 +0800

    Add solution and test-cases for problem 3163

--- leetcode/3101-3200/3163.String-Compression-III/Solution_test.go
+++ leetcode/3101-3200/3163.String-Compression-III/Solution_test.go
@@ -10,12 +10,11 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs string
+		expect string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "abcde", "1a1b1c1d1e"},
+		{"TestCase2", "aaaaaaaaaaaaaabb", "9a5a2b"},
 	}
 
 	//	开始测试
@@ -30,10 +29,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
