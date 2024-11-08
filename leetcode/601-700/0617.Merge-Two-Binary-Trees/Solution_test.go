5e3c6344d3dab7a51413b325a850c8c8ab93711a
0xff-dev <stevenshuang521@gmail.com>
2024-11-08 09:15:17 +0800

    Add solution and test-cases for problem 617

--- leetcode/601-700/0617.Merge-Two-Binary-Trees/Solution_test.go
+++ leetcode/601-700/0617.Merge-Two-Binary-Trees/Solution_test.go
@@ -10,30 +10,57 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		r1, r2 *TreeNode
+		expect *TreeNode
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", &TreeNode{
+			Val: 1,
+			Left: &TreeNode{
+				Val:  3,
+				Left: &TreeNode{Val: 5},
+			},
+			Right: &TreeNode{Val: 2},
+		}, &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val:   1,
+				Right: &TreeNode{Val: 4},
+			},
+			Right: &TreeNode{
+				Val:   3,
+				Right: &TreeNode{Val: 7},
+			},
+		}, &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val:   4,
+				Left:  &TreeNode{Val: 5},
+				Right: &TreeNode{Val: 4},
+			},
+			Right: &TreeNode{
+				Val:   5,
+				Right: &TreeNode{Val: 7},
+			},
+		}},
+		{"TestCase2", &TreeNode{Val: 1}, &TreeNode{Val: 1, Left: &TreeNode{Val: 2}}, &TreeNode{Val: 2, Left: &TreeNode{Val: 2}}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.r1, c.r2)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.r1, c.r2)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
