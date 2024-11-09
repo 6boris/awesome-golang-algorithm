68370931d7c7c5bc1fd83e541a01797ee23f676d
0xff-dev <stevenshuang521@gmail.com>
2024-11-09 16:51:52 +0800

    Add solution and test-cases for problem 3133

--- leetcode/3101-3200/3133.Minimum-Array-End/Solution.go
+++ leetcode/3101-3200/3133.Minimum-Array-End/Solution.go
@@ -1,5 +1,33 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int, x int) int64 {
+	ans := int64(x)
+	if n == 1 {
+		return ans
+	}
+	nn := int64(n) - 1
+	zero := 0
+	c := int64(0)
+	for {
+		zero++
+		c = 1 << (zero - 1)
+		if nn <= c {
+			break
+		}
+		nn -= c
+	}
+	c |= (nn - 1)
+	cur := int64(1)
+	for ; c > 0; c >>= 1 {
+		for ans&cur != 0 {
+			cur <<= 1
+		}
+		now := c & 1
+		if now == 1 {
+			ans |= cur
+			continue
+		}
+		cur <<= 1
+	}
+	return ans
 }
