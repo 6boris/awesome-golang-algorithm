74dd1af58ddabd9eaa3c87fce990ccf7251514fd
0xff-dev <stevenshuang521@gmail.com>
2024-11-13 09:27:42 +0800

    Add solution and test-cases for problem 2563

--- leetcode/2501-2600/2563.Count-the-Number-of-Fair-Pairs/Solution.go
+++ leetcode/2501-2600/2563.Count-the-Number-of-Fair-Pairs/Solution.go
@@ -1,5 +1,27 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(nums []int, lower, upper int) int64 {
+	sort.Ints(nums)
+	var ans int64 = 0
+	l := len(nums)
+	for i := 0; i < len(nums); i++ {
+		// 1, 2, 3, 4
+		tmpL := l - i - 1
+		leftIndex := sort.Search(tmpL, func(ii int) bool {
+			return nums[i]+nums[ii+1+i] >= lower
+		})
+		if leftIndex == tmpL {
+			continue
+		}
+		rightIndex := sort.Search(tmpL, func(ii int) bool {
+			return nums[i]+nums[ii+1+i] > upper
+		})
+		if rightIndex == tmpL && nums[i]+nums[l-1] > upper {
+			continue
+		}
+		ans += int64(rightIndex - leftIndex)
+	}
+	return ans
 }
--- leetcode/2501-2600/2563.Count-the-Number-of-Fair-Pairs/Solution_test.go
+++ leetcode/2501-2600/2563.Count-the-Number-of-Fair-Pairs/Solution_test.go
@@ -10,30 +10,30 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		nums   []int
+		l, p   int
+		expect int64
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{0, 1, 7, 4, 4, 5}, 3, 6, 6},
+		{"TestCase2", []int{1, 7, 9, 2, 5}, 11, 11, 1},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.nums, c.l, c.p)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.nums, c.l, c.p)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
