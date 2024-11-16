94ed8ea4708ebe615f5234dce8322a4665bc0ca8
0xff-dev <stevenshuang521@gmail.com>
2024-11-16 12:49:49 +0800

    Add solution and test-cases for problem 3254

--- leetcode/3201-3300/3254.Find-the-Power-of-K-Size-Subarrays-I/Solution.go
+++ leetcode/3201-3300/3254.Find-the-Power-of-K-Size-Subarrays-I/Solution.go
@@ -1,5 +1,23 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, k int) []int {
+	l := len(nums)
+	ml := make([]int, l)
+	ml[0] = 1
+	for i := 1; i < l; i++ {
+		if nums[i]-nums[i-1] == 1 {
+			ml[i] = ml[i-1] + 1
+			continue
+		}
+		ml[i] = 1
+	}
+	ans := make([]int, l-k+1)
+	idx := 0
+	for i := k - 1; i < l; i, idx = i+1, idx+1 {
+		ans[idx] = -1
+		if ml[i] >= k {
+			ans[idx] = nums[i]
+		}
+	}
+	return ans
 }
--- leetcode/3201-3300/3254.Find-the-Power-of-K-Size-Subarrays-I/Solution_test.go
+++ leetcode/3201-3300/3254.Find-the-Power-of-K-Size-Subarrays-I/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		nums   []int
+		k      int
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 2, 3, 4, 3, 2, 5}, 3, []int{3, 4, -1, -1, -1}},
+		{"TestCase2", []int{2, 2, 2, 2, 2}, 4, []int{-1, -1}},
+		{"TestCase3", []int{3, 2, 3, 2, 3, 2}, 2, []int{-1, 3, -1, 3, -1}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.nums, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.nums, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
