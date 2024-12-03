0a7b5821ba70060ce44d9100f57416e5db87f1b1
0xff-dev <stevenshuang521@gmail.com>
2024-12-03 09:13:02 +0800

    Add solution and test-cases for problem 1324

--- leetcode/1301-1400/1324.Print-Words-Vertically/Solution.go
+++ leetcode/1301-1400/1324.Print-Words-Vertically/Solution.go
@@ -1,5 +1,31 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "strings"
+
+func Solution(s string) []string {
+	words := strings.Split(s, " ")
+	ml := 0
+	for _, w := range words {
+		ml = max(ml, len(w))
+	}
+	ans := make([]string, 0)
+	buf := strings.Builder{}
+	for index := 0; index < ml; index++ {
+		tailSpaces := 0
+		for _, w := range words {
+			if index < len(w) {
+				buf.WriteByte(w[index])
+				tailSpaces = 0
+				continue
+			}
+			buf.WriteByte(' ')
+			tailSpaces++
+		}
+		cur := buf.String()
+		l := len(cur) - tailSpaces
+		cur = cur[:l]
+		ans = append(ans, cur)
+		buf.Reset()
+	}
+	return ans
 }
