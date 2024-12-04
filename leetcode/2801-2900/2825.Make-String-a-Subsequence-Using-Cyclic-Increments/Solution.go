01c3d750d20e86c7aff7d0df8b017f82bef9b3de
0xff-dev <stevenshuang521@gmail.com>
2024-12-04 09:26:21 +0800

    Add solution and test-cases for problem 2825

--- leetcode/2801-2900/2825.Make-String-a-Subsequence-Using-Cyclic-Increments/Solution.go
+++ leetcode/2801-2900/2825.Make-String-a-Subsequence-Using-Cyclic-Increments/Solution.go
@@ -1,5 +1,16 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(str1 string, str2 string) bool {
+	if len(str2) > len(str1) {
+		return false
+	}
+	b := 0
+	for i := 0; i < len(str1) && b < len(str2); i++ {
+		t1 := str1[i] - 'a'
+		t2 := str2[b] - 'a'
+		if t1 == t2 || t2 == (t1+1+26)%26 {
+			b++
+		}
+	}
+	return b == len(str2)
 }
