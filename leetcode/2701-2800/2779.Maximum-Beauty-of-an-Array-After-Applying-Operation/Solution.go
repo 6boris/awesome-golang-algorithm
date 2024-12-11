634fec46f47c750ddf97e2c44e33cb477f6b8ba2
0xff-dev <stevenshuang521@gmail.com>
2024-12-11 09:30:36 +0800

    Add solution and test-cases for problem 2779

--- leetcode/2701-2800/2779.Maximum-Beauty-of-an-Array-After-Applying-Operation/Solution.go
+++ leetcode/2701-2800/2779.Maximum-Beauty-of-an-Array-After-Applying-Operation/Solution.go
@@ -1,5 +1,39 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(nums []int, k int) int {
+	sort.Ints(nums)
+	ans := 0
+	start, end := 0, 0
+	// 排序以后，能够出现交集的情况就是
+	// 最小的元素定义了右边界
+	// 最大的元素定义了左边界
+	// re 就是交集的右边界
+	re := nums[0] + k
+
+	for ; end < len(nums); end++ {
+		// 还在同一个范围
+		s := nums[end] - k
+		if s <= re {
+			// 还有可以增加de
+			continue
+		}
+		diff := end - start
+		ans = max(ans, diff)
+
+		// 开始减少左侧的数据
+		// 尝试移除start
+		start++
+		for ; start <= end; start++ {
+			re = nums[start] + k
+			if s <= re {
+				break
+			}
+		}
+	}
+	diff := end - start
+	ans = max(ans, diff)
+
+	return ans
 }
