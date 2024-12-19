06779af2605228d9f2ca566029063103ffbb0447
0xff-dev <stevenshuang521@gmail.com>
2024-12-19 12:40:10 +0800

    Add solution and test-cases for problem 923

--- leetcode/901-1000/0923.3Sum-With-Multiplicity/Solution.go
+++ leetcode/901-1000/0923.3Sum-With-Multiplicity/Solution.go
@@ -1,5 +1,25 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+const mod923 = 1000000007
+
+func Solution(arr []int, target int) int {
+	dp := make([][101]int, len(arr))
+	dp[0][arr[0]] = 1
+	for i := 1; i < len(arr); i++ {
+		for j := 0; j < 101; j++ {
+			dp[i][j] = dp[i-1][j]
+		}
+		dp[i][arr[i]]++
+	}
+
+	ans := 0
+	for i := 2; i < len(arr); i++ {
+		cur := arr[i]
+		for pre := i - 1; pre > 0; pre-- {
+			if diff := target - arr[pre] - cur; diff >= 0 && diff <= 100 {
+				ans = (ans + dp[pre-1][diff]) % mod923
+			}
+		}
+	}
+	return ans
 }
