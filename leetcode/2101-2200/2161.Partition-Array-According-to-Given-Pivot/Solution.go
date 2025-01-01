7206d9c7f9fc889f4fd3ba2bed5dba44cd57c36b
0xff-dev <stevenshuang521@gmail.com>
2025-01-01 21:10:25 +0800

    Add solution and test-cases for problem 2161

--- leetcode/2101-2200/2161.Partition-Array-According-to-Given-Pivot/Solution.go
+++ leetcode/2101-2200/2161.Partition-Array-According-to-Given-Pivot/Solution.go
@@ -1,5 +1,22 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, pivot int) []int {
+	i, j := 0, len(nums)-1
+	res := make([]int, len(nums))
+	for k := 0; k < len(nums); k++ {
+		if nums[k] < pivot {
+			res[i] = nums[k]
+			i++
+		}
+	}
+	for k := len(nums) - 1; k >= 0; k-- {
+		if nums[k] > pivot {
+			res[j] = nums[k]
+			j--
+		}
+	}
+	for ; i <= j; i++ {
+		res[i] = pivot
+	}
+	return res
 }
--- leetcode/2101-2200/2161.Partition-Array-According-to-Given-Pivot/Solution_test.go
+++ leetcode/2101-2200/2161.Partition-Array-According-to-Given-Pivot/Solution_test.go
@@ -10,30 +10,30 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		nums   []int
+		pivot  int
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{9, 12, 5, 10, 14, 3, 10}, 10, []int{9, 5, 3, 10, 10, 12, 14}},
+		{"TestCase2", []int{-3, 4, 3, 2}, 2, []int{-3, 2, 4, 3}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.nums, c.pivot)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.nums, c.pivot)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
