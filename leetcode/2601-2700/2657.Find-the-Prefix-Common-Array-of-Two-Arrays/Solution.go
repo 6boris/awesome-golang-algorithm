0f8737a592b9e3eeb055f55c01a6effc0d1645b1
0xff-dev <stevenshuang521@gmail.com>
2025-01-14 08:59:33 +0800

    Add solution and test-cases for problem 2657

--- leetcode/2601-2700/2657.Find-the-Prefix-Common-Array-of-Two-Arrays/Solution.go
+++ leetcode/2601-2700/2657.Find-the-Prefix-Common-Array-of-Two-Arrays/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(A []int, B []int) []int {
+	ans := make([]int, len(A))
+	count := make([]uint8, len(A)+1)
+	for i := 0; i < len(A); i++ {
+		if i > 0 {
+			ans[i] = ans[i-1]
+		}
+		count[A[i]]++
+		count[B[i]]++
+		if A[i] == B[i] {
+			ans[i]++
+			continue
+		}
+		if count[A[i]] == 2 {
+			ans[i]++
+		}
+		if count[B[i]] == 2 {
+			ans[i]++
+		}
+	}
+	return ans
 }
