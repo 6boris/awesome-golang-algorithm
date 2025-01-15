c91f7ed445470cf4015438ee293942764c59fb4f
0xff-dev <stevenshuang521@gmail.com>
2025-01-15 09:46:25 +0800

    Add solution and test-cases for problem 2429

--- leetcode/2401-2500/2429.Minimize-XOR/Solution_test.go
+++ leetcode/2401-2500/2429.Minimize-XOR/Solution_test.go
@@ -10,30 +10,29 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		n1, n2 int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 3, 5, 3},
+		{"TestCase2", 1, 12, 3},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n1, c.n2)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.n1, c.n2)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
