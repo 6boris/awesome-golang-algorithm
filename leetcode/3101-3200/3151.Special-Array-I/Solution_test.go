628e46daa949541625dcc2a4a59b2070aad3b50d
0xff-dev <stevenshuang521@gmail.com>
2025-02-01 09:02:31 +0800

    Add solution and test-cases for problem 3151

--- leetcode/3101-3200/3151.Special-Array-I/Solution_test.go
+++ leetcode/3101-3200/3151.Special-Array-I/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
+		inputs []int
 		expect bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1}, true},
+		{"TestCase2", []int{2, 1, 4}, true},
+		{"TestCase3", []int{4, 3, 1, 6}, false},
 	}
 
 	//	开始测试
@@ -30,10 +30,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
