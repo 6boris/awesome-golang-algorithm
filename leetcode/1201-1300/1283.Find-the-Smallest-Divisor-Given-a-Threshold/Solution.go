793b77ce0c65e340baa123052da9cfc3ceda8709
0xff-dev <stevenshuang521@gmail.com>
2025-02-06 22:25:42 +0800

    Add solution and test-cases for problem 1283

--- leetcode/1201-1300/1283.Find-the-Smallest-Divisor-Given-a-Threshold/Solution.go
+++ leetcode/1201-1300/1283.Find-the-Smallest-Divisor-Given-a-Threshold/Solution.go
@@ -1,5 +1,29 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"slices"
+	"sort"
+)
+
+func Solution(nums []int, threshold int) int {
+	m := slices.Max(nums)
+	var ok func(int) int
+	ok = func(divisor int) int {
+		sum := 0
+		for _, n := range nums {
+			a := n / divisor
+			if n%divisor != 0 {
+				a++
+			}
+			sum += a
+		}
+		return sum
+	}
+	idx := sort.Search(m+1, func(n int) bool {
+		if n == 0 {
+			return false
+		}
+		return ok(n) <= threshold
+	})
+	return idx
 }
