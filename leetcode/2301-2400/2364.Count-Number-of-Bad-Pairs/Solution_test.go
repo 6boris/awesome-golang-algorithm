e9fa65e97f8c873c42bd4cb87016ae18ea8a1e6d
0xff-dev <stevenshuang521@gmail.com>
2025-02-09 17:25:30 +0800

    Add solution and test-cases for problem 2364

--- leetcode/2301-2400/2364.Count-Number-of-Bad-Pairs/Solution_test.go
+++ leetcode/2301-2400/2364.Count-Number-of-Bad-Pairs/Solution_test.go
@@ -10,12 +10,11 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		expect int64
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{4, 1, 3, 3}, 5},
+		{"TestCase2", []int{1, 2, 3, 4, 5}, 0},
 	}
 
 	//	开始测试
@@ -30,10 +29,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
