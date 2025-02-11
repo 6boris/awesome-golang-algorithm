1b429f666e72e92d8cf04248e2d05bd8a15324e4
0xff-dev <stevenshuang521@gmail.com>
2025-02-11 09:00:36 +0800

    Add solution for problem 1910

--- leetcode/1901-2000/1910.Remove-All-Occurrences-of-a-Substring/Solution.go
+++ leetcode/1901-2000/1910.Remove-All-Occurrences-of-a-Substring/Solution.go
@@ -19,3 +19,31 @@ func Solution(s string, part string) string {
 
 	return s
 }
+
+func Solution1(s string, part string) string {
+	bs := []byte(s)
+	index := -1
+	l := len(part)
+	lastByte := part[l-1]
+	for i := 0; i < len(s); i++ {
+		index++
+		bs[index] = s[i]
+		if s[i] != lastByte {
+			continue
+		}
+
+		if start := index - l + 1; start >= 0 {
+			if string(bs[start:index+1]) == part {
+				index = start - 1
+			}
+		}
+
+	}
+	if start := index - l + 1; start >= 0 {
+		if string(bs[start:index+1]) == part {
+			index = start - 1
+		}
+	}
+
+	return string(bs[:index+1])
+}
