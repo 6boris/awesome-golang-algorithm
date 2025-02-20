222a9f8e7e21d74e55801a688efe9df0359a4343
0xff-dev <stevenshuang521@gmail.com>
2025-02-20 09:16:47 +0800

    Add solution and test-cases for problem 942

--- leetcode/901-1000/0942.DI-String-Match/Solution.go
+++ leetcode/901-1000/0942.DI-String-Match/Solution.go
@@ -1,5 +1,28 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string) []int {
+	res := make([]int, len(s)+1)
+	for i := range res {
+		res[i] = -1
+	}
+
+	index := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == 'D' {
+			continue
+		}
+		res[i] = index
+		index++
+		for pre := i - 1; pre >= 0 && res[pre] == -1; pre-- {
+			res[pre] = index
+			index++
+		}
+	}
+	res[len(s)] = index
+	index++
+	for pre := len(s) - 1; pre >= 0 && res[pre] == -1; pre-- {
+		res[pre] = index
+		index++
+	}
+	return res
 }
--- leetcode/901-1000/0942.DI-String-Match/Solution_test.go
+++ leetcode/901-1000/0942.DI-String-Match/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs string
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "IDID", []int{0, 2, 1, 4, 3}},
+		{"TestCase2", "III", []int{0, 1, 2, 3}},
+		{"TestCase3", "DDI", []int{2, 1, 0, 3}},
 	}
 
 	//	开始测试
@@ -30,10 +30,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
