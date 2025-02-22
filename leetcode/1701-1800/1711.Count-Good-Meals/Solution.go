c7cfecde83976d21f67dc497dfbf5108395532bf
0xff-dev <stevenshuang521@gmail.com>
2025-02-22 20:35:13 +0800

    Add solution and test-cases for problem 1711

--- leetcode/1701-1800/1711.Count-Good-Meals/Solution.go
+++ leetcode/1701-1800/1711.Count-Good-Meals/Solution.go
@@ -1,5 +1,31 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+const mod = 1000000007
+
+func Solution(deliciousness []int) int {
+	cur := int64(1)
+	cnt := make(map[int64]int)
+	for _, n := range deliciousness {
+		cnt[int64(n)]++
+	}
+	ans := 0
+	for i := 0; i <= 41; i++ {
+		skip := map[int64]struct{}{}
+		for d, n := range cnt {
+			if _, ok := skip[d]; ok {
+				continue
+			}
+			diff := cur - d
+			if v, ok := cnt[diff]; ok {
+				skip[diff] = struct{}{}
+				if diff == d {
+					ans = (ans + n*(n-1)/2) % mod
+				} else {
+					ans = (ans + n*v) % mod
+				}
+			}
+		}
+		cur <<= 1
+	}
+	return ans
 }
