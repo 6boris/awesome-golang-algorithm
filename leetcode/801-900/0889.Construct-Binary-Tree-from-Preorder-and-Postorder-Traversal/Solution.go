60d09e7f0a9df900bcd594af918f3b4c28742905
0xff-dev <stevenshuang521@gmail.com>
2025-02-23 16:29:59 +0800

    Add solution and test-cases for problem 889

--- leetcode/801-900/0889.Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal/Solution.go
+++ leetcode/801-900/0889.Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal/Solution.go
@@ -1,5 +1,39 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func Solution(preorder []int, postorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{
+		Val: preorder[0],
+	}
+	if len(preorder) == 1 {
+		return root
+	}
+
+	postIndex := 0
+	next := preorder[1]
+	for i := range postorder {
+		if postorder[i] == next {
+			postIndex = i
+			break
+		}
+	}
+	preIndex := 0
+	m := map[int]int{}
+	for i, n := range preorder {
+		m[n] = i
+	}
+	for i := 0; i <= postIndex; i++ {
+		preIndex = max(preIndex, m[postorder[i]])
+	}
+	root.Left = Solution(preorder[1:preIndex+1], postorder[:postIndex+1])
+	root.Right = Solution(preorder[preIndex+1:], postorder[postIndex+1:len(postorder)-1])
+	return root
 }
--- leetcode/801-900/0889.Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal/Solution_test.go
+++ leetcode/801-900/0889.Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal/Solution_test.go
@@ -9,31 +9,46 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name                string
+		preorder, postorder []int
+		expect              *TreeNode
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 2, 4, 5, 3, 6, 7}, []int{4, 5, 2, 6, 7, 3, 1}, &TreeNode{
+			Val: 1,
+			Left: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val: 4,
+				},
+				Right: &TreeNode{Val: 5},
+			},
+			Right: &TreeNode{
+				Val: 3,
+				Left: &TreeNode{
+					Val: 6,
+				},
+				Right: &TreeNode{Val: 7},
+			},
+		}},
+		{"TestCase2", []int{1}, []int{1}, &TreeNode{Val: 1}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.preorder, c.postorder)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.preorder, c.postorder)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
