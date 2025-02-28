99dcf12fb21864bf56d657c8946c45fd57d9723a
0xff-dev <stevenshuang521@gmail.com>
2025-02-28 09:16:14 +0800

    Add solution and test-cases for problem 917

--- leetcode/901-1000/0917.Reverse-Only-Letters/Solution.go
+++ leetcode/901-1000/0917.Reverse-Only-Letters/Solution.go
@@ -1,5 +1,20 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func ok(b byte) bool {
+	return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z')
+}
+
+func Solution(s string) string {
+	bs := []byte(s)
+	l, r := 0, len(bs)-1
+	for l < r {
+		for ; l < r && !ok(bs[l]); l++ {
+
+		}
+		for ; r > l && !ok(bs[r]); r-- {
+		}
+		bs[l], bs[r] = bs[r], bs[l]
+		l, r = l+1, r-1
+	}
+	return string(bs)
 }
