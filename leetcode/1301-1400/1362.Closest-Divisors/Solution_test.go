7ba49f497e12291ba8a23735242312fd45ae0adb
0xff-dev <stevenshuang521@gmail.com>
2025-03-04 09:22:58 +0800

    Add solution and test-cases for problem 1362

--- leetcode/1301-1400/1362.Closest-Divisors/Solution_test.go
+++ leetcode/1301-1400/1362.Closest-Divisors/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs int
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 8, []int{3, 3}},
+		{"TestCase2", 123, []int{5, 25}},
+		{"TestCase3", 999, []int{25, 40}},
 	}
 
 	//	开始测试
@@ -30,10 +30,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
