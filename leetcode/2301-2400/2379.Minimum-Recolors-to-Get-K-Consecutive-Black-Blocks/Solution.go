ab240d30fbc8cc9ad2c4d69f3101a3e69f764a78
0xff-dev <stevenshuang521@gmail.com>
2025-03-08 20:03:57 +0800

    Add solution and test-cases for problem 2379

--- leetcode/2301-2400/2379.Minimum-Recolors-to-Get-K-Consecutive-Black-Blocks/Solution.go
+++ leetcode/2301-2400/2379.Minimum-Recolors-to-Get-K-Consecutive-Black-Blocks/Solution.go
@@ -1,5 +1,30 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(blocks string, k int) int {
+	w, b := 0, 0
+	for i := 0; i < k; i++ {
+		if blocks[i] == 'W' {
+			w++
+			continue
+		}
+		b++
+	}
+	ans := w
+	start, end := 0, k
+	for ; end < len(blocks); end++ {
+		if blocks[end] == 'W' {
+			w++
+		} else {
+			b++
+		}
+		if blocks[start] == 'W' {
+			w--
+		} else {
+			b--
+		}
+		ans = min(ans, w)
+		start++
+	}
+	return ans
+
 }
