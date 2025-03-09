9bae5d73377af1716fef23c32554d658d42a48a5
0xff-dev <stevenshuang521@gmail.com>
2025-03-09 16:17:18 +0800

    Add solution and test-cases for problem 3208

--- leetcode/3201-3300/3208.Alternating-Groups-II/Solution.go
+++ leetcode/3201-3300/3208.Alternating-Groups-II/Solution.go
@@ -1,5 +1,31 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(colors []int, k int) int {
+	l := len(colors)
+	dp := make([]int, len(colors))
+	dp[0] = 1
+	for i := 1; i < l; i++ {
+		if colors[i] != colors[i-1] {
+			dp[i] = dp[i-1] + 1
+			continue
+		}
+		dp[i] = 1
+	}
+	ans := 0
+	for start := 0; start <= l-k; start++ {
+		end := start + k - 1
+		if dp[end] >= k {
+			ans++
+		}
+	}
+	if colors[0] != colors[l-1] {
+		for start := l - k + 1; start < l; start++ {
+			end := (start+k)%l - 1
+			if dp[l-1] >= l-start && dp[end] >= end+1 {
+				ans++
+			}
+		}
+	}
+
+	return ans
 }
