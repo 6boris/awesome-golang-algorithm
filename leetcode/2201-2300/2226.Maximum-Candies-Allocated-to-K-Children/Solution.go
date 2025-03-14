2ab530e1604d8d98b2d234a4b666daca68211043
0xff-dev <stevenshuang521@gmail.com>
2025-03-14 09:16:51 +0800

    Add solution and test-cases for problem 2226

--- leetcode/2201-2300/2226.Maximum-Candies-Allocated-to-K-Children/Solution.go
+++ leetcode/2201-2300/2226.Maximum-Candies-Allocated-to-K-Children/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
+import (
+	"slices"
+	"sort"
+)
+
+func Solution(candies []int, k int64) int {
+	m := slices.Max(candies)
+	var ok func(int) bool
+	ok = func(n int) bool {
+		cnt := int64(0)
+		for _, c := range candies {
+			cnt += int64(c / n)
+		}
+		return cnt >= k
+	}
+	// 0, 1, 2, 3, ,4
+	// 1, 2, 3, 4, ,5
+	x := sort.Search(m+1, func(i int) bool {
+		return !ok(i + 1)
+	})
 	return x
 }
--- leetcode/2201-2300/2226.Maximum-Candies-Allocated-to-K-Children/Solution_test.go
+++ leetcode/2201-2300/2226.Maximum-Candies-Allocated-to-K-Children/Solution_test.go
@@ -9,31 +9,31 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name    string
+		candies []int
+		k       int64
+		expect  int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{5, 8, 6}, 3, 5},
+		{"TestCase2", []int{2, 5}, 11, 0},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.candies, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.candies, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
