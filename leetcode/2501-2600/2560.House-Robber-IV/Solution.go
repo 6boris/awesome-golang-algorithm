c339b5fe8c6fc6d7df672cc9df20c66caace12ae
0xff-dev <stevenshuang521@gmail.com>
2025-03-15 18:36:51 +0800

    Add solution and test-cases for problem 2560

--- leetcode/2501-2600/2560.House-Robber-IV/Solution.go
+++ leetcode/2501-2600/2560.House-Robber-IV/Solution.go
@@ -1,5 +1,37 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "slices"
+
+func Solution(nums []int, k int) int {
+	l := len(nums)
+	left, right := 0, slices.Max(nums)
+
+	capFn := func(capability int) bool {
+		count := 0
+		i := 0
+
+		for i < l {
+			if nums[i] <= capability {
+				count++
+				i++
+				if i < l {
+					i++
+				}
+			} else {
+				i++
+			}
+		}
+		return count >= k
+	}
+
+	for left < right {
+		mid := (left + right) / 2
+		if capFn(mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left
 }
