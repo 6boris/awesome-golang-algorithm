dba89629329d1d82cb30d7ec6f0951cd0ba77d79
0xff-dev <stevenshuang521@gmail.com>
2025-03-16 17:10:46 +0800

    Add solution and test-cases for problem 2594

--- leetcode/2501-2600/2594.Minimum-Time-to-Repair-Cars/Solution.go
+++ leetcode/2501-2600/2594.Minimum-Time-to-Repair-Cars/Solution.go
@@ -1,5 +1,31 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"math"
+	"slices"
+)
+
+func Solution(ranks []int, cars int) int64 {
+	var ok func(int64) bool
+	ok = func(minutes int64) bool {
+		c := int64(0)
+		for _, r := range ranks {
+			nn := minutes / int64(r)
+			i := int64(math.Sqrt(float64(nn)))
+			c += i
+		}
+		return c >= int64(cars)
+	}
+
+	m := slices.Max(ranks)
+	l, r := int64(0), int64(m)*int64(cars)*int64(cars)+1
+	for l < r {
+		m := (l + r) / 2
+		if ok(m) {
+			r = m
+			continue
+		}
+		l = m + 1
+	}
+	return l
 }
