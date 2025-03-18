a4c494137fbcd0ddaf7bd661a344b8996081386c
0xff-dev <stevenshuang521@gmail.com>
2025-03-18 22:28:42 +0800

    Add solution and test-cases for problem 2401

--- leetcode/2401-2500/2401.Longest-Nice-Subarray/Solution.go
+++ leetcode/2401-2500/2401.Longest-Nice-Subarray/Solution.go
@@ -1,5 +1,29 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) int {
+	if len(nums) == 1 {
+		return 1
+	}
+
+	cur := 0
+	start, end := 0, 0
+	ans := 0
+	for ; end < len(nums); end++ {
+		if cur&nums[end] == 0 {
+			cur |= nums[end]
+			continue
+		}
+		ans = max(ans, end-start)
+
+		for ; start <= end; start++ {
+			cur ^= nums[start]
+			if cur&nums[end] == 0 {
+				start++
+				cur |= nums[end]
+				break
+			}
+		}
+	}
+	ans = max(ans, end-start)
+	return ans
 }
