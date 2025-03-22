7ffa49d11a74feb92f8adbb58a5bd0b309a4de07
0xff-dev <stevenshuang521@gmail.com>
2025-03-22 20:42:35 +0800

    Add solution and test-cases for problem 789

--- leetcode/701-800/0789.Escape-The-Ghosts/Solution_test.go
+++ leetcode/701-800/0789.Escape-The-Ghosts/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
+		ghosts [][]int
+		target []int
 		expect bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{{1, 0}, {0, 3}}, []int{0, 1}, true},
+		{"TestCase2", [][]int{{1, 0}}, []int{2, 0}, false},
+		{"TestCase3", [][]int{{2, 0}}, []int{1, 0}, false},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.ghosts, c.target)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.ghosts, c.target)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
