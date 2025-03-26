46ad1ab582258519cb59e2e5da1fe078037a561f
0xff-dev <stevenshuang521@gmail.com>
2025-03-26 09:25:08 +0800

    Add solution and test-cases for problem 2033

--- leetcode/2001-2100/2033.Minimum-Operations-to-Make-a-Uni-Value-Grid/Solution.go
+++ leetcode/2001-2100/2033.Minimum-Operations-to-Make-a-Uni-Value-Grid/Solution.go
@@ -1,5 +1,27 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(grid [][]int, x int) int {
+	list := []int{}
+	rows, cols := len(grid), len(grid[0])
+	for i := range rows {
+		for j := range cols {
+			list = append(list, grid[i][j])
+		}
+	}
+	sort.Ints(list)
+	mid := list[len(list)/2]
+	ans := 0
+	for _, n := range list {
+		if n%x != mid%x {
+			return -1
+		}
+		diff := n - mid
+		if diff < 0 {
+			diff = -diff
+		}
+		ans += diff / x
+	}
+	return ans
 }
