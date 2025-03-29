ed5c176d42875951e1b01fad3959310efc8483f6
0xff-dev <stevenshuang521@gmail.com>
2025-03-29 21:25:37 +0800

    Add solution and test-cases for problem 2818

--- leetcode/2801-2900/2818.Apply-Operations-to-Maximize-Score/Solution_test.go
+++ leetcode/2801-2900/2818.Apply-Operations-to-Maximize-Score/Solution_test.go
@@ -10,30 +10,30 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		nums   []int
+		k      int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{8, 3, 9, 3, 8}, 2, 81},
+		{"TestCase2", []int{19, 12, 14, 6, 10, 18}, 3, 4788},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.nums, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.nums, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
