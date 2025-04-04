e1092756cc69d3ca8a2b1b6b9ec88712d03d9c8e
0xff-dev <stevenshuang521@gmail.com>
2025-04-04 19:35:11 +0800

    Add solution and test-cases for problem 434

--- leetcode/401-500/0434.Number-of-Segments-in-a-String/Solution.go
+++ leetcode/401-500/0434.Number-of-Segments-in-a-String/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string) int {
+	cnt := 0
+	start := -1
+	end := 0
+	for ; end < len(s); end++ {
+		if s[end] == ' ' {
+			if start == -1 {
+				continue
+			}
+			cnt++
+			start = -1
+			continue
+		}
+		if start == -1 {
+			start = end
+		}
+	}
+	if start != -1 {
+		cnt++
+	}
+	return cnt
 }
