21132515974157b3600f407ff1340b04e562afdd
0xff-dev <stevenshuang521@gmail.com>
2025-04-06 19:16:07 +0800

    Add solution and test-cases for problem 845

--- leetcode/801-900/0845.Longest-Mountain-in-Array/Solution.go
+++ leetcode/801-900/0845.Longest-Mountain-in-Array/Solution.go
@@ -1,5 +1,27 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(arr []int) int {
+	left := make([]int, len(arr))
+	left[0] = 0
+	for i := 1; i < len(arr); i++ {
+		if arr[i] > arr[i-1] {
+			left[i] = left[i-1] + 1
+		}
+	}
+	ans := 0
+	cnt := 0
+	for i := len(arr) - 2; i > 0; i-- {
+		if arr[i] > arr[i+1] {
+			cnt++
+		} else {
+			cnt = 0
+		}
+		if left[i] != 0 && cnt != 0 {
+			ans = max(ans, left[i]+cnt)
+		}
+	}
+	if ans > 0 {
+		ans++
+	}
+	return ans
 }
--- leetcode/801-900/0845.Longest-Mountain-in-Array/Solution_test.go
+++ leetcode/801-900/0845.Longest-Mountain-in-Array/Solution_test.go
@@ -10,12 +10,11 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{2, 1, 4, 7, 3, 2, 5}, 5},
+		{"TestCase2", []int{2, 2, 2}, 0},
 	}
 
 	//	开始测试
@@ -30,10 +29,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
