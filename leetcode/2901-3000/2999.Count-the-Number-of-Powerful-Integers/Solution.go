e51d3816f4952ef7021aa12908085ab01ae3c8c5
0xff-dev <stevenshuang521@gmail.com>
2025-04-10 22:43:22 +0800

    Add solution and test-cases for problem 2999

--- leetcode/2901-3000/2999.Count-the-Number-of-Powerful-Integers/Solution.go
+++ leetcode/2901-3000/2999.Count-the-Number-of-Powerful-Integers/Solution.go
@@ -1,5 +1,55 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"fmt"
+	"strings"
+)
+
+func Solution(start int64, finish int64, limit int, s string) int64 {
+	low := fmt.Sprintf("%d", start)
+	high := fmt.Sprintf("%d", finish)
+	n := len(high)
+	low = strings.Repeat("0", n-len(low)) + low // align digits
+	pre_len := n - len(s)                       // prefix length
+	memo := make([]int64, n)
+	for i := range memo {
+		memo[i] = -1
+	}
+
+	var dfs func(int, bool, bool) int64
+	dfs = func(i int, limit_low, limit_high bool) int64 {
+		// recursive boundary
+		if i == n {
+			return 1
+		}
+		if !limit_low && !limit_high && memo[i] != -1 {
+			return memo[i]
+		}
+		lo := 0
+		if limit_low {
+			lo = int(low[i] - '0')
+		}
+		hi := 9
+		if limit_high {
+			hi = int(high[i] - '0')
+		}
+
+		var res int64 = 0
+		if i < pre_len {
+			for digit := lo; digit <= min(hi, limit); digit++ {
+				res += dfs(i+1, limit_low && digit == lo, limit_high && digit == hi)
+			}
+		} else {
+			x := int(s[i-pre_len] - '0')
+			if lo <= x && x <= min(hi, limit) {
+				res = dfs(i+1, limit_low && x == lo, limit_high && x == hi)
+			}
+		}
+
+		if !limit_low && !limit_high {
+			memo[i] = res
+		}
+		return res
+	}
+	return dfs(0, true, true)
 }
--- leetcode/2901-3000/2999.Count-the-Number-of-Powerful-Integers/Solution_test.go
+++ leetcode/2901-3000/2999.Count-the-Number-of-Powerful-Integers/Solution_test.go
@@ -9,31 +9,33 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name          string
+		start, finish int64
+		limit         int
+		s             string
+		expect        int64
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 1, 6000, 4, "124", 5},
+		{"TestCase2", 15, 215, 6, "10", 2},
+		{"TestCase3", 1000, 2000, 4, "3000", 0},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.start, c.finish, c.limit, c.s)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v %v",
+					c.expect, got, c.start, c.finish, c.limit, c.s)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
