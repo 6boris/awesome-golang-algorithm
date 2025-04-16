3a5ba351b5fcce487f57ecbc0d694b884b27dc67
0xff-dev <stevenshuang521@gmail.com>
2025-04-16 09:34:15 +0800

    Add solution and test-cases for problem 2537

--- leetcode/2501-2600/2537.Count-the-Number-of-Good-Subarrays/Solution.go
+++ leetcode/2501-2600/2537.Count-the-Number-of-Good-Subarrays/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, k int) int64 {
+	l := len(nums)
+	count := make(map[int]int64)
+	start, end := 0, 0
+
+	var (
+		ans, pairs int64
+	)
+
+	i64k := int64(k)
+	for ; end < l; end++ {
+		pairs += count[nums[end]]
+		count[nums[end]]++
+
+		for ; start < end && pairs >= i64k; start++ {
+			ans += int64(l - end)
+			count[nums[start]]--
+			pairs -= count[nums[start]]
+		}
+	}
+	return ans
 }
