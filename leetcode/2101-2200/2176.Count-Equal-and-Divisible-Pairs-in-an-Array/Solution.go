0f11f1ddf1f2f056350f325f4e999e071d7a5fb2
0xff-dev <stevenshuang521@gmail.com>
2025-04-17 09:33:28 +0800

    Add solution and test-cases for problem 2176

--- leetcode/2101-2200/2176.Count-Equal-and-Divisible-Pairs-in-an-Array/Solution.go
+++ leetcode/2101-2200/2176.Count-Equal-and-Divisible-Pairs-in-an-Array/Solution.go
@@ -1,5 +1,20 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, k int) int {
+	count := make(map[int][]int)
+	for i, n := range nums {
+		count[n] = append(count[n], i)
+	}
+	ans := 0
+	for _, list := range count {
+		l := len(list)
+		for i := 0; i < l-1; i++ {
+			for j := i + 1; j < l; j++ {
+				if (list[i]*list[j])%k == 0 {
+					ans++
+				}
+			}
+		}
+	}
+	return ans
 }
