7ff16bb8760427e20277060a83f4b42a52527351
0xff-dev <stevenshuang521@gmail.com>
2025-04-19 20:21:52 +0800

    Add solution and test-cases for problem 1276

--- leetcode/1201-1300/1276.Number-of-Burgers-with-No-Waste-of-Ingredients/Solution.go
+++ leetcode/1201-1300/1276.Number-of-Burgers-with-No-Waste-of-Ingredients/Solution.go
@@ -1,5 +1,16 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(tomatoSlices int, cheeseSlices int) []int {
+	// 4x + 2y = tomatoSlices
+	// 2x + 2y = 2*cheeseSlices
+	jumbo := tomatoSlices - 2*cheeseSlices
+	if jumbo < 0 || jumbo&1 == 1 {
+		return []int{}
+	}
+	a := jumbo / 2
+	b := cheeseSlices - a
+	if b < 0 {
+		return []int{}
+	}
+	return []int{a, b}
 }
