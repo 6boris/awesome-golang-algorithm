ff5f109ad95123aa4e4f31af65b16ccb264ded34
0xff-dev <stevenshuang521@gmail.com>
2025-04-21 09:15:07 +0800

    Add solution and test-cases for problem 2145

--- leetcode/2101-2200/2145.Count-the-Hidden-Sequences/Solution_test.go
+++ leetcode/2101-2200/2145.Count-the-Hidden-Sequences/Solution_test.go
@@ -9,31 +9,32 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name         string
+		differences  []int
+		lower, upper int
+		expect       int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, -3, 4}, 1, 6, 2},
+		{"TestCase2", []int{3, -4, 5, 1, -2}, -4, 5, 4},
+		{"TestCase3", []int{4, -7, 2}, 3, 6, 0},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.differences, c.lower, c.upper)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.differences, c.lower, c.upper)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
