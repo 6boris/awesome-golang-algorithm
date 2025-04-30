2fdec23e4afa7f2db0d02a09a9c395baca033617
0xff-dev <stevenshuang521@gmail.com>
2025-04-30 09:19:10 +0800

    Add solution and test-cases for problem 1295

--- leetcode/1201-1300/1295.Find-Numbers-with-Even-Number-of-Digits/Solution.go
+++ leetcode/1201-1300/1295.Find-Numbers-with-Even-Number-of-Digits/Solution.go
@@ -12,3 +12,22 @@ func Solution(nums []int) int {
 	}
 	return count
 }
+
+func Solution1(nums []int) int {
+	var isEven func(int) bool
+	isEven = func(n int) bool {
+		c := 0
+		for n > 0 {
+			n /= 10
+			c++
+		}
+		return c&1 == 0
+	}
+	ans := 0
+	for _, n := range nums {
+		if isEven(n) {
+			ans++
+		}
+	}
+	return ans
+}
