37d49e4099336533b7e96aaac647c377064a24be
0xff-dev <stevenshuang521@gmail.com>
2025-05-03 14:13:08 +0800

    Add solution and test-cases for problem 822

--- leetcode/801-900/0822.Card-Flipping-Game/Solution.go
+++ leetcode/801-900/0822.Card-Flipping-Game/Solution.go
@@ -1,5 +1,41 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(fronts []int, backs []int) int {
+	fm := make(map[int]struct{})
+	for _, n := range fronts {
+		fm[n] = struct{}{}
+	}
+	ans := 0
+	for _, n := range backs {
+		if _, ok := fm[n]; !ok {
+			if ans == 0 || ans > n {
+				ans = n
+			}
+		}
+	}
+	mm := make(map[int][]int)
+	keys := make([]int, 0)
+	for i, f := range fronts {
+		if _, ok := mm[f]; !ok {
+			mm[f] = make([]int, 0)
+			keys = append(keys, f)
+		}
+		mm[f] = append(mm[f], backs[i])
+	}
+	for i := 0; i < len(keys); i++ {
+		ok := true
+		for _, v := range mm[keys[i]] {
+			if v == keys[i] {
+				ok = false
+				break
+			}
+		}
+		if !ok {
+			continue
+		}
+		if ans == 0 || ans > keys[i] {
+			ans = keys[i]
+		}
+	}
+	return ans
 }
