6db9637fb54c8b3535be8b7af666baac1a9dd176
0xff-dev <stevenshuang521@gmail.com>
2025-05-24 15:01:18 +0800

    Add solution and test-cases for problem 2942

--- leetcode/2901-3000/2942.Find-Words-Containing-Character/Solution.go
+++ leetcode/2901-3000/2942.Find-Words-Containing-Character/Solution.go
@@ -1,5 +1,18 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(words []string, x byte) []int {
+	var ans []int
+	for i, word := range words {
+		ok := false
+		for _, b := range []byte(word) {
+			if b == x {
+				ok = true
+				break
+			}
+		}
+		if ok {
+			ans = append(ans, i)
+		}
+	}
+	return ans
 }
--- leetcode/2901-3000/2942.Find-Words-Containing-Character/Solution_test.go
+++ leetcode/2901-3000/2942.Find-Words-Containing-Character/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []string
+		x      byte
+		expect []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []string{"leet", "code"}, 'e', []int{0, 1}},
+		{"TestCase2", []string{"abc", "bcd", "aaaa", "cbc"}, 'a', []int{0, 2}},
+		{"TestCase3", []string{"abc", "bcd", "aaaa", "cbc"}, 'z', nil},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.x)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.inputs, c.x)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
