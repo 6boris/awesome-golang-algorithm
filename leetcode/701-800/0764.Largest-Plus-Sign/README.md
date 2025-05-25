c1389e77175bc316b97de42e4f1bd2f876a5b9cd
0xff-dev <stevenshuang521@gmail.com>
2025-05-25 15:56:14 +0800

    Add solution and test-cases for problem 764

--- leetcode/701-800/0764.Largest-Plus-Sign/README.md
+++ leetcode/701-800/0764.Largest-Plus-Sign/README.md
@@ -1,28 +1,31 @@
 # [764.Largest Plus Sign][title]
 
-> [!WARNING|style:flat]
-> This question is temporarily unanswered if you have good ideas. Welcome to [Create Pull Request PR](https://github.com/kylesliu/awesome-golang-algorithm)
-
 ## Description
+You are given an integer `n`. You have an `n x n` binary `grid` grid with all values initially `1`'s except for some indices given in the array `mines`. The `ith` element of the array `mines` is defined as `mines[i] = [xi, yi]` where `grid[xi][yi] == 0`.
 
-**Example 1:**
+Return the order of the largest **axis-aligned** plus sign of 1's contained in `grid`. If there is none, return `0`.
+
+An **axis-aligned** plus sign of `1`'s of order `k` has some center `grid[r][c] == 1` along with four arms of length `k - 1` going up, down, left, and right, and made of `1`'s. Note that there could be `0`'s or `1`'s beyond the arms of the plus sign, only the relevant area of the plus sign is checked for `1`'s.
+
+**Example 1:**  
+
+![1](./1.jpg)
 
 ```
-Input: a = "11", b = "1"
-Output: "100"
+Input: n = 5, mines = [[4,2]]
+Output: 2
+Explanation: In the above grid, the largest plus sign can only be of order 2. One of them is shown.
 ```
 
-## 题意
-> ...
+**Example 2:**  
 
-## 题解
+![2](./2.jpg)
 
-### 思路1
-> ...
-Largest Plus Sign
-```go
 ```
-
+Input: n = 1, mines = [[0,0]]
+Output: 0
+Explanation: There is no plus sign, so return 0.
+```
 
 ## 结语
 
--- leetcode/701-800/0764.Largest-Plus-Sign/Solution.go
+++ leetcode/701-800/0764.Largest-Plus-Sign/Solution.go
@@ -1,5 +1,82 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+type l764 struct {
+	up, down, left, right int
+}
+
+func Solution(n int, mines [][]int) int {
+	var ans int
+	checker := make(map[int]map[int]struct{})
+	for _, m := range mines {
+		if _, ok := checker[m[0]]; !ok {
+			checker[m[0]] = map[int]struct{}{}
+		}
+		checker[m[0]][m[1]] = struct{}{}
+	}
+	order := make([][]l764, n)
+	for i := range n {
+		order[i] = make([]l764, n)
+	}
+
+	for i := 0; i < n; i++ {
+		one := 0
+		for j := 0; j < n; j++ {
+			if v, ok := checker[i]; ok {
+				if _, ok1 := v[j]; ok1 {
+					one = 0
+					continue
+				}
+			}
+			order[i][j].left = one
+			one++
+		}
+		one = 0
+		for j := n - 1; j >= 0; j-- {
+			if v, ok := checker[i]; ok {
+				if _, ok1 := v[j]; ok1 {
+					one = 0
+					continue
+				}
+			}
+			order[i][j].right = one
+			one++
+
+		}
+
+		one = 0
+		for j := 0; j < n; j++ {
+			if v, ok := checker[j]; ok {
+				if _, ok1 := v[i]; ok1 {
+					one = 0
+					continue
+				}
+			}
+			order[j][i].up = one
+			one++
+		}
+		one = 0
+		for j := n - 1; j >= 0; j-- {
+			if v, ok := checker[j]; ok {
+				if _, ok1 := v[i]; ok1 {
+					one = 0
+					continue
+				}
+			}
+			order[j][i].down = one
+			one++
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if v, ok := checker[i]; ok {
+				if _, ok1 := v[j]; ok1 {
+					continue
+				}
+			}
+
+			ans = max(ans, min(order[i][j].down, order[i][j].up, order[i][j].left, order[i][j].right)+1)
+		}
+	}
+	return ans
 }
