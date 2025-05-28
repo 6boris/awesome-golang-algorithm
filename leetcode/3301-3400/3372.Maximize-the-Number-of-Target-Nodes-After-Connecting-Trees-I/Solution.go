06ac46aac66cc082ea490c1de3afa43463940009
0xff-dev <stevenshuang521@gmail.com>
2025-05-28 22:09:03 +0800

    Add solution and test-cases for problem 3372

--- leetcode/3301-3400/3372.Maximize-the-Number-of-Target-Nodes-After-Connecting-Trees-I/Solution.go
+++ leetcode/3301-3400/3372.Maximize-the-Number-of-Target-Nodes-After-Connecting-Trees-I/Solution.go
@@ -1,5 +1,59 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "slices"
+
+func dfs3372(adj map[int][]int, start, parent, limit int) int {
+	if limit < 0 {
+		return 0
+	}
+
+	cnt := 1 // self
+	for _, rel := range adj[start] {
+		if rel == parent {
+			continue
+		}
+		cnt += dfs3372(adj, rel, start, limit-1)
+	}
+	return cnt
+
+}
+
+func calDistance(adj map[int][]int, n, limit int) []int {
+	distance := make([]int, n)
+	for i := 0; i < n; i++ {
+		distance[i] = dfs3372(adj, i, -1, limit)
+	}
+	return distance
+}
+
+func Solution(edges1 [][]int, edges2 [][]int, k int) []int {
+	n, m := 0, 0
+	for _, e := range edges1 {
+		n = max(n, e[0], e[1])
+	}
+	for _, e := range edges2 {
+		m = max(m, e[0], e[1])
+	}
+	n, m = n+1, m+1
+	adj1 := make(map[int][]int)
+	adj2 := make(map[int][]int)
+
+	for _, e := range edges1 {
+		adj1[e[0]] = append(adj1[e[0]], e[1])
+		adj1[e[1]] = append(adj1[e[1]], e[0])
+	}
+	for _, e := range edges2 {
+		adj2[e[0]] = append(adj2[e[0]], e[1])
+		adj2[e[1]] = append(adj2[e[1]], e[0])
+	}
+
+	dis1 := calDistance(adj1, n, k)
+	dis2 := calDistance(adj2, m, k-1)
+
+	cnt := slices.Max(dis2)
+	for i := range n {
+		dis1[i] += cnt
+	}
+
+	return dis1
 }
--- leetcode/3301-3400/3372.Maximize-the-Number-of-Target-Nodes-After-Connecting-Trees-I/Solution_test.go
+++ leetcode/3301-3400/3372.Maximize-the-Number-of-Target-Nodes-After-Connecting-Trees-I/Solution_test.go
@@ -9,31 +9,31 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name           string
+		edges1, edges2 [][]int
+		k              int
+		expect         []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{{0, 1}, {0, 2}, {2, 3}, {2, 4}}, [][]int{{0, 1}, {0, 2}, {0, 3}, {2, 7}, {1, 4}, {1, 5}, {4, 5}, {4, 6}}, 2, []int{9, 7, 9, 8, 8}},
+		{"TestCase2", [][]int{{0, 1}, {0, 2}, {0, 3}, {0, 4}}, [][]int{{0, 1}, {1, 2}, {2, 3}}, 1, []int{6, 3, 3, 3, 3}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.edges1, c.edges2, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.edges1, c.edges2, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
