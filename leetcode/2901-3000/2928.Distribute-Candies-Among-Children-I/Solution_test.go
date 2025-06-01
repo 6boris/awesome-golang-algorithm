bf0cc94778f12d00e31992281e8ca96a8e994848
0xff-dev <stevenshuang521@gmail.com>
2025-06-01 17:03:26 +0800

    Add solution and test-cases for problem 2928

--- leetcode/2901-3000/2928.Distribute-Candies-Among-Children-I/Solution_test.go
+++ leetcode/2901-3000/2928.Distribute-Candies-Among-Children-I/Solution_test.go
@@ -9,31 +9,30 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name     string
+		n, limit int
+		expect   int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 5, 2, 3},
+		{"TestCase2", 3, 3, 10},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.limit)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.n, c.limit)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
