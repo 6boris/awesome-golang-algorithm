1645f1b20dbb815c9f2f272ca96b96e394f4694b
0xff-dev <stevenshuang521@gmail.com>
2025-06-03 09:25:36 +0800

    Add solution and test-cases for problem 1298

--- leetcode/1201-1300/1298.Maximum-Candies-You-Can-Get-from-Boxes/Solution.go
+++ leetcode/1201-1300/1298.Maximum-Candies-You-Can-Get-from-Boxes/Solution.go
@@ -1,5 +1,39 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(status []int, candies []int, keys [][]int, containedBoxes [][]int, initialBoxes []int) int {
+	queue := make([]int, 0)
+	toBeUsed := make(map[int]struct{})
+	for _, b := range initialBoxes {
+		if status[b] == 1 {
+			queue = append(queue, b)
+			continue
+		}
+		toBeUsed[b] = struct{}{}
+	}
+
+	ans := 0
+	for len(queue) > 0 {
+		nq := make([]int, 0)
+		for _, b := range queue {
+			ans += candies[b]
+			for _, unlock := range keys[b] {
+				status[unlock] = 1
+				if _, ok := toBeUsed[unlock]; ok {
+					nq = append(nq, unlock)
+					delete(toBeUsed, unlock)
+				}
+			}
+
+			for _, box := range containedBoxes[b] {
+				if status[box] == 1 {
+					nq = append(nq, box)
+					continue
+				}
+				toBeUsed[box] = struct{}{}
+			}
+
+		}
+		queue = nq
+	}
+	return ans
 }
--- leetcode/1201-1300/1298.Maximum-Candies-You-Can-Get-from-Boxes/Solution_test.go
+++ leetcode/1201-1300/1298.Maximum-Candies-You-Can-Get-from-Boxes/Solution_test.go
@@ -9,31 +9,32 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name                 string
+		status, candies      []int
+		keys, containedBoxes [][]int
+		initialBoxes         []int
+		expect               int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 0, 1, 0}, []int{7, 5, 4, 100}, [][]int{{}, {}, {1}, {}}, [][]int{{1, 2}, {3}, {}, {}}, []int{0}, 16},
+		{"TestCase2", []int{1, 0, 0, 0, 0, 0}, []int{1, 1, 1, 1, 1, 1}, [][]int{{1, 2, 3, 4, 5}, {}, {}, {}, {}, {}}, [][]int{{1, 2, 3, 4, 5}, {}, {}, {}, {}, {}}, []int{0}, 6},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.status, c.candies, c.keys, c.containedBoxes, c.initialBoxes)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v %v %v",
+					c.expect, got, c.status, c.candies, c.keys, c.containedBoxes, c.initialBoxes)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
