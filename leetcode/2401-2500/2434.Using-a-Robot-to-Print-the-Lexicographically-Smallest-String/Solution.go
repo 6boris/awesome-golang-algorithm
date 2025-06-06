03a916e9b847f28231801edfb1b39fb3ec0e961d
0xff-dev <stevenshuang521@gmail.com>
2025-06-06 09:29:29 +0800

    Add solution and test-cases for problem 2434

--- leetcode/2401-2500/2434.Using-a-Robot-to-Print-the-Lexicographically-Smallest-String/Solution.go
+++ leetcode/2401-2500/2434.Using-a-Robot-to-Print-the-Lexicographically-Smallest-String/Solution.go
@@ -1,5 +1,25 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string) string {
+	cnt := make([]int, 26)
+	for _, c := range s {
+		cnt[c-'a']++
+	}
+
+	stack := []rune{}
+	res := []rune{}
+	minCharacter := 'a'
+	for _, c := range s {
+		stack = append(stack, c)
+		cnt[c-'a']--
+		for minCharacter != 'z' && cnt[minCharacter-'a'] == 0 {
+			minCharacter++
+		}
+		for len(stack) > 0 && stack[len(stack)-1] <= minCharacter {
+			res = append(res, stack[len(stack)-1])
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return string(res)
 }
