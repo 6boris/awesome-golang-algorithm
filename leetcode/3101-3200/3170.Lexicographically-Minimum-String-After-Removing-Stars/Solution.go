d1634f858c6eca3accd48e6582afc653a3fc36c0
0xff-dev <stevenshuang521@gmail.com>
2025-06-07 20:32:18 +0800

    Add solution and test-cases for problem 3170

--- leetcode/3101-3200/3170.Lexicographically-Minimum-String-After-Removing-Stars/Solution.go
+++ leetcode/3101-3200/3170.Lexicographically-Minimum-String-After-Removing-Stars/Solution.go
@@ -1,5 +1,30 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string) string {
+	cnt := make([][]int, 26)
+	for i := range cnt {
+		cnt[i] = make([]int, 0)
+	}
+	arr := []rune(s)
+	for i, c := range arr {
+		if c != '*' {
+			cnt[c-'a'] = append(cnt[c-'a'], i)
+		} else {
+			for j := 0; j < 26; j++ {
+				if len(cnt[j]) > 0 {
+					last := len(cnt[j]) - 1
+					arr[cnt[j][last]] = '*'
+					cnt[j] = cnt[j][:last]
+					break
+				}
+			}
+		}
+	}
+	var ans []rune
+	for _, c := range arr {
+		if c != '*' {
+			ans = append(ans, c)
+		}
+	}
+	return string(ans)
 }
