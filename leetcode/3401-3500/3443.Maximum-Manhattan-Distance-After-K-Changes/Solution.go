9bd9192decebf8e79d239224fc998f7556abb017
0xff-dev <stevenshuang521@gmail.com>
2025-06-20 09:27:09 +0800

    Add solution and test-cases for problem 3443

--- leetcode/3401-3500/3443.Maximum-Manhattan-Distance-After-K-Changes/Solution.go
+++ leetcode/3401-3500/3443.Maximum-Manhattan-Distance-After-K-Changes/Solution.go
@@ -1,5 +1,32 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string, k int) int {
+	n1, s1, e1, w1 := 0, 0, 0, 0
+	var ans int
+	for _, b := range s {
+		if b == 'N' {
+			n1++
+		}
+		if b == 'S' {
+			s1++
+		}
+		if b == 'W' {
+			w1++
+		}
+		if b == 'E' {
+			e1++
+		}
+		// 我们只需要将少的移动的多的方向，另外就是，南北，东西变化可以增加，但是南东这种变化没用
+		minY := min(n1, s1)
+		maxY := max(n1, s1)
+		a := min(minY, k)
+
+		minX := min(e1, w1)
+		maxX := max(e1, w1)
+		b := min(minX, k-a)
+		// n1 - (s1-a) + a
+		ans = max(ans, maxY-minY+a*2+maxX-minX+b*2)
+
+	}
+	return ans
 }
