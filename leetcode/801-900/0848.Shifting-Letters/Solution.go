95ed7dc30d6037e44d5acb4a80bcd13ace0c6609
0xff-dev <stevenshuang521@gmail.com>
2025-06-28 17:49:28 +0800

    Add solution and test-cases for problem 848

--- leetcode/801-900/0848.Shifting-Letters/Solution.go
+++ leetcode/801-900/0848.Shifting-Letters/Solution.go
@@ -1,5 +1,12 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string, shifts []int) string {
+	ans := []byte(s)
+	sum := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		sum += shifts[i]
+		now := (int(ans[i]-'a') + sum) % 26
+		ans[i] = byte(now) + 'a'
+	}
+	return string(ans)
 }
