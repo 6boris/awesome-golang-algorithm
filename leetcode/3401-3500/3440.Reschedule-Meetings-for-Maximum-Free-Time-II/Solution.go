060d7756eb7cfe36d0c63e702297348735cdd4f7
0xff-dev <stevenshuang521@gmail.com>
2025-07-10 09:06:05 +0800

    Add solution and test-cases for problem 3440

--- leetcode/3401-3500/3440.Reschedule-Meetings-for-Maximum-Free-Time-II/Solution.go
+++ leetcode/3401-3500/3440.Reschedule-Meetings-for-Maximum-Free-Time-II/Solution.go
@@ -1,5 +1,44 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(eventTime int, startTime []int, endTime []int) int {
+	n := len(startTime)
+	q := make([]bool, n)
+	t1, t2 := 0, 0
+	for i := 0; i < n; i++ {
+		if endTime[i]-startTime[i] <= t1 {
+			q[i] = true
+		}
+		if i == 0 {
+			t1 = max(t1, startTime[i])
+		} else {
+			t1 = max(t1, startTime[i]-endTime[i-1])
+		}
+
+		if endTime[n-i-1]-startTime[n-i-1] <= t2 {
+			q[n-i-1] = true
+		}
+		if i == 0 {
+			t2 = max(t2, eventTime-endTime[n-1])
+		} else {
+			t2 = max(t2, startTime[n-i]-endTime[n-i-1])
+		}
+	}
+
+	res := 0
+	for i := 0; i < n; i++ {
+		left := 0
+		if i != 0 {
+			left = endTime[i-1]
+		}
+		right := eventTime
+		if i != n-1 {
+			right = startTime[i+1]
+		}
+		if q[i] {
+			res = max(res, right-left)
+		} else {
+			res = max(res, right-left-(endTime[i]-startTime[i]))
+		}
+	}
+	return res
 }
