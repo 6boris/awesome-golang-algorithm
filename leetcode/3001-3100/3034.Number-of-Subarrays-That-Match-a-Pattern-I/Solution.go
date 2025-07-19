7b3cf846426f5ad86dbdc9d1466fedc08530f0c5
0xff-dev <stevenshuang521@gmail.com>
2025-07-19 09:20:47 +0800

    Add solution and test-cases for problem 3034

--- leetcode/3001-3100/3034.Number-of-Subarrays-That-Match-a-Pattern-I/Solution.go
+++ leetcode/3001-3100/3034.Number-of-Subarrays-That-Match-a-Pattern-I/Solution.go
@@ -1,5 +1,27 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, pattern []int) int {
+	var ans int
+	m := len(pattern)
+	n := len(nums)
+	k := 0
+	for i := 0; i < n-m; i++ {
+		k = 0
+		for ; k < m; k++ {
+			if pattern[k] == 1 && nums[i+k+1] > nums[i+k] {
+				continue
+			}
+			if pattern[k] == 0 && nums[i+k+1] == nums[i+k] {
+				continue
+			}
+			if pattern[k] == -1 && nums[i+k+1] < nums[i+k] {
+				continue
+			}
+			break
+		}
+		if k == m {
+			ans++
+		}
+	}
+	return ans
 }
