20efae7bcd5168c93b0f8708bfb6c958bfa7e8b7
0xff-dev <stevenshuang521@gmail.com>
2025-07-25 08:56:25 +0800

    Add solution and test-cases for problem 3487

--- leetcode/3401-3500/3487.Maximum-Unique-Subarray-Sum-After-Deletion/Solution.go
+++ leetcode/3401-3500/3487.Maximum-Unique-Subarray-Sum-After-Deletion/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(nums []int) int {
+	l := len(nums)
+	sort.Ints(nums)
+	index := sort.Search(l, func(i int) bool {
+		return nums[i] >= 0
+	})
+	sum := nums[l-1]
+	if index == l {
+		return sum
+	}
+	pre := sum
+	for i := l - 2; i >= index; i-- {
+		if nums[i] == pre {
+			continue
+		}
+		pre = nums[i]
+		sum += nums[i]
+	}
+	return sum
 }
