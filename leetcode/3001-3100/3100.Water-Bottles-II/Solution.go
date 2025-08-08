f9f3feb5a96831e4aeb48243ff1010fabe60d837
0xff-dev <stevenshuang521@gmail.com>
2025-08-08 09:08:27 +0800

    Add solution and test-cases for problem 3100

--- leetcode/3001-3100/3100.Water-Bottles-II/Solution.go
+++ leetcode/3001-3100/3100.Water-Bottles-II/Solution.go
@@ -1,5 +1,22 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(numBottles int, numExchange int) int {
+	var ans int = numBottles
+	drink := 0
+	empty := numBottles
+	for {
+		if empty >= numExchange {
+			drink++
+			empty -= numExchange
+			numExchange++
+			continue
+		}
+		empty += drink
+		ans += drink
+		drink = 0
+		if empty < numExchange {
+			break
+		}
+	}
+	return ans + drink
 }
