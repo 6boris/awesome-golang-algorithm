769470321c95250b55b38ac04ceb6651a3dc57e2
0xff-dev <stevenshuang521@gmail.com>
2025-08-10 16:53:55 +0800

    Add solution and test-cases for problem 2574

--- leetcode/2501-2600/2574.Left-and-Right-Sum-Differences/Solution.go
+++ leetcode/2501-2600/2574.Left-and-Right-Sum-Differences/Solution.go
@@ -1,5 +1,20 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) []int {
+	n := len(nums)
+	left, right := make([]int, n), make([]int, n)
+	left[0], right[n-1] = 0, 0
+	for i := 0; i < n-1; i++ {
+		left[i+1] = nums[i] + left[i]
+	}
+	for i := n - 1; i > 0; i-- {
+		right[i-1] = right[i] + nums[i]
+	}
+	for i := 0; i < n; i++ {
+		left[i] -= right[i]
+		if left[i] < 0 {
+			left[i] = -left[i]
+		}
+	}
+	return left
 }
