94e154a68271dd3b953da19332181e78a361ebaf
0xff-dev <stevenshuang521@gmail.com>
2025-08-17 22:29:47 +0800

    Add solution and test-cases for problem 3239

--- leetcode/3201-3300/3239.Minimum-Number-of-Flips-to-Make-Binary-Grid-Palindromic-I/Solution.go
+++ leetcode/3201-3300/3239.Minimum-Number-of-Flips-to-Make-Binary-Grid-Palindromic-I/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(grid [][]int) int {
+	rows, cols := len(grid), len(grid[0])
+	ans := rows * cols
+	need := 0
+	for i := 0; i < rows; i++ {
+		for s, e := 0, cols-1; s < e; s, e = s+1, e-1 {
+			if grid[i][s] != grid[i][e] {
+				need++
+			}
+		}
+	}
+	ans = min(ans, need)
+	need = 0
+	for i := 0; i < cols; i++ {
+		for s, e := 0, rows-1; s < e; s, e = s+1, e-1 {
+			if grid[s][i] != grid[e][i] {
+				need++
+			}
+		}
+	}
+	return min(ans, need)
 }
--- leetcode/3201-3300/3239.Minimum-Number-of-Flips-to-Make-Binary-Grid-Palindromic-I/Solution_test.go
+++ leetcode/3201-3300/3239.Minimum-Number-of-Flips-to-Make-Binary-Grid-Palindromic-I/Solution_test.go
@@ -10,12 +10,12 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs [][]int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{{1, 0, 0}, {0, 0, 0}, {0, 0, 1}}, 2},
+		{"TestCase2", [][]int{{0, 1}, {0, 1}, {0, 0}}, 1},
+		{"TestCase3", [][]int{{1}, {0}}, 0},
 	}
 
 	//	开始测试
@@ -30,10 +30,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
