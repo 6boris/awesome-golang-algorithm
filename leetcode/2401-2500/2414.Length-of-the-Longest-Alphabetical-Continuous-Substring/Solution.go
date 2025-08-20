b3873f7fae760729d8e8a01b7e3de82a45b2c9e1
0xff-dev <stevenshuang521@gmail.com>
2025-08-20 08:47:41 +0800

    Add solution and test-cases for problem 2414

--- leetcode/2401-2500/2414.Length-of-the-Longest-Alphabetical-Continuous-Substring/Solution.go
+++ leetcode/2401-2500/2414.Length-of-the-Longest-Alphabetical-Continuous-Substring/Solution.go
@@ -1,5 +1,15 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string) int {
+	ans := 1
+	cur := 1
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1]+1 {
+			cur++
+		} else {
+			cur = 1
+		}
+		ans = max(ans, cur)
+	}
+	return ans
 }
