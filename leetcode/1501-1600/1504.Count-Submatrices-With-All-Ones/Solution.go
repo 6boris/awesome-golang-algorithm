36f271874ad41d137da5160e1dff974caa7519f1
0xff-dev <stevenshuang521@gmail.com>
2025-08-21 09:00:18 +0800

    Add solution and test-cases for problem 1504

--- leetcode/1501-1600/1504.Count-Submatrices-With-All-Ones/Solution.go
+++ leetcode/1501-1600/1504.Count-Submatrices-With-All-Ones/Solution.go
@@ -1,5 +1,28 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(mat [][]int) int {
+	n := len(mat[0])
+	heights := make([]int, n)
+	res := 0
+	for _, row := range mat {
+		for i := 0; i < n; i++ {
+			if row[i] == 0 {
+				heights[i] = 0
+			} else {
+				heights[i]++
+			}
+		}
+		stack := [][3]int{{-1, 0, -1}}
+		for i, h := range heights {
+			for len(stack) > 1 && stack[len(stack)-1][2] >= h {
+				stack = stack[:len(stack)-1]
+			}
+			top := stack[len(stack)-1]
+			j, prev := top[0], top[1]
+			cur := prev + (i-j)*h
+			stack = append(stack, [3]int{i, cur, h})
+			res += cur
+		}
+	}
+	return res
 }
