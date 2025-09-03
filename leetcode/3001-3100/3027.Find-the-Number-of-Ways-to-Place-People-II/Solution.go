cdf6eb7b4610ac455a9b14de56250c544a260571
0xff-dev <stevenshuang521@gmail.com>
2025-09-03 08:53:15 +0800

    Add solution and test-cases for problem 3027

--- leetcode/3001-3100/3027.Find-the-Number-of-Ways-to-Place-People-II/Solution.go
+++ leetcode/3001-3100/3027.Find-the-Number-of-Ways-to-Place-People-II/Solution.go
@@ -1,5 +1,35 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"math"
+	"sort"
+)
+
+func Solution(points [][]int) int {
+	ans := 0
+	sort.Slice(points, func(i, j int) bool {
+		if points[i][0] == points[j][0] {
+			return points[i][1] > points[j][1]
+		}
+		return points[i][0] < points[j][0]
+	})
+
+	for i := 0; i < len(points)-1; i++ {
+		pointA := points[i]
+		xMin := pointA[0] - 1
+		xMax := math.MaxInt32
+		yMin := math.MinInt32
+		yMax := pointA[1] + 1
+
+		for j := i + 1; j < len(points); j++ {
+			pointB := points[j]
+			if pointB[0] > xMin && pointB[0] < xMax &&
+				pointB[1] > yMin && pointB[1] < yMax {
+				ans++
+				xMin = pointB[0]
+				yMin = pointB[1]
+			}
+		}
+	}
+	return ans
 }
