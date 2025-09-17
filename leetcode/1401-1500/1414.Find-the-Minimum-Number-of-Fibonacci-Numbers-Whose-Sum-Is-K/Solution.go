196ee0e26737864e2c10817f4e71b3f44758154b
0xff-dev <stevenshuang521@gmail.com>
2025-09-17 09:07:16 +0800

    Add solution and test-cases for problem 1414

--- leetcode/1401-1500/1414.Find-the-Minimum-Number-of-Fibonacci-Numbers-Whose-Sum-Is-K/Solution.go
+++ leetcode/1401-1500/1414.Find-the-Minimum-Number-of-Fibonacci-Numbers-Whose-Sum-Is-K/Solution.go
@@ -1,5 +1,18 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(k int) int {
+	fb := []int{1, 1}
+	for i := 2; i < 44 && fb[i-1]+fb[i-2] <= k; i++ {
+		fb = append(fb, fb[i-1]+fb[i-2])
+	}
+	ret := 0
+	// 5
+	for index := len(fb) - 1; index >= 0 && k > 0; index-- {
+		if k < fb[index] {
+			continue
+		}
+		ret += k / fb[index]
+		k %= fb[index]
+	}
+	return ret
 }
