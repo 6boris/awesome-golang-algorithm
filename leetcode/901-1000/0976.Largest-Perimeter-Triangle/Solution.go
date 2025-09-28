534ef6458d18a383b14da7af4a9e48761e8b87cd
0xff-dev <stevenshuang521@gmail.com>
2025-09-28 16:53:36 +0800

    Add solution and test-cases for problem 976

--- leetcode/901-1000/0976.Largest-Perimeter-Triangle/Solution.go
+++ leetcode/901-1000/0976.Largest-Perimeter-Triangle/Solution.go
@@ -1,5 +1,16 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(nums []int) int {
+	sort.Ints(nums)
+	// 1, 1, 2, 10
+	l := len(nums)
+	for i := l - 1; i >= 2; i-- {
+		a, b, c := nums[i], nums[i-1], nums[i-2]
+		if a+b > c && a+c > b && b+c > a {
+			return a + b + c
+		}
+	}
+	return 0
 }
