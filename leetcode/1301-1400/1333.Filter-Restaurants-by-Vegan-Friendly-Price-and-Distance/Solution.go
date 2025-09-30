6790ea881318f5787a778a1bc011101ca2599427
0xff-dev <stevenshuang521@gmail.com>
2025-09-30 14:27:21 +0800

    Add solution and test-cases for problem 1333

--- leetcode/1301-1400/1333.Filter-Restaurants-by-Vegan-Friendly-Price-and-Distance/Solution.go
+++ leetcode/1301-1400/1333.Filter-Restaurants-by-Vegan-Friendly-Price-and-Distance/Solution.go
@@ -1,5 +1,31 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(restaurants [][]int, veganFriendly int, maxPrice int, maxDistance int) []int {
+	left := make([]int, 0)
+	for i := 0; i < len(restaurants); i++ {
+		if veganFriendly == 1 && restaurants[i][2] == 0 {
+			continue
+		}
+		if restaurants[i][3] > maxPrice {
+			continue
+		}
+		if restaurants[i][4] > maxDistance {
+			continue
+		}
+		left = append(left, i)
+	}
+	sort.Slice(left, func(i, j int) bool {
+		a, b := restaurants[left[i]], restaurants[left[j]]
+		if a[1] == b[1] {
+			return a[0] > b[0]
+		}
+		return a[1] > b[1]
+	})
+	ret := make([]int, len(left))
+	for i, idx := range left {
+		ret[i] = restaurants[idx][0]
+	}
+	return ret
 }
--- leetcode/1301-1400/1333.Filter-Restaurants-by-Vegan-Friendly-Price-and-Distance/Solution_test.go
+++ leetcode/1301-1400/1333.Filter-Restaurants-by-Vegan-Friendly-Price-and-Distance/Solution_test.go
@@ -9,31 +9,38 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name                                 string
+		restaurants                          [][]int
+		veganFriendly, maxPrice, maxDistance int
+		expect                               []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{
+			{1, 4, 1, 40, 10}, {2, 8, 0, 50, 5}, {3, 8, 1, 30, 4}, {4, 10, 0, 10, 3}, {5, 1, 1, 15, 1},
+		}, 1, 50, 10, []int{3, 1, 5}},
+		{"TestCase2", [][]int{
+			{1, 4, 1, 40, 10}, {2, 8, 0, 50, 5}, {3, 8, 1, 30, 4}, {4, 10, 0, 10, 3}, {5, 1, 1, 15, 1},
+		}, 0, 50, 10, []int{4, 3, 2, 1, 5}},
+		{"TestCase3", [][]int{
+			{1, 4, 1, 40, 10}, {2, 8, 0, 50, 5}, {3, 8, 1, 30, 4}, {4, 10, 0, 10, 3}, {5, 1, 1, 15, 1},
+		}, 0, 30, 3, []int{4, 5}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.restaurants, c.veganFriendly, c.maxPrice, c.maxDistance)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v %v",
+					c.expect, got, c.restaurants, c.veganFriendly, c.maxPrice, c.maxDistance)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
