850ba08bad7987c1c9e9b55c809ea2a680bd6a57
0xff-dev <stevenshuang521@gmail.com>
2025-10-01 16:24:15 +0800

    Add solution and test-cases for problem 1583

--- leetcode/1501-1600/1583.Count-Unhappy-Friends/Solution.go
+++ leetcode/1501-1600/1583.Count-Unhappy-Friends/Solution.go
@@ -1,5 +1,45 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int, preferences [][]int, pairs [][]int) int {
+	likeIndex := make([]map[int]int, len(preferences))
+	for i, p := range preferences {
+		likeIndex[i] = make(map[int]int)
+		for idx, like := range p {
+			likeIndex[i][like] = idx
+		}
+	}
+
+	pair := make(map[int]int)
+	for _, p := range pairs {
+		pair[p[0]] = p[1]
+		pair[p[1]] = p[0]
+	}
+	var ret int
+	for _, p := range pairs {
+		x, y := p[0], p[1]
+		for _, u := range preferences[x] {
+			if u == y {
+				break
+			}
+			if v, ok := pair[u]; ok {
+				if likeIndex[u][x] < likeIndex[u][v] {
+					ret++
+					break
+				}
+			}
+		}
+
+		for _, u := range preferences[y] {
+			if u == x {
+				break
+			}
+			if v, ok := pair[u]; ok {
+				if likeIndex[u][y] < likeIndex[u][v] {
+					ret++
+					break
+				}
+			}
+		}
+	}
+	return ret
 }
--- leetcode/1501-1600/1583.Count-Unhappy-Friends/Solution_test.go
+++ leetcode/1501-1600/1583.Count-Unhappy-Friends/Solution_test.go
@@ -9,31 +9,41 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name        string
+		n           int
+		preferences [][]int
+		pairs       [][]int
+		expect      int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 4, [][]int{
+			{1, 2, 3}, {3, 2, 0}, {3, 1, 0}, {1, 2, 0},
+		}, [][]int{
+			{0, 1}, {2, 3},
+		}, 2},
+		{"TestCase2", 2, [][]int{
+			{1}, {0},
+		}, [][]int{{1, 0}}, 0},
+		{"TestCase3", 4, [][]int{
+			{1, 3, 2}, {2, 3, 0}, {1, 3, 0}, {0, 2, 1},
+		}, [][]int{{1, 3}, {0, 2}}, 4},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.preferences, c.pairs)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.n, c.preferences, c.pairs)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
