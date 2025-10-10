8424a432b0121ddf3466bb23029b7ed6b5c62a32
0xff-dev <stevenshuang521@gmail.com>
2025-10-10 09:11:12 +0800

    Add solution and test-cases for problem 3147

--- leetcode/3101-3200/3147.Taking-Maximum-Energy-From-the-Mystic-Dungeon/Solution_test.go
+++ leetcode/3101-3200/3147.Taking-Maximum-Energy-From-the-Mystic-Dungeon/Solution_test.go
@@ -10,30 +10,30 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		k      int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{5, 2, -10, -5, 1}, 3, 3},
+		{"TestCase2", []int{-2, -3, -1}, 2, -1},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.inputs, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
