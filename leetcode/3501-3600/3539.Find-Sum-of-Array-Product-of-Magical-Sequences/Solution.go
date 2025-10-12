f42a846a4ec621b1dee0c88f77e4ecf6b5d18a44
0xff-dev <stevenshuang521@gmail.com>
2025-10-12 15:27:10 +0800

    Add solution and test-cases for problem 3539

--- leetcode/3501-3600/3539.Find-Sum-of-Array-Product-of-Magical-Sequences/Solution.go
+++ leetcode/3501-3600/3539.Find-Sum-of-Array-Product-of-Magical-Sequences/Solution.go
@@ -1,5 +1,84 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "math/bits"
+
+func quickmul(x, y, mod int64) int64 {
+	res, cur := int64(1), x%mod
+	for y > 0 {
+		if y&1 == 1 {
+			res = res * cur % mod
+		}
+		y >>= 1
+		cur = cur * cur % mod
+	}
+	return res
+}
+
+func Solution(m int, k int, nums []int) int {
+	mod := int64(1000000007)
+	fac := make([]int64, m+1)
+	fac[0] = 1
+	for i := 1; i <= m; i++ {
+		fac[i] = fac[i-1] * int64(i) % mod
+	}
+	ifac := make([]int64, m+1)
+	ifac[0] = 1
+	ifac[1] = 1
+	for i := 2; i <= m; i++ {
+		ifac[i] = quickmul(int64(i), mod-2, mod)
+	}
+	for i := 2; i <= m; i++ {
+		ifac[i] = ifac[i-1] * ifac[i] % mod
+	}
+
+	numsPower := make([][]int64, len(nums))
+	for i := range nums {
+		numsPower[i] = make([]int64, m+1)
+		numsPower[i][0] = 1
+		for j := 1; j <= m; j++ {
+			numsPower[i][j] = numsPower[i][j-1] * int64(nums[i]) % mod
+		}
+	}
+
+	f := make([][][][]int64, len(nums))
+	for i := range nums {
+		f[i] = make([][][]int64, m+1)
+		for j := 0; j <= m; j++ {
+			f[i][j] = make([][]int64, m*2+1)
+			for p := 0; p <= m*2; p++ {
+				f[i][j][p] = make([]int64, k+1)
+			}
+		}
+	}
+
+	for j := 0; j <= m; j++ {
+		f[0][j][j][0] = numsPower[0][j] * ifac[j] % mod
+	}
+	for i := 0; i+1 < len(nums); i++ {
+		for j := 0; j <= m; j++ {
+			for p := 0; p <= m*2; p++ {
+				for q := 0; q <= k; q++ {
+					q2 := p%2 + q
+					if q2 > k {
+						break
+					}
+					for r := 0; r+j <= m; r++ {
+						p2 := p/2 + r
+						f[i+1][j+r][p2][q2] += f[i][j][p][q] * numsPower[i+1][r] % mod * ifac[r] % mod
+						f[i+1][j+r][p2][q2] %= mod
+					}
+				}
+			}
+		}
+	}
+
+	res := int64(0)
+	for p := 0; p <= m*2; p++ {
+		for q := 0; q <= k; q++ {
+			if bits.OnesCount(uint(p))+q == k {
+				res = (res + f[len(nums)-1][m][p][q]*fac[m]%mod) % mod
+			}
+		}
+	}
+	return int(res)
 }
--- leetcode/3501-3600/3539.Find-Sum-of-Array-Product-of-Magical-Sequences/Solution_test.go
+++ leetcode/3501-3600/3539.Find-Sum-of-Array-Product-of-Magical-Sequences/Solution_test.go
@@ -10,30 +10,31 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		m, k   int
+		nums   []int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 5, 5, []int{1, 10, 100, 10000, 1000000}, 991600007},
+		{"TestCase2", 2, 2, []int{5, 4, 3, 2, 1}, 170},
+		{"TestCase3", 1, 1, []int{28}, 28},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.m, c.k, c.nums)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.m, c.k, c.nums)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
