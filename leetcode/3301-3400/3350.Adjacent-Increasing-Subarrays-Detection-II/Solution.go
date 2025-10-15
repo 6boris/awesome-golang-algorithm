326ef279f0ae9fc5a0a83872c849ad2ea8b97ea3
0xff-dev <stevenshuang521@gmail.com>
2025-10-15 08:57:44 +0800

    Add solution and test-cases for problem 3350

--- leetcode/3301-3400/3350.Adjacent-Increasing-Subarrays-Detection-II/Solution.go
+++ leetcode/3301-3400/3350.Adjacent-Increasing-Subarrays-Detection-II/Solution.go
@@ -1,5 +1,42 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func ok(nums []int, k int) bool {
+	// 存储所有ok的字数组的下标
+	// 然后找index的间隔是否存在不想等就可以了
+	indies := []int{}
+	start, end := 0, 0
+	curLen := 0
+	pre := -1001
+	// 6, 13, -17, -20, 2
+	for ; end < len(nums); end++ {
+		if nums[end] <= pre {
+			start, curLen = end, 1
+		} else {
+			curLen++
+		}
+		pre = nums[end]
+		if curLen == k {
+			indies = append(indies, start)
+			start++
+			curLen--
+		}
+	}
+	keys := make(map[int]struct{})
+	for _, index := range indies {
+		keys[index] = struct{}{}
+	}
+	for i := 0; i < len(indies)-1; i++ {
+		if _, ok := keys[indies[i]+k]; ok {
+			return true
+		}
+	}
+	return false
+}
+func Solution(nums []int) int {
+	index := sort.Search(len(nums)/2, func(i int) bool {
+		return !ok(nums, i+1)
+	})
+	return index
 }
