4bea7075e23bc702d59e12f2ec5ff54e2ef54c58
0xff-dev <stevenshuang521@gmail.com>
2025-10-18 09:09:39 +0800

    Add solution and test-cases for problem 3397

--- leetcode/3301-3400/3397.Maximum-Number-of-Distinct-Elements-After-Operations/Solution.go
+++ leetcode/3301-3400/3397.Maximum-Number-of-Distinct-Elements-After-Operations/Solution.go
@@ -1,5 +1,21 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"math"
+	"sort"
+)
+
+func Solution(nums []int, k int) int {
+	sort.Ints(nums)
+	cnt := 0
+	prev := math.MinInt32
+
+	for _, num := range nums {
+		curr := min(max(num-k, prev+1), num+k)
+		if curr > prev {
+			cnt++
+			prev = curr
+		}
+	}
+	return cnt
 }
