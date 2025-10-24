b00e532c9488c9710ef368fe84b887b2c07e5c93
0xff-dev <stevenshuang521@gmail.com>
2025-10-24 21:48:55 +0800

    Add solution and test-cases for problem 2048

--- leetcode/2001-2100/2048.Next-Greater-Numerically-Balanced-Number/Solution_test.go
+++ leetcode/2001-2100/2048.Next-Greater-Numerically-Balanced-Number/Solution_test.go
@@ -10,12 +10,13 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 1, 22},
+		{"TestCase2", 1000, 1333},
+		{"TestCase3", 3000, 3133},
+		{"TestCase4", 0, 1},
 	}
 
 	//	开始测试
@@ -30,10 +31,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
