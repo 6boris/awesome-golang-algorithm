8ac42a2ecb37f2030eea689406475e1828d470a0
0xff-dev <stevenshuang521@gmail.com>
2025-11-01 22:33:36 +0800

    Add solution and test-cases for problem 3280

---
+++ leetcode/3201-3300/3280.Convert-Date-to-Binary/Solution_test.go
@@ -0,0 +1,38 @@
+package Solution
+
+import (
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	//	测试用例
+	cases := []struct {
+		name   string
+		inputs string
+		expect string
+	}{
+		{"TestCase1", "2080-02-29", "100000100000-10-11101"},
+		{"TestCase2", "1900-01-01", "11101101100-1-1"},
+	}
+
+	//	开始测试
+	for i, c := range cases {
+		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
+			got := Solution(c.inputs)
+			if !reflect.DeepEqual(got, c.expect) {
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+					c.expect, got, c.inputs)
+			}
+		})
+	}
+}
+
+// 压力测试
+func BenchmarkSolution(b *testing.B) {
+}
+
+// 使用案列
+func ExampleSolution() {
+}
