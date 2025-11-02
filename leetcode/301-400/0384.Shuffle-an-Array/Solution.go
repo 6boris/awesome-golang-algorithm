338c4894a0198d1820268560dd6e05c9cc7f3fbf
0xff-dev <stevenshuang521@gmail.com>
2025-11-02 20:25:45 +0800

    Add solution and test-cases for problem 384

--- leetcode/301-400/0384.Shuffle-an-Array/Solution.go
+++ leetcode/301-400/0384.Shuffle-an-Array/Solution.go
@@ -1,5 +1,46 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"math/rand"
+	"time"
+)
+
+type Shuffle struct {
+	origin []int
+	r      *rand.Rand
+}
+
+func Constructor(nums []int) Shuffle {
+	return Shuffle{
+		origin: nums,
+		r:      rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+}
+
+func (this *Shuffle) Reset() []int {
+	this.r = rand.New(rand.NewSource(time.Now().UnixNano()))
+	return this.origin
+}
+
+func (this *Shuffle) Shuffle() []int {
+	n := len(this.origin)
+	cur := make([]int, len(this.origin))
+	copy(cur, this.origin)
+	for i := n - 1; i > 0; i-- {
+		j := this.r.Intn(i + 1)
+		cur[i], cur[j] = cur[j], cur[i]
+	}
+	return cur
+}
+func Solution(nums []int, op []string) [][]int {
+	c := Constructor(nums)
+	var ret [][]int
+	for _, name := range op {
+		if name == "shuffle" {
+			ret = append(ret, c.Shuffle())
+			continue
+		}
+		ret = append(ret, c.Reset())
+	}
+	return ret
 }
--- leetcode/301-400/0384.Shuffle-an-Array/Solution_test.go
+++ leetcode/301-400/0384.Shuffle-an-Array/Solution_test.go
@@ -10,30 +10,29 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		op     []string
+		expect [][]int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1}, []string{"shuffle", "rest"}, [][]int{{1}, {1}}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.inputs, c.op)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.inputs, c.op)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
