6bda3ef91cdd92b56f24c32053d8be5897e7c368
0xff-dev <stevenshuang521@gmail.com>
2025-11-09 12:04:39 +0800

    Add solution and test-cases for problem 2169

--- leetcode/2101-2200/2169.Count-Operations-to-Obtain-Zero/Solution.go
+++ leetcode/2101-2200/2169.Count-Operations-to-Obtain-Zero/Solution.go
@@ -1,5 +1,17 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(num1 int, num2 int) int {
+	var ret, x int
+	for num1 > 0 && num2 > 0 {
+		if num1 > num2 {
+			x = num1 / num2
+			ret += x
+			num1 -= num2 * x
+			continue
+		}
+		x = num2 / num1
+		ret += x
+		num2 -= num1 * x
+	}
+	return ret
 }
