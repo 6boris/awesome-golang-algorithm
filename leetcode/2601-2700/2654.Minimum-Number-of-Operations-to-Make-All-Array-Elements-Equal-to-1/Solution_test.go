e4c408e7feecf1fe905eeb9b4694f055d8c5c1b8
0xff-dev <stevenshuang521@gmail.com>
2025-11-12 22:30:29 +0800

    Add solution and test-cases for problem 2654

--- leetcode/2601-2700/2654.Minimum-Number-of-Operations-to-Make-All-Array-Elements-Equal-to-1/Solution_test.go
+++ leetcode/2601-2700/2654.Minimum-Number-of-Operations-to-Make-All-Array-Elements-Equal-to-1/Solution_test.go
@@ -10,12 +10,11 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs []int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{2, 6, 3, 4}, 4},
+		{"TestCase2", []int{2, 10, 6, 14}, -1},
 	}
 
 	//	开始测试
@@ -30,10 +29,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
