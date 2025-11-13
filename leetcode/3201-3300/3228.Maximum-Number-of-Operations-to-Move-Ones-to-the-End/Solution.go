f6405c706801d9efccb83b6e4e55bdefa248d27c
0xff-dev <stevenshuang521@gmail.com>
2025-11-13 22:14:28 +0800

    Add solution and test-cases for problem 3228

--- leetcode/3201-3300/3228.Maximum-Number-of-Operations-to-Move-Ones-to-the-End/Solution.go
+++ leetcode/3201-3300/3228.Maximum-Number-of-Operations-to-Move-Ones-to-the-End/Solution.go
@@ -1,5 +1,19 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string) int {
+	countOne := 0
+	ans := 0
+	i := 0
+	for i < len(s) {
+		if s[i] == '0' {
+			for i+1 < len(s) && s[i+1] == '0' {
+				i++
+			}
+			ans += countOne
+		} else {
+			countOne++
+		}
+		i++
+	}
+	return ans
 }
