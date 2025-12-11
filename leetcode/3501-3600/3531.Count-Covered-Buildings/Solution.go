094181ac2b2a061bd06d075b97a17d218a5abacf
0xff-dev <stevenshuang521@gmail.com>
2025-12-11 22:47:42 +0800

    Add solution and test-cases for problem 3531

--- leetcode/3501-3600/3531.Count-Covered-Buildings/Solution.go
+++ leetcode/3501-3600/3531.Count-Covered-Buildings/Solution.go
@@ -1,5 +1,45 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(n int, buildings [][]int) int {
+	var ret int
+	l := len(buildings)
+	byY := make([][]int, l)
+	byX := make([][]int, l)
+	copy(byY, buildings)
+	copy(byX, buildings)
+	sort.Slice(byY, func(i, j int) bool {
+		return byY[i][1] < byY[j][1]
+	})
+	sort.Slice(byX, func(i, j int) bool {
+		return byX[i][0] < byX[j][0]
+	})
+
+	yLoc := make(map[int][]int)
+	xLoc := make(map[int][]int)
+	for _, build := range byY {
+		xLoc[build[0]] = append(xLoc[build[0]], build[1])
+	}
+	for _, build := range byX {
+		yLoc[build[1]] = append(yLoc[build[1]], build[0])
+	}
+
+	for _, build := range buildings {
+		x, y := build[0], build[1]
+		index := sort.Search(len(xLoc[x]), func(i int) bool {
+			return xLoc[x][i] >= y
+		})
+		if index > 0 && index < len(xLoc[x])-1 {
+			j := sort.Search(len(yLoc[y]), func(ii int) bool {
+				return yLoc[y][ii] >= x
+			})
+			if j > 0 && j < len(yLoc[y])-1 {
+				ret++
+			}
+		}
+
+	}
+
+	return ret
 }
