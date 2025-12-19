12230854377255cdcbd429ecfed5e02b4de0eaa1
0xff-dev <stevenshuang521@gmail.com>
2025-12-19 21:41:40 +0800

    Add solution and test-cases for problem 3652

--- leetcode/3601-3700/3652.Best-Time-to-Buy-and-Sell-Stock-using-Strategy/Solution.go
+++ leetcode/3601-3700/3652.Best-Time-to-Buy-and-Sell-Stock-using-Strategy/Solution.go
@@ -1,5 +1,19 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(prices []int, strategy []int, k int) int64 {
+	n := len(prices)
+	profitSum := make([]int64, n+1)
+	priceSum := make([]int64, n+1)
+	for i := 0; i < n; i++ {
+		profitSum[i+1] = profitSum[i] + int64(prices[i])*int64(strategy[i])
+		priceSum[i+1] = priceSum[i] + int64(prices[i])
+	}
+	res := profitSum[n]
+	for i := k - 1; i < n; i++ {
+		leftProfit := profitSum[i-k+1]
+		rightProfit := profitSum[n] - profitSum[i+1]
+		changeProfit := priceSum[i+1] - priceSum[i-k/2+1]
+		res = max(res, leftProfit+changeProfit+rightProfit)
+	}
+	return res
 }
--- leetcode/3601-3700/3652.Best-Time-to-Buy-and-Sell-Stock-using-Strategy/Solution_test.go
+++ leetcode/3601-3700/3652.Best-Time-to-Buy-and-Sell-Stock-using-Strategy/Solution_test.go
@@ -9,31 +9,31 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name             string
+		prices, strategy []int
+		k                int
+		expect           int64
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{4, 2, 8}, []int{-1, 0, 1}, 2, 10},
+		{"TestCase2", []int{5, 4, 3}, []int{1, 1, 0}, 2, 9},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.prices, c.strategy, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.prices, c.strategy, c.k)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
