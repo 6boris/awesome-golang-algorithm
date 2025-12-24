e7a19083fe1389900ba751b9ef3b0d04a36a7c02
0xff-dev <stevenshuang521@gmail.com>
2025-12-24 22:39:24 +0800

    Add solution and test-cases for problem 3074

--- leetcode/3001-3100/3074.Apple-Redistribution-into-Boxes/Solution.go
+++ leetcode/3001-3100/3074.Apple-Redistribution-into-Boxes/Solution.go
@@ -1,5 +1,17 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(apple []int, capacity []int) int {
+	sum := 0
+	for _, n := range apple {
+		sum += n
+	}
+	var ret int
+	sort.Ints(capacity)
+	for i := len(capacity) - 1; i >= 0 && sum > 0; i-- {
+		sum -= capacity[i]
+		ret++
+	}
+	return ret
 }
