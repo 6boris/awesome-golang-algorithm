f395320c96725a76c02b87aad25bba533a7bd16b
0xff-dev <stevenshuang521@gmail.com>
2026-01-03 20:35:12 +0800

    Add solution and test-cases for problem 982

--- leetcode/901-1000/0982.Triples-with-Bitwise-AND-Equal-To-Zero/Solution.go
+++ leetcode/901-1000/0982.Triples-with-Bitwise-AND-Equal-To-Zero/Solution.go
@@ -1,5 +1,28 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) int {
+	freq := make(map[int]int)
+	for _, n := range nums {
+		freq[n]++
+	}
+
+	values := make([]int, 0)
+	for v := range freq {
+		values = append(values, v)
+	}
+	var ret int
+	pairs := make(map[int]int)
+	for _, a := range values {
+		for _, b := range values {
+			pairs[a&b] += freq[a] * freq[b]
+		}
+	}
+	for v, c := range pairs {
+		for _, n := range values {
+			if v&n == 0 {
+				ret += c * freq[n]
+			}
+		}
+	}
+	return ret
 }
