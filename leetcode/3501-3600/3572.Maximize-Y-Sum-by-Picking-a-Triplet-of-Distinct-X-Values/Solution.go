a28e08777762166b31335b3d8ff1b55aa52a94a5
0xff-dev <stevenshuang521@gmail.com>
2026-01-03 18:30:35 +0800

    Add solution and test-cases for problem 3572

--- leetcode/3501-3600/3572.Maximize-Y-Sum-by-Picking-a-Triplet-of-Distinct-X-Values/Solution.go
+++ leetcode/3501-3600/3572.Maximize-Y-Sum-by-Picking-a-Triplet-of-Distinct-X-Values/Solution.go
@@ -1,5 +1,28 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import (
+	"sort"
+)
+
+func Solution(x []int, y []int) int {
+	maxItem := make(map[int]int)
+	for index, n := range x {
+		maxItem[n] = max(maxItem[n], y[index])
+	}
+	if len(maxItem) < 3 {
+		return -1
+	}
+	values := make([]int, 0)
+	for _, v := range maxItem {
+		values = append(values, v)
+	}
+	sort.Slice(values, func(i, j int) bool {
+		return values[i] > values[j]
+	})
+
+	ret := 0
+	for i := range 3 {
+		ret += values[i]
+	}
+	return ret
 }
