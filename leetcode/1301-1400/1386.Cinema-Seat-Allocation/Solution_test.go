436f0bb83a29f856e017193aae28733a0cd8435b
0xff-dev <stevenshuang521@gmail.com>
2026-01-11 20:10:15 +0800

    Add solution and test-cases for proble 1386

--- leetcode/1301-1400/1386.Cinema-Seat-Allocation/Solution_test.go
+++ leetcode/1301-1400/1386.Cinema-Seat-Allocation/Solution_test.go
@@ -9,31 +9,32 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name          string
+		n             int
+		reservedSeats [][]int
+		expect        int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 3, [][]int{{1, 2}, {1, 3}, {1, 8}, {2, 6}, {3, 1}, {3, 10}}, 4},
+		{"TestCase2", 2, [][]int{{2, 1}, {1, 8}, {2, 6}}, 2},
+		{"TestCase3", 4, [][]int{{4, 3}, {1, 4}, {4, 6}, {1, 7}}, 4},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.reservedSeats)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.n, c.reservedSeats)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
