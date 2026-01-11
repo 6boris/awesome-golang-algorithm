dc22f349f102802763c167672c9585e7d78c075a
0xff-dev <stevenshuang521@gmail.com>
2026-01-11 21:53:18 +0800

    Add solution and test-cases for problem 1567

--- leetcode/1501-1600/1567.Maximum-Length-of-Subarray-With-Positive-Product/Solution.go
+++ leetcode/1501-1600/1567.Maximum-Length-of-Subarray-With-Positive-Product/Solution.go
@@ -1,5 +1,41 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) int {
+	var ret int
+	cnt := 0
+	start := -1
+	first, last := -1, -1
+	l := len(nums)
+	for idx, n := range nums {
+		if n == 0 {
+			tmp := idx - start - 1
+			if cnt&1 == 0 {
+				ret = max(ret, tmp)
+			} else {
+				left := first - start
+				right := idx - last
+				ret = max(ret, tmp-left, tmp-right)
+			}
+			start = idx
+			first, last, cnt = -1, -1, 0
+			continue
+		}
+		if n > 0 {
+			continue
+		}
+		if first == -1 {
+			first = idx
+		}
+		last = idx
+		cnt++
+	}
+	tmp := l - start - 1
+	if cnt&1 == 0 {
+		ret = max(ret, tmp)
+	} else {
+		left := first - start
+		right := l - last
+		ret = max(ret, tmp-left, tmp-right)
+	}
+	return ret
 }
