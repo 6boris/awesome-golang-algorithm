2611ca26723f32863a068fbdefd222bef50ccb0e
0xff-dev <stevenshuang521@gmail.com>
2026-01-15 22:30:14 +0800

    Add solution and test-cases for problem 2943

--- leetcode/2901-3000/2943.Maximize-Area-of-Square-Hole-in-Grid/Solution_test.go
+++ leetcode/2901-3000/2943.Maximize-Area-of-Square-Hole-in-Grid/Solution_test.go
@@ -9,31 +9,32 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name         string
+		n, m         int
+		hBars, vBars []int
+		expect       int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 2, 1, []int{2, 3}, []int{2}, 4},
+		{"TestCase2", 1, 1, []int{2}, []int{2}, 4},
+		{"TestCase3", 2, 3, []int{2, 3}, []int{2, 4}, 4},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.m, c.hBars, c.vBars)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v %v",
+					c.expect, got, c.n, c.m, c.hBars, c.vBars)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
