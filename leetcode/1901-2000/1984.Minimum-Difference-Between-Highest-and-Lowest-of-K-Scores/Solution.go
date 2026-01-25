27de3bd2d8dab72d32a2c02d32961e220629d928
0xff-dev <stevenshuang521@gmail.com>
2026-01-25 20:05:47 +0800

    Add solution and test-cases for problem 1984

--- leetcode/1901-2000/1984.Minimum-Difference-Between-Highest-and-Lowest-of-K-Scores/Solution.go
+++ leetcode/1901-2000/1984.Minimum-Difference-Between-Highest-and-Lowest-of-K-Scores/Solution.go
@@ -1,5 +1,16 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(nums []int, k int) int {
+	if k == 1 {
+		return 0
+	}
+
+	ret, end := 100001, len(nums)-k
+	sort.Ints(nums)
+	for i := 0; i <= end; i++ {
+		ret = min(ret, nums[i+k-1]-nums[i])
+	}
+	return ret
 }
