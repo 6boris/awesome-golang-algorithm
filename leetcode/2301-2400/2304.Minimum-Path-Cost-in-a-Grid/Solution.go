d900643844a07f24afc4074b81d8c001e0612f7e
0xff-dev <stevenshuang521@gmail.com>
2026-01-31 18:22:27 +0800

    Add solution and test-cases for problem 2304

--- leetcode/2301-2400/2304.Minimum-Path-Cost-in-a-Grid/Solution.go
+++ leetcode/2301-2400/2304.Minimum-Path-Cost-in-a-Grid/Solution.go
@@ -1,5 +1,36 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "slices"
+
+const INF2304 = 0x7fffffff
+
+func Solution(grid [][]int, moveCost [][]int) int {
+	rows, cols := len(grid), len(grid[0])
+	dp := [2][]int{}
+	for i := range 2 {
+		dp[i] = make([]int, cols)
+		for j := range cols {
+			dp[i][j] = INF2304
+		}
+	}
+	for i := range cols {
+		dp[0][i] = INF2304
+	}
+	for i := range cols {
+		dp[1][i] = grid[rows-1][i]
+	}
+
+	index := 0
+	for i := rows - 2; i >= 0; i-- {
+		for c := 0; c < cols; c++ {
+			for next := 0; next < cols; next++ {
+				dp[index][c] = min(dp[index][c], grid[i][c]+dp[1-index][next]+moveCost[grid[i][c]][next])
+			}
+		}
+		index = 1 - index
+		for j := range cols {
+			dp[index][j] = INF2304
+		}
+	}
+	return slices.Min(dp[1-index])
 }
