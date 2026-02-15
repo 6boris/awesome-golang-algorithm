fb79461e8f228f58b027b1e556a0524a06bcf494
0xff-dev <stevenshuang521@gmail.com>
2026-02-15 23:07:48 +0800

    Add solution and test-cases for problem 385

--- leetcode/301-400/0385.Mini-Parser/Solution_test.go
+++ leetcode/301-400/0385.Mini-Parser/Solution_test.go
@@ -10,30 +10,39 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs string
+		expect *NestedInteger
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "324", &NestedInteger{Value: 324, IsNum: true}},
+		{"TestCase2", "[123,[456,[789]]]", &NestedInteger{
+			Child: []NestedInteger{
+				{Value: 123, IsNum: true},
+				{Child: []NestedInteger{
+					{Value: 456, IsNum: true},
+					{Child: []NestedInteger{{Value: 789, IsNum: true}}},
+				}},
+			},
+		}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
 			got := Solution(c.inputs)
-			if !reflect.DeepEqual(got, c.expect) {
+			s1 := c.expect.serialize()
+			s2 := got.serialize()
+			if !reflect.DeepEqual(s1, s2) {
 				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+					s1, s2, c.inputs)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
