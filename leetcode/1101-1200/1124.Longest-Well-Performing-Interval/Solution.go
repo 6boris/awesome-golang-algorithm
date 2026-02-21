99ad2da430704fcb67a65b290bbd3668eac99423
0xff-dev <stevenshuang521@gmail.com>
2026-02-21 21:43:05 +0800

    Add solution and test-cases for problem 1124

--- leetcode/1101-1200/1124.Longest-Well-Performing-Interval/Solution.go
+++ leetcode/1101-1200/1124.Longest-Well-Performing-Interval/Solution.go
@@ -1,5 +1,39 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(hours []int) int {
+	n := len(hours)
+	score := make([]int, n)
+
+	for i, h := range hours {
+		if h > 8 {
+			score[i] = 1
+		} else {
+			score[i] = -1
+		}
+	}
+
+	diff := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		diff[i] = diff[i-1] + score[i-1]
+	}
+
+	stack := []int{}
+	for i := 0; i <= n; i++ {
+		if len(stack) == 0 || diff[i] < diff[stack[len(stack)-1]] {
+			stack = append(stack, i)
+		}
+	}
+
+	maxLength := 0
+	for i := n; i >= 0; i-- {
+		for len(stack) > 0 && diff[i] > diff[stack[len(stack)-1]] {
+			j := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if i-j > maxLength {
+				maxLength = i - j
+			}
+		}
+	}
+
+	return maxLength
 }
