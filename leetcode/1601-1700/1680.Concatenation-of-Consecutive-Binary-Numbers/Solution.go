5afeb4985e3ca0a489fcaebc8e2ca762a845432a
0xff-dev <stevenshuang521@gmail.com>
2026-02-28 16:09:42 +0800

    Add solution and test-cases for problem 1680

--- leetcode/1601-1700/1680.Concatenation-of-Consecutive-Binary-Numbers/Solution.go
+++ leetcode/1601-1700/1680.Concatenation-of-Consecutive-Binary-Numbers/Solution.go
@@ -1,5 +1,23 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+const mod1680 = 1000000007
+
+func Solution(n int) int {
+	bits := func(x int) int {
+		mask := 1 << 31
+		cnt := 0
+		for mask&x == 0 {
+			mask >>= 1
+			cnt++
+		}
+		return 32 - cnt
+	}
+	ret := 0
+	for i := 1; i <= n; i++ {
+		shift := bits(i)
+		ret <<= shift
+		ret %= mod1680
+		ret = (ret + i) % mod1680
+	}
+	return ret
 }
