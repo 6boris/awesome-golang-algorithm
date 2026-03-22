42574606d469821835e716cd1b83c6703548174a
0xff-dev <stevenshuang521@gmail.com>
2026-03-22 20:55:00 +0800

    Add solution and test-cases for problem 1909

--- leetcode/1901-2000/1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing/Solution.go
+++ leetcode/1901-2000/1909.Remove-One-Element-to-Make-the-Array-Strictly-Increasing/Solution.go
@@ -1,5 +1,28 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) bool {
+	pivot := -1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] <= nums[i-1] {
+			if pivot == -1 {
+				pivot = i
+				continue
+			}
+			return false
+		}
+	}
+	if pivot == -1 {
+		return true
+	}
+
+	// remove pivot-1
+	if pivot-2 >= 0 && nums[pivot] > nums[pivot-2] {
+		return true
+	}
+	// remove pivot
+	if pivot < len(nums)-1 && nums[pivot+1] > nums[pivot-1] {
+		return true
+	}
+
+	return pivot == len(nums)-1 || pivot == 1
 }
