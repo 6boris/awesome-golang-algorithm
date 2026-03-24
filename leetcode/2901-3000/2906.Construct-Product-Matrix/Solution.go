b5eddf7da907e34accf550a3fe08274070f66d8e
0xff-dev <stevenshuang521@gmail.com>
2026-03-24 22:57:11 +0800

    Add solution and test-cases for problem 2906

--- leetcode/2901-3000/2906.Construct-Product-Matrix/Solution.go
+++ leetcode/2901-3000/2906.Construct-Product-Matrix/Solution.go
@@ -1,5 +1,49 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(grid [][]int) [][]int {
+	/*
+			1, 2, 3
+		   4, 5, 6
+		   7, 8, 9
+	*/
+	m, n := len(grid), len(grid[0])
+	ret := make([][]int, m)
+	total := m * n
+	left, right := make([]int, total), make([]int, total)
+	index, prod := 0, 1
+
+	for i := range m {
+		ret[i] = make([]int, n)
+		for j := range n {
+			prod = (prod * grid[i][j]) % 12345
+			left[index] = prod
+			index++
+		}
+	}
+
+	index = total - 1
+	prod = 1
+	for i := m - 1; i >= 0; i-- {
+		for j := n - 1; j >= 0; j-- {
+			prod = (prod * grid[i][j]) % 12345
+			right[index] = prod
+			index--
+		}
+	}
+
+	base := 1
+	for i := range m {
+		for j := range n {
+			base = 1
+			index = i*n + j
+			if index >= 1 {
+				base = (base * left[index-1]) % 12345
+			}
+			if index < total-1 {
+				base = (base * right[index+1]) % 12345
+			}
+			ret[i][j] = base
+		}
+	}
+	return ret
 }
--- leetcode/2901-3000/2906.Construct-Product-Matrix/Solution_test.go
+++ leetcode/2901-3000/2906.Construct-Product-Matrix/Solution_test.go
@@ -10,12 +10,19 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs [][]int
+		expect [][]int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", [][]int{
+			{1, 2}, {3, 4},
+		}, [][]int{
+			{24, 12}, {8, 6},
+		}},
+		{"TestCase2", [][]int{
+			{12345, 2, 1},
+		}, [][]int{
+			{2, 0, 0},
+		}},
 	}
 
 	//	开始测试
@@ -30,10 +37,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
