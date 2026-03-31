3975ef4150ca0ddef929e629d77c70f770de4fd2
0xff-dev <stevenshuang521@gmail.com>
2026-03-31 22:47:16 +0800

    Add solution and test-cases for problem 3474

--- leetcode/3401-3500/3474.Lexicographically-Smallest-Generated-String/Solution.go
+++ leetcode/3401-3500/3474.Lexicographically-Smallest-Generated-String/Solution.go
@@ -1,5 +1,47 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(str1 string, str2 string) string {
+	n, m := len(str1), len(str2)
+	s := make([]byte, n+m-1)
+	fixed := make([]int, n+m-1)
+
+	for i := range s {
+		s[i] = 'a'
+	}
+
+	for i := 0; i < n; i++ {
+		if str1[i] == 'T' {
+			for j := i; j < i+m; j++ {
+				if fixed[j] == 1 && s[j] != str2[j-i] {
+					return ""
+				} else {
+					s[j] = str2[j-i]
+					fixed[j] = 1
+				}
+			}
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if str1[i] == 'F' {
+			flag := false
+			idx := -1
+			for j := i + m - 1; j >= i; j-- {
+				if str2[j-i] != s[j] {
+					flag = true
+				}
+				if idx == -1 && fixed[j] == 0 {
+					idx = j
+				}
+			}
+			if flag {
+				continue
+			} else if idx != -1 {
+				s[idx] = 'b'
+			} else {
+				return ""
+			}
+		}
+	}
+	return string(s)
 }
--- leetcode/3401-3500/3474.Lexicographically-Smallest-Generated-String/Solution_test.go
+++ leetcode/3401-3500/3474.Lexicographically-Smallest-Generated-String/Solution_test.go
@@ -9,31 +9,31 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name       string
+		str1, str2 string
+		expect     string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "TFTF", "ab", "ababa"},
+		{"TestCase2", "TFTF", "abc", ""},
+		{"TestCase3", "F", "d", "a"},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.str1, c.str2)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.str1, c.str2)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
