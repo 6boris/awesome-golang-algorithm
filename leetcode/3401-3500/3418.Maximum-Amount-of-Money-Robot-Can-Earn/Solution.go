f554f9b49d02b875970a0fa8e680e4fb186bdb55
0xff-dev <stevenshuang521@gmail.com>
2026-04-02 22:50:56 +0800

    Add solution and test-cases for problem 3418

--- leetcode/3401-3500/3418.Maximum-Amount-of-Money-Robot-Can-Earn/Solution.go
+++ leetcode/3401-3500/3418.Maximum-Amount-of-Money-Robot-Can-Earn/Solution.go
@@ -1,5 +1,47 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(coins [][]int) int {
+	m, n := len(coins), len(coins[0])
+	memo := make([][][]int, m)
+	for i := range memo {
+		memo[i] = make([][]int, n)
+		for j := range memo[i] {
+			memo[i][j] = make([]int, 3)
+			for k := range memo[i][j] {
+				memo[i][j][k] = -1 << 31
+			}
+		}
+	}
+
+	var dfs func(i, j, k int) int
+	dfs = func(i, j, k int) int {
+		if i >= m || j >= n {
+			return -1 << 31
+		}
+
+		x := coins[i][j]
+		// arrive at the destination
+		if i == m-1 && j == n-1 {
+			if k > 0 {
+				return max(0, x)
+			}
+			return x
+		}
+
+		if memo[i][j][k] != -1<<31 {
+			return memo[i][j][k]
+		}
+
+		// not neutralize
+		res := max(dfs(i+1, j, k), dfs(i, j+1, k)) + x
+		if k > 0 && x < 0 {
+			// neutralize
+			res = max(res, max(dfs(i+1, j, k-1), dfs(i, j+1, k-1)))
+		}
+
+		memo[i][j][k] = res
+		return res
+	}
+
+	return dfs(0, 0, 2)
 }
