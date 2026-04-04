64843ccf7996d5058de3a1c6e28819fef67cd0ae
0xff-dev <stevenshuang521@gmail.com>
2026-04-04 15:35:15 +0800

    Add solution and test-cases for problem 2075

--- leetcode/2001-2100/2075.Decode-the-Slanted-Ciphertext/Solution.go
+++ leetcode/2001-2100/2075.Decode-the-Slanted-Ciphertext/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "bytes"
+
+func Solution(encodedText string, rows int) string {
+	if rows == 1 || encodedText == "" {
+		return encodedText
+	}
+	size := len(encodedText)
+	cols := size / rows
+	var buf bytes.Buffer
+	for i := 0; i <= cols-rows+1; i++ {
+		for j := i; j < size; j += cols + 1 {
+			buf.WriteByte(encodedText[j])
+		}
+	}
+
+	ret := buf.String()
+	end := len(ret) - 1
+	for ; ret[end] == ' '; end-- {
+
+	}
+	return ret[:end+1]
 }
