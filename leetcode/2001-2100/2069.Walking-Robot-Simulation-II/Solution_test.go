dd129f60734fb76cc5031e49ce62ac34ffe217ce
0xff-dev <stevenshuang521@gmail.com>
2026-04-07 23:06:01 +0800

    Add solution and test-cases for problem 2069

--- leetcode/2001-2100/2069.Walking-Robot-Simulation-II/Solution_test.go
+++ leetcode/2001-2100/2069.Walking-Robot-Simulation-II/Solution_test.go
@@ -9,31 +9,37 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name          string
+		width, height int
+		inputs        []input
+		expect        []any
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 6, 3, []input{
+			{"step", 2}, {"step", 2},
+			{"pos", 0}, {"dir", 0},
+			{"step", 2}, {"step", 1}, {"step", 4},
+			{"pos", 0}, {"dir", 0},
+		}, []any{
+			[]int{4, 0}, "East", []int{1, 2}, "West",
+		}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.width, c.height, c.inputs)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v",
+					c.expect, got, c.width, c.height, c.inputs)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
