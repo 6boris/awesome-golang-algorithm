ee334b38cf6cac77f52781b24473f7d6eb4cc725
0xff-dev <stevenshuang521@gmail.com>
2026-04-09 22:54:05 +0800

    Add solution and test-cases for problem 3655

--- leetcode/3601-3700/3655.XOR-After-Range-Multiplication-Queries-II/Solution_test.go
+++ leetcode/3601-3700/3655.XOR-After-Range-Multiplication-Queries-II/Solution_test.go
@@ -9,31 +9,33 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name    string
+		nums    []int
+		queries [][]int
+		expect  int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{1, 1, 1}, [][]int{{0, 2, 1, 4}}, 4},
+		{"TestCase2", []int{2, 3, 1, 5, 4}, [][]int{
+			{1, 4, 2, 3}, {0, 2, 1, 2},
+		}, 31},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.nums, c.queries)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.nums, c.queries)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
