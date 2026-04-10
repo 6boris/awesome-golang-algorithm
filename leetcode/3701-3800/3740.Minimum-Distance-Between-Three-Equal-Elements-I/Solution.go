4a4eaf31dd85859b6efb89f0a5043062a84b7fad
0xff-dev <stevenshuang521@gmail.com>
2026-04-10 22:16:43 +0800

    Add solution and test-cases for problem 3740

--- leetcode/3701-3800/3740.Minimum-Distance-Between-Three-Equal-Elements-I/Solution.go
+++ leetcode/3701-3800/3740.Minimum-Distance-Between-Three-Equal-Elements-I/Solution.go
@@ -1,5 +1,21 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) int {
+	group := make(map[int][]int)
+	for i := range nums {
+		group[nums[i]] = append(group[nums[i]], i)
+	}
+	ret := -1
+	for key := range group {
+		if len(group[key]) < 3 {
+			continue
+		}
+		for end := 2; end < len(group[key]); end++ {
+			v := 2 * (group[key][end] - group[key][end-2])
+			if ret == -1 || ret > v {
+				ret = v
+			}
+		}
+	}
+	return ret
 }
