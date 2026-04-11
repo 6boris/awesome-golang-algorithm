df9107807eb6331520960d7a6c3f4ebadfcab590
0xff-dev <stevenshuang521@gmail.com>
2026-04-11 10:25:56 +0800

    Add solution and test-cases for problem 3741

--- leetcode/3701-3800/3741.Minimum-Distance-Between-Three-Equal-Elements-II/Solution.go
+++ leetcode/3701-3800/3741.Minimum-Distance-Between-Three-Equal-Elements-II/Solution.go
@@ -1,5 +1,23 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) int {
+	found := false
+	ret := 0x7fffffff
+	group := make(map[int][]int)
+	for i := range nums {
+		group[nums[i]] = append(group[nums[i]], i)
+	}
+	for _, v := range group {
+		if len(v) < 3 {
+			continue
+		}
+		found = true
+		for i := 2; i < len(v); i++ {
+			ret = min(ret, 2*(v[i]-v[i-2]))
+		}
+	}
+	if !found {
+		return -1
+	}
+	return ret
 }
