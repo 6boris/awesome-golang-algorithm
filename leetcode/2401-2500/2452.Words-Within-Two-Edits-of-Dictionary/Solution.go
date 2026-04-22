0ac33aff47b5c6d994546be721c8e6afc5718e3c
0xff-dev <stevenshuang521@gmail.com>
2026-04-22 22:55:04 +0800

    Add solution and test-cases for problem 2452

--- leetcode/2401-2500/2452.Words-Within-Two-Edits-of-Dictionary/Solution.go
+++ leetcode/2401-2500/2452.Words-Within-Two-Edits-of-Dictionary/Solution.go
@@ -1,5 +1,27 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(queries []string, dictionary []string) []string {
+	var ret []string
+	dict := make(map[string]struct{})
+	for i := range dictionary {
+		dict[dictionary[i]] = struct{}{}
+	}
+	for i := range queries {
+		for j := range dictionary {
+			diff := 0
+			for k := range queries[i] {
+				if queries[i][k] != dictionary[j][k] {
+					diff++
+					if diff > 2 {
+						break
+					}
+				}
+			}
+			if diff <= 2 {
+				ret = append(ret, queries[i])
+				break
+			}
+		}
+	}
+	return ret
 }
