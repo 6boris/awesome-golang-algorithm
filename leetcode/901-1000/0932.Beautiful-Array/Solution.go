87e25334879ceab09273ab5f4e22db2e30557c7d
0xff-dev <stevenshuang521@gmail.com>
2026-04-27 22:29:52 +0800

    Add solution and test-cases for problem 932

--- leetcode/901-1000/0932.Beautiful-Array/Solution.go
+++ leetcode/901-1000/0932.Beautiful-Array/Solution.go
@@ -1,5 +1,33 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int) []int {
+	memo := make(map[int][]int)
+
+	var helper func(int) []int
+	helper = func(size int) []int {
+		if size == 1 {
+			return []int{1}
+		}
+
+		if res, ok := memo[size]; ok {
+			return res
+		}
+
+		res := make([]int, size)
+		index := 0
+
+		for _, x := range helper((size + 1) / 2) {
+			res[index] = 2*x - 1
+			index++
+		}
+		for _, x := range helper(size / 2) {
+			res[index] = 2 * x
+			index++
+		}
+
+		memo[size] = res
+		return res
+	}
+
+	return helper(n)
 }
