22263f4cec17045dbb633d8e289473176cd1772e
0xff-dev <stevenshuang521@gmail.com>
2026-05-02 23:04:23 +0800

    Add solution and test-cases for problem 3225

--- leetcode/3201-3300/3225.Maximum-Score-From-Grid-Operations/README.md
+++ leetcode/3201-3300/3225.Maximum-Score-From-Grid-Operations/README.md
@@ -1,28 +1,39 @@
 # [3225.Maximum Score From Grid Operations][title]
 
-> [!WARNING|style:flat]
-> This question is temporarily unanswered if you have good ideas. Welcome to [Create Pull Request PR](https://github.com/kylesliu/awesome-golang-algorithm)
-
 ## Description
+You are given a 2D matrix `grid` of size `n x n`. Initially, all cells of the grid are colored white. In one operation, you can select any cell of indices `(i, j)`, and color black all the cells of the `jth` column starting from the top row down to the `ith` row.
 
-**Example 1:**
+The grid score is the sum of all `grid[i][j]` such that cell `(i, j)` is white and it has a horizontally adjacent black cell.
+
+Return the **maximum** score that can be achieved after some number of operations.
+
+**Example 1:**  
+
+![1](./1.png)
 
 ```
-Input: a = "11", b = "1"
-Output: "100"
+Input: grid = [[0,0,0,0,0],[0,0,3,0,0],[0,1,0,0,0],[5,0,0,3,0],[0,0,0,0,2]]
+
+Output: 11
+
+Explanation:
+
+In the first operation, we color all cells in column 1 down to row 3, and in the second operation, we color all cells in column 4 down to the last row. The score of the resulting grid is grid[3][0] + grid[1][2] + grid[3][3] which is equal to 11.
 ```
 
-## 题意
-> ...
+**Example 2:**  
 
-## 题解
+![2](./2.png)
 
-### 思路1
-> ...
-Maximum Score From Grid Operations
-```go
 ```
+Input: grid = [[10,9,0,0,15],[7,1,0,8,0],[5,20,0,11,0],[0,0,0,1,2],[8,12,1,10,3]]
 
+Output: 94
+
+Explanation:
+
+We perform operations on 1, 2, and 3 down to rows 1, 4, and 0, respectively. The score of the resulting grid is grid[0][0] + grid[1][0] + grid[2][1] + grid[4][1] + grid[1][3] + grid[2][3] + grid[3][3] + grid[4][3] + grid[0][4] which is equal to 94.
+```
 
 ## 结语
 
--- leetcode/3201-3300/3225.Maximum-Score-From-Grid-Operations/Solution.go
+++ leetcode/3201-3300/3225.Maximum-Score-From-Grid-Operations/Solution.go
@@ -1,5 +1,71 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(grid [][]int) int64 {
+	n := len(grid[0])
+	if n == 1 {
+		return 0
+	}
+
+	dp := make([][][]int64, n)
+	for i := range dp {
+		dp[i] = make([][]int64, n+1)
+		for j := range dp[i] {
+			dp[i][j] = make([]int64, n+1)
+		}
+	}
+
+	prevMax := make([][]int64, n+1)
+	for i := range prevMax {
+		prevMax[i] = make([]int64, n+1)
+	}
+	prevSuffixMax := make([][]int64, n+1)
+	for i := range prevSuffixMax {
+		prevSuffixMax[i] = make([]int64, n+1)
+	}
+
+	colSum := make([][]int64, n)
+	for c := 0; c < n; c++ {
+		colSum[c] = make([]int64, n+1)
+		for r := 1; r <= n; r++ {
+			colSum[c][r] = colSum[c][r-1] + int64(grid[r-1][c])
+		}
+	}
+
+	for i := 1; i < n; i++ {
+		for currH := 0; currH <= n; currH++ {
+			for prevH := 0; prevH <= n; prevH++ {
+				if currH <= prevH {
+					extraScore := colSum[i][prevH] - colSum[i][currH]
+					dp[i][currH][prevH] = max(dp[i][currH][prevH], prevSuffixMax[prevH][0]+extraScore)
+				} else {
+					extraScore := colSum[i-1][currH] - colSum[i-1][prevH]
+					dp[i][currH][prevH] = max(dp[i][currH][prevH],
+						max(prevSuffixMax[prevH][currH], prevMax[prevH][currH]+extraScore))
+				}
+			}
+		}
+
+		for currH := 0; currH <= n; currH++ {
+			prevMax[currH][0] = dp[i][currH][0]
+			for prevH := 1; prevH <= n; prevH++ {
+				var penalty int64 = 0
+				if prevH > currH {
+					penalty = colSum[i][prevH] - colSum[i][currH]
+				}
+				prevMax[currH][prevH] = max(prevMax[currH][prevH-1], dp[i][currH][prevH]-penalty)
+			}
+
+			prevSuffixMax[currH][n] = dp[i][currH][n]
+			for prevH := n - 1; prevH >= 0; prevH-- {
+				prevSuffixMax[currH][prevH] = max(prevSuffixMax[currH][prevH+1], dp[i][currH][prevH])
+			}
+		}
+	}
+
+	var ans int64 = 0
+	for k := 0; k <= n; k++ {
+		ans = max(ans, max(dp[n-1][n][k], dp[n-1][0][k]))
+	}
+
+	return ans
 }
