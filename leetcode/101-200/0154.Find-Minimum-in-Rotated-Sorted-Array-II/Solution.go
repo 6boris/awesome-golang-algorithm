41aa2fb966e2eb18ba1110d79856ab1f8da895bd
0xff-dev <stevenshuang521@gmail.com>
2026-05-16 17:09:05 +0800

    Add solution and test-cases for problem 154

--- leetcode/101-200/0154.Find-Minimum-in-Rotated-Sorted-Array-II/Solution.go
+++ leetcode/101-200/0154.Find-Minimum-in-Rotated-Sorted-Array-II/Solution.go
@@ -1,5 +1,16 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int) int {
+	low, high := 0, len(nums)-1
+	for low < high {
+		mid := low + (high-low)/2
+		if nums[mid] > nums[high] {
+			low = mid + 1
+		} else if nums[mid] < nums[high] {
+			high = mid
+		} else {
+			high--
+		}
+	}
+	return nums[low]
 }
