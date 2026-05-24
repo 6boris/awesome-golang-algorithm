9fe608f4906e15641458dada76a98a3a65574cf3
0xff-dev <stevenshuang521@gmail.com>
2026-05-24 17:31:48 +0800

    Add solution and test-cases for problem 1340

--- leetcode/1301-1400/1340.Jump-Game-V/Solution.go
+++ leetcode/1301-1400/1340.Jump-Game-V/Solution.go
@@ -1,5 +1,34 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(arr []int, d int) int {
+	size := len(arr)
+	cache := make(map[int]int)
+
+	var dfs func(int) int
+	dfs = func(index int) int {
+		if v, ok := cache[index]; ok {
+			return v
+		}
+		cur := 0
+		for step := 1; step <= d && index-step >= 0; step++ {
+			if arr[index-step] >= arr[index] {
+				break
+			}
+			cur = max(cur, dfs(index-step))
+		}
+		for step := 1; step <= d && index+step < size; step++ {
+			if arr[index+step] >= arr[index] {
+				break
+			}
+			cur = max(cur, dfs(index+step))
+		}
+		cache[index] = cur + 1
+		return cache[index]
+	}
+
+	var ret int
+	for i := 0; i < size; i++ {
+		ret = max(ret, dfs(i))
+	}
+	return ret
 }
