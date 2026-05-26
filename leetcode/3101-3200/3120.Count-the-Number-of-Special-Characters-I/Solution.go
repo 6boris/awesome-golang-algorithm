0fed34a4394d6d98c01fd5815abf809d16b846b9
0xff-dev <stevenshuang521@gmail.com>
2026-05-26 22:35:35 +0800

    Add solution and test-cases for problem 3120

--- leetcode/3101-3200/3120.Count-the-Number-of-Special-Characters-I/Solution.go
+++ leetcode/3101-3200/3120.Count-the-Number-of-Special-Characters-I/Solution.go
@@ -1,5 +1,19 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(word string) int {
+	a, b := [26]int{}, [26]int{}
+	for i := range word {
+		if word[i] >= 'a' && word[i] <= 'z' {
+			a[word[i]-'a']++
+			continue
+		}
+		b[word[i]-'A']++
+	}
+	var ret int
+	for i := range 26 {
+		if a[i] > 0 && b[i] > 0 {
+			ret++
+		}
+	}
+	return ret
 }
