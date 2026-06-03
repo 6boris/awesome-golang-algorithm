68db4380eea1c1b8e37a7af2e99a2ca8476259ae
0xff-dev <stevenshuang521@gmail.com>
2026-06-03 22:05:18 +0800

    Add solution and test-cases for problem 3635

--- leetcode/3601-3700/3635.Earliest-Finish-Time-for-Land-and-Water-Rides-II/Solution_test.go
+++ leetcode/3601-3700/3635.Earliest-Finish-Time-for-Land-and-Water-Rides-II/Solution_test.go
@@ -9,31 +9,33 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name           string
+		landStartTime  []int
+		landDuration   []int
+		waterStartTime []int
+		waterDuration  []int
+		expect         int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{2, 8}, []int{4, 1}, []int{6}, []int{3}, 9},
+		{"TestCase2", []int{5}, []int{3}, []int{1}, []int{10}, 14},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.landStartTime, c.landDuration, c.waterStartTime, c.waterDuration)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v %v",
+					c.expect, got, c.landStartTime, c.landDuration, c.waterStartTime, c.waterDuration)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
