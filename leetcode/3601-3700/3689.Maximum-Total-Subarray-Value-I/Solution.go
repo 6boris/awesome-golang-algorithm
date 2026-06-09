adc38aec99290157deb597e6a8788eaabc524555
0xff-dev <stevenshuang521@gmail.com>
2026-06-09 23:00:38 +0800

    Add solution and test-cases for problem 3689

--- leetcode/3601-3700/3689.Maximum-Total-Subarray-Value-I/Solution.go
+++ leetcode/3601-3700/3689.Maximum-Total-Subarray-Value-I/Solution.go
@@ -1,5 +1,11 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, k int) int64 {
+	a, b := nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		a = max(a, nums[i])
+		b = min(b, nums[i])
+	}
+
+	return int64(k) * int64(a-b)
 }
