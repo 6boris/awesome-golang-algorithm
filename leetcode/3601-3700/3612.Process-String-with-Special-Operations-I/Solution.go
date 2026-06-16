c2f0cc48a0abeeab93c41a4cc59e8e402ced57ec
0xff-dev <stevenshuang521@gmail.com>
2026-06-16 20:52:48 +0800

    Add solution and test-cases for problem 3612

--- leetcode/3601-3700/3612.Process-String-with-Special-Operations-I/Solution.go
+++ leetcode/3601-3700/3612.Process-String-with-Special-Operations-I/Solution.go
@@ -1,5 +1,29 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string) string {
+	result := make([]byte, 0)
+
+	for i := 0; i < len(s); i++ {
+		char := s[i]
+
+		switch {
+		case char >= 'a' && char <= 'z':
+			result = append(result, char)
+
+		case char == '*':
+			if len(result) > 0 {
+				result = result[:len(result)-1]
+			}
+
+		case char == '#':
+			result = append(result, result...)
+
+		case char == '%':
+			for left, right := 0, len(result)-1; left < right; left, right = left+1, right-1 {
+				result[left], result[right] = result[right], result[left]
+			}
+		}
+	}
+
+	return string(result)
 }
--- leetcode/3601-3700/3612.Process-String-with-Special-Operations-I/Solution_test.go
+++ leetcode/3601-3700/3612.Process-String-with-Special-Operations-I/Solution_test.go
@@ -10,12 +10,11 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		inputs string
+		expect string
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", "a#b%*", "ba"},
+		{"TestCase2", "z*#", ""},
 	}
 
 	//	开始测试
@@ -30,10 +29,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
