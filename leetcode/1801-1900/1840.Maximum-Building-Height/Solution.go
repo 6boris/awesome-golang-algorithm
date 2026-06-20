9fb7d2ac8a3fe16ae65f2f719aa467353d9271f4
0xff-dev <stevenshuang521@gmail.com>
2026-06-20 10:18:41 +0800

    Add solution and test-cases for problem 1840

--- leetcode/1801-1900/1840.Maximum-Building-Height/Solution.go
+++ leetcode/1801-1900/1840.Maximum-Building-Height/Solution.go
@@ -1,5 +1,43 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(n int, restrictions [][]int) int {
+	restrictions = append(restrictions, []int{1, 0})
+
+	sort.Slice(restrictions, func(i, j int) bool {
+		return restrictions[i][0] < restrictions[j][0]
+	})
+
+	m := len(restrictions)
+
+	for i := 1; i < m; i++ {
+		dist := restrictions[i][0] - restrictions[i-1][0]
+		if restrictions[i][1] > restrictions[i-1][1]+dist {
+			restrictions[i][1] = restrictions[i-1][1] + dist
+		}
+	}
+
+	for i := m - 2; i >= 0; i-- {
+		dist := restrictions[i+1][0] - restrictions[i][0]
+		if restrictions[i][1] > restrictions[i+1][1]+dist {
+			restrictions[i][1] = restrictions[i+1][1] + dist
+		}
+	}
+
+	ret := 0
+
+	for i := 0; i < m-1; i++ {
+		id1, h1 := restrictions[i][0], restrictions[i][1]
+		id2, h2 := restrictions[i+1][0], restrictions[i+1][1]
+
+		peak := (h1 + h2 + (id2 - id1)) / 2
+		ret = max(ret, peak)
+	}
+
+	lastId := restrictions[m-1][0]
+	lastH := restrictions[m-1][1]
+	ret = max(ret, lastH+(n-lastId))
+
+	return ret
 }
