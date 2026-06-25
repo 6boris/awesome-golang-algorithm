5953bd8f2cdd8dc8e1cf63b433cb958153dc41b5
0xff-dev <stevenshuang521@gmail.com>
2026-06-25 09:08:14 +0800

    Add solution and test-cases for problem 3737

--- leetcode/3701-3800/3737.Count-Subarrays-With-Majority-Element-I/Solution.go
+++ leetcode/3701-3800/3737.Count-Subarrays-With-Majority-Element-I/Solution.go
@@ -1,5 +1,27 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, target int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+
+	offset := n
+	count := make([]int, 2*n+1)
+	count[offset] = 1
+	var ans int = 0
+	curSum := 0
+	lessThanCount := 0
+	for _, num := range nums {
+		if num == target {
+			lessThanCount += count[curSum+offset]
+			curSum++
+		} else {
+			curSum--
+			lessThanCount -= count[curSum+offset]
+		}
+		ans += lessThanCount
+		count[curSum+offset]++
+	}
+	return ans
 }
