33006f4c70b8dd16ede2a8de527782f4603f758a
0xff-dev <stevenshuang521@gmail.com>
2026-06-28 14:48:04 +0800

    Add solution and test-cases for problem 2679

--- leetcode/2601-2700/2679.Sum-in-a-Matrix/Solution.go
+++ leetcode/2601-2700/2679.Sum-in-a-Matrix/Solution.go
@@ -1,5 +1,19 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+import "sort"
+
+func Solution(nums [][]int) int {
+	n := len(nums[0])
+	var ret int
+	col := make([]int, n)
+	for i := range nums {
+		sort.Ints(nums[i])
+		for j := range n {
+			col[j] = max(col[j], nums[i][j])
+		}
+	}
+	for i := range col {
+		ret += col[i]
+	}
+	return ret
 }
