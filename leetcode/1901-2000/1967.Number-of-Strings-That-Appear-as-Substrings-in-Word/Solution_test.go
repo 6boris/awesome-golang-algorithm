0d7705a0d7d302969fae289e287cb0a5158b7fa2
0xff-dev <stevenshuang521@gmail.com>
2026-06-29 21:40:18 +0800

    Add solution and test-cases for problem 1967

--- leetcode/1901-2000/1967.Number-of-Strings-That-Appear-as-Substrings-in-Word/Solution_test.go
+++ leetcode/1901-2000/1967.Number-of-Strings-That-Appear-as-Substrings-in-Word/Solution_test.go
@@ -9,31 +9,32 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name     string
+		patterns []string
+		word     string
+		expect   int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []string{"a", "abc", "bc", "d"}, "abc", 3},
+		{"TestCase2", []string{"a", "b", "c"}, "aaaaabbbbb", 2},
+		{"TestCase3", []string{"a", "a", "a"}, "ab", 3},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.patterns, c.word)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.patterns, c.word)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
