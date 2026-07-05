7748d275c20aaf7fdb5cc55d543e131e71feda6e
0xff-dev <stevenshuang521@gmail.com>
2026-07-05 17:22:42 +0800

    Add solution and test-cases for problem 1301

--- leetcode/1301-1400/1301.Number-of-Paths-with-Max-Score/README.md
+++ leetcode/1301-1400/1301.Number-of-Paths-with-Max-Score/README.md
@@ -1,28 +1,34 @@
 # [1301.Number of Paths with Max Score][title]
 
-> [!WARNING|style:flat]
-> This question is temporarily unanswered if you have good ideas. Welcome to [Create Pull Request PR](https://github.com/kylesliu/awesome-golang-algorithm)
-
 ## Description
+You are given a square `board` of characters. You can move on the board starting at the bottom right square marked with the character `'S'`.
+
+You need to reach the top left square marked with the character `'E'`. The rest of the squares are labeled either with a numeric character `1, 2, ..., 9` or with an obstacle `'X'`. In one move you can go up, left or up-left (diagonally) only if there is no obstacle there.
+
+Return a list of two integers: the first integer is the maximum sum of numeric characters you can collect, and the second is the number of such paths that you can take to get that maximum sum, **taken modulo** `10^9 + 7`.
+
+In case there is no path, return `[0, 0]`.
 
 **Example 1:**
 
 ```
-Input: a = "11", b = "1"
-Output: "100"
+Input: board = ["E23","2X2","12S"]
+Output: [7,1]
 ```
 
-## 题意
-> ...
+**Example 2:**
 
-## 题解
-
-### 思路1
-> ...
-Number of Paths with Max Score
-```go
+```
+Input: board = ["E12","1X1","21S"]
+Output: [4,2]
 ```
 
+**Example 3:**
+
+```
+Input: board = ["E11","XXX","11S"]
+Output: [0,0]
+```
 
 ## 结语
 
--- leetcode/1301-1400/1301.Number-of-Paths-with-Max-Score/Solution.go
+++ leetcode/1301-1400/1301.Number-of-Paths-with-Max-Score/Solution.go
@@ -1,5 +1,65 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+const mod1301 = 1000000007
+
+type pathItem struct {
+	score, cnt int
+}
+
+func Solution(board []string) []int {
+	m, n := len(board), len(board[0])
+	cache := make([][]pathItem, m)
+	for i := range n {
+		cache[i] = make([]pathItem, n)
+		for j := range n {
+			cache[i][j] = pathItem{-1, 0}
+		}
+	}
+
+	cache[m-1][n-1] = pathItem{0, 1}
+	cache[0][0] = pathItem{0, 0}
+
+	for i := m - 1; i >= 0; i-- {
+		for j := n - 1; j >= 0; j-- {
+			if board[i][j] == 'X' || board[i][j] == 'S' {
+				continue
+			}
+			right := j + 1
+			down := i + 1
+			score := 0
+			if i != 0 || j != 0 {
+				score = int(board[i][j] - '0')
+			}
+			if right < n && cache[i][right].score != -1 {
+				try := cache[i][right].score + score
+				if try > cache[i][j].score {
+					cache[i][j].score = try
+					cache[i][j].cnt = cache[i][right].cnt
+				} else if try == cache[i][j].score {
+					cache[i][j].cnt = (cache[i][j].cnt + cache[i][right].cnt) % mod1301
+				}
+			}
+
+			if down < m && cache[down][j].score != -1 {
+				try := cache[down][j].score + score
+				if try > cache[i][j].score {
+					cache[i][j].score = try
+					cache[i][j].cnt = cache[down][j].cnt
+				} else if try == cache[i][j].score {
+					cache[i][j].cnt = (cache[i][j].cnt + cache[down][j].cnt) % mod1301
+				}
+			}
+
+			if down < m && right < n && cache[down][right].score != -1 {
+				try := cache[down][right].score + score
+				if try > cache[i][j].score {
+					cache[i][j].score = try
+					cache[i][j].cnt = cache[down][right].cnt
+				} else if try == cache[i][j].score {
+					cache[i][j].cnt = (cache[i][j].cnt + cache[down][right].cnt) % mod1301
+				}
+			}
+		}
+	}
+	return []int{cache[0][0].score, cache[0][0].cnt}
 }
