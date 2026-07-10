aabf8aaf2d679c3eaa02d9a0ee207cd933a972d6
0xff-dev <stevenshuang521@gmail.com>
2026-07-10 21:23:03 +0800

    Add solution and test-cases for problem 3534

--- leetcode/3501-3600/3534.Path-Existence-Queries-in-a-Graph-II/Solution_test.go
+++ leetcode/3501-3600/3534.Path-Existence-Queries-in-a-Graph-II/Solution_test.go
@@ -9,31 +9,34 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name    string
+		n       int
+		nums    []int
+		maxDiff int
+		queries [][]int
+		expect  []int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 5, []int{1, 8, 3, 4, 2}, 3, [][]int{{0, 3}, {2, 4}}, []int{1, 1}},
+		{"TestCase2", 5, []int{5, 3, 1, 9, 10}, 2, [][]int{{0, 1}, {0, 2}, {2, 3}, {4, 3}}, []int{1, 2, -1, 1}},
+		{"TestCase3", 3, []int{3, 6, 1}, 1, [][]int{{0, 0}, {0, 1}, {1, 2}}, []int{0, -1, -1}},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.nums, c.maxDiff, c.queries)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v %v %v",
+					c.expect, got, c.n, c.nums, c.maxDiff, c.queries)
 			}
 		})
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
