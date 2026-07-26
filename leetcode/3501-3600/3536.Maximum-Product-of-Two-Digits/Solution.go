6ff59dae1f52457538d68c1740eab05e55b0e7a0
0xff-dev <stevenshuang521@gmail.com>
2026-07-26 09:32:39 +0800

    Add solution and test-cases for problem 3536

--- leetcode/3501-3600/3536.Maximum-Product-of-Two-Digits/Solution.go
+++ leetcode/3501-3600/3536.Maximum-Product-of-Two-Digits/Solution.go
@@ -1,5 +1,21 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int) int {
+	digits := [10]int{}
+	for ; n > 0; n /= 10 {
+		digits[n%10]++
+	}
+	ret, cnt := 1, 2
+	for i := 9; i >= 0 && cnt > 0; i-- {
+		if digits[i] == 0 {
+			continue
+		}
+		digits[i]--
+		cnt--
+		ret *= i
+		if digits[i] > 0 {
+			i++
+		}
+	}
+	return ret
 }
