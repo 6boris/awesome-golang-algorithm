37bc8a707f23380b1aa1f8ee2284855077134244
0xff-dev <stevenshuang521@gmail.com>
2026-07-28 21:44:26 +0800

    Add solution and test-cases for problem 3517

--- leetcode/3501-3600/3517.Smallest-Palindromic-Rearrangement-I/Solution.go
+++ leetcode/3501-3600/3517.Smallest-Palindromic-Rearrangement-I/Solution.go
@@ -1,5 +1,28 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(s string) string {
+	cnt := [26]int{}
+	ret := make([]byte, len(s))
+	for i := range s {
+		cnt[s[i]-'a']++
+	}
+	oddIndex := -1
+	for i := range 26 {
+		if cnt[i]&1 == 1 {
+			oddIndex = i
+			break
+		}
+	}
+	left, right := 0, len(s)-1
+	for i := range 26 {
+		char := byte(i) + 'a'
+		for ; cnt[i] >= 2; cnt[i] -= 2 {
+			ret[left], ret[right] = char, char
+			left, right = left+1, right-1
+		}
+	}
+	if oddIndex != -1 {
+		ret[len(s)/2] = byte(oddIndex) + 'a'
+	}
+	return string(ret)
 }
