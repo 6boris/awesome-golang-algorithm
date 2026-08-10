34b06fffd4edde1b2a8996fa3e99e1a79ec55825
0xff-dev <stevenshuang521@gmail.com>
2026-08-10 22:19:42 +0800

    Add solution and test-cases for problem 1510

--- leetcode/1501-1600/1510.Stone-Game-IV/Solution.go
+++ leetcode/1501-1600/1510.Stone-Game-IV/Solution.go
@@ -1,5 +1,39 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int) bool {
+	dp := make([]bool, n+1)
+	for i := 1; i <= n; i++ {
+		for j := 1; j*j <= i; j++ {
+			if !dp[i-j*j] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[n]
+}
+
+func Solution1(n int) bool {
+	cache := map[int]bool{}
+	var dfs func(int) bool
+	dfs = func(c int) bool {
+		if c == 0 {
+			return false
+		}
+
+		if v, ok := cache[c]; ok {
+			return v
+		}
+
+		for x := 1; x*x <= c; x++ {
+			left := c - x*x
+			if !dfs(left) {
+				cache[c] = true
+				break
+			}
+		}
+		return cache[c]
+	}
+
+	return dfs(n)
 }
--- leetcode/1501-1600/1510.Stone-Game-IV/Solution_test.go
+++ leetcode/1501-1600/1510.Stone-Game-IV/Solution_test.go
@@ -10,19 +10,21 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
+		inputs int
 		expect bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 1, true},
+		{"TestCase2", 2, false},
+		{"TestCase3", 4, true},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
 			got := Solution(c.inputs)
-			if !reflect.DeepEqual(got, c.expect) {
+			got1 := Solution1(c.inputs)
+
+			if got != got1 || !reflect.DeepEqual(got, c.expect) {
 				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
 					c.expect, got, c.inputs)
 			}
@@ -30,10 +32,10 @@ func TestSolution(t *testing.T) {
 	}
 }
 
-//	压力测试
+// 压力测试
 func BenchmarkSolution(b *testing.B) {
 }
 
-//	使用案列
+// 使用案列
 func ExampleSolution() {
 }
