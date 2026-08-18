17bb6546f1286cc558ddf655da86e6faaa62c77e
0xff-dev <stevenshuang521@gmail.com>
2026-08-18 22:48:24 +0800

    Add solution and test-cases for problem 3471

--- leetcode/3401-3500/3471.Find-the-Largest-Almost-Missing-Integer/Solution.go
+++ leetcode/3401-3500/3471.Find-the-Largest-Almost-Missing-Integer/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, k int) int {
+	cnt := map[int]int{}
+	for i := range nums {
+		if k == len(nums) {
+			cnt[nums[i]] = 1
+			continue
+		}
+
+		cnt[nums[i]]++
+		if k == 1 || i == 0 || i == len(nums)-1 {
+			continue
+		}
+		cnt[nums[i]] += 1
+	}
+	ret := -1
+	for i := range nums {
+		if cnt[nums[i]] == 1 {
+			ret = max(ret, nums[i])
+		}
+	}
+	return ret
 }
