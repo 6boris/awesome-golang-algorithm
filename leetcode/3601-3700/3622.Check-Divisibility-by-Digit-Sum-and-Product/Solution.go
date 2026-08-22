5ead331133f79d056f2bfa70cd2359da51fd4ac8
0xff-dev <stevenshuang521@gmail.com>
2026-08-22 11:07:16 +0800

    Add solution and test-cases for problem 3622

--- leetcode/3601-3700/3622.Check-Divisibility-by-Digit-Sum-and-Product/Solution.go
+++ leetcode/3601-3700/3622.Check-Divisibility-by-Digit-Sum-and-Product/Solution.go
@@ -1,5 +1,12 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int) bool {
+	sum, prod := 0, 1
+	var tmp int
+	for base := n; base > 0; base /= 10 {
+		tmp = base % 10
+		sum += tmp
+		prod *= tmp
+	}
+	return n%(sum+prod) == 0
 }
