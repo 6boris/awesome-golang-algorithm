6817e69714dff55acc7bbb10514a43775b04e561
0xff-dev <stevenshuang521@gmail.com>
2026-08-25 21:24:08 +0800

    Add solution and test-cases for problem 3718

--- leetcode/3701-3800/3718.Smallest-Missing-Multiple-of-K/Solution.go
+++ leetcode/3701-3800/3718.Smallest-Missing-Multiple-of-K/Solution.go
@@ -1,5 +1,18 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(nums []int, k int) int {
+	m := 0
+	in := make(map[int]struct{})
+	for i := range nums {
+		if nums[i]%k == 0 {
+			in[nums[i]] = struct{}{}
+			m = max(m, nums[i])
+		}
+	}
+	for i := k; i <= m; i += k {
+		if _, ok := in[i]; !ok {
+			return i
+		}
+	}
+	return m + k
 }
