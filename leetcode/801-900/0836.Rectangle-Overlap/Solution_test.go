3771b221f9455e512fb325a56a2488daa33260b2
0xff-dev <stevenshuang521@gmail.com>
2026-09-14 08:43:28 +0800

    Add solution and test-cases for problem 836

--- leetcode/801-900/0836.Rectangle-Overlap/Solution_test.go
+++ leetcode/801-900/0836.Rectangle-Overlap/Solution_test.go
@@ -9,22 +9,22 @@ import (
 func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
-		name   string
-		inputs bool
-		expect bool
+		name       string
+		rec1, rec2 []int
+		expect     bool
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", []int{0, 0, 2, 2}, []int{1, 1, 3, 3}, true},
+		{"TestCase2", []int{0, 0, 1, 1}, []int{1, 0, 2, 1}, false},
+		{"TestCase3", []int{0, 0, 1, 1}, []int{2, 2, 3, 3}, false},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.rec1, c.rec2)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.rec1, c.rec2)
 			}
 		})
 	}
