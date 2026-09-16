f3d2e8c8b7b95438aba8e217d301941e474b7196
0xff-dev <stevenshuang521@gmail.com>
2026-09-16 08:40:27 +0800

    Add solution and test-cases for problem 1621

--- leetcode/1601-1700/1621.Number-of-Sets-of-K-Non-Overlapping-Line-Segments/Solution.go
+++ leetcode/1601-1700/1621.Number-of-Sets-of-K-Non-Overlapping-Line-Segments/Solution.go
@@ -1,5 +1,24 @@
 package Solution
 
-func Solution(x bool) bool {
-	return x
+func Solution(n int, k int) int {
+	const mod = 1000000007
+
+	m := n + k - 1
+	r := 2 * k
+
+	if r > m || r < 0 {
+		return 0
+	}
+
+	c := make([][]int, m+1)
+	for i := range c {
+		c[i] = make([]int, i+1)
+		c[i][0] = 1
+		c[i][i] = 1
+		for j := 1; j < i; j++ {
+			c[i][j] = (c[i-1][j-1] + c[i-1][j]) % mod
+		}
+	}
+
+	return c[m][r]
 }
--- leetcode/1601-1700/1621.Number-of-Sets-of-K-Non-Overlapping-Line-Segments/Solution_test.go
+++ leetcode/1601-1700/1621.Number-of-Sets-of-K-Non-Overlapping-Line-Segments/Solution_test.go
@@ -10,21 +10,21 @@ func TestSolution(t *testing.T) {
 	//	测试用例
 	cases := []struct {
 		name   string
-		inputs bool
-		expect bool
+		n, k   int
+		expect int
 	}{
-		{"TestCase", true, true},
-		{"TestCase", true, true},
-		{"TestCase", false, false},
+		{"TestCase1", 4, 2, 5},
+		{"TestCase2", 3, 1, 3},
+		{"TestCase3", 30, 7, 796297179},
 	}
 
 	//	开始测试
 	for i, c := range cases {
 		t.Run(c.name+" "+strconv.Itoa(i), func(t *testing.T) {
-			got := Solution(c.inputs)
+			got := Solution(c.n, c.k)
 			if !reflect.DeepEqual(got, c.expect) {
-				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
-					c.expect, got, c.inputs)
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v %v",
+					c.expect, got, c.n, c.k)
 			}
 		})
 	}
